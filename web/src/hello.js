ba47fbe4ffa3c4043ed41b7bd43425f15af92e64
Greg Walker <michael.walker@gsa.gov>
2017-12-07 16:26:59 -0600

    initialize repo

---
+++ web/src/hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Hello = props => (
+  <div>Hello {props.name}!</div>
+);
+
+Hello.propTypes = {
+  name: PropTypes.string
+};
+Hello.defaultProps = {
+  name: 'Unidentified Person'
+};
+
+export default Hello;
