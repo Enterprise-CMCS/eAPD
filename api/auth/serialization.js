4a2ca207d51a78b5ce4b4335999a8e0a8adcd862
Greg Walker <mgwalker@gmail.com>
2017-12-15 21:48:03 -0600

    comments

--- api/auth/serialization.js
+++ api/auth/serialization.js
@@ -1,7 +1,11 @@
+// Serialize a user into a stringy type that will get
+// pushed into their session cookie.
 module.exports.serializeUser = (user, done) => {
   done(null, user.id);
 };
 
+// Deserialize a stringy from the user's session cookie
+// into a user object.
 module.exports.deserializeUser = (userID, done) => {
   done(null, { username: 'hello', email: 'hello@world.com', id: userID });
 };
