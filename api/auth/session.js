b8fc50111ec6a2da10a02480a6b5d90bfd202d1a
Greg Walker <mgwalker@gmail.com>
2017-12-15 22:37:44 -0600

    fix cookie name

--- api/auth/session.js
+++ api/auth/session.js
@@ -4,7 +4,7 @@ const clientSessions = require('client-sessions');
 // configures the cookie that will be used.
 module.exports.getSessionFunction = function getSessionFunction(sessions = clientSessions) {
   return sessions({
-    cookieName: 'user', // passport's session support writes to the 'user' cookie, so we have to use that name
+    cookieName: 'session', // passport's session support writes to the 'session' cookie, so we have to use that name
     secret: process.env.SESSION_SECRET, // crypto secret, used to encrypt the session cookie
     duration: 14 * 24 * 60 * 60 * 1000, // session lifetime, in milliseconds
     cookie: {
