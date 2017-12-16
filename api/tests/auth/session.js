5c404ce1a2fb12c8ee6bc2ecb4bbbf2418205f2f
Greg Walker <mgwalker@gmail.com>
2017-12-15 22:37:44 -0600

    fix cookie name

--- api/tests/auth/session.js
+++ api/tests/auth/session.js
@@ -11,7 +11,7 @@ tap.test('session function', (sessionTest) => {
 
   sessionTest.ok(sessions.calledWith(sinon.match.object), 'called with a configuration object');
   const config = sessions.args[0][0];
-  sessionTest.equal(config.cookieName, 'user', 'session uses the "user" cookie - required by passport');
+  sessionTest.equal(config.cookieName, 'session', 'session uses the "user" cookie - required by passport');
   sessionTest.equal(config.cookie.httpOnly, true, 'session cookie is HTTP-only');
   sessionTest.equal(config.secret, 'secret', 'session secret is set from environment');
   sessionTest.ok(config.duration < 30 * 24 * 60 * 60 * 1000, 'session cookie lasts less than 30 days');
