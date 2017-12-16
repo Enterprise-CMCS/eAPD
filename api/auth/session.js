db46b4a868eb00204fc95873f54a6e5699c4960a
Greg Walker <mgwalker@gmail.com>
2017-12-15 21:48:03 -0600

    comments

--- api/auth/session.js
+++ api/auth/session.js
@@ -1,9 +1,11 @@
 const clientSessions = require('client-sessions');
 
+// Sets up sessions to be stored in browser cookies.  This
+// configures the cookie that will be used.
 module.exports.getSessionFunction = function getSessionFunction(sessions = clientSessions) {
   return sessions({
-    cookieName: 'user', // this is the cookie name passport writes to
-    secret: process.env.SESSION_SECRET,
+    cookieName: 'user', // passport's session support writes to the 'user' cookie, so we have to use that name
+    secret: process.env.SESSION_SECRET, // crypto secret, used to encrypt the session cookie
     duration: 14 * 24 * 60 * 60 * 1000, // session lifetime, in milliseconds
     cookie: {
       httpOnly: true
