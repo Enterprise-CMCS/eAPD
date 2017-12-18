dce980f0d3957a459816905eec9db77f40f03648
Brendan Sudol <brendan.sudol@gsa.gov>
2017-12-18 18:35:12 -0500

    add react-router, integrate with redux

---
+++ web/src/components/Root.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+
+import App from './App';
+
+const Root = ({ history, store }) => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+);
+
+Root.propTypes = {
+  history: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  store: PropTypes.object.isRequired // eslint-disable-line react/forbid-prop-types
+};
+
+export default Root;
