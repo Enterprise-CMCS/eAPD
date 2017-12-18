21bd3b18e6220adae74d5c9a97e02a2b7dc100e8
Brendan Sudol <brendan.sudol@gsa.gov>
2017-12-18 14:13:13 -0500

    add starter redux bits

---
+++ web/src/actions/index.js
@@ -0,0 +1,5 @@
+export const INCREMENT_COUNT = 'INCREMENT_COUNT';
+export const DECREMENT_COUNT = 'DECREMENT_COUNT';
+
+export const increment = () => ({ type: INCREMENT_COUNT });
+export const decrement = () => ({ type: DECREMENT_COUNT });
