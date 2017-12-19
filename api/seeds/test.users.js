bdca42a3d8b543cd44575702529ddeb9b87f8f84
Greg Walker <mgwalker@gmail.com>
2017-12-18 21:10:58 -0600

    comment about seed

--- api/seeds/test.users.js
+++ api/seeds/test.users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 
 // Deletes ALL existing entries
 exports.seed = (knex) => {
+  // Don't seed this data if we're not in a test environment.
   if (process.env.NODE_ENV !== 'test') {
     return Promise.resolve();
   }
