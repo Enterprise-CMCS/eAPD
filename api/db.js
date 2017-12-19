fe6696c30ae198a5706cefb9b5db2236e366c62a
Greg Walker <mgwalker@gmail.com>
2017-12-18 20:34:25 -0600

    fix db injection; update auth; tests

--- api/db.js
+++ api/db.js
@@ -1,1 +1,4 @@
-module.exports = (knex, config) => knex(config)[process.env.NODE_ENV];
+const defaultKnex = require('knex');
+const defaultConfig = require('./knexfile');
+
+module.exports = (knex = defaultKnex, config = defaultConfig) => knex(config[process.env.NODE_ENV]);
--- api/tests/db.js
+++ api/tests/db.js
@@ -5,16 +5,17 @@ const db = require('../db');
 
 tap.test('database object module', (dbTest) => {
   const knex = sinon.stub().returns({
-    env1: 'environment 1',
-    env2: 'environment 2'
+    thisIs: 'a database'
   });
 
+  const nodeEnv = process.env.NODE_ENV;
   process.env.NODE_ENV = 'env2';
-  const result = db(knex, { config: true });
+  const result = db(knex, { env1: 'environemtn 1', env2: 'environment 2' });
 
   dbTest.ok(knex.calledOnce, 'knex is configured');
-  dbTest.ok(knex.calledWith({ config: true }), 'knex is configured with the passed value');
-  dbTest.same(result, 'environment 2', 'returns the expected object based on env');
+  dbTest.ok(knex.calledWith('environment 2'), 'knex is configured with the passed value');
+  dbTest.same(result, { thisIs: 'a database' }, 'returns the expected object based on env');
 
+  process.env.NODE_ENV = nodeEnv;
   dbTest.done();
 });
