92ea661047894b0a9f63c723bd09381ebf3cefdd
Greg Walker <mgwalker@gmail.com>
2017-12-18 21:02:41 -0600

    fix auth and tests

--- api/auth/authenticate.js
+++ api/auth/authenticate.js
@@ -4,24 +4,26 @@ const defaultBcrypt = require('bcryptjs');
 module.exports = (
   db = defaultDB,
   bcrypt = defaultBcrypt
-) => ((username, password, done) => {
-  const pwHash = bcrypt.hashSync(password);
-  return db('users')
-    .where({
-      email: username,
-      password: pwHash
-    })
-    .select()
-    .then((u) => {
-      if (u.length) {
-        // If there is a matching user, return it
-        done(null, { username: u[0].email, id: u[0].id });
-      } else {
-        // Otherwise, callback with an error.
-        done('Unknown user');
-      }
-    })
-    .catch(() => {
-      done('Database error');
-    });
-});
+) => ((username, password, done) => db('users')
+  .where({
+    email: username
+  })
+  .select()
+  .then((users) => {
+    let user = { password: '' };
+    if (users.length) {
+      user = users[0];
+    }
+
+    // If there is a matching user, return it
+    if (bcrypt.compareSync(password, user.password)) {
+      done(null, { username: user.email, id: user.id });
+    } else {
+      // Otherwise, callback with an error.
+      done('Unknown user');
+    }
+  })
+  .catch(() => {
+    done('Database error');
+  })
+);
