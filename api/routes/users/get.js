2b50863ee553392a55995934e0109ac50c6e38f0
Greg Walker <michael.walker@gsa.gov>
2017-12-22 11:08:26 -0600

    fix codeclimate warning

--- api/routes/users/get.js
+++ api/routes/users/get.js
@@ -1,38 +1,41 @@
 const defaultDB = require('../../db')();
 const loggedIn = require('../../auth/middleware').loggedIn;
 
-module.exports = (app, db = defaultDB) => {
-  app.get('/users', loggedIn, (req, res) => {
+const allUsersHandler = (req, res, db) => {
+  db('users')
+    .select('id', 'email')
+    .then(users => {
+      res.send(users);
+    })
+    .catch(() => {
+      res.status(500).end();
+    });
+};
+
+const oneUserHandler = (req, res, db) => {
+  if (req.params.id && !Number.isNaN(Number(req.params.id))) {
     db('users')
-      .select('id', 'email')
-      .then(users => {
-        res.send(users);
+      .where({ id: Number(req.params.id) })
+      .first('id', 'email')
+      .then(user => {
+        if (user) {
+          res.send(user);
+        } else {
+          res.status(404).end();
+        }
       })
       .catch(() => {
         res.status(500).end();
       });
-  });
+  } else {
+    res
+      .status(400)
+      .send('get-user-invalid')
+      .end();
+  }
+};
 
-  app.get('/user/:id', loggedIn, (req, res) => {
-    if (req.params.id && !Number.isNaN(Number(req.params.id))) {
-      db('users')
-        .where({ id: Number(req.params.id) })
-        .first('id', 'email')
-        .then(user => {
-          if (user) {
-            res.send(user);
-          } else {
-            res.status(404).end();
-          }
-        })
-        .catch(() => {
-          res.status(500).end();
-        });
-    } else {
-      res
-        .status(400)
-        .send('get-user-invalid')
-        .end();
-    }
-  });
+module.exports = (app, db = defaultDB) => {
+  app.get('/users', loggedIn, (req, res) => allUsersHandler(req, res, db));
+  app.get('/user/:id', loggedIn, (req, res) => oneUserHandler(req, res, db));
 };
