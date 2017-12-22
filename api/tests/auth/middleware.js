40a862280e9c82cc549621ceac8a8f0ef7d6fe3b
Greg Walker <michael.walker@gsa.gov>
2017-12-22 09:48:47 -0600

    prettier

--- api/tests/auth/middleware.js
+++ api/tests/auth/middleware.js
@@ -10,7 +10,7 @@ const res = {
 };
 const next = sandbox.spy();
 
-tap.beforeEach((done) => {
+tap.beforeEach(done => {
   sandbox.reset();
 
   res.status.returns(res);
@@ -20,26 +20,35 @@ tap.beforeEach((done) => {
   done();
 });
 
-tap.test('logged in middleware', (loggedInMiddlewareTest) => {
+tap.test('logged in middleware', loggedInMiddlewareTest => {
   const loggedIn = middleware.loggedIn;
-  loggedInMiddlewareTest.test('rejects if the user is not logged in', (invalidTest) => {
-    loggedIn({ }, res, next);
+  loggedInMiddlewareTest.test(
+    'rejects if the user is not logged in',
+    invalidTest => {
+      loggedIn({}, res, next);
 
-    invalidTest.ok(res.status.calledWith(403), 'HTTP status set to 403');
-    invalidTest.ok(res.end.called, 'response is terminated');
-    invalidTest.ok(next.notCalled, 'endpoint handling chain is not continued');
-    invalidTest.done();
-  });
+      invalidTest.ok(res.status.calledWith(403), 'HTTP status set to 403');
+      invalidTest.ok(res.end.called, 'response is terminated');
+      invalidTest.ok(
+        next.notCalled,
+        'endpoint handling chain is not continued'
+      );
+      invalidTest.done();
+    }
+  );
 
-  loggedInMiddlewareTest.test('continues if the user is logged in', (validTest) => {
-    loggedIn({ user: true }, res, next);
+  loggedInMiddlewareTest.test(
+    'continues if the user is logged in',
+    validTest => {
+      loggedIn({ user: true }, res, next);
 
-    validTest.ok(res.send.notCalled, 'no body is sent');
-    validTest.ok(res.status.notCalled, 'HTTP status not set');
-    validTest.ok(res.end.notCalled, 'response is not terminated');
-    validTest.ok(next.called, 'endpoint handling chain is continued');
-    validTest.done();
-  });
+      validTest.ok(res.send.notCalled, 'no body is sent');
+      validTest.ok(res.status.notCalled, 'HTTP status not set');
+      validTest.ok(res.end.notCalled, 'response is not terminated');
+      validTest.ok(next.called, 'endpoint handling chain is continued');
+      validTest.done();
+    }
+  );
 
   loggedInMiddlewareTest.done();
 });
--- api/tests/routes/users/index.js
+++ api/tests/routes/users/index.js
@@ -3,12 +3,15 @@ const sinon = require('sinon');
 
 const usersIndex = require('../../../routes/users');
 
-tap.test('users endpoint setup', (endpointTest) => {
-  const app = { };
+tap.test('users endpoint setup', endpointTest => {
+  const app = {};
   const postEndpoint = sinon.spy();
 
   usersIndex(app, postEndpoint);
 
-  endpointTest.ok(postEndpoint.calledWith(app), 'users POST endpoint is setup with the app');
+  endpointTest.ok(
+    postEndpoint.calledWith(app),
+    'users POST endpoint is setup with the app'
+  );
   endpointTest.done();
 });
