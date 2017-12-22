bac737c69ce86d325c333f32752253ac94ea42db
Greg Walker <mgwalker@gmail.com>
2017-12-21 18:07:54 -0600

    make email field unique

---
+++ api/migrations/20171221233429_unique-email.js
@@ -0,0 +1,8 @@
+
+exports.up = knex => knex.schema.alterTable('users', (table) => {
+  table.unique('email');
+});
+
+exports.down = knex => knex.schema.alterTable('users', (table) => {
+  table.dropUnique('email');
+});
