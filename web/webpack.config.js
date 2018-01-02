ebdf098835708b4503e6725d92c692c307e52c32
Brendan Sudol <brendan.sudol@gsa.gov>
2018-01-02 15:56:30 -0500

    add tota11y to pages (on dev)

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env', 'react']
+            presets: ['env', 'react', 'stage-0']
           }
         }
       }
