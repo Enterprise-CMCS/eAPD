b53ee391e39333694ddd4924983c9767a49ba0e6
Greg Walker <michael.walker@gsa.gov>
2018-01-03 15:46:42 -0600

    add tests for JSON body

--- api/endpoint-tests/auth/login.js
+++ api/endpoint-tests/auth/login.js
@@ -1,5 +1,5 @@
-const tap = require('tap');
-const request = require('request');
+const tap = require('tap'); // eslint-disable-line import/no-extraneous-dependencies
+const request = require('request'); // eslint-disable-line import/no-extraneous-dependencies
 
 tap.test('login endpoint | /auth/login', loginTest => {
   const url = `http://${process.env.API_HOST || 'localhost'}:${process.env.API_PORT || process.env.PORT || 8000}/auth/login`;
@@ -13,39 +13,68 @@ tap.test('login endpoint | /auth/login', loginTest => {
 
   const invalidCases = [{
     title: 'with a post body, but no username or password',
-    form: { }
+    data: { }
   }, {
     title: 'with a post body, but no username',
-    form: { password: 'test-password' }
+    data: { password: 'test-password' }
   }, {
     title: 'with a post body, but no password',
-    form: { username: 'test-username' }
+    data: { username: 'test-username' }
   }];
 
   invalidCases.forEach(invalidCase => {
-    loginTest.test(invalidCase.title, invalidTest => {
-      request.post(url, { form: invalidCase.form }, (err, response) => {
+    loginTest.test(`Form body: ${invalidCase.title}`, invalidTest => {
+      request.post(url, { form: invalidCase.data }, (err, response) => {
+        invalidTest.equal(response.statusCode, 400, 'gives a 400 status code');
+        invalidTest.done();
+      });
+    });
+
+    loginTest.test(`JSON body: ${invalidCase.title}`, invalidTest => {
+      request.post(url, { json: invalidCase.data }, (err, response) => {
         invalidTest.equal(response.statusCode, 400, 'gives a 400 status code');
         invalidTest.done();
       });
     });
   });
 
-  loginTest.test('with invalid username, valid password', invalidTest => {
-    request.post(url, { form: { username: 'nobody', password: 'password' } }, (err, response) => {
-      invalidTest.equal(response.statusCode, 401, 'gives a 401 status code');
-      invalidTest.done();
+  const badCredentialsCases = [{
+    title: 'with invalid username, invalid password',
+    data: {
+      username: 'nobody',
+      password: 'nothing'
+    }
+  }, {
+    title: 'with invalid username, valid password',
+    data: {
+      username: 'nobody',
+      password: 'password'
+    }
+  }, {
+    title: 'with valid username, invalid password',
+    data: {
+      username: 'em@il.com',
+      password: 'nothing'
+    }
+  }];
+
+  badCredentialsCases.forEach(badCredentialsCase => {
+    loginTest.test(`Form body: ${badCredentialsCase.title}`, invalidTest => {
+      request.post(url, { form: badCredentialsCase.data }, (err, response) => {
+        invalidTest.equal(response.statusCode, 401, 'gives a 401 status code');
+        invalidTest.done();
+      });
+    });
+
+    loginTest.test(`JSON body: ${badCredentialsCase.title}`, invalidTest => {
+      request.post(url, { json: badCredentialsCase.data }, (err, response) => {
+        invalidTest.equal(response.statusCode, 401, 'gives a 401 status code');
+        invalidTest.done();
+      });
     });
   });
 
-  loginTest.test('with valid username, invalid password', invalidTest => {
-    request.post(url, { form: { username: 'em@il.com', password: 'nothing' } }, (err, response) => {
-      invalidTest.equal(response.statusCode, 401, 'gives a 401 status code');
-      invalidTest.done();
-    });
-  });
-
-  loginTest.test('with valid username and valid password', validTest => {
+  loginTest.test('Form body: with valid username and valid password', validTest => {
     // isolate cookies, so request doesn't reuse them
     const cookies = request.jar();
     request.post(url, { jar: cookies, form: { username: 'em@il.com', password: 'password' } }, (err, response, body) => {
@@ -56,6 +85,16 @@ tap.test('login endpoint | /auth/login', loginTest => {
     });
   });
 
+  loginTest.test('JSON body: with valid username and valid password', validTest => {
+    // isolate cookies, so request doesn't reuse them
+    const cookies = request.jar();
+    request.post(url, { jar: cookies, json: { username: 'em@il.com', password: 'password' } }, (err, response, body) => {
+      validTest.equal(response.statusCode, 200, 'gives a 200 status code');
+      validTest.ok(response.headers['set-cookie'].some(cookie => cookie.startsWith('session=') && cookie.endsWith('; httponly')), 'sends an http-only session cookie');
+      validTest.notOk(body, 'does not send a body');
+      validTest.done();
+    });
+  });
 
   loginTest.done();
 });
