191888e3d1b0742673a22faa74f051706f43a35c
Greg Walker <michael.walker@gsa.gov>
2018-01-18 09:13:28 -0600

    refactor smaller; pull out deletion code since it's common, make sure it's ordered correctly

--- api/seeds/test/users.js
+++ api/seeds/test/users.js
@@ -2,15 +2,11 @@ const bcrypt = require('bcryptjs');
 
 // Deletes ALL existing entries
 exports.seed = knex =>
-  knex('users')
-    .del()
-    .then(() =>
-      knex('users').insert([
-        {
-          id: 57,
-          email: 'em@il.com',
-          password: bcrypt.hashSync('password'),
-          auth_role: 'admin'
-        }
-      ])
-    );
+  knex('users').insert([
+    {
+      id: 57,
+      email: 'em@il.com',
+      password: bcrypt.hashSync('password'),
+      auth_role: 'admin'
+    }
+  ]);
