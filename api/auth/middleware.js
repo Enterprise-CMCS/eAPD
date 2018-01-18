86c80a18069d07f51ded32b1d25001466dd0fb97
Greg Walker <michael.walker@gsa.gov>
2018-01-18 10:22:24 -0600

    add 'can' middleware

--- api/auth/middleware.js
+++ api/auth/middleware.js
@@ -1,7 +1,19 @@
 module.exports.loggedIn = (req, res, next) => {
-  if (!req.user) {
-    res.status(403).end();
-  } else {
+  if (req.user) {
     next();
+  } else {
+    res.status(403).end();
   }
+};
+
+module.exports.can = activity => (req, res, next) => {
+  // First check if they're logged in
+  module.exports.loggedIn(req, res, () => {
+    // Then check if they have the activity
+    if (req.user.activities.includes(activity)) {
+      next();
+    } else {
+      res.status(401).end();
+    }
+  });
 };
