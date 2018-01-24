98b4c857fcbc742807413662fb601607e333cf1d
Greg Walker <michael.walker@gsa.gov>
2018-01-24 09:44:34 -0600

    playtime

---
+++ api/db/user.js
@@ -0,0 +1,25 @@
+const defaultBookshelf = require('bookshelf');
+const AuthRole = require('./authorization').roles;
+
+let model;
+
+module.exports = (bookshelf = defaultBookshelf) => {
+  if (!model) {
+    model = bookshelf.Model.extend({
+      tableName: 'users',
+
+      role() {
+        return this.hasOne(AuthRole(), 'name', 'auth_role');
+      },
+
+      activities() {
+        if (this.relations.role) {
+          return Promise.resolve(this.related('role').related('activities').pluck('name'));
+        }
+        return this.load('role.activities').then(() => this.related('role').related('activities').pluck('name'));
+      }
+    });
+  }
+
+  return model;
+};
