d9a032d7e47a44124d3c6fad12a4681430ae66ac
Greg Walker <michael.walker@gsa.gov>
2018-01-24 14:35:04 -0600

    await /users/get

--- api/routes/users/get.test.js
+++ api/routes/users/get.test.js
@@ -56,44 +56,40 @@ tap.test('user GET endpoint', endpointTest => {
 
     handlerTest.test(
       'sends a server error code if there is a database error',
-      invalidTest => {
+      async invalidTest => {
         select.rejects();
 
-        handler({}, res);
+        await handler({}, res);
 
-        setTimeout(() => {
-          invalidTest.ok(db.calledWith('users'), 'queries the users table');
-          invalidTest.ok(
-            select.calledWith('id', 'email'),
-            'selects only user ID and email'
-          );
-          invalidTest.ok(res.status.calledWith(500), 'HTTP status set to 500');
-          invalidTest.ok(res.send.notCalled, 'no body is sent');
-          invalidTest.ok(res.end.called, 'response is terminated');
-          invalidTest.done();
-        }, 20);
-      }
-    );
-
-    handlerTest.test('sends back a list of users', validTest => {
-      const users = [{ id: 1, email: 'hi' }, { id: 2, email: 'bye' }];
-      select.resolves(users);
-
-      handler({}, res);
-
-      setTimeout(() => {
-        validTest.ok(db.calledWith('users'), 'queries the users table');
-        validTest.ok(
+        invalidTest.ok(db.calledWith('users'), 'queries the users table');
+        invalidTest.ok(
           select.calledWith('id', 'email'),
           'selects only user ID and email'
         );
-        validTest.ok(res.status.notCalled, 'HTTP status is not explicitly set');
-        validTest.ok(
-          res.send.calledWith(users),
-          'body is set to the list of users'
-        );
-        validTest.done();
-      }, 20);
+        invalidTest.ok(res.status.calledWith(500), 'HTTP status set to 500');
+        invalidTest.ok(res.send.notCalled, 'no body is sent');
+        invalidTest.ok(res.end.called, 'response is terminated');
+        invalidTest.done();
+      }
+    );
+
+    handlerTest.test('sends back a list of users', async validTest => {
+      const users = [{ id: 1, email: 'hi' }, { id: 2, email: 'bye' }];
+      select.resolves(users);
+
+      await handler({}, res);
+
+      validTest.ok(db.calledWith('users'), 'queries the users table');
+      validTest.ok(
+        select.calledWith('id', 'email'),
+        'selects only user ID and email'
+      );
+      validTest.ok(res.status.notCalled, 'HTTP status is not explicitly set');
+      validTest.ok(
+        res.send.calledWith(users),
+        'body is set to the list of users'
+      );
+      validTest.done();
     });
 
     handlerTest.done();
@@ -137,74 +133,68 @@ tap.test('user GET endpoint', endpointTest => {
 
     handlerTest.test(
       'sends a server error code if there is a database error',
-      invalidTest => {
+      async invalidTest => {
         first.rejects();
 
-        handler({ params: { id: 1 } }, res);
+        await handler({ params: { id: 1 } }, res);
 
-        setTimeout(() => {
-          invalidTest.ok(db.calledWith('users'), 'queries the users table');
-          invalidTest.ok(
-            where.calledWith({ id: 1 }),
-            'looks for only the specific user'
-          );
-          invalidTest.ok(
-            first.calledWith('id', 'email'),
-            'selects only user ID and email'
-          );
-          invalidTest.ok(res.status.calledWith(500), 'HTTP status set to 500');
-          invalidTest.ok(res.send.notCalled, 'no body is sent');
-          invalidTest.ok(res.end.called, 'response is terminated');
-          invalidTest.done();
-        }, 20);
+        invalidTest.ok(db.calledWith('users'), 'queries the users table');
+        invalidTest.ok(
+          where.calledWith({ id: 1 }),
+          'looks for only the specific user'
+        );
+        invalidTest.ok(
+          first.calledWith('id', 'email'),
+          'selects only user ID and email'
+        );
+        invalidTest.ok(res.status.calledWith(500), 'HTTP status set to 500');
+        invalidTest.ok(res.send.notCalled, 'no body is sent');
+        invalidTest.ok(res.end.called, 'response is terminated');
+        invalidTest.done();
       }
     );
 
     handlerTest.test(
       'sends a not-found error if the requested user does not exist',
-      invalidTest => {
+      async invalidTest => {
         first.resolves();
-        handler({ params: { id: 1 } }, res);
+        await handler({ params: { id: 1 } }, res);
 
-        setTimeout(() => {
-          invalidTest.ok(db.calledWith('users'), 'queries the users table');
-          invalidTest.ok(
-            where.calledWith({ id: 1 }),
-            'looks for only the specific user'
-          );
-          invalidTest.ok(
-            first.calledWith('id', 'email'),
-            'selects only user ID and email'
-          );
-          invalidTest.ok(res.status.calledWith(404), 'HTTP status set to 404');
-          invalidTest.ok(res.send.notCalled, 'no body is sent');
-          invalidTest.ok(res.end.called, 'response is terminated');
-          invalidTest.done();
-        }, 20);
-      }
-    );
-
-    handlerTest.test('sends the requested user', validTest => {
-      first.resolves({ id: 1, email: 'test-email@dotcom.com' });
-      handler({ params: { id: 1 } }, res);
-
-      setTimeout(() => {
-        validTest.ok(db.calledWith('users'), 'queries the users table');
-        validTest.ok(
+        invalidTest.ok(db.calledWith('users'), 'queries the users table');
+        invalidTest.ok(
           where.calledWith({ id: 1 }),
           'looks for only the specific user'
         );
-        validTest.ok(
+        invalidTest.ok(
           first.calledWith('id', 'email'),
           'selects only user ID and email'
         );
-        validTest.ok(res.status.notCalled, 'HTTP status is not explicitly set');
-        validTest.ok(
-          res.send.calledWith({ id: 1, email: 'test-email@dotcom.com' }),
-          'requested user is sent'
-        );
-        validTest.done();
-      }, 20);
+        invalidTest.ok(res.status.calledWith(404), 'HTTP status set to 404');
+        invalidTest.ok(res.send.notCalled, 'no body is sent');
+        invalidTest.ok(res.end.called, 'response is terminated');
+        invalidTest.done();
+      }
+    );
+
+    handlerTest.test('sends the requested user', async validTest => {
+      first.resolves({ id: 1, email: 'test-email@dotcom.com' });
+      await handler({ params: { id: 1 } }, res);
+
+      validTest.ok(db.calledWith('users'), 'queries the users table');
+      validTest.ok(
+        where.calledWith({ id: 1 }),
+        'looks for only the specific user'
+      );
+      validTest.ok(
+        first.calledWith('id', 'email'),
+        'selects only user ID and email'
+      );
+      validTest.ok(res.status.notCalled, 'HTTP status is not explicitly set');
+      validTest.ok(
+        res.send.calledWith({ id: 1, email: 'test-email@dotcom.com' }),
+        'requested user is sent'
+      );
+      validTest.done();
     });
 
     handlerTest.done();
