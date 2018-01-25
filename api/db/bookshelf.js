e8ea86c68d077beb3b1ef28bc72a6960fbd2f5e9
Greg Walker <michael.walker@gsa.gov>
2018-01-25 13:45:03 -0600

    rename for now; expose created models

---
+++ api/db/bookshelf.js
@@ -0,0 +1,27 @@
+const defaultKnex = require('knex');
+const defaultBookshelf = require('bookshelf');
+const defaultConfig = require('../knexfile');
+
+const user = require('./user');
+const authorization = require('./authorization');
+
+const exportedModels = { };
+
+const setup = (
+  knex = defaultKnex,
+  bookshelf = defaultBookshelf,
+  config = defaultConfig,
+  models = { user, authorization }
+) => {
+  const db = knex(config[process.env.NODE_ENV]);
+  const orm = bookshelf(db);
+
+  Object.keys(models).forEach(modelName => {
+    exportedModels[modelName] = models[modelName](orm);
+  });
+};
+
+module.exports = {
+  setup,
+  models: exportedModels
+};
