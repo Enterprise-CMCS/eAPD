1fcc40a242c56d6b9303338ced5c02d6b50a065c
Greg Walker <michael.walker@gsa.gov>
2018-01-25 11:11:40 -0600

    switch to bookshelf

--- api/auth/authenticate.js
+++ api/auth/authenticate.js
@@ -1,66 +1,22 @@
-const defaultDB = require('../db')();
 const defaultBcrypt = require('bcryptjs');
-require('../db/index').setup();
-const usersModel = require('../db/user')();
+const defaultUserModel = require('../db/user')();
 
-module.exports = (db = defaultDB, bcrypt = defaultBcrypt) => async (
-  username,
-  password,
-  done
-) =>
-  usersModel
-    .where({ email: username })
-    .fetch()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.get('password'))) {
-        done(null, {
-          username: user.get('email'),
-          id: user.get('id')
-        });
-      } else {
-        done(null, false);
-      }
-    })
-    .catch(() => {
-      done('Database error');
-    });
+module.exports = (
+  userModel = defaultUserModel,
+  bcrypt = defaultBcrypt
+) => async (username, password, done) => {
+  try {
+    const user = await userModel.where({ email: username }).fetch();
 
-  // db('users')
-  //   .where({ email: username })
-  //   .first()
-  //   .then(user => {
-  //     // If there is a matching user, return it
-  //     if (user && bcrypt.compareSync(password, user.password)) {
-  //       usersModel
-  //         .where({ email: username })
-  //         .fetch({ withRelated: ['role.activities'] })
-  //         .then(bookshelfUser => {
-  //           console.log({
-  //             username: bookshelfUser.get('email'),
-  //             id: bookshelfUser.get('id'),
-  //             activities: bookshelfUser.activities()
-  //           });
-  //           // done(null, {
-  //           //   username: user.get('email'),
-  //           //   id: user.get('id'),
-  //           //   activities: user.activities()
-  //           // });
-  //         })
-  //         .catch(() => {
-  //           done('Could not deserialize user');
-  //         });
-  //
-  //       done(null, { username: user.email, id: user.id });
-  //     } else {
-  //       // Otherwise, callback with a false user.  If we
-  //       // send an error, Passport will send a status 500,
-  //       // but if we send no error and a false user, it will
-  //       // send a 401, which is what we really want.  So...
-  //       // I guess a failed login isn't an error. :P
-  //       done(null, false);
-  //     }
-  //   })
-  //   .catch((e) => {
-  //     console.log(e);
-  //     done('Database error');
-  //   });
+    if (user && (await bcrypt.compare(password, user.get('password')))) {
+      done(null, {
+        username: user.get('email'),
+        id: user.get('id')
+      });
+    } else {
+      done(null, false);
+    }
+  } catch (e) {
+    done('Database error');
+  }
+};
--- api/auth/serialization.js
+++ api/auth/serialization.js
@@ -1,6 +1,4 @@
-const defaultDB = require('../db')();
-
-const usersModel = require('../db/user')();
+const defaultUsersModel = require('../db/user')();
 
 // Serialize a user into a stringy type that will get
 // pushed into their session cookie.
@@ -10,7 +8,11 @@ module.exports.serializeUser = (user, done) => {
 
 // Deserialize a stringy from the user's session cookie
 // into a user object.
-module.exports.deserializeUser = async (userID, done) => {
+module.exports.deserializeUser = async (
+  userID,
+  done,
+  usersModel = defaultUsersModel
+) => {
   try {
     const user = await usersModel.where({ id: userID }).fetch();
     done(null, {
@@ -22,44 +24,3 @@ module.exports.deserializeUser = async (userID, done) => {
     done('Could not deserialize user');
   }
 };
-
-// module.exports.deserializeUser = (userID, done, db = defaultDB) =>
-//   db('users')
-//     .where({ id: userID })
-//     .select()
-//     .then(users => {
-//       if (users.length) {
-//         const role = users[0].auth_role;
-//         if (role) {
-//           db('auth_role_activity_mapping')
-//             .fullOuterJoin(
-//               'auth_roles',
-//               'auth_roles.id',
-//               'auth_role_activity_mapping.role_id'
-//             )
-//             .where({ 'auth_roles.name': role })
-//             .innerJoin(
-//               'auth_activities',
-//               'auth_activities.id',
-//               'auth_role_activity_mapping.activity_id'
-//             )
-//             .select()
-//             .then(activityRows => {
-//               const activities = activityRows.map(row => row.name);
-//               done(null, {
-//                 username: users[0].email,
-//                 id: users[0].id,
-//                 activities
-//               });
-//             });
-//         } else {
-//           done(null, {
-//             username: users[0].email,
-//             id: users[0].id,
-//             activities: []
-//           });
-//         }
-//       } else {
-//         done('Could not deserialize user');
-//       }
-//     });
