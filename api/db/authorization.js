0a0978e293423dcb54e9081442f20a7d702e607b
Greg Walker <michael.walker@gsa.gov>
2018-01-25 21:00:49 -0600

    prettier; refactor for a shorter single method

--- api/db/authorization.js
+++ api/db/authorization.js
@@ -3,33 +3,37 @@ const defaultBookshelf = require('bookshelf');
 let activitiesModel;
 let rolesModel;
 
+const activitiesModelDefinition = {
+  tableName: 'auth_activities',
+  roles() {
+    return this.belongsToMany(
+      rolesModel,
+      'auth_role_activity_mapping',
+      'activity_id',
+      'role_id'
+    );
+  }
+};
+
+const rolesModelDefinition = {
+  tableName: 'auth_roles',
+  activities() {
+    return this.belongsToMany(
+      activitiesModel,
+      'auth_role_activity_mapping',
+      'role_id',
+      'activity_id'
+    );
+  }
+};
+
 module.exports = (bookshelf = defaultBookshelf) => {
   if (!activitiesModel) {
-    activitiesModel = bookshelf.Model.extend({
-      tableName: 'auth_activities',
-      roles() {
-        return this.belongsToMany(
-          rolesModel,
-          'auth_role_activity_mapping',
-          'activity_id',
-          'role_id'
-        );
-      }
-    });
+    activitiesModel = bookshelf.Model.extend(activitiesModelDefinition);
   }
 
   if (!rolesModel) {
-    rolesModel = bookshelf.Model.extend({
-      tableName: 'auth_roles',
-      activities() {
-        return this.belongsToMany(
-          activitiesModel,
-          'auth_role_activity_mapping',
-          'role_id',
-          'activity_id'
-        );
-      }
-    });
+    rolesModel = bookshelf.Model.extend(rolesModelDefinition);
   }
 
   return { activities: activitiesModel, roles: rolesModel };
