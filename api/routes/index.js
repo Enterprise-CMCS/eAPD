42ed56a6f1b1c795b44c922cad3ef1ba947f29c0
Greg Walker <michael.walker@gsa.gov>
2018-02-05 13:47:46 -0600

    prettier

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,11 @@
 const users = require('./users');
 const formLogger = require('./logForm');
 
-module.exports = (app, usersEndpoint = users, formLoggerEndopint = formLogger) => {
+module.exports = (
+  app,
+  usersEndpoint = users,
+  formLoggerEndopint = formLogger
+) => {
   usersEndpoint(app);
   formLoggerEndopint(app);
 };
