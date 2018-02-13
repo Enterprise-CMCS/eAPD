1b97a155c02b12f5499cef39ea6ee950872e7fc1
Greg Walker <michael.walker@gsa.gov>
2018-02-13 11:01:39 -0600

    add GET endpoint

--- api/seeds/01-roles-and-activities.js
+++ api/seeds/01-roles-and-activities.js
@@ -4,6 +4,9 @@ const setupActivities = async activitiesTable => {
   await activitiesTable.insert({ id: 3, name: 'send-apd-to-state' });
   await activitiesTable.insert({ id: 4, name: 'view-own-apds' });
   await activitiesTable.insert({ id: 5, name: 'create-apd' });
+  await activitiesTable.insert({ id: 6, name: 'view-roles' });
+  await activitiesTable.insert({ id: 7, name: 'create-roles' });
+  await activitiesTable.insert({ id: 8, name: 'edit-roles' });
 };
 
 const setupRoles = async rolesTable => {
@@ -19,6 +22,9 @@ const setupMappings = async mappingTable => {
   await mappingTable.insert({ role_id: 1, activity_id: 3 });
   await mappingTable.insert({ role_id: 1, activity_id: 4 });
   await mappingTable.insert({ role_id: 1, activity_id: 5 });
+  await mappingTable.insert({ role_id: 1, activity_id: 6 });
+  await mappingTable.insert({ role_id: 1, activity_id: 7 });
+  await mappingTable.insert({ role_id: 1, activity_id: 8 });
 
   // CMS reviewers
   await mappingTable.insert({ role_id: 2, activity_id: 1 });
