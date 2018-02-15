088f4239e6bb66c5de8bf52f0acc5bf8614d6c1b
Greg Walker <michael.walker@gsa.gov>
2018-02-15 09:26:41 -0600

    open API docs for auth and user endpoint

--- api/routes/index.test.js
+++ api/routes/index.test.js
@@ -4,7 +4,9 @@ const sinon = require('sinon');
 const endpointIndex = require('./index');
 
 tap.test('endpoint setup', async endpointTest => {
-  const app = {};
+  const app = {
+    get: sinon.spy()
+  };
   const usersEndpoint = sinon.spy();
   const formLoggerEndpoint = sinon.spy();
 
@@ -17,5 +19,10 @@ tap.test('endpoint setup', async endpointTest => {
   endpointTest.ok(
     formLoggerEndpoint.calledWith(app),
     'form logger endpoint is setup with the app'
+  );
+
+  endpointTest.ok(
+    app.get.calledWith('/open-api', sinon.match.func),
+    'sets up an endpoint to fetch OpenAPI spec'
   );
 });
