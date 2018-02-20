f78162eccd5476c78189ac54cae943739e7d740f
Greg Walker <michael.walker@gsa.gov>
2018-02-20 14:17:38 -0600

    re-seed database before tests that rely on db values

--- api/endpoint-tests/roles/put.js
+++ api/endpoint-tests/roles/put.js
@@ -1,70 +1,82 @@
 const tap = require('tap'); // eslint-disable-line import/no-extraneous-dependencies
-const {
-  db, request, getFullPath, login
-} = require('../utils');
+const { db, request, getFullPath, login } = require('../utils');
 
 tap.test('roles endpoint | PUT /roles/:roleID', async postRolesTests => {
+  await db().seed.run();
+
   const url = getFullPath('/roles');
 
-  const invalidCases = [{
-    name: 'with no body'
-  }, {
-    name: 'with no activities',
-    body: { name: 'new-role' }
-  }, {
-    name: 'with activities not an array',
-    body: { name: 'new-role', activities: 'wrong' }
-  }, {
-    name: 'with activities where some are not numbers',
-    body: { name: 'new-role', activities: [0, 'wrong', 2] }
-  }, {
-    name: 'with activities that are not valid',
-    body: { name: 'new-role', activities: [1, 2, 9001] }
-  }];
+  const invalidCases = [
+    {
+      name: 'with no body'
+    },
+    {
+      name: 'with no activities',
+      body: { name: 'new-role' }
+    },
+    {
+      name: 'with activities not an array',
+      body: { name: 'new-role', activities: 'wrong' }
+    },
+    {
+      name: 'with activities where some are not numbers',
+      body: { name: 'new-role', activities: [0, 'wrong', 2] }
+    },
+    {
+      name: 'with activities that are not valid',
+      body: { name: 'new-role', activities: [1, 2, 9001] }
+    }
+  ];
 
-  postRolesTests.test('when unauthenticated, invalid ID', async unauthenticatedTests => {
-    [
-      ...invalidCases,
-      {
-        name: 'with a valid body',
-        body: { activities: [1, 2] }
-      }
-    ].forEach(situation => {
-      unauthenticatedTests.test(situation.name, async unauthenticatedTest => {
-        const { response, body } = await request.put(`${url}/9001`, {
-          json: situation.body
+  postRolesTests.test(
+    'when unauthenticated, invalid ID',
+    async unauthenticatedTests => {
+      [
+        ...invalidCases,
+        {
+          name: 'with a valid body',
+          body: { activities: [1, 2] }
+        }
+      ].forEach(situation => {
+        unauthenticatedTests.test(situation.name, async unauthenticatedTest => {
+          const { response, body } = await request.put(`${url}/9001`, {
+            json: situation.body
+          });
+          unauthenticatedTest.equal(
+            response.statusCode,
+            403,
+            'gives a 403 status code'
+          );
+          unauthenticatedTest.notOk(body, 'does not send a body');
         });
-        unauthenticatedTest.equal(
-          response.statusCode,
-          403,
-          'gives a 403 status code'
-        );
-        unauthenticatedTest.notOk(body, 'does not send a body');
       });
-    });
-  });
+    }
+  );
 
-  postRolesTests.test('when unauthenticated, valid ID', async unauthenticatedTests => {
-    [
-      ...invalidCases,
-      {
-        name: 'with a valid body',
-        body: { activities: [1, 2] }
-      }
-    ].forEach(situation => {
-      unauthenticatedTests.test(situation.name, async unauthenticatedTest => {
-        const { response, body } = await request.put(`${url}/1`, {
-          json: situation.body
+  postRolesTests.test(
+    'when unauthenticated, valid ID',
+    async unauthenticatedTests => {
+      [
+        ...invalidCases,
+        {
+          name: 'with a valid body',
+          body: { activities: [1, 2] }
+        }
+      ].forEach(situation => {
+        unauthenticatedTests.test(situation.name, async unauthenticatedTest => {
+          const { response, body } = await request.put(`${url}/1`, {
+            json: situation.body
+          });
+          unauthenticatedTest.equal(
+            response.statusCode,
+            403,
+            'gives a 403 status code'
+          );
+          unauthenticatedTest.notOk(body, 'does not send a body');
         });
-        unauthenticatedTest.equal(
-          response.statusCode,
-          403,
-          'gives a 403 status code'
-        );
-        unauthenticatedTest.notOk(body, 'does not send a body');
       });
-    });
-  });
+    }
+  );
 
   postRolesTests.test('when authenticated', async authenticatedTests => {
     const cookies = await login();
@@ -107,7 +119,11 @@ tap.test('roles endpoint | PUT /roles/:roleID', async postRolesTests => {
         .select('activity_id')
         .map(activity => activity.activity_id);
 
-      validTest.same(activities, [1], 'sets up a mapping between the role and the newly-requested activities');
+      validTest.same(
+        activities,
+        [1],
+        'sets up a mapping between the role and the newly-requested activities'
+      );
     });
   });
 });
