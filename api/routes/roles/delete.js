19f5bece66b7637038e0d6386dd2337c6355c58d
Greg Walker <michael.walker@gsa.gov>
2018-02-21 14:34:59 -0600

    prettier

--- api/routes/roles/delete.js
+++ api/routes/roles/delete.js
@@ -1,23 +1,16 @@
 const defaultRoleModel = require('../../db').models.role;
 const can = require('../../auth/middleware').can;
 
-module.exports = (
-  app,
-  RoleModel = defaultRoleModel
-) => {
+module.exports = (app, RoleModel = defaultRoleModel) => {
   app.delete('/roles/:id', can('delete-roles'), async (req, res) => {
     const targetRole = await RoleModel.where({ id: req.params.id }).fetch();
     if (!targetRole) {
-      return res
-        .status(404)
-        .end();
+      return res.status(404).end();
     }
 
     const userRole = await RoleModel.where({ name: req.user.role }).fetch();
     if (userRole.get('id') === targetRole.get('id')) {
-      return res
-        .status(401)
-        .end();
+      return res.status(401).end();
     }
 
     try {
