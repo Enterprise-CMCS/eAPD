aec70cf04a40d509fe16a6127ecdebf202be1114
Greg Walker <michael.walker@gsa.gov>
2018-02-22 17:26:59 -0600

    add file logging option

--- api/env.js
+++ api/env.js
@@ -5,7 +5,9 @@ const defaults = {
   PORT: 8000,
   SESSION_SECRET: crypto.randomBytes(32).toString('hex'),
   NODE_ENV: 'development',
-  LOG_LEVEL: 'info'
+  LOG_LEVEL: 'info',
+  LOG_FILE: false,
+  LOG_CONSOLE: true
 };
 
 dotenv.config();
