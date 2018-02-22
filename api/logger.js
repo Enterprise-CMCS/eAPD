713e620f726bf06a4c9c2c44d1932db819a1a2bc
Greg Walker <michael.walker@gsa.gov>
2018-02-22 17:26:59 -0600

    add file logging option

--- api/logger.js
+++ api/logger.js
@@ -4,14 +4,37 @@ const loggers = {};
 
 module.exports = name => {
   if (!loggers[name]) {
-    winston.loggers.add(name, {
-      console: {
+    const transports = {};
+
+    if (process.env.LOG_FILE === 'true') {
+      transports.file = {
+        filename: 'hitech_api.log',
+        label: name,
+        level: process.env.LOG_LEVEL,
+        maxFiles: 5,
+        maxsize: 2097152,
+        tailable: true,
+        timestamp: true,
+        zippedArchive: true
+      };
+    }
+
+    if (process.env.LOG_CONSOLE === 'true') {
+      transports.console = {
         colorize: true,
         label: name,
         level: process.env.LOG_LEVEL,
-        silent: process.argv.includes('--silent')
-      }
-    });
+        prettyPrint: true,
+        silent: process.argv.includes('--silent'),
+        timestamp: true
+      };
+    } else {
+      transports.console = {
+        silent: true
+      };
+    }
+
+    winston.loggers.add(name, transports);
     loggers[name] = true;
   }
 
