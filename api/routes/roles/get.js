7e988e44c9a6e52b238e6e321e837c5de66a0bf6
Greg Walker <michael.walker@gsa.gov>
2018-02-22 17:52:00 -0600

    more logging

--- api/routes/roles/get.js
+++ api/routes/roles/get.js
@@ -1,10 +1,14 @@
+const logger = require('../../logger')('roles route get');
 const defaultRoleModel = require('../../db').models.role;
 const can = require('../../auth/middleware').can;
 
 module.exports = (app, RoleModel = defaultRoleModel) => {
+  logger.silly('setting up GET /roles route');
   app.get('/roles', can('view-roles'), async (req, res) => {
+    logger.silly('handling up GET /roles route');
     try {
       const roles = await RoleModel.fetchAll({ columns: ['id', 'name'] });
+      logger.silly('getting all the activities for all the roles...');
       const sendRoles = await Promise.all(
         roles.map(async role => ({
           id: role.get('id'),
@@ -12,8 +16,11 @@ module.exports = (app, RoleModel = defaultRoleModel) => {
           activities: await role.getActivities()
         }))
       );
+      logger.silly('got all the roles');
+      logger.silly(sendRoles);
       res.send(sendRoles);
     } catch (e) {
+      logger.error(e);
       res.status(500).end();
     }
   });
