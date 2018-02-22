e14081e8d868fbd18fcdedb7e4616fb874de2d25
Greg Walker <michael.walker@gsa.gov>
2018-02-22 11:15:11 -0600

    provide a flag to turn off logging; turn off logging in unit tests

--- api/logger.js
+++ api/logger.js
@@ -6,9 +6,10 @@ module.exports = name => {
   if (!loggers[name]) {
     winston.loggers.add(name, {
       console: {
-        level: process.env.LOG_LEVEL,
         colorize: true,
-        label: name
+        label: name,
+        level: process.env.LOG_LEVEL,
+        silent: process.argv.includes('--silent')
       }
     });
     loggers[name] = true;
