02428e9ef1ddd357e95febcf790072bbe5e51917
Greg Walker <michael.walker@gsa.gov>
2018-02-28 12:51:17 -0600

    yup

--- api/db/user.js
+++ api/db/user.js
@@ -4,6 +4,10 @@ module.exports = {
   user: {
     tableName: 'users',
 
+    initialize() {
+      this.on('saving', this.validate);
+    },
+
     role() {
       return this.hasOne('role', 'name', 'auth_role');
     },
@@ -18,6 +22,31 @@ module.exports = {
       return this.related('role')
         .related('activities')
         .pluck('name');
+    },
+
+    validate(model) {
+      console.log('validation:');
+      console.log(model.attributes);
+      console.log(model.hasChanged('email'));
+    },
+
+    format(attributes) {
+      const out = { ...attributes };
+
+      if (out.phone) {
+        // Remove non-number characters
+        out.phone = out.phone.replace(/[^\d]/g, '');
+
+        // If the phone number is 11 digits and begins with a 1,
+        // just strip off the leading 1, it's probably a country
+        // code and we don't need it since all US states and
+        // territories use the same country code.
+        if (out.phone.length === 11 && out.phone.startsWith('1')) {
+          out.phone = out.phone.substr(1);
+        }
+      }
+
+      return out;
     }
   }
 };
