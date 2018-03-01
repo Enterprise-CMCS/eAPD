78d631470b65fb10db1bf757665d8a2747a86bcc
Brendan Sudol <brendan.sudol@gsa.gov>
2018-03-01 16:57:36 -0500

    add state info

---
+++ api/migrations/20180301154028_add-state-info.js
@@ -0,0 +1,13 @@
+exports.up = async knex => {
+  await knex.schema.alterTable('states', statesTable => {
+    statesTable.json('medicaid_office');
+    statesTable.json('state_pocs');
+  });
+};
+
+exports.down = async knex => {
+  await knex.schema.alterTable('states', statesTable => {
+    statesTable.dropColumn('medicaid_office');
+    statesTable.dropColumn('state_pocs');
+  });
+};
--- api/seeds/01-states.js
+++ api/seeds/01-states.js
@@ -61,6 +61,33 @@ const stateEntries = [
   { id: 'vi', name: 'U.S. Virgin Islands' }
 ];
 
+const vermontOffice = {
+  address: 'Department of Vermont Health Access',
+  city: 'Waterbury',
+  zip: '05671-1010'
+};
+
+const vermontPocs = [
+  {
+    name: 'Jane Doe',
+    position: 'Director',
+    email: 'jane.doe@vermont.gov'
+  },
+  {
+    name: 'Richard Roe',
+    position: 'CIO',
+    email: 'richard.roe@vermont.gov'
+  }
+];
+
 exports.seed = async knex => {
   await knex('states').insert(stateEntries);
+
+  await knex
+    .table('states')
+    .where({ id: 'vt' })
+    .update({
+      medicaid_office: JSON.stringify(vermontOffice),
+      state_pocs: JSON.stringify(vermontPocs)
+    });
 };
