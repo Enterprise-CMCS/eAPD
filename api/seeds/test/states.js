ee3a9f60b7723d30d17dfa034f1734f20aa9017d
Greg Walker <michael.walker@gsa.gov>
2018-03-02 11:16:34 -0600

    endpoint tests

---
+++ api/seeds/test/states.js
@@ -0,0 +1,9 @@
+exports.seed = async knex => {
+  await knex
+    .table('states')
+    .where({ id: 'mn' })
+    .update({
+      program_benefits: 'The program will have benefits',
+      program_vision: 'The program vision is 20/20'
+    });
+};
