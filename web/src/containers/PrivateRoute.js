caccef89a0fc16e7be1274134b422cc9172da6f5
Brendan Sudol <brendan.sudol@gsa.gov>
2018-03-02 17:59:06 -0500

    remove cruft

--- web/src/containers/PrivateRoute.js
+++ web/src/containers/PrivateRoute.js
@@ -1,18 +1,7 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-
-class AuthTry extends Component {
-  componentDidMount() {
-    console.log('auth try mount');
-  }
-
-  render() {
-    const { from } = this.props;
-    return <Redirect to={from} />;
-  }
-}
 
 const PrivateRoute = ({ authenticated, component: Component, ...rest }) => (
   <Route
