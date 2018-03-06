f1273b8137f123608cd8ebd41be887b9b04bb6ff
Greg Walker <michael.walker@gsa.gov>
2018-03-06 16:18:03 -0600

    make state endpoint generic

--- api/seeds/test/states.js
+++ api/seeds/test/states.js
@@ -3,7 +3,9 @@ exports.seed = async knex => {
     .table('states')
     .where({ id: 'mn' })
     .update({
+      medicaid_office: '{"office":"address"}',
       program_benefits: 'The program will have benefits',
-      program_vision: 'The program vision is 20/20'
+      program_vision: 'The program vision is 20/20',
+      state_pocs: '{"pocs":"people"}'
     });
 };
