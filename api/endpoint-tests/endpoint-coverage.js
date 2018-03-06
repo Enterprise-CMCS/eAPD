2a792be77480a0a348ace8390a48244b4b7a91b2
Greg Walker <mgwalker@gmail.com>
2018-03-06 14:50:27 -0600

    run endpoint coverage analysis

---
+++ api/endpoint-tests/endpoint-coverage.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const readline = require('readline');
+
+const openAPI = require('../routes/openAPI');
+
+const allEndpoints = JSON.parse(
+  fs.readFileSync(`${__dirname}/endpoints-all.txt`)
+).reduce((acc, { path, method }) => {
+  const methodStatus = {
+    tested: false,
+    documented: false
+  };
+  if (acc[path]) {
+    acc[path][method] = methodStatus;
+  } else {
+    acc[path] = {
+      [method]: methodStatus
+    };
+  }
+
+  const openAPIpath = path.replace(/\/:(.+?)(\/|$)/, '/{$1}$2');
+  const oa = openAPI.paths[openAPIpath];
+  if (oa && oa[method] && oa[method].responses) {
+    acc[path][method].documented = Object.keys(oa[method].responses)
+      .map(r => +r)
+      .sort();
+  }
+
+  return acc;
+}, {});
+
+const hitsStream = fs.createReadStream(`${__dirname}/endpoints-hit.txt`);
+const hitsReader = readline.createInterface({ input: hitsStream });
+
+hitsReader.on('line', line => {
+  const { path, method, status } = JSON.parse(line);
+
+  if (!allEndpoints[path][method.toLowerCase()].tested) {
+    allEndpoints[path][method.toLowerCase()].tested = [];
+  }
+  if (!allEndpoints[path][method.toLowerCase()].tested.includes(status)) {
+    allEndpoints[path][method.toLowerCase()].tested.push(status);
+  }
+  allEndpoints[path][method.toLowerCase()].tested.sort();
+});
+
+hitsReader.on('close', () => {
+  const messages = [];
+  const methodMessages = [];
+
+  Object.keys(allEndpoints).forEach(path => {
+    messages.length = 0;
+
+    Object.keys(allEndpoints[path]).forEach(method => {
+      methodMessages.length = 0;
+      const tests = allEndpoints[path][method].tested;
+      const docs = allEndpoints[path][method].documented;
+
+      if (!tests) {
+        methodMessages.push(`    not tested`);
+      }
+      if (!docs) {
+        methodMessages.push(`    not documented`);
+      }
+
+      if (methodMessages.length) {
+        messages.push(`  ${method}`, ...methodMessages);
+        return;
+      }
+
+      tests.forEach(status => {
+        if (!docs.includes(status)) {
+          methodMessages.push(
+            `     tests for status ${status}, but that status is not documented`
+          );
+        }
+      });
+      docs.forEach(status => {
+        if (!tests.includes(status)) {
+          methodMessages.push(
+            `     documents status ${status}, but that status is not tested`
+          );
+        }
+      });
+
+      if (methodMessages.length) {
+        messages.push(`  ${method}`, ...methodMessages);
+      }
+    });
+
+    if (messages.length) {
+      console.log(`\n${path}`);
+      console.log(messages.join('\n'));
+    }
+  });
+  // console.log(JSON.stringify(allEndpoints, false, 2));
+});
