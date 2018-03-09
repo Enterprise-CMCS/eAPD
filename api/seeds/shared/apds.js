32be9f778dd99c48788838fe2b29037145188b9e
Brendan Sudol <brendan.sudol@gsa.gov>
2018-03-09 17:35:54 -0500

    seed apds

---
+++ api/seeds/shared/apds.js
@@ -0,0 +1,10 @@
+const { states } = require('../../util/states');
+
+const apdEntries = states.map(state => ({
+  status: 'draft',
+  state_id: state.id
+}));
+
+exports.seed = async knex => {
+  await knex('apds').insert(apdEntries);
+};
