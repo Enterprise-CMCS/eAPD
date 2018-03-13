2fb6893af24f31320b0c1bcdddbc116cd6c0dcda
Greg Walker <michael.walker@gsa.gov>
2018-03-13 10:04:24 -0500

    delete activities etc

--- api/seeds/shared/delete-everything.js
+++ api/seeds/shared/delete-everything.js
@@ -8,6 +8,12 @@ exports.seed = async knex => {
   await knex('auth_roles').del();
   await knex('auth_activities').del();
 
+  // have cascading foreign key relationships, so make sure we
+  // delete them in the right order
+  await knex('activity_goal_objectives').del();
+  await knex('activity_goals').del();
+  await knex('activities').del();
+
   await knex('apds').del();
   await knex('states').del();
 };
