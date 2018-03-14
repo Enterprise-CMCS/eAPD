6ba773db8db3f4bf9f887428b6d7a44de57e5f9b
Greg Walker <michael.walker@gsa.gov>
2018-03-14 11:03:53 -0500

    add/edit activities, goals, objectives

--- web/src/containers/ActivitiesList.js
+++ web/src/containers/ActivitiesList.js
@@ -1,57 +1,81 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { push } from 'react-router-redux';
-import { bindActionCreators } from 'redux';
 
 import withSidebar from '../components/withSidebar';
+import { fetchApdDataIfNeeded } from '../actions/apd';
 
-// [Activity name, Link text, Link href]
-// TODO(bren): update href for "Administration" entry once
-// that page is created
-const activities = [
-  ['Administration', 'Start', '/activity-overview'],
-  ['Designing and Building HIE System', 'Edit', '#!'],
-  ['Technical Assistance', 'Start', '#!'],
-  ['Outreach', 'Start', '#!']
-];
+class ActivitiesList extends Component {
+  componentDidMount() {
+    this.props.fetchApdDataIfNeeded();
+  }
 
-const ActivitiesList = ({ goTo }) => (
-  <div>
-    <h1>Activities</h1>
-    <div className="mb3">
-      {activities.map(([name, status, href]) => (
-        <div key={name} className="py1 relative border-bottom border-silver">
-          <div className="absolute right-0">
-            {href === '#!' ? (
-              <div className="gray">{status}</div>
-            ) : (
-              <Link to={href}>{status}</Link>
-            )}
-          </div>
-          {name}
-        </div>
-      ))}
-    </div>
-    <button
-      type="button"
-      className="btn btn-outline blue mr1"
-      onClick={() => goTo('/activities-start')}
-    >
-      Back
-    </button>
-    <button type="button" className="btn btn-primary">
-      Add another activity
-    </button>
-  </div>
-);
+  render() {
+    const { apd, activities, goTo } = this.props;
+
+    return (
+      <div>
+        <h1>Activities</h1>
+        {!apd.loaded ? (
+          <p>Loading...</p>
+        ) : (
+          <Fragment>
+            <div className="mb3">
+              {activities.map(({ name, status }) => (
+                <div
+                  key={name}
+                  className="py1 relative border-bottom border-silver"
+                >
+                  <div className="absolute right-0">
+                    <Link to={`/activity-overview/${name}`}>
+                      {status ? 'Edit' : 'Start'}
+                    </Link>
+                  </div>
+                  {name}
+                </div>
+              ))}
+            </div>
+            <button
+              type="button"
+              className="btn btn-outline blue mr1"
+              onClick={() => goTo('/activities-start')}
+            >
+              Back
+            </button>
+            <button type="button" className="btn btn-primary">
+              Add another activity
+            </button>
+          </Fragment>
+        )}
+      </div>
+    );
+  }
+}
 
 ActivitiesList.propTypes = {
+  activities: PropTypes.array.isRequired,
+  apd: PropTypes.object.isRequired,
+  fetchApdDataIfNeeded: PropTypes.func.isRequired,
   goTo: PropTypes.func.isRequired
 };
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ goTo: path => push(path) }, dispatch);
+const mapStateToProps = ({ apd }) => ({
+  apd,
+  activities: apd.data.activities.map(activity => ({
+    id: activity.id,
+    name: activity.name,
+    // TODO [GW] figure out how this should be set
+    started: true
+  }))
+});
 
-export default connect(null, mapDispatchToProps)(withSidebar(ActivitiesList));
+const mapDispatchToProps = {
+  goTo: push,
+  fetchApdDataIfNeeded
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withSidebar(ActivitiesList)
+);
