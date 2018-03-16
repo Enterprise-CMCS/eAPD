5aae7c2ba0d2779f8b16c78e5c0702e8f7550311
Brendan Sudol <brendan.sudol@gsa.gov>
2018-03-16 15:42:28 -0400

    more tests

--- web/src/i18n/index.test.js
+++ web/src/i18n/index.test.js
@@ -1,10 +1,25 @@
-import { t } from './index';
+import I18n, { t } from './index';
 
 import en from './locales/en.json';
+import fr from './locales/fr.json';
 
 describe('i18n translations', () => {
+  beforeEach(() => {
+    I18n.locale = 'en';
+  });
+
   test('t function', () => {
     expect(t('test')).toBe(en.test);
     expect(t('nonsense')).toBe('[missing "en.nonsense" translation]');
+  });
+
+  test('changing locale to one that is supported', () => {
+    I18n.locale = 'fr';
+    expect(t('test')).toBe(fr.test);
+  });
+
+  test('changing locale to one that is not supported', () => {
+    I18n.locale = 'pig-latin';
+    expect(t('test')).toBe('[missing "pig-latin.test" translation]');
   });
 });
