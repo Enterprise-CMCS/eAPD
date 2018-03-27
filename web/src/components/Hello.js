d4b8f8025f6d92365b4ff7bd69aa7738bca71bda
Brendan Sudol <brendan.sudol@gsa.gov>
2018-03-27 12:39:58 -0400

    add state icons

--- web/src/components/Hello.js
+++ web/src/components/Hello.js
@@ -3,12 +3,19 @@ import React from 'react';
 import Container from './Container';
 import UsaAnimated from './UsaAnimated';
 import { t } from '../i18n';
+import { STATES } from '../util';
 
 const Hello = () => (
   <Container>
     <div className="my3 py4 center">
       <UsaAnimated />
       <p className="mt3">{t('test')}</p>
+      <hr />
+      <div className="left-align">
+        {STATES.slice(0, 50).map(s => (
+          <img src={`/static/img/${s.id}.svg`} alt={s.name} />
+        ))}
+      </div>
     </div>
   </Container>
 );
