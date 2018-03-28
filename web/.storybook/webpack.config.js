5d5137a059a3bbec7aba947f00526e61c5ef9a45
Brendan Sudol <brendan.sudol@gsa.gov>
2018-03-28 15:54:16 -0400

    add collapsible component and storybook

---
+++ web/.storybook/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  }
+};
