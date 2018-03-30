bd0d385b31bf5ede72df71e2407c9daecd5e5d24
Greg Walker <michael.walker@gsa.gov>
2018-03-30 12:58:13 -0500

    uhh... don't reuse IDs

--- api/seeds/test/apds.js
+++ api/seeds/test/apds.js
@@ -1,12 +1,17 @@
 exports.seed = async knex => {
+  // 40xx IDs refer to apds
+  // 41xx IDs refer to activities
+  // 42xx IDs refer to goals
+  // 43xx IDs refer to objectives
+
   await knex('apds').insert([
     {
-      id: 1000,
+      id: 4000,
       state_id: 'mn',
       status: 'draft'
     },
     {
-      id: 1001,
+      id: 4001,
       state_id: 'az',
       status: 'draft'
     }
@@ -14,82 +19,82 @@ exports.seed = async knex => {
 
   await knex('activities').insert([
     {
-      id: 1000,
+      id: 4100,
       name: 'Find Success',
       description: 'Some text goes here',
-      apd_id: 1000
+      apd_id: 4000
     },
     {
-      id: 1001,
+      id: 4101,
       name: 'My Second Activity',
       description: 'More gunk',
-      apd_id: 1000
+      apd_id: 4000
     },
     {
-      id: 2000,
+      id: 4110,
       name: 'Inaccessible Activity',
-      apd_id: 1001
+      apd_id: 4001
     }
   ]);
 
   await knex('activity_goals').insert([
     {
-      id: 1000,
+      id: 4200,
       description: 'Be a super successful artist',
-      activity_id: 1000
+      activity_id: 4100
     },
     {
-      id: 1001,
+      id: 4201,
       description: 'Win a Nobel prize for physics',
-      activity_id: 1000
+      activity_id: 4100
     },
     {
-      id: 1002,
+      id: 4202,
       description: 'Go on Ellen',
-      activity_id: 1000
+      activity_id: 4100
     }
   ]);
 
   await knex('activity_goal_objectives').insert([
     {
-      id: 1000,
+      id: 4300,
       description: 'Paint a pretty picture',
-      activity_goal_id: 1000
+      activity_goal_id: 4200
     },
     {
-      id: 1001,
+      id: 4301,
       description: 'Paint a confusing picture',
-      activity_goal_id: 1000
+      activity_goal_id: 4200
     },
     {
-      id: 1002,
+      id: 4302,
       description: 'Paint an offensive picture',
-      activity_goal_id: 1000
+      activity_goal_id: 4200
     },
     {
-      id: 1003,
+      id: 4303,
       description: '...Profit',
-      activity_goal_id: 1000
+      activity_goal_id: 4200
     },
     {
-      id: 1004,
+      id: 4304,
       description: 'Discover a new particle',
-      activity_goal_id: 1001
+      activity_goal_id: 4201
     },
     {
-      id: 1005,
+      id: 4305,
       description: 'Lie about the Moonmen',
-      activity_goal_id: 1001
+      activity_goal_id: 4201
     },
     {
-      id: 1006,
+      id: 4306,
       description: 'Learn to dance',
-      activity_goal_id: 1002
+      activity_goal_id: 4202
     },
     {
-      id: 1007,
+      id: 4307,
       description: 'Bring audience gifts',
-      activity_goal_id: 1002
+      activity_goal_id: 4202
     }
   ]);
 };
