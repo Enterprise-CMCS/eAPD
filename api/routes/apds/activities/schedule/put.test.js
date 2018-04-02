25b1f4aa47f95cdc47a3683320e0a5c0b08d558a
Greg Walker <michael.walker@gsa.gov>
2018-04-02 10:57:27 -0500

    update tests

--- api/routes/apds/activities/schedule/put.test.js
+++ api/routes/apds/activities/schedule/put.test.js
@@ -8,7 +8,7 @@ const {
 } = require('../../../../middleware');
 const putEndpoint = require('./put');
 
-tap.test('apd activity approach PUT endpoint', async endpointTest => {
+tap.test('apd activity schedule PUT endpoint', async endpointTest => {
   const sandbox = sinon.createSandbox();
   const app = { put: sandbox.stub() };
 
@@ -16,7 +16,7 @@ tap.test('apd activity approach PUT endpoint', async endpointTest => {
     where: sandbox.stub(),
     fetch: sandbox.stub()
   };
-  const ApproachModel = {
+  const ScheduleModel = {
     forge: sandbox.stub()
   };
 
@@ -52,144 +52,137 @@ tap.test('apd activity approach PUT endpoint', async endpointTest => {
   });
 
   endpointTest.test('setup', async setupTest => {
-    putEndpoint(app, ActivityModel, ApproachModel);
+    putEndpoint(app, ActivityModel, ScheduleModel);
 
     setupTest.ok(
       app.put.calledWith(
-        '/activities/:id/approaches',
+        '/activities/:id/schedule',
         loggedIn,
         loadActivity(),
         userCanEditAPD(ActivityModel),
         sinon.match.func
       ),
-      'apd activity approach PUT endpoint is registered'
+      'apd activity schedule PUT endpoint is registered'
     );
   });
 
-  endpointTest.test(
-    'set APD activity approaches handler',
-    async handlerTest => {
-      let handler;
-      handlerTest.beforeEach(async () => {
-        putEndpoint(app, ActivityModel, ApproachModel);
-        handler = app.put.args.find(
-          args => args[0] === '/activities/:id/approaches'
-        )[4];
-      });
+  endpointTest.test('set APD activity schedule handler', async handlerTest => {
+    let handler;
+    handlerTest.beforeEach(async () => {
+      putEndpoint(app, ActivityModel, ScheduleModel);
+      handler = app.put.args.find(
+        args => args[0] === '/activities/:id/schedule'
+      )[4];
+    });
 
-      handlerTest.test(
-        'sends a server error if anything goes wrong',
-        async saveTest => {
-          const req = {
-            user: { id: 1 },
-            params: { id: 1 },
-            body: { status: 'foo' },
-            meta: null
-          };
-
-          await handler(req, res);
-
-          saveTest.ok(res.status.calledWith(500), 'HTTP status set to 500');
-        }
-      );
-
-      handlerTest.test(
-        'sends an error if request body is not an array',
-        async invalidTest => {
-          const req = {
-            user: { id: 1 },
-            params: { id: 1 },
-            body: 'hello',
-            meta: { activity: activityObj }
-          };
-          activityObj.get.withArgs('id').returns('apd-id');
-
-          await handler(req, res);
-
-          invalidTest.ok(res.status.calledWith(400), 'HTTP status set to 400');
-          invalidTest.ok(
-            res.send.calledWith({ error: 'edit-activity-invalid-approaches' }),
-            'sends an error string'
-          );
-          invalidTest.ok(res.end.calledOnce, 'response is terminated');
-        }
-      );
-
-      handlerTest.test('updates valid approaches', async validTest => {
+    handlerTest.test(
+      'sends a server error if anything goes wrong',
+      async saveTest => {
         const req = {
           user: { id: 1 },
           params: { id: 1 },
-          body: [
-            {
-              description: 'approach 1',
-              alternatives: 'alt 1',
-              explanation: 'exp 1'
-            },
-            {
-              description: 'approach 2',
-              alternatives: 'alt 2',
-              explanation: 'exp 2'
-            },
-            {
-              invalid: 'this one is ignored',
-              because: 'it does not have any expected fields'
-            }
-          ],
+          body: { status: 'foo' },
+          meta: null
+        };
+
+        await handler(req, res);
+
+        saveTest.ok(res.status.calledWith(500), 'HTTP status set to 500');
+      }
+    );
+
+    handlerTest.test(
+      'sends an error if request body is not an array',
+      async invalidTest => {
+        const req = {
+          user: { id: 1 },
+          params: { id: 1 },
+          body: 'hello',
           meta: { activity: activityObj }
         };
         activityObj.get.withArgs('id').returns('apd-id');
 
-        const destroyExisting = sinon.stub();
-        const existingApproaches = [
-          { destroy: destroyExisting },
-          { destroy: destroyExisting },
-          { destroy: destroyExisting }
-        ];
-
-        activityObj.related.withArgs('approaches').returns(existingApproaches);
-
-        const approach = {
-          save: sandbox.stub().resolves()
-        };
-
-        ApproachModel.forge.returns(approach);
-
-        ActivityModel.fetch.resolves({
-          toJSON: sandbox.stub().returns('activity-from-json')
-        });
-
         await handler(req, res);
 
-        validTest.ok(res.status.notCalled, 'HTTP status not explicitly set');
-        validTest.ok(
-          res.send.calledWith('activity-from-json'),
-          'sends JSON-ified activity'
+        invalidTest.ok(res.status.calledWith(400), 'HTTP status set to 400');
+        invalidTest.ok(
+          res.send.calledWith({ error: 'edit-activity-invalid-schedule' }),
+          'sends an error string'
         );
+        invalidTest.ok(res.end.calledOnce, 'response is terminated');
+      }
+    );
 
-        validTest.ok(
-          destroyExisting.calledThrice,
-          'all of the old approaches are deleted'
-        );
-        validTest.ok(
-          ApproachModel.forge.calledTwice,
-          'two approaches are created'
-        );
-        validTest.ok(
-          ApproachModel.forge.calledWith({
-            description: 'approach 1',
-            alternatives: 'alt 1',
-            explanation: 'exp 1',
-            activity_id: 1
-          }) &&
-            ApproachModel.forge.calledWith({
-              description: 'approach 2',
-              alternatives: 'alt 2',
-              explanation: 'exp 2',
-              activity_id: 1
-            }),
-          'the two expected approaches are created'
-        );
+    handlerTest.test('updates valid schedule entries', async validTest => {
+      const req = {
+        user: { id: 1 },
+        params: { id: 1 },
+        body: [
+          {
+            milestone: 'milestone 1',
+            status: 'status 1'
+          },
+          {
+            milestone: 'milestone 2',
+            status: 'status 2'
+          },
+          {
+            invalid: 'this one is ignored',
+            because: 'it does not have any expected fields'
+          }
+        ],
+        meta: { activity: activityObj }
+      };
+      activityObj.get.withArgs('id').returns('apd-id');
+
+      const destroyExisting = sinon.stub();
+      const existingSchedule = [
+        { destroy: destroyExisting },
+        { destroy: destroyExisting },
+        { destroy: destroyExisting }
+      ];
+
+      activityObj.related.withArgs('schedule').returns(existingSchedule);
+
+      const schedule = {
+        save: sandbox.stub().resolves()
+      };
+
+      ScheduleModel.forge.returns(schedule);
+
+      ActivityModel.fetch.resolves({
+        toJSON: sandbox.stub().returns('activity-from-json')
       });
-    }
-  );
+
+      await handler(req, res);
+
+      validTest.ok(res.status.notCalled, 'HTTP status not explicitly set');
+      validTest.ok(
+        res.send.calledWith('activity-from-json'),
+        'sends JSON-ified activity'
+      );
+
+      validTest.ok(
+        destroyExisting.calledThrice,
+        'all of the old schedules are deleted'
+      );
+      validTest.ok(
+        ScheduleModel.forge.calledTwice,
+        'two schedule entries are created'
+      );
+      validTest.ok(
+        ScheduleModel.forge.calledWith({
+          milestone: 'milestone 1',
+          status: 'status 1',
+          activity_id: 1
+        }) &&
+          ScheduleModel.forge.calledWith({
+            milestone: 'milestone 2',
+            status: 'status 2',
+            activity_id: 1
+          }),
+        'the two expected schedule entries are created'
+      );
+    });
+  });
 });
