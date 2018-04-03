b6947b3cfe962692a90a62dcf1b7194a1bddbe7d
Greg Walker <mgwalker@gmail.com>
2018-04-02 19:26:53 -0500

    update activity endpoints to use model class props

--- api/routes/apds/activities/expenses/put.js
+++ api/routes/apds/activities/expenses/put.js
@@ -82,7 +82,7 @@ module.exports = (
         const updatedActivity = await ActivityModel.where({
           id: req.meta.activity.get('id')
         }).fetch({
-          withRelated: ['expenses.entries']
+          withRelated: ActivityModel.withRelated
         });
 
         return res.send(updatedActivity.toJSON());
