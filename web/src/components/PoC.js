4fcbdc4db6dfb35070f377a95237224abdfacd2c
Brendan Sudol <brendan.sudol@gsa.gov>
2018-04-12 17:24:59 -0400

    tooltips, year column action buttons, etc

--- web/src/components/PoC.js
+++ web/src/components/PoC.js
@@ -9,7 +9,8 @@ import Collapsible from './Collapsible';
 import FormActivityApproach from './FormActivityApproach';
 import FormActivityGoals from './FormActivityGoals';
 import FormActivitySchedule from './FormActivitySchedule';
-import { faHelp, faHelpSolid, faSignOut } from './Icons';
+import { faCog, faHelp, faHelpSolid, faSignOut } from './Icons';
+import Tooltip from './Tooltip';
 import { getParams, stateLookup } from '../util';
 import { EDITOR_CONFIG } from '../util/editor';
 
@@ -215,66 +216,119 @@ ActivityDescription.propTypes = {
   activity: PropTypes.object.isRequired
 };
 
-const StatePersonnel = ({ activity }) => (
-  <div>
-    <div className="overflow-auto">
-      <table
-        className="mb2 h5 table table-condensed table-fixed"
-        style={{ minWidth: 700 }}
-      >
-        <thead>
-          <tr>
-            <th className="col-1" />
-            <th className="col-4" />
-            <th className="col-5" />
-            {activity.years.map(year => (
-              <th key={year} className="col-4" colSpan="2">
-                {year} Cost
-              </th>
-            ))}
-          </tr>
-          <tr>
-            <th className="col-1">#</th>
-            <th className="col-4">Title</th>
-            <th className="col-5">Description</th>
-            {activity.years.map(year => (
-              <Fragment key={year}>
-                <th>Amount</th>
-                <th>% FTE</th>
-              </Fragment>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {[...Array(5)].map((_, i) => (
-            <tr key={i}>
-              <td className="mono">{i + 1}.</td>
-              <td>
-                <input type="text" className="m0 input" />
-              </td>
-              <td>
-                <textarea className="mb2 textarea" rows="3" />
-              </td>
-              {activity.years.map(year => (
-                <Fragment key={year}>
-                  <td>
-                    <input type="text" className="m0 input" />
-                  </td>
-                  <td>
-                    <input type="text" className="m0 input" />
-                  </td>
-                </Fragment>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    <button type="button" className="btn btn-primary bg-black">
-      Add entry
-    </button>
-  </div>
+const YearActions = ({ update }) => (
+  <Tooltip
+    theme="dark"
+    trigger="click"
+    position="top-end"
+    interactive
+    render={
+      <div className="py1 fs-10p">
+        <button
+          type="button"
+          className="block col-12 btn btn-primary btn-small bg-white black"
+          onClick={update}
+        >
+          Not applicable
+        </button>
+      </div>
+    }
+  >
+    <Icon icon={faCog} size="sm" />
+  </Tooltip>
 );
+
+YearActions.propTypes = {
+  update: PropTypes.func.isRequired
+};
+
+class StatePersonnel extends Component {
+  state = { naYears: { '2018': false, '2019': false, '2020': false } };
+
+  update = year => () => {
+    this.setState(prev => ({
+      naYears: {
+        ...prev.naYears,
+        [year]: !prev.naYears[year]
+      }
+    }));
+  };
+
+  render() {
+    const { activity } = this.props;
+    const { naYears } = this.state;
+
+    return (
+      <div>
+        <div className="overflow-auto">
+          <table
+            className="mb2 h5 table table-condensed table-fixed"
+            style={{ minWidth: 700 }}
+          >
+            <thead>
+              <tr>
+                <th className="col-1" />
+                <th className="col-4" />
+                <th className="col-5" />
+                {activity.years.map((year, i) => (
+                  <th key={year} className="col-4" colSpan="2">
+                    {year} Cost{' '}
+                    {i > 0 && <YearActions update={this.update(year)} />}
+                  </th>
+                ))}
+              </tr>
+              <tr>
+                <th className="col-1">#</th>
+                <th className="col-4">Title</th>
+                <th className="col-5">Description</th>
+                {activity.years.map(year => (
+                  <Fragment key={year}>
+                    <th>Amount</th>
+                    <th>% FTE</th>
+                  </Fragment>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {[...Array(5)].map((_, i) => (
+                <tr key={i}>
+                  <td className="mono">{i + 1}.</td>
+                  <td>
+                    <input type="text" className="m0 input" />
+                  </td>
+                  <td>
+                    <textarea className="mb2 textarea" rows="3" />
+                  </td>
+                  {activity.years.map(year => (
+                    <Fragment key={year}>
+                      <td>
+                        <input
+                          type="text"
+                          className="m0 input"
+                          disabled={naYears[year]}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="text"
+                          className="m0 input"
+                          disabled={naYears[year]}
+                        />
+                      </td>
+                    </Fragment>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <button type="button" className="btn btn-primary bg-black">
+          Add entry
+        </button>
+      </div>
+    );
+  }
+}
 
 StatePersonnel.propTypes = {
   activity: PropTypes.object.isRequired
@@ -494,18 +548,24 @@ const ProgramSummary = ({ editYears, selectedYears }) => (
   <Section>
     <SectionTitle>Program Summary</SectionTitle>
     <Collapsible title="Overview" open>
-      <div className="mb1 bold">What years does this APD cover?</div>
-      {['2018', '2019', '2020'].map(year => (
-        <label key={year} className="mr1">
-          <input
-            type="checkbox"
-            value={year}
-            checked={selectedYears.includes(year)}
-            onChange={editYears(year, selectedYears)}
-          />
-          {year}
-        </label>
-      ))}
+      <div className="mb3">
+        <div className="mb1 bold">What years does this APD cover?</div>
+        {['2018', '2019', '2020'].map(year => (
+          <label key={year} className="mr1">
+            <input
+              type="checkbox"
+              value={year}
+              checked={selectedYears.includes(year)}
+              onChange={editYears(year, selectedYears)}
+            />
+            {year}
+          </label>
+        ))}
+      </div>
+      <div>
+        <div className="mb1 bold">Please...</div>
+        <Editor toolbar={EDITOR_CONFIG} />
+      </div>
     </Collapsible>
     <Collapsible title="HIT Narrative">
       <div>
---
+++ web/src/components/Tooltip.js
@@ -0,0 +1,138 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+
+import tippy from 'tippy.js/dist/tippy.standalone';
+
+import 'tippy.js/dist/tippy.css';
+import '../styles/app/tooltip.css';
+
+const applyIfFunction = fn => (typeof fn === 'function' ? fn() : fn);
+
+class Tooltip extends Component {
+  componentDidMount() {
+    this.initTippy();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!this.tippy) return;
+
+    // enable and disabled
+    if (this.props.disabled === false && prevProps.disabled === true) {
+      this.tippy.enable();
+      return;
+    }
+
+    if (this.props.disabled === true && prevProps.disabled === false) {
+      this.tippy.disable();
+      return;
+    }
+
+    // open
+    if (this.props.open === true && !prevProps.open) {
+      setTimeout(() => {
+        this.showTooltip();
+      }, 0);
+    }
+
+    if (this.props.open === false && prevProps.open === true) {
+      this.hideTooltip();
+    }
+  }
+
+  componentWillUnmount() {
+    this.destroyTippy();
+  }
+
+  showTooltip = () => {
+    this.tippy.show();
+  };
+
+  hideTooltip = () => {
+    this.tippy.hide();
+  };
+
+  contentRoot = () => {
+    if (!this.contentRootEl && typeof window === 'object') {
+      this.contentRootEl = window.document.createElement('div');
+    }
+
+    return this.contentRootEl;
+  };
+
+  initTippy = () => {
+    const { animation, arrow, render, theme, title, ...rest } = this.props;
+    this.tooltipDOM.setAttribute('title', title);
+
+    tippy(this.tooltipDOM, {
+      animation,
+      arrow,
+      theme,
+      title,
+      ...rest,
+      html: render ? this.contentRoot() : undefined,
+      dynamicTitle: true,
+      performance: true
+    });
+
+    this.tippy = this.tooltipDOM._tippy; // eslint-disable-line no-underscore-dangle
+
+    if (this.props.open) this.showTooltip();
+  };
+
+  destroyTippy = () => {
+    this.tippy.hide();
+    this.tippy.destroy();
+    this.tippy = null;
+  };
+
+  render() {
+    const { children, className, render, style, tabIndex, title } = this.props;
+
+    return (
+      <div
+        ref={tooltip => {
+          this.tooltipDOM = tooltip;
+        }}
+        title={title}
+        className={className}
+        tabIndex={tabIndex}
+        style={{ display: 'inline', ...style }}
+      >
+        {children}
+        {render && this.contentRoot()
+          ? createPortal(applyIfFunction(render), this.contentRoot())
+          : null}
+      </div>
+    );
+  }
+}
+
+Tooltip.propTypes = {
+  animation: PropTypes.string,
+  arrow: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  open: PropTypes.bool,
+  render: PropTypes.node,
+  style: PropTypes.object,
+  tabIndex: PropTypes.number,
+  theme: PropTypes.string,
+  title: PropTypes.string
+};
+
+Tooltip.defaultProps = {
+  animation: 'fade',
+  arrow: true,
+  className: '',
+  disabled: false,
+  open: false,
+  render: null,
+  style: {},
+  tabIndex: undefined,
+  theme: 'light bordered',
+  title: null
+};
+
+export default Tooltip;
