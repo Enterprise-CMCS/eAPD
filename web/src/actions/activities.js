272649f38f86e71cc60c403720973facfb83c582
Greg Walker <michael.walker@gsa.gov>
2018-04-23 16:58:46 -0500

    add actions and reducers

--- web/src/actions/activities.js
+++ web/src/actions/activities.js
@@ -1,10 +1,27 @@
 export const ADD_ACTIVITY = 'ADD_ACTIVITY';
+export const ADD_ACTIVITY_CONTRACTOR_RESOURCE =
+  'ADD_ACTIVITY_CONTRACTOR_RESOURCE';
 export const ADD_ACTIVITY_GOAL = 'ADD_ACTIVITY_GOAL';
 export const ADD_ACTIVITY_MILESTONE = 'ADD_ACTIVITY_MILESTONE';
+export const REMOVE_ACTIVITY_CONTRACTOR_RESOURCE =
+  'REMOVE_ACTIVITY_CONTRACTOR_RESOURCE';
 export const REMOVE_ACTIVITY_MILESTONE = 'REMOVE_ACTIVITY_MILESTONE';
 export const UPDATE_ACTIVITY = 'UPDATE_ACTIVITY';
 
 export const addActivity = () => ({ type: ADD_ACTIVITY });
+
+export const addActivityContractorResource = id => ({
+  type: ADD_ACTIVITY_CONTRACTOR_RESOURCE,
+  id
+});
+export const removeActivityContractorResource = (
+  id,
+  contractorResourceIdx
+) => ({
+  type: REMOVE_ACTIVITY_CONTRACTOR_RESOURCE,
+  id,
+  contractorResourceIdx
+});
 
 export const addActivityGoal = id => ({ type: ADD_ACTIVITY_GOAL, id });
 
