ee156314477a6a07cfcffaa09ebd15233d239c53
Brendan Sudol <brendan.sudol@gsa.gov>
2018-04-23 14:42:40 -0400

    reduxify milestones

--- web/src/actions/activities.js
+++ web/src/actions/activities.js
@@ -1,9 +1,24 @@
 export const ADD_ACTIVITY = 'ADD_ACTIVITY';
 export const ADD_ACTIVITY_GOAL = 'ADD_ACTIVITY_GOAL';
+export const ADD_ACTIVITY_MILESTONE = 'ADD_ACTIVITY_MILESTONE';
+export const REMOVE_ACTIVITY_MILESTONE = 'REMOVE_ACTIVITY_MILESTONE';
 export const UPDATE_ACTIVITY = 'UPDATE_ACTIVITY';
 
 export const addActivity = () => ({ type: ADD_ACTIVITY });
+
 export const addActivityGoal = id => ({ type: ADD_ACTIVITY_GOAL, id });
+
+export const addActivityMilestone = id => ({
+  type: ADD_ACTIVITY_MILESTONE,
+  id
+});
+
+export const removeActivityMilestone = (id, milestoneIdx) => ({
+  type: REMOVE_ACTIVITY_MILESTONE,
+  id,
+  milestoneIdx
+});
+
 export const updateActivity = (id, updates) => ({
   type: UPDATE_ACTIVITY,
   id,
