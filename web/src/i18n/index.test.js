8d221e54c43a0458e8329dce29e74e8d643c7f47
Greg Walker <michael.walker@gsa.gov>
2018-04-26 14:22:52 -0500

    move methods out of constructor

--- web/src/i18n/index.test.js
+++ web/src/i18n/index.test.js
@@ -1,10 +1,18 @@
-import I18n, { t } from './index';
+import I18n, { initI18n, t } from './index';
 
 import fr from './locales/fr.json';
 
 describe('i18n translations', () => {
   beforeEach(() => {
     I18n.locale = 'en';
+  });
+
+  describe('initialization', () => {
+    test('defaults if browser language is not detected', () => {
+      global.navigator.initI18n();
+
+      expect(I18n.locale).toBe('en');
+    });
   });
 
   test('t function', () => {
