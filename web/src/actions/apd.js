c3b25455972c13920c845aaf25cd5fb845a8745e
Brendan Sudol <brendan.sudol@gsa.gov>
2018-05-02 10:52:53 -0400

    add key personnel

--- web/src/actions/apd.js
+++ web/src/actions/apd.js
@@ -1,20 +1,19 @@
 import axios from '../util/api';
 
+export const ADD_APD_KEY_PERSON = 'ADD_APD_KEY_PERSON';
 export const GET_APD_REQUEST = 'GET_APD_REQUEST';
 export const GET_APD_SUCCESS = 'GET_APD_SUCCESS';
 export const GET_APD_FAILURE = 'GET_APD_FAILURE';
+export const REMOVE_APD_KEY_PERSON = 'REMOVE_APD_KEY_PERSON';
 export const UPDATE_APD = 'UPDATE_APD';
 
 export const requestApd = () => ({ type: GET_APD_REQUEST });
-
 export const receiveApd = data => ({ type: GET_APD_SUCCESS, data });
-
 export const failApd = error => ({ type: GET_APD_FAILURE, error });
 
-export const updateApd = updates => ({
-  type: UPDATE_APD,
-  updates
-});
+export const addApdKeyPerson = () => ({ type: ADD_APD_KEY_PERSON });
+export const removeApdKeyPerson = id => ({ type: REMOVE_APD_KEY_PERSON, id });
+export const updateApd = updates => ({ type: UPDATE_APD, updates });
 
 export const fetchApd = () => dispatch => {
   dispatch(requestApd());
