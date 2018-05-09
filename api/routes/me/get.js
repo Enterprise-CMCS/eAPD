21f22b6dfc172273903647fc1fb74f34b9eaccd6
Greg Walker <michael.walker@gsa.gov>
2018-05-09 15:58:16 -0500

    add tests for /me endpoint

--- api/routes/me/get.js
+++ api/routes/me/get.js
@@ -4,6 +4,8 @@ const loggedIn = require('../../middleware').loggedIn;
 module.exports = app => {
   logger.silly('setting up GET endpoint');
   app.get('/me', loggedIn, (req, res) => {
-    res.send(req.user);
+    // Get rid of the model object before sending
+    // it back to the client.
+    res.send({ ...req.user, model: undefined });
   });
 };
