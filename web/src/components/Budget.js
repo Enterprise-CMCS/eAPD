913e6b1b74427aa8456f3f38a610e2a8642adc66
Brendan Sudol <brendan.sudol@gsa.gov>
2018-05-15 10:36:08 -0400

    data-driven budget table

--- web/src/components/Budget.js
+++ web/src/components/Budget.js
@@ -3,8 +3,132 @@ import React, { Component, Fragment } from 'react';
 
 import Container from './Container';
 import Collapsible from './Collapsible';
+import { formatMoney } from '../util/formats';
 
-class NumberRow extends Component {
+const fakeData = {
+  hit: {
+    stateStaff: {
+      '2018': 1000,
+      '2019': 1200
+    },
+    nonPersonnel: {
+      '2018': 1000,
+      '2019': 1200
+    },
+    contractors: {
+      '2018': 1000,
+      '2019': 1200
+    }
+  },
+  hie: {
+    stateStaff: {
+      '2018': 5000,
+      '2019': 6000
+    },
+    nonPersonnel: {
+      '2018': 4000,
+      '2019': 4000
+    },
+    contractors: {
+      '2018': 10000,
+      '2019': 12000
+    }
+  },
+  mmis: {
+    stateStaff: {
+      '2018': 2000,
+      '2019': 3000
+    },
+    nonPersonnel: {
+      '2018': 500,
+      '2019': 500
+    },
+    contractors: {
+      '2018': 0,
+      '2019': 0
+    }
+  }
+};
+
+const categoryLookup = {
+  stateStaff: 'Project state staff',
+  nonPersonnel: 'Non-personnel',
+  contractors: 'Contracted resources',
+  totals: 'Subtotal'
+};
+
+const add = (a, b) => a + b;
+
+const toObj = (arr, initVal = 0) =>
+  Object.assign({}, ...arr.map(a => ({ [a]: initVal })));
+
+// TODO: this is gross, but it will all be deleted soon
+// (just a PoC for data-driven component)
+const enrichData = data => {
+  let dataNew = JSON.parse(JSON.stringify(data)); // (hacky) deep copy of data
+
+  const cats = ['hit', 'hie', 'mmis'];
+  const subCats = ['stateStaff', 'nonPersonnel', 'contractors'];
+  const yearFields = ['2018', '2019', 'total'];
+  const allTotals = toObj(yearFields);
+
+  /* eslint-disable no-restricted-syntax */
+  for (const cat of cats) {
+    const catTotals = toObj(yearFields);
+    for (const subCat of subCats) {
+      // cross-year total
+      const byYear = dataNew[cat][subCat];
+      const total = Object.values(byYear).reduce(add, 0);
+      dataNew[cat][subCat] = { ...byYear, total };
+
+      // cross-category totals
+      const byYearEntries = Object.entries(dataNew[cat][subCat]);
+      for (const [year, value] of byYearEntries) {
+        allTotals[year] += value;
+        catTotals[year] += value;
+      }
+    }
+
+    dataNew[cat] = { ...dataNew[cat], totals: catTotals };
+  }
+
+  dataNew = { ...dataNew, totals: allTotals };
+
+  return dataNew;
+};
+
+const enrichedFakedData = enrichData(fakeData);
+
+const DataRowDetails = ({ colSpan, isVisible }) => (
+  <tr className={`bg-white ${!isVisible ? 'display-none' : ''}`}>
+    <td colSpan={colSpan}>
+      <div className="p1">
+        <p className="bold">
+          Details will go here! Could be words or another table:
+        </p>
+        <table className="table-bordered col-6">
+          <tbody>
+            <tr>
+              <td>abc</td>
+              <td>123</td>
+            </tr>
+            <tr>
+              <td>def</td>
+              <td>456</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </td>
+  </tr>
+);
+
+DataRowDetails.propTypes = {
+  colSpan: PropTypes.number.isRequired,
+  isVisible: PropTypes.bool.isRequired
+};
+
+class DataRow extends Component {
   state = { detailsOpen: false };
 
   toggleDetails = () => {
@@ -12,8 +136,10 @@ class NumberRow extends Component {
   };
 
   render() {
-    const { n, title } = this.props;
+    const { data, title } = this.props;
     const { detailsOpen } = this.state;
+
+    const years = Object.keys(data);
 
     return (
       <Fragment>
@@ -28,125 +154,125 @@ class NumberRow extends Component {
             </button>
             {title}
           </td>
-          {[...Array(n)].map((_, i) => (
-            <td key={i} className="mono right-align">
-              123
-            </td>
-          ))}
+          {years.map(yr => {
+            const val = data[yr];
+            return (
+              <Fragment key={yr}>
+                <td className="mono right-align">{formatMoney(val)}</td>
+                <td className="mono right-align">{formatMoney(val * 0.9)}</td>
+                <td className="mono right-align">{formatMoney(val * 0.1)}</td>
+              </Fragment>
+            );
+          })}
         </tr>
-        {detailsOpen && (
-          <tr className="bg-white">
-            <td colSpan={n + 1}>
-              <div className="p1">
-                <p className="bold">
-                  Details will go here! Could be words or another table:
-                </p>
-                <table className="table-bordered col-6">
-                  <tbody>
-                    <tr>
-                      <td>abc</td>
-                      <td>123</td>
-                    </tr>
-                    <tr>
-                      <td>def</td>
-                      <td>456</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </td>
-          </tr>
-        )}
+        <DataRowDetails
+          colSpan={years.length * 3 + 1}
+          isVisible={detailsOpen}
+        />
       </Fragment>
     );
   }
 }
 
-NumberRow.propTypes = {
-  n: PropTypes.number,
+DataRow.propTypes = {
+  data: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired
 };
 
-NumberRow.defaultProps = {
-  n: 9
-};
-
-const Filler = () => (
+const DataRowGroup = ({ data }) => (
   <Fragment>
-    <NumberRow title="Project state staff" />
-    <NumberRow title="Non-personnel" />
-    <NumberRow title="Contracted resources" />
-    <NumberRow title="Subtotal" />
+    {Object.keys(data).map(key => (
+      <DataRow key={key} data={data[key]} title={categoryLookup[key]} />
+    ))}
   </Fragment>
 );
 
-const BudgetTable = () => (
-  <Container>
-    <Collapsible title="Budget Table" open>
-      <div className="py1 overflow-auto">
-        <table
-          className="h6 table-fixed table-bordered table-budget"
-          style={{ minWidth: 1000 }}
-        >
-          <thead>
-            <tr>
-              <th style={{ width: 200 }} />
-              <th className="bg-black white center" colSpan="3">
-                FFY 2018
-              </th>
-              <th className="bg-black white center" colSpan="3">
-                FFY 2019
-              </th>
-              <th className="bg-black white center" colSpan="3">
-                Total
-              </th>
-            </tr>
-            <tr>
-              <th />
-              {[...Array(3)].map((_, i) => (
-                <Fragment key={i}>
-                  <th className="col-4">Total</th>
-                  <th className="col-4">Federal share</th>
-                  <th className="col-4">State share</th>
-                </Fragment>
-              ))}
-            </tr>
-          </thead>
-          <tbody className="bg-light-blue">
-            <tr>
-              <td className="bold">HIT activities</td>
-              {[...Array(9)].map((_, i) => <td key={i} />)}
-            </tr>
-            <Filler />
-          </tbody>
-          <tbody className="bg-light-yellow">
-            <tr>
-              <td className="bold">HIT activities</td>
-              {[...Array(9)].map((_, i) => <td key={i} />)}
-            </tr>
-            <Filler />
-          </tbody>
-          <tbody className="bg-light-green">
-            <tr>
-              <td className="bold">HIE activities</td>
-              {[...Array(9)].map((_, i) => <td key={i} />)}
-            </tr>
-            <Filler />
-          </tbody>
-          <tbody>
-            <tr className="bold">
-              <td>Project total</td>
-              {[...Array(9)].map((_, i) => (
-                <td key={i} className="mono right-align">
-                  123
-                </td>
-              ))}
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </Collapsible>
-  </Container>
+DataRowGroup.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+const HeaderRow = ({ title }) => (
+  <tr>
+    <td className="bold">{title}</td>
+    {[...Array(9)].map((_, i) => <td key={i} />)}
+  </tr>
 );
+
+HeaderRow.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+const BudgetTable = () => {
+  const { hit, hie, mmis, totals } = enrichedFakedData;
+
+  return (
+    <Container>
+      <Collapsible title="Budget Table" open>
+        <div className="py1 overflow-auto">
+          <table
+            className="h6 table-fixed table-bordered table-budget"
+            style={{ minWidth: 1000 }}
+          >
+            <thead>
+              <tr>
+                <th style={{ width: 200 }} />
+                <th className="bg-black white center" colSpan="3">
+                  FFY 2018
+                </th>
+                <th className="bg-black white center" colSpan="3">
+                  FFY 2019
+                </th>
+                <th className="bg-black white center" colSpan="3">
+                  Total
+                </th>
+              </tr>
+              <tr>
+                <th />
+                {[...Array(3)].map((_, i) => (
+                  <Fragment key={i}>
+                    <th className="col-4">Total</th>
+                    <th className="col-4">Federal share</th>
+                    <th className="col-4">State share</th>
+                  </Fragment>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="bg-light-blue">
+              <HeaderRow title="HIT activities" />
+              <DataRowGroup data={hit} />
+            </tbody>
+            <tbody className="bg-light-yellow">
+              <HeaderRow title="HIE activities" />
+              <DataRowGroup data={hie} />
+            </tbody>
+            <tbody className="bg-light-green">
+              <HeaderRow title="MMIS activities" />
+              <DataRowGroup data={mmis} />
+            </tbody>
+            <tbody>
+              <tr className="bold">
+                <td>Project total</td>
+                {Object.keys(totals).map(yr => {
+                  const val = totals[yr];
+                  return (
+                    <Fragment key={yr}>
+                      <td className="mono right-align">{formatMoney(val)}</td>
+                      <td className="mono right-align">
+                        {formatMoney(val * 0.9)}
+                      </td>
+                      <td className="mono right-align">
+                        {formatMoney(val * 0.1)}
+                      </td>
+                    </Fragment>
+                  );
+                })}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </Collapsible>
+    </Container>
+  );
+};
 
 export default BudgetTable;
---
+++ web/src/util/formats.js
@@ -0,0 +1,10 @@
+import { format } from 'd3-format';
+
+const isNumeric = n => !Number.isNaN(parseFloat(n)) && Number.isFinite(n);
+
+export const fmt = (val, spec = ',.2f') =>
+  isNumeric(val) ? format(spec)(val) : '--';
+
+export const formatDec = (x, n = 2) => fmt(x, `,.${n}f`);
+export const formatMoney = (x, n = 0) => fmt(x, `($,.${n}f`);
+export const formatPerc = (x, n = 0) => fmt(x, `+.${n}%`);
