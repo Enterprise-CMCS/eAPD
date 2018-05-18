9d5bcb31af864b272860b5e2c4a1fe8321ecdcc6
Greg Walker <michael.walker@gsa.gov>
2018-05-18 11:04:58 -0500

    closes #550

--- api/migrations/20180518153227_update-data-models.js
+++ api/migrations/20180518153227_update-data-models.js
@@ -5,6 +5,24 @@ exports.up = async knex => {
     table.text('cost_allocation_methodology');
     table.json('types');
   });
+
+  await knex.schema.alterTable('activity_goals', table => {
+    table.text('objective');
+  });
+
+  // Move goal objective from the separate table into the new column
+  const goalObjectives = await knex('activity_goal_objectives').select();
+  await Promise.all(
+    goalObjectives.map(
+      async ({ activity_goal_id: id, description: objective }) => {
+        await knex('activity_goals')
+          .where({ id })
+          .update({ objective });
+      }
+    )
+  );
+
+  await knex.schema.dropTable('activity_goal_objectives');
 };
 
 exports.down = async () => {};
