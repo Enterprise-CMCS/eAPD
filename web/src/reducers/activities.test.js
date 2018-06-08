efe0a15675998534a4bbecc946114ddf41947e0f
Greg Walker <michael.walker@gsa.gov>
2018-06-08 17:50:47 -0500

    activity reducer 100%

--- web/src/reducers/activities.test.js
+++ web/src/reducers/activities.test.js
@@ -1,4 +1,9 @@
-import activities from './activities';
+import activities, {
+  aggregateByYear,
+  getCategoryTotals,
+  getActivityCategoryTotals,
+  getActivityTotals
+} from './activities';
 
 describe('activities reducer', () => {
   const initialState = {
@@ -12,14 +17,14 @@ describe('activities reducer', () => {
     end: '',
     name: '',
     start: '',
-    years: { '1': 0, '2': 0 }
+    years: { '2018': 0, '2019': 0 }
   };
 
   const newPerson = {
     id: 0,
     desc: '',
     title: '',
-    years: { '1': { amt: '', perc: '' }, '2': { amt: '', perc: '' } }
+    years: { '2018': { amt: '', perc: '' }, '2019': { amt: '', perc: '' } }
   };
 
   const newGoal = { desc: '', obj: '' };
@@ -28,7 +33,7 @@ describe('activities reducer', () => {
     id: 0,
     category: 'Hardware, software, and licensing',
     desc: '',
-    years: { '1': 0, '2': 0 }
+    years: { '2018': 0, '2019': 0 }
   };
 
   const newMilestone = { end: '', name: '', start: '' };
@@ -52,8 +57,8 @@ describe('activities reducer', () => {
     ],
     costAllocateDesc: '',
     costFFP: {
-      '1': { fed: 90, other: 0, state: 10 },
-      '2': { fed: 90, other: 0, state: 10 }
+      '2018': { fed: 90, other: 0, state: 10 },
+      '2019': { fed: 90, other: 0, state: 10 }
     },
     descLong: '',
     descShort: '',
@@ -105,7 +110,7 @@ describe('activities reducer', () => {
         id: 3
       }
     ],
-    years: ['1', '2']
+    years: ['2018', '2019']
   };
 
   const stateWithOne = { byId: { '1': newActivity }, allIds: [1] };
@@ -118,7 +123,7 @@ describe('activities reducer', () => {
     expect(
       activities(initialState, {
         type: 'ADD_ACTIVITY',
-        years: ['1', '2']
+        years: ['2018', '2019']
       })
     ).toEqual({
       byId: {
@@ -154,7 +159,7 @@ describe('activities reducer', () => {
         activities(stateWithOne, {
           type: action,
           id: 1,
-          years: ['1', '2']
+          years: ['2018', '2019']
         })
       ).toEqual({
         ...stateWithOne,
@@ -311,7 +316,7 @@ describe('activities reducer', () => {
       expect(
         activities(stateWithOne, {
           type,
-          updates: { years: ['1', '2', '3'] }
+          updates: { years: ['2018', '2019', '2020'] }
         })
       ).toEqual({
         ...stateWithOne,
@@ -323,48 +328,48 @@ describe('activities reducer', () => {
                 ...stateWithOne.byId['1'].contractorResources[0],
                 years: {
                   ...stateWithOne.byId['1'].contractorResources[0].years,
-                  '3': 0
+                  '2020': 0
                 }
               },
               {
                 ...stateWithOne.byId['1'].contractorResources[1],
                 years: {
                   ...stateWithOne.byId['1'].contractorResources[1].years,
-                  '3': 0
+                  '2020': 0
                 }
               },
               {
                 ...stateWithOne.byId['1'].contractorResources[2],
                 years: {
                   ...stateWithOne.byId['1'].contractorResources[2].years,
-                  '3': 0
+                  '2020': 0
                 }
               }
             ],
             costFFP: {
               ...stateWithOne.byId['1'].costFFP,
-              '3': { fed: 90, other: 0, state: 10 }
+              '2020': { fed: 90, other: 0, state: 10 }
             },
             expenses: [
               {
                 ...stateWithOne.byId['1'].expenses[0],
                 years: {
                   ...stateWithOne.byId['1'].expenses[0].years,
-                  '3': 0
+                  '2020': 0
                 }
               },
               {
                 ...stateWithOne.byId['1'].expenses[1],
                 years: {
                   ...stateWithOne.byId['1'].expenses[1].years,
-                  '3': 0
+                  '2020': 0
                 }
               },
               {
                 ...stateWithOne.byId['1'].expenses[2],
                 years: {
                   ...stateWithOne.byId['1'].expenses[2].years,
-                  '3': 0
+                  '2020': 0
                 }
               }
             ],
@@ -373,25 +378,25 @@ describe('activities reducer', () => {
                 ...stateWithOne.byId['1'].statePersonnel[0],
                 years: {
                   ...stateWithOne.byId['1'].statePersonnel[0].years,
-                  '3': { amt: '', perc: '' }
+                  '2020': { amt: '', perc: '' }
                 }
               },
               {
                 ...stateWithOne.byId['1'].statePersonnel[1],
                 years: {
                   ...stateWithOne.byId['1'].statePersonnel[1].years,
-                  '3': { amt: '', perc: '' }
+                  '2020': { amt: '', perc: '' }
                 }
               },
               {
                 ...stateWithOne.byId['1'].statePersonnel[2],
                 years: {
                   ...stateWithOne.byId['1'].statePersonnel[2].years,
-                  '3': { amt: '', perc: '' }
+                  '2020': { amt: '', perc: '' }
                 }
               }
             ],
-            years: ['1', '2', '3']
+            years: ['2018', '2019', '2020']
           }
         }
       });
@@ -399,7 +404,7 @@ describe('activities reducer', () => {
 
     it('removes year-based pieces if a year was removed', () => {
       expect(
-        activities(stateWithOne, { type, updates: { years: ['1'] } })
+        activities(stateWithOne, { type, updates: { years: ['2018'] } })
       ).toEqual({
         ...stateWithOne,
         byId: {
@@ -409,42 +414,42 @@ describe('activities reducer', () => {
               {
                 ...stateWithOne.byId['1'].contractorResources[0],
                 years: {
-                  '1': 0
+                  '2018': 0
                 }
               },
               {
                 ...stateWithOne.byId['1'].contractorResources[1],
                 years: {
-                  '1': 0
+                  '2018': 0
                 }
               },
               {
                 ...stateWithOne.byId['1'].contractorResources[2],
                 years: {
-                  '1': 0
+                  '2018': 0
                 }
               }
             ],
             costFFP: {
-              '1': { fed: 90, other: 0, state: 10 }
+              '2018': { fed: 90, other: 0, state: 10 }
             },
             expenses: [
               {
                 ...stateWithOne.byId['1'].expenses[0],
                 years: {
-                  '1': 0
+                  '2018': 0
                 }
               },
               {
                 ...stateWithOne.byId['1'].expenses[1],
                 years: {
-                  '1': 0
+                  '2018': 0
                 }
               },
               {
                 ...stateWithOne.byId['1'].expenses[2],
                 years: {
-                  '1': 0
+                  '2018': 0
                 }
               }
             ],
@@ -452,26 +457,350 @@ describe('activities reducer', () => {
               {
                 ...stateWithOne.byId['1'].statePersonnel[0],
                 years: {
-                  '1': { amt: '', perc: '' }
+                  '2018': { amt: '', perc: '' }
                 }
               },
               {
                 ...stateWithOne.byId['1'].statePersonnel[1],
                 years: {
-                  '1': { amt: '', perc: '' }
+                  '2018': { amt: '', perc: '' }
                 }
               },
               {
                 ...stateWithOne.byId['1'].statePersonnel[2],
                 years: {
-                  '1': { amt: '', perc: '' }
+                  '2018': { amt: '', perc: '' }
                 }
               }
             ],
-            years: ['1']
+            years: ['2018']
           }
         }
       });
     });
+  });
+
+  describe('handles receiving an APD', () => {
+    it(`adds a default activity if the APD doesn't have any`, () => {
+      expect(
+        activities(
+          {
+            anything: 'we',
+            want: 'because',
+            it: 'gets',
+            replaced: true
+          },
+          {
+            type: 'GET_APD_SUCCESS',
+            data: {
+              activities: []
+            }
+          }
+        )
+      ).toEqual({
+        byId: {
+          '1': {
+            ...newActivity,
+            id: 1,
+            name: 'Program Administration',
+            fundingSource: 'HIT',
+            years: ['2018', '2019']
+          }
+        },
+        allIds: ['1']
+      });
+    });
+
+    it('ingests activities from the incoming APD object', () => {
+      expect(
+        activities(
+          {
+            anything: 'we',
+            want: 'because',
+            it: 'gets',
+            replaced: true
+          },
+          {
+            type: 'GET_APD_SUCCESS',
+            data: {
+              activities: [
+                {
+                  id: 998,
+                  name: 'activity 998',
+                  // fundingSource TODO:
+                  summary: 'summary',
+                  description: 'description',
+                  alternatives: 'different things',
+                  costAllocationNarrative: {
+                    methodology: 'how',
+                    otherSources: 'which'
+                  },
+                  costAllocation: [
+                    { year: 2018, federal: 0.8, state: 0.15, other: 0.05 },
+                    { year: 2019, federal: 0.6, state: 0.2, other: 0.2 }
+                  ],
+                  goals: [
+                    {
+                      description: 'goal 1 description',
+                      objective: 'goal 1 objective'
+                    },
+                    {
+                      description: 'goal 2 description',
+                      objective: 'goal 2 objective'
+                    }
+                  ],
+                  schedule: [
+                    {
+                      id: 'm1',
+                      milestone: 'milestone name',
+                      plannedStart: 'start',
+                      plannedEnd: 'end'
+                    }
+                  ],
+                  statePersonnel: [
+                    {
+                      id: 'person 1',
+                      title: 'job title 1',
+                      description: 'desc 1',
+                      years: [
+                        { year: '2018', cost: 100, fte: 0.5 },
+                        { year: '2019', cost: 200, fte: 0.8 }
+                      ]
+                    },
+                    {
+                      id: 'person 2',
+                      title: 'job title 2',
+                      description: 'desc 2',
+                      years: [
+                        { year: '2018', cost: 300, fte: 0.3 },
+                        { year: '2019', cost: 400, fte: 0.6 }
+                      ]
+                    }
+                  ],
+                  contractorResources: [
+                    {
+                      id: 'contractor 1',
+                      name: 'contractor 1',
+                      description: 'desc 1',
+                      start: 'start 1',
+                      end: 'end 1',
+                      years: [
+                        { year: '2018', cost: '1000' },
+                        { year: '2019', cost: '2000' }
+                      ]
+                    },
+                    {
+                      id: 'contractor 2',
+                      name: 'contractor 2',
+                      description: 'desc 2',
+                      start: 'start 2',
+                      end: 'end 2',
+                      years: [
+                        { year: '2018', cost: '3000' },
+                        { year: '2019', cost: '4000' }
+                      ]
+                    }
+                  ],
+                  expenses: [
+                    {
+                      id: 'e1',
+                      category: 'category 1',
+                      description: 'desc 1',
+                      entries: [
+                        { year: '2018', amount: 10 },
+                        { year: '2019', amount: 20 }
+                      ]
+                    },
+                    {
+                      id: 'e2',
+                      category: 'category 2',
+                      description: 'desc 2',
+                      entries: [
+                        { year: '2018', amount: 30 },
+                        { year: '2019', amount: 40 }
+                      ]
+                    }
+                  ],
+                  standardsAndConditions: {
+                    businessResults: 'biz results',
+                    documentation: 'docs',
+                    industryStandards: 'standards',
+                    interoperability: 'interop',
+                    keyPersonnel: 'locksmiths',
+                    leverage: 'lever',
+                    modularity: 'lego blocks',
+                    minimizeCost: 'save money',
+                    mita: 'meeta',
+                    mitigationStrategy: 'run away',
+                    reporting: 'moop moop'
+                  }
+                }
+              ],
+              years: ['2018', '2019']
+            }
+          }
+        )
+      ).toEqual({
+        byId: {
+          '998': {
+            id: 998,
+            name: 'activity 998',
+            fundingSource: 'HIT',
+            years: ['2018', '2019'],
+            descShort: 'summary',
+            descLong: 'description',
+            altApproach: 'different things',
+            costAllocateDesc: 'how',
+            otherFundingDesc: 'which',
+            otherFundingAmt: 0,
+            costFFP: {
+              2018: { fed: 80, state: 15, other: 5 },
+              2019: { fed: 60, state: 20, other: 20 }
+            },
+            goals: [
+              { desc: 'goal 1 description', obj: 'goal 1 objective' },
+              { desc: 'goal 2 description', obj: 'goal 2 objective' }
+            ],
+            milestones: [
+              { id: 'm1', name: 'milestone name', start: 'start', end: 'end' }
+            ],
+            statePersonnel: [
+              {
+                id: 'person 1',
+                title: 'job title 1',
+                desc: 'desc 1',
+                years: {
+                  2018: { amt: 100, perc: 0.5 }, // TODO: multiply by 100
+                  2019: { amt: 200, perc: 0.8 } // TODO: multiply by 100
+                }
+              },
+              {
+                id: 'person 2',
+                title: 'job title 2',
+                desc: 'desc 2',
+                years: {
+                  2018: { amt: 300, perc: 0.3 }, // TODO: multiply by 100
+                  2019: { amt: 400, perc: 0.6 } // TODO: multiply by 100
+                }
+              }
+            ],
+            contractorResources: [
+              {
+                id: 'contractor 1',
+                name: 'contractor 1',
+                desc: 'desc 1',
+                start: 'start 1',
+                end: 'end 1',
+                years: { 2018: 1000, 2019: 2000 }
+              },
+              {
+                id: 'contractor 2',
+                name: 'contractor 2',
+                desc: 'desc 2',
+                start: 'start 2',
+                end: 'end 2',
+                years: { 2018: 3000, 2019: 4000 }
+              }
+            ],
+            expenses: [
+              {
+                id: 'e1',
+                category: 'category 1',
+                desc: 'desc 1',
+                years: { 2018: 10, 2019: 20 }
+              },
+              {
+                id: 'e2',
+                category: 'category 2',
+                desc: 'desc 2',
+                years: { 2018: 30, 2019: 40 }
+              }
+            ],
+            standardsAndConditions: {
+              bizResults: 'biz results',
+              documentation: 'docs',
+              industry: 'standards',
+              interoperability: 'interop',
+              keyPersonnel: 'locksmiths',
+              leverage: 'lever',
+              minimizeCost: 'save money',
+              mita: 'meeta',
+              mitigation: 'run away',
+              modularity: 'lego blocks',
+              reporting: 'moop moop'
+            },
+            meta: { expanded: false }
+          }
+        },
+        allIds: ['998']
+      });
+    });
+  });
+
+  it('aggregates data by year', () => {
+    expect(
+      aggregateByYear(
+        [{ '2018': 10, '2019': 7 }, { '2018': 46, '2019': 8 }],
+        ['2018', '2019']
+      )
+    ).toEqual({ '2018': 56, '2019': 15 });
+  });
+
+  it('gets yearly totals for a category', () => {
+    expect(
+      getCategoryTotals([
+        { years: { '2018': 10, '2019': 7 } },
+        { years: { '2018': 46, '2019': 8 } }
+      ])
+    ).toEqual({ '2018': 56, '2019': 15 });
+  });
+
+  it('get cost totals per category per year for an activity', () => {
+    expect(
+      getActivityCategoryTotals({
+        contractorResources: [
+          { years: { '2018': 1, '2019': 10 } },
+          { years: { '2018': 2, '2019': 20 } },
+          { years: { '2018': 3, '2019': 30 } }
+        ],
+        expenses: [
+          { years: { '2018': 2, '2019': 20 } },
+          { years: { '2018': 4, '2019': 40 } },
+          { years: { '2018': 6, '2019': 60 } }
+        ],
+        statePersonnel: [
+          { years: { '2018': { amt: 3 }, '2019': { amt: 30 } } },
+          { years: { '2018': { amt: 6 }, '2019': { amt: 60 } } },
+          { years: { '2018': { amt: 9 }, '2019': { amt: 90 } } }
+        ]
+      })
+    ).toEqual({
+      contractors: { '2018': 6, '2019': 60 },
+      expenses: { '2018': 12, '2019': 120 },
+      statePersonnel: { '2018': 18, '2019': 180 }
+    });
+  });
+
+  it('get cost totals per year for an activity', () => {
+    expect(
+      getActivityTotals({
+        contractorResources: [
+          { years: { '2018': 1, '2019': 10 } },
+          { years: { '2018': 2, '2019': 20 } },
+          { years: { '2018': 3, '2019': 30 } }
+        ],
+        expenses: [
+          { years: { '2018': 2, '2019': 20 } },
+          { years: { '2018': 4, '2019': 40 } },
+          { years: { '2018': 6, '2019': 60 } }
+        ],
+        statePersonnel: [
+          { years: { '2018': { amt: 3 }, '2019': { amt: 30 } } },
+          { years: { '2018': { amt: 6 }, '2019': { amt: 60 } } },
+          { years: { '2018': { amt: 9 }, '2019': { amt: 90 } } }
+        ],
+        years: ['2018', '2019']
+      })
+    ).toEqual({ '2018': 36, '2019': 360 });
   });
 });
