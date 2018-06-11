0a13d7e1562b06c376c5b158efc4b2ff18b926db
Greg Walker <mgwalker@gmail.com>
2018-06-10 19:05:27 -0500

    i18n tests for debugging

--- web/src/i18n/index.test.js
+++ web/src/i18n/index.test.js
@@ -1,26 +1,59 @@
-import I18n, { t } from './index';
-
 import fr from './locales/fr.json';
 
 describe('i18n translations', () => {
-  beforeEach(() => {
-    I18n.locale = 'en';
-  });
+  let container;
+
+  const load = () => {
+    jest.resetModules();
+    container = require('./index'); // eslint-disable-line global-require
+    container.I18n = container.default;
+    container.I18n.locale = 'en';
+  };
+
+  beforeEach(() => load());
 
   test('t function', () => {
-    expect(t('title', { place: 'Tortuga', year: 2018 })).toBe(
+    expect(container.t('title', { place: 'Tortuga', year: 2018 })).toBe(
       '2018 Tortuga HITECH APD'
     );
-    expect(t('nonsense')).toBe('[missing "en.nonsense" translation]');
+
+    expect(container.t('nonsense')).toBe('[missing "en.nonsense" translation]');
+  });
+
+  test('t function, debug mode', () => {
+    const env = process.env.NODE_ENV;
+    const { hash } = window.location;
+
+    process.env.NODE_ENV = 'dev';
+    window.location.hash = 'i18n';
+    load();
+
+    expect(
+      container.t(['i18n', 'resource', 'path'], {
+        place: 'Tortuga',
+        year: 2018
+      })
+    ).toBe('[i18n.resource.path]');
+
+    process.env.NODE_ENV = env;
+    window.location.hash = hash;
   });
 
   test('changing locale to one that is supported', () => {
-    I18n.locale = 'fr';
-    expect(t('test')).toBe(fr.test);
+    container.I18n.locale = 'fr';
+    expect(container.t('test')).toBe(fr.test);
   });
 
   test('changing locale to one that is not supported', () => {
-    I18n.locale = 'pig-latin';
-    expect(t('test')).toBe('[missing "pig-latin.test" translation]');
+    container.I18n.locale = 'pig-latin';
+    expect(container.t('test')).toBe('[missing "pig-latin.test" translation]');
+  });
+
+  test('sets the default locale', () => {
+    const locales = [...container.SUPPORTED_LOCALES];
+    container.SUPPORTED_LOCALES.splice(0, container.SUPPORTED_LOCALES.length);
+    container.initI18n();
+    expect(container.I18n.locale).toBe('en');
+    container.SUPPORTED_LOCALES.push(...locales);
   });
 });
