73d4066b9c6ce1586d070d6a1a3a4bf4ce0817b2
Greg Walker <mgwalker@gmail.com>
2018-06-10 20:12:31 -0500

    util tests

---
+++ web/src/util/index.test.js
@@ -0,0 +1,77 @@
+import sinon from 'sinon';
+
+const load = () => {
+  jest.resetModules();
+  return require('./index'); // eslint-disable-line global-require
+};
+
+describe('utility arrays', () => {
+  test('constant shared arrays are as expected', () => {
+    const { ACTIVITY_FUNDING_SOURCES, STATES, STANDARDS } = load();
+
+    expect(ACTIVITY_FUNDING_SOURCES).toEqual(['HIT', 'HIE', 'MMIS']);
+    expect(STATES.length).toBe(56);
+    expect(STANDARDS.length).toBe(11);
+  });
+});
+
+describe('provides default years based on now', () => {
+  test('before October', () => {
+    const clock = sinon.useFakeTimers();
+    // tick forward 10 days, so we're not on the weird date boundary
+    clock.tick(864000000);
+    const { defaultAPDYearOptions, defaultAPDYears } = load();
+
+    expect(defaultAPDYearOptions).toEqual(['1970', '1971', '1972']);
+    expect(defaultAPDYears).toEqual(['1970', '1971']);
+
+    clock.restore();
+  });
+
+  test('after October', () => {
+    const clock = sinon.useFakeTimers();
+    // tick forward 360 days
+    clock.tick(31104000000);
+    const { defaultAPDYearOptions, defaultAPDYears } = load();
+
+    expect(defaultAPDYearOptions).toEqual(['1971', '1972', '1973']);
+    expect(defaultAPDYears).toEqual(['1971', '1972']);
+
+    clock.restore();
+  });
+});
+
+describe('utility functions', () => {
+  const { arrToObj, getParams, nextSequence, stateLookup } = load();
+
+  test('finds a state by two-letter code', () => {
+    expect(stateLookup('Mo')).toEqual({ id: 'mo', name: 'Missouri' });
+    expect(stateLookup('MO')).toEqual({ id: 'mo', name: 'Missouri' });
+    expect(stateLookup('mo')).toEqual({ id: 'mo', name: 'Missouri' });
+    expect(stateLookup('bob')).toEqual(undefined);
+  });
+
+  test('parses parameters from a URL query string', () => {
+    expect(getParams('?hello=world&seven=7&bob&long=this%20is%20text')).toEqual(
+      {
+        hello: 'world',
+        seven: '7',
+        bob: null,
+        long: 'this is text'
+      }
+    );
+  });
+
+  test('gets the next number in a sequence', () => {
+    expect(nextSequence([1, 2, 6, 38, 3, 19])).toEqual(39);
+  });
+
+  test('converts an array into an object with array values as object keys', () => {
+    expect(arrToObj(['a', 'b', 'c'])).toEqual({ a: 0, b: 0, c: 0 });
+    expect(arrToObj(['a', 'b', 'c'], 'boop')).toEqual({
+      a: 'boop',
+      b: 'boop',
+      c: 'boop'
+    });
+  });
+});
