3d99129f2d91ed699961cabc0cd880b57dde08fc
Greg Walker <mgwalker@gmail.com>
2018-06-13 19:02:50 -0500

    actions and UI for creating APD

--- web/src/containers/Landing.js
+++ web/src/containers/Landing.js
@@ -2,12 +2,17 @@ import PropTypes from 'prop-types';
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 
-import { selectApd } from '../actions/apd';
+import { createApd, selectApd } from '../actions/apd';
 
 class Landing extends Component {
   handleChange = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
+  };
+
+  createApd = e => {
+    e.preventDefault();
+    this.props.createApd();
   };
 
   pickApd = id => e => {
@@ -20,15 +25,25 @@ class Landing extends Component {
     return (
       <Fragment>
         <h1>Landing!</h1>
-        {apds.map(({ status, years, id }) => (
-          <div key={id}>
-            <a href="#!" onClick={this.pickApd(id)}>
-              {status} APD{years
-                ? ` for ${years.join(', ')}`
-                : ' (years not defined yet)'}
-            </a>
-          </div>
-        ))}
+        <a href="#!" onClick={this.createApd}>
+          Create an APD
+        </a>
+        {apds.length > 0 && (
+          <span>
+            <h3>...or existing APDs:</h3>
+            <ul>
+              {apds.map(({ status, years, id }) => (
+                <li key={id}>
+                  <a href="#!" onClick={this.pickApd(id)}>
+                    {status} APD{years
+                      ? ` for ${years.join(', ')}`
+                      : ' (years not defined yet)'}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </span>
+        )}
       </Fragment>
     );
   }
@@ -36,11 +51,12 @@ class Landing extends Component {
 
 Landing.propTypes = {
   apds: PropTypes.array.isRequired,
+  createApd: PropTypes.func.isRequired,
   selectApd: PropTypes.func.isRequired
 };
 
 const mapStateToProps = ({ apd }) => ({ apds: Object.values(apd.byId) });
 
-const mapDispatchToProps = { selectApd };
+const mapDispatchToProps = { createApd, selectApd };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Landing);
