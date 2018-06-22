64b3bb3a226bd0ace02fd1d9bee63f813320b6cf
Brendan Sudol <brendan.sudol@gsa.gov>
2018-06-22 12:54:01 -0400

    cleanup

--- web/src/components/temp/MiscPage.js
+++ web/src/components/temp/MiscPage.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 
 import Container from '../Container';
-import Snack from '../Snack';
 
 const MiscPage = () => (
   <Container>
     <h1 className="h2">Misc Components (for testing purposes)</h1>
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-    <Snack message="Saved!" />
   </Container>
 );
 
