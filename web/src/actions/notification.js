0b9c3a774bf92bd5a41d42df57ea9eb12b610b85
Brendan Sudol <brendan.sudol@gsa.gov>
2018-06-22 12:41:03 -0400

    clean up actions

--- web/src/actions/notification.js
+++ web/src/actions/notification.js
@@ -6,6 +6,7 @@ export const addNotification = message => ({ type: ADD_NOTIFICATION, message });
 export const closeNotification = () => ({ type: CLOSE_NOTIFICATION });
 
 const queueHasMessages = state => state.notification.queue.length > 0;
+const isOpen = state => state.notification.open;
 
 export const processQueue = () => (dispatch, getState) => {
   if (queueHasMessages(getState())) return dispatch({ type: PROCESS_QUEUE });
@@ -14,8 +15,6 @@ export const processQueue = () => (dispatch, getState) => {
 
 export const notify = message => (dispatch, getState) => {
   dispatch(addNotification(message));
-
-  const isOpen = getState().notification.open;
-  if (isOpen) return dispatch(closeNotification());
+  if (isOpen(getState())) return dispatch(closeNotification());
   return dispatch(processQueue());
 };
