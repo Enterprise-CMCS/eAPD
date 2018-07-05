2dc2310a4279325bed3780046d4389fa9ce2cec7
brendan sudol <brendansudol@gmail.com>
2018-07-05 11:45:54 -0700

    add total to exec summary entry

--- web/src/containers/ExecutiveSummary.js
+++ web/src/containers/ExecutiveSummary.js
@@ -6,6 +6,7 @@ import ExecutiveSummaryBudget from './ExecutiveSummaryBudget';
 import { Section, Subsection } from '../components/Section';
 import { t } from '../i18n';
 import { aggregateByYear, getActivityTotals } from '../reducers/activities';
+import { addObjVals } from '../util';
 import { formatMoney } from '../util/formats';
 
 const ExecutiveSummary = ({ data, years }) => (
@@ -14,9 +15,9 @@ const ExecutiveSummary = ({ data, years }) => (
       {data.map((d, i) => (
         <div
           key={d.id}
-          className="mb2 p2 sm-flex items-center alert alert-success"
+          className="mb2 md-flex items-center alert alert-success"
         >
-          <div className="p1 sm-m0 flex-auto">
+          <div className="p2 sm-m0 flex-auto">
             {d.id !== 'all' && (
               <div className="h5">
                 {t('activities.namePrefixAndNum', { number: i + 1 })}
@@ -25,15 +26,19 @@ const ExecutiveSummary = ({ data, years }) => (
             <div className="h3 bold">{d.name}</div>
             {d.descShort && <div>{d.descShort}</div>}
           </div>
-          <div className="sm-flex sm-col-5">
+          <div className="md-flex md-col-7">
             {years.map(year => (
-              <div key={year} className="p1 flex-auto">
+              <div key={year} className="px2 py3 flex-auto">
                 <div className="h5">{t('ffy', { year })}</div>
                 <div className="h3 mono bold">
                   {formatMoney(d.totals[year])}
                 </div>
               </div>
             ))}
+            <div className="px2 py3 flex-auto">
+              <div className="h5">{t('executiveSummary.total')}</div>
+              <div className="h3 mono bold">{formatMoney(d.combined)}</div>
+            </div>
           </div>
         </div>
       ))}
@@ -52,18 +57,27 @@ ExecutiveSummary.propTypes = {
 const mapStateToProps = ({ activities, apd }) => {
   const activitiesArray = Object.values(activities.byId);
 
-  const data = activitiesArray.map(a => ({
-    id: a.id,
-    name: a.name,
-    descShort: a.descShort,
-    totals: getActivityTotals(a)
-  }));
+  const data = activitiesArray.map(a => {
+    const { id, name, descShort } = a;
+    const totals = getActivityTotals(a);
+
+    return {
+      id,
+      name,
+      descShort,
+      totals,
+      combined: addObjVals(totals)
+    };
+  });
+
+  const allTotals = aggregateByYear(data.map(d => d.totals), apd.data.years);
 
   data.push({
     id: 'all',
     name: 'Total Cost',
     descShort: null,
-    totals: aggregateByYear(data.map(d => d.totals), apd.data.years)
+    totals: allTotals,
+    combined: addObjVals(allTotals)
   });
 
   return {
