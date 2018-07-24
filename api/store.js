348f9921ba05602625cc39e2ef3069db10a50549
Greg Walker <michael.walker@gsa.gov>
2018-07-24 17:15:13 -0500

    generic/pluggable blob storage

---
+++ api/store.js
@@ -0,0 +1,176 @@
+const logger = require('./logger')('blob store');
+const fs = require('fs');
+const stream = require('stream');
+const fsBlobStore = require('fs-blob-store');
+
+const isReadableStream = obj => obj instanceof stream.Readable;
+
+// Creates a filestream blob store that reads/writes to
+// /dev/null.  Can't use the fsBlobStore for this because
+// it tries to create /dev/null and...  well, that doesn't
+// fails, as we might reasonably expect.
+const nullBlobStore = () => ({
+  createReadStream: () => fs.createReadStream('/dev/null'),
+  createWriteStream: () => fs.createWriteStream('/dev/null'),
+  exists: cb => {
+    cb(null, true);
+  },
+  remove: (key, cb) => {
+    cb(null);
+  }
+});
+
+/**
+ * Gets blob store based on configuration.
+ * @param {String} type - Blob store type
+ * @param {String} path - Path info for the blob store
+ * @returns Abstract blob store: https://github.com/maxogden/abstract-blob-store
+ */
+const getBlobStore = (type, path) => {
+  switch (type.toLowerCase()) {
+    case 'fs':
+      logger.silly('creating fs blob store');
+      return fsBlobStore(path);
+    default:
+      logger.silly('creating null blob store');
+      return nullBlobStore();
+  }
+};
+
+logger.silly('setting up...');
+// const store = getBlobStore(process.env.STORE_TYPE, process.env.STORE_PATH);
+
+class Store {
+  constructor(store) {
+    this.store = store;
+  }
+
+  /**
+   * @async
+   * Checks if a blob exists
+   * @param {String} key - Blob identifier
+   * @returns True if it exists, false if it doesn't.
+   */
+  async exists(key) {
+    return new Promise((resolve, reject) => {
+      logger.silly(`checking if blob exists [${key}]`);
+      this.store.exists(key, (err, exists) => {
+        if (err) {
+          logger.error(err);
+          reject(err);
+        } else {
+          logger.silly(`blob [${key}] does ${exists ? '' : 'not '}exist`);
+          resolve(exists);
+        }
+      });
+    });
+  }
+
+  /**
+   * @async
+   * Gets the data from a specified blob.
+   * @param {String} key - Blob identifier
+   * @returns {Buffer} A buffer of the data in the blob.
+   */
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      logger.silly(`getting data from blob [${key}]`);
+      // TODO - make this a buffer or byte array, but not a string
+      let out = '';
+      const readStream = this.store.createReadStream(key);
+      readStream.on('data', chunk => {
+        out += chunk;
+      });
+      readStream.on('end', () => {
+        resolve(out);
+      });
+      readStream.on('error', e => {
+        logger.error(e);
+        reject(e);
+      });
+    });
+  }
+
+  /**
+   * Get a readable stream for a specified blob.
+   * @param {String} key - Blob identifier
+   * @returns {ReadableStream} A readable stream
+   */
+  getReadStream(key) {
+    logger.silly(`getting a read stream for blob [${key}]`);
+    return this.store.createReadStream(key);
+  }
+
+  /**
+   * @async
+   * Remove a blob
+   * @param {String} key - Blob identifier
+   */
+  async remove(key) {
+    return new Promise(resolve => {
+      logger.silly(`removing blob [${key}]`);
+      this.store.remove(key, () => resolve());
+    });
+  }
+
+  /**
+   * @async
+   * Write data into a blob with a specified key.  The blob
+   * will be created if it does not already exist.
+   * @param {String} key - Blob identifier
+   * @param {(ReadableStream|*)} - The data to write.  If a ReadableStream is
+   *    provided, its contents will be piped into the blob.  For everything
+   *    else, it will be written directly to the blob.
+   * @param {*} opts - Returned value when the write is finished.
+   */
+  async write(key, data, opts) {
+    return new Promise((resolve, reject) => {
+      logger.silly(`writing to blob [${key}]`);
+      const writeStream = this.store.createWriteStream(key);
+      let rejected = false;
+
+      const done = () => {
+        logger.silly('finished writing blob');
+        if (!rejected) {
+          resolve(opts);
+        }
+      };
+
+      const errHandler = e => {
+        rejected = true;
+        logger.error(e);
+        reject(e);
+      };
+
+      if (isReadableStream(data)) {
+        logger.silly('source is a stream');
+        data.pipe(writeStream);
+
+        data.on('close', () => {
+          writeStream.end();
+          done();
+        });
+
+        data.on('error', errHandler);
+        writeStream.on('error', errHandler);
+      } else {
+        logger.silly('source is memory data');
+        writeStream.write(data);
+        writeStream.end(done);
+        writeStream.on('error', errHandler);
+      }
+    });
+  }
+}
+
+const def = new Store(
+  getBlobStore(process.env.STORE_TYPE || '', process.env.STORE_PATH || '')
+);
+
+Store.exists = def.exists.bind(def);
+Store.get = def.get.bind(def);
+Store.getReadStream = def.getReadStream.bind(def);
+Store.remove = def.remove.bind(def);
+Store.write = def.write.bind(def);
+
+module.exports = Store;
---
+++ api/store.test.js
@@ -0,0 +1,249 @@
+const tap = require('tap');
+const sinon = require('sinon');
+const { ReadableMock } = require('stream-mock');
+
+const Store = require('./store');
+
+tap.test('file/blob store', async tests => {
+  const sandbox = sinon.createSandbox();
+
+  const mockStore = {
+    createReadStream: sandbox.stub(),
+    createWriteStream: sandbox.stub(),
+    exists: sandbox.stub(),
+    remove: sandbox.stub()
+  };
+
+  const store = new Store(mockStore);
+
+  tests.beforeEach(async () => {
+    sandbox.resetBehavior();
+    sandbox.resetHistory();
+  });
+
+  tests.test('checks if a blob exists', async existTests => {
+    existTests.test(
+      'handles an error from the underlying blob store',
+      async test => {
+        const err = new Error('an error');
+        mockStore.exists.yields(err, null);
+        try {
+          await store.exists('key');
+          test.ok(false, 'rejects with the expected error');
+        } catch (e) {
+          test.equal(e, err, 'rejects with the expected error');
+          test.ok(
+            mockStore.exists.calledWith('key'),
+            'underlying store is called'
+          );
+        }
+      }
+    );
+
+    existTests.test('gives back the answer', async test => {
+      mockStore.exists.yields(null, 'something something something, dark side');
+      const out = await store.exists('key');
+
+      test.equal(
+        out,
+        'something something something, dark side',
+        'resolved the result from the underlying store'
+      );
+      test.ok(mockStore.exists.calledWith('key'), 'underlying store is called');
+    });
+  });
+
+  tests.test('gets data from a blob', async getTests => {
+    getTests.test('rejects if there is an error', async test => {
+      const readStream = {
+        on: sinon.stub()
+      };
+      mockStore.createReadStream.returns(readStream);
+
+      const err = new Error('err');
+      readStream.on.withArgs('error').yields(err);
+
+      try {
+        await store.get('key');
+      } catch (e) {
+        test.equal(e, err, 'rejects with the expected error');
+        test.ok(
+          mockStore.createReadStream.calledWith('key'),
+          'underlying store is called'
+        );
+      }
+    });
+
+    getTests.test('resolves data if there is no error', async test => {
+      const readStream = new ReadableMock(['hello', 'world', 'zip', 'zorp']);
+      mockStore.createReadStream.returns(readStream);
+      const out = await store.get('key');
+
+      test.equal(out, 'helloworldzipzorp', 'resolves with the expected data');
+      test.ok(
+        mockStore.createReadStream.calledWith('key'),
+        'underlying store is called'
+      );
+    });
+  });
+
+  tests.test('gets readable stream for a blob', async test => {
+    mockStore.createReadStream.returns('bob');
+    const out = store.getReadStream('key');
+
+    test.equal(out, 'bob', 'returns the expected value');
+    test.ok(
+      mockStore.createReadStream.calledWith('key'),
+      'underlying store is called'
+    );
+  });
+
+  tests.test('removes a blob', async test => {
+    mockStore.remove.yields();
+    await store.remove('key');
+    test.ok(mockStore.remove.calledWith('key'), 'underlying store is called');
+  });
+
+  tests.test('writes to a blob', async writeTests => {
+    writeTests.test(
+      'writes from a stream, encounters read error',
+      async test => {
+        const writeStream = { end: sinon.spy(), on: sinon.spy() };
+        mockStore.createWriteStream.returns(writeStream);
+
+        const err = new Error('err');
+
+        const readStream = new ReadableMock([]);
+        readStream.pipe = sinon.spy();
+        readStream.on = sinon.stub();
+        readStream.on.withArgs('error').yields(err);
+
+        try {
+          await store.write('key', readStream);
+          test.ok(false, 'rejects with the expected error');
+        } catch (e) {
+          test.equal(e, err, 'rejects with the expected error');
+          test.ok(
+            readStream.pipe.calledWith(writeStream),
+            'stream is piped to the blob'
+          );
+          test.ok(
+            mockStore.createWriteStream.calledWith('key'),
+            'underlying store is called'
+          );
+        }
+      }
+    );
+
+    writeTests.test(
+      'writes from a stream, encounters write error',
+      async test => {
+        const err = new Error('err');
+
+        const writeStream = {
+          end: sinon.spy(),
+          on: sinon
+            .stub()
+            .withArgs('error')
+            .yields(err)
+        };
+        mockStore.createWriteStream.returns(writeStream);
+
+        const readStream = new ReadableMock([]);
+        readStream.pipe = sinon.spy();
+        readStream.on = sinon.stub();
+
+        try {
+          await store.write('key', readStream);
+          test.ok(false, 'rejects with the expected error');
+        } catch (e) {
+          test.equal(e, err, 'rejects with the expected error');
+          test.ok(
+            readStream.pipe.calledWith(writeStream),
+            'stream is piped to the blob'
+          );
+          test.ok(
+            mockStore.createWriteStream.calledWith('key'),
+            'underlying store is called'
+          );
+        }
+      }
+    );
+
+    writeTests.test('writes from a stream, without error', async test => {
+      const writeStream = { end: sinon.spy(), on: sinon.spy() };
+      mockStore.createWriteStream.returns(writeStream);
+
+      const readStream = new ReadableMock([]);
+      readStream.pipe = sinon.spy();
+      readStream.on = sinon
+        .stub()
+        .withArgs('close')
+        .yields();
+
+      await store.write('key', readStream);
+
+      test.ok(
+        readStream.pipe.calledWith(writeStream),
+        'stream is piped to the blob'
+      );
+      test.ok(
+        mockStore.createWriteStream.calledWith('key'),
+        'underlying store is called'
+      );
+    });
+
+    writeTests.test(
+      'writes non-stream data directly, encounters an error',
+      async test => {
+        const writeStream = {
+          write: sinon.spy(),
+          end: sinon.spy(),
+          on: sinon.stub()
+        };
+        const err = new Error('boop');
+        writeStream.on.withArgs('error').yields(err);
+
+        mockStore.createWriteStream.returns(writeStream);
+
+        try {
+          await store.write('key', 'write this blob!');
+        } catch (e) {
+          test.equal(e, err, 'rejects with the expected error');
+          test.ok(
+            writeStream.write.calledWith('write this blob!'),
+            'data is written to the stream'
+          );
+          test.ok(
+            mockStore.createWriteStream.calledWith('key'),
+            'underlying store is called'
+          );
+        }
+      }
+    );
+    writeTests.test(
+      'writes non-stream data directly, without error',
+      async test => {
+        const writeStream = {
+          write: sinon.spy(),
+          end: sinon.stub(),
+          on: sinon.spy()
+        };
+        writeStream.end.yields();
+
+        mockStore.createWriteStream.returns(writeStream);
+
+        await store.write('key', 'write this blob!');
+
+        test.ok(
+          writeStream.write.calledWith('write this blob!'),
+          'data is written to the stream'
+        );
+        test.ok(
+          mockStore.createWriteStream.calledWith('key'),
+          'underlying store is called'
+        );
+      }
+    );
+  });
+});
