bc34d6165dc400ba0939707ca2001fb1a0f7f74a
Brendan Sudol <brendan.sudol@gsa.gov>
2018-07-31 11:39:54 -0400

    new help text stylez

--- web/src/components/HelpText.js
+++ web/src/components/HelpText.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Md from './Md';
 import { t } from '../i18n';
 
 const HelpText = ({ text: textResource, reminder: reminderResource }) => {
@@ -12,7 +13,11 @@ const HelpText = ({ text: textResource, reminder: reminderResource }) => {
       <div className="mb2">
         {text && <div>{text}</div>}
         {reminder && (
-          <div className="my1 p1 h6 alert alert-info">{reminder}</div>
+          <Md
+            content={reminder}
+            wrapper="div"
+            className="my1 p1 h6 alert alert-help"
+          />
         )}
       </div>
     )
