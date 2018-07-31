35b78a594ec974501f884cb676bbe112da96704f
Brendan Sudol <brendansudol@gmail.com>
2018-07-31 13:52:18 -0400

    new help text stylez (#888)
    * new help text stylez
    
    * tweaks

--- web/src/util/md.js
+++ web/src/util/md.js
@@ -1,5 +1,5 @@
 import MarkdownIt from 'markdown-it';
 
-const md = new MarkdownIt({ xhtmlOut: true, breaks: true });
+const md = new MarkdownIt({ html: true, xhtmlOut: true, breaks: true });
 
 export default md;
