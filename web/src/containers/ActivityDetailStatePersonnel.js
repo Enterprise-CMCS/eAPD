1c4a85f57ea92f478a3dab4ba8ec09619ac0300f
Greg Walker <mgwalker@users.noreply.github.com>
2018-08-02 15:49:08 -0500

    Fix table header/cell mappings (accessibility work) [skip deploy] (#907)
    * fix table td headers attrs
    
    * lint fix

--- web/src/containers/ActivityDetailStatePersonnel.js
+++ web/src/containers/ActivityDetailStatePersonnel.js
@@ -53,42 +53,56 @@ class ActivityDetailStatePersonnel extends Component {
             >
               <thead>
                 <tr>
-                  <th className="col-1" />
-                  <th className="col-4" />
-                  <th className="col-5" />
+                  <th className="col-1" id="act_state_person_null1" />
+                  <th className="col-4" id="act_state_person_null2" />
+                  <th className="col-5" id="act_state_person_null3" />
                   {years.map(year => (
-                    <th key={year} className="col-4" colSpan="2">
+                    <th
+                      key={year}
+                      className="col-4"
+                      colSpan="2"
+                      id={`act_state_person_${year}`}
+                    >
                       {t('activities.statePersonnel.labels.yearCost', {
                         year
                       })}
                     </th>
                   ))}
-                  <th className="col-1" />
+                  <th className="col-1" id="act_state_person_null4" />
                 </tr>
                 <tr>
-                  <th className="col-1">
+                  <th className="col-1" id="act_state_person_number">
                     {t('activities.statePersonnel.labels.entryNum')}
                   </th>
-                  <th className="col-4">
+                  <th className="col-4" id="act_state_person_title">
                     {t('activities.statePersonnel.labels.title')}
                   </th>
-                  <th className="col-5">
+                  <th className="col-5" id="act_state_person_desc">
                     {t('activities.statePersonnel.labels.desc')}
                   </th>
                   {years.map(year => (
                     <Fragment key={year}>
-                      <th>{t('activities.statePersonnel.labels.costAmt')}</th>
-                      <th>{t('activities.statePersonnel.labels.costPerc')}</th>
+                      <th id={`act_state_person_amt_${year}`}>
+                        {t('activities.statePersonnel.labels.costAmt')}
+                      </th>
+                      <th id={`act_state_person_pct_${year}`}>
+                        {t('activities.statePersonnel.labels.costPerc')}
+                      </th>
                     </Fragment>
                   ))}
-                  <th className="col-1" />
+                  <th className="col-1" id="act_state_person_null5" />
                 </tr>
               </thead>
               <tbody>
                 {statePersonnel.map((d, i) => (
                   <tr key={d.key}>
-                    <td className="mono">{i + 1}.</td>
-                    <td>
+                    <td
+                      className="mono"
+                      headers="act_state_person_null1 act_state_person_number"
+                    >
+                      {i + 1}.
+                    </td>
+                    <td headers="act_state_person_null2 act_state_person_title">
                       <Input
                         name={`state-person-${d.key}-title`}
                         label={t('activities.statePersonnel.labels.title')}
@@ -106,7 +120,7 @@ class ActivityDetailStatePersonnel extends Component {
                         {t('activities.statePersonnel.labels.keyPersonnel')}
                       </label>
                     </td>
-                    <td>
+                    <td headers="act_state_person_null3 act_state_person_desc">
                       <Textarea
                         name={`state-person-${d.key}-desc`}
                         label={t('activities.statePersonnel.labels.desc')}
@@ -118,7 +132,9 @@ class ActivityDetailStatePersonnel extends Component {
                     </td>
                     {years.map(year => (
                       <Fragment key={year}>
-                        <td>
+                        <td
+                          headers={`act_state_person_${year} act_state_person_amt_${year}`}
+                        >
                           <DollarInput
                             name={`state-person-${d.key}-${year}-amt`}
                             label={t(
@@ -129,7 +145,9 @@ class ActivityDetailStatePersonnel extends Component {
                             onChange={this.handleChange(i, 'amt', year)}
                           />
                         </td>
-                        <td>
+                        <td
+                          headers={`act_state_person_${year} act_state_person_pct_${year}`}
+                        >
                           <PercentInput
                             name={`state-person-${d.key}-${year}-perc`}
                             label={t(
@@ -142,7 +160,10 @@ class ActivityDetailStatePersonnel extends Component {
                         </td>
                       </Fragment>
                     ))}
-                    <td className="center">
+                    <td
+                      className="center"
+                      headers="act_state_person_null4 act_state_person_null5"
+                    >
                       <Btn
                         kind="outline"
                         extraCss="px1 py-tiny mt-tiny"
