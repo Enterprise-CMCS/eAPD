6cc12339ce25de128a9f7e51eafb78cc6ba94ade
Brendan Sudol <brendansudol@gmail.com>
2018-08-15 11:21:51 -0400

    better date inputs (#932)
    * start
    
    * progress
    
    * fix broken stuff
    
    * add datepicker to activity schedule
    
    * allow all dates

--- web/src/app.js
+++ web/src/app.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 
 import createHistory from 'history/createBrowserHistory';
 import React from 'react';
+import 'react-dates/initialize';
 import { render } from 'react-dom';
 import { routerMiddleware } from 'react-router-redux';
 import { createStore, applyMiddleware } from 'redux';
