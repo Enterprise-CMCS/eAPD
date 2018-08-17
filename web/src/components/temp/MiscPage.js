2b3575e5c49a0b6ffa9992605bf2cd26198301a5
Brendan Sudol <brendan.sudol@gsa.gov>
2018-08-17 11:12:19 -0400

    start browser upgrade module

--- web/src/components/temp/MiscPage.js
+++ web/src/components/temp/MiscPage.js
@@ -1,3 +1,4 @@
+import is from 'is_js';
 import React from 'react';
 
 import Container from '../Container';
@@ -5,6 +6,15 @@ import Container from '../Container';
 const MiscPage = () => (
   <Container>
     <h1>Misc UI bits for testing...</h1>
+    <div>{is.chrome() ? 'Chrome' : 'Not Chrome'}</div>
+    <div className="p2 bg-white rounded md-col-6">
+      <h2 className="mt0">Please upgrade your browser.</h2>
+      <p>
+        It looks like you may be using an out-of-date web browser that we don't
+        support. Please download or upgrade to one of these browsers to use this
+        site.
+      </p>
+    </div>
   </Container>
 );
 
