b23dee3deddb32bf263a6e7f7dfd7ec12fe23ac9
Ron Bronson <ronald.bronson@gsa.gov>
2018-08-20 12:55:54 -0400

    CTA copy change
    Made the change references in the repo.

--- web/src/components/temp/MiscPage.js
+++ web/src/components/temp/MiscPage.js
@@ -1,3 +1,4 @@
+import is from 'is_js';
 import React from 'react';
 
 import Container from '../Container';
@@ -5,7 +6,13 @@ import Container from '../Container';
 const MiscPage = () => (
   <Container>
     <h1>Misc UI bits for testing...</h1>
+    <div>{is.chrome() ? 'Chrome' : 'Not Chrome'}</div>
+    <div className="p2 bg-white rounded md-col-6">
+      <h2 className="mt0">Your computer is using an out-of-date browser.</h2>
+      <p>
+      Upgrade using one of the browser links below to access this site.
+      </p>
+    </div>
   </Container>
 );
-
 export default MiscPage;
