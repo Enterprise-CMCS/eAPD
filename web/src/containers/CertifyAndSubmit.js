3fc63fb35d7ef5e1ce676030470bebfb429635e9
Brendan Sudol <brendan.sudol@gsa.gov>
2018-08-30 12:26:38 -0400

    certify submit ui tweaks

--- web/src/containers/CertifyAndSubmit.js
+++ web/src/containers/CertifyAndSubmit.js
@@ -17,32 +17,37 @@ import { STATES } from '../util';
 
 const AlreadySubmitted = ({ dashboard, state, year }) => (
   <Fragment>
-    <Icon icon={faCheckCircle} color="green" className="absolute left-0" />
-    <p className="bold">
-      {t('certifyAndSubmit.certify.submitted.thanks.header')}
-    </p>
-
-    <p>
-      {t('certifyAndSubmit.certify.submitted.thanks.helpText', {
-        state,
-        year,
-        date: 'ddd',
-        time: 'time'
-      })}
-    </p>
-
-    <Icon icon={faClock} color="blue" className="absolute left-0" />
-    <p className="bold">
-      {t('certifyAndSubmit.certify.submitted.next.header')}
-    </p>
-
-    <p>{t('certifyAndSubmit.certify.submitted.next.helpText')}</p>
-
-    <Btn onClick={() => dashboard()}>
-      {t('certifyAndSubmit.certify.submitted.buttonText')}
-    </Btn>
+    <div className="flex">
+      <Icon icon={faCheckCircle} color="green" className="mt-tiny" />
+      <div className="ml1">
+        <p className="bold">
+          {t('certifyAndSubmit.certify.submitted.thanks.header')}
+        </p>
+        <p>
+          {t('certifyAndSubmit.certify.submitted.thanks.helpText', {
+            state,
+            year,
+            date: 'ddd',
+            time: 'time'
+          })}
+        </p>
+      </div>
+    </div>
+    <div className="flex">
+      <Icon icon={faClock} color="blue" className="mt-tiny" />
+      <div className="ml1">
+        <p className="bold">
+          {t('certifyAndSubmit.certify.submitted.next.header')}
+        </p>
+        <p>{t('certifyAndSubmit.certify.submitted.next.helpText')}</p>
+        <Btn onClick={() => dashboard()}>
+          {t('certifyAndSubmit.certify.submitted.buttonText')}
+        </Btn>
+      </div>
+    </div>
   </Fragment>
 );
+
 AlreadySubmitted.propTypes = {
   dashboard: PropTypes.func.isRequired,
   state: PropTypes.string.isRequired,
@@ -63,47 +68,40 @@ class Submit extends Component {
     const { submitter } = this.state;
 
     return (
-      <Fragment>
-        <Icon
-          icon={faExclamationTriangle}
-          color="orange"
-          className="absolute left-0"
-        />
-
-        <p className="bold">{t('certifyAndSubmit.certify.draft.header')}</p>
-
-        <p>{t('certifyAndSubmit.certify.draft.helpText')}</p>
-
-        <Input
-          name="submit_submitter_name"
-          label="First and last name"
-          value={submitter}
-          onChange={e => this.setState({ submitter: e.target.value })}
-        />
-
-        <Btn onClick={() => submitAPD()} disabled={submitter.length === 0}>
-          {t('certifyAndSubmit.certify.buttonText')}
-        </Btn>
-      </Fragment>
+      <div className="flex">
+        <Icon icon={faExclamationTriangle} color="orange" className="mt-tiny" />
+        <div className="ml1">
+          <p className="bold">{t('certifyAndSubmit.certify.draft.header')}</p>
+          <p>{t('certifyAndSubmit.certify.draft.helpText')}</p>
+          <Input
+            name="submit_submitter_name"
+            label="First and last name"
+            value={submitter}
+            onChange={e => this.setState({ submitter: e.target.value })}
+          />
+          <Btn onClick={() => submitAPD()} disabled={submitter.length === 0}>
+            {t('certifyAndSubmit.certify.buttonText')}
+          </Btn>
+        </div>
+      </div>
     );
   }
 }
+
 Submit.propTypes = { submitAPD: PropTypes.func.isRequired };
 
 const CertifyAndSubmit = ({ canSubmit, pushRoute, state, submitAPD, year }) => (
   <Section id="certify-submit" resource="certifyAndSubmit">
     <Subsection id="certify-submit-submit" resource="certifyAndSubmit.certify">
-      <div className="pl3 relative">
-        {canSubmit ? (
-          <Submit submitAPD={submitAPD} />
-        ) : (
-          <AlreadySubmitted
-            dashboard={() => pushRoute('/dash')}
-            state={state}
-            year={year}
-          />
-        )}
-      </div>
+      {canSubmit ? (
+        <Submit submitAPD={submitAPD} />
+      ) : (
+        <AlreadySubmitted
+          dashboard={() => pushRoute('/dash')}
+          state={state}
+          year={year}
+        />
+      )}
     </Subsection>
   </Section>
 );
@@ -116,7 +114,11 @@ CertifyAndSubmit.propTypes = {
   year: PropTypes.string.isRequired
 };
 
-const mapStateToProps = ({ apd: { data: { state, status, years } } }) => ({
+const mapStateToProps = ({
+  apd: {
+    data: { state, status, years }
+  }
+}) => ({
   canSubmit: status === 'draft',
   state: STATES.find(s => s.id === state).name,
   year: years[0]
@@ -124,4 +126,7 @@ const mapStateToProps = ({ apd: { data: { state, status, years } } }) => ({
 
 const mapDispatchToProps = { submitAPD: submitAPDAction, pushRoute: push };
 
-export default connect(mapStateToProps, mapDispatchToProps)(CertifyAndSubmit);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CertifyAndSubmit);
