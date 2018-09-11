7836cdb7d25bf712900f8cbf2f704c66dedb2638
Greg Walker <michael.walker@gsa.gov>
2018-09-11 13:25:03 -0500

    a few more tweaks

--- web/src/containers/StateDashboardCurrentDocuments.js
+++ web/src/containers/StateDashboardCurrentDocuments.js
@@ -28,7 +28,10 @@ const ProgressDot = ({ done, started, text }) => {
   return (
     <div className="center">
       <Icon icon={done ? faCheckCircle : faCircle} color={color} size="2x" />
-      <div className={`absolute left-align h5 ${labelColor}`.trim()}>
+      <div
+        className={`absolute left-align h5 ${labelColor}`.trim()}
+        style={{ maxWidth: '7rem' }}
+      >
         <div className="bold">{text}</div>
         {done && <div>10/1/1904</div>}
       </div>
@@ -64,120 +67,112 @@ ProgressLine.defaultProps = {
 };
 
 const DocumentItem = ({ name, status, buttonClick }) => {
-  const states = {
-    one: {
-      done: false,
-      started: true,
-      title: 'Submitted'
-    },
-    two: {
-      done: false,
-      started: false,
-      title: 'Received'
-    },
-    three: {
-      done: false,
-      started: false,
-      title: 'Under Review'
-    },
-    four: {
-      done: false,
-      started: false,
-      title: 'In Clearance'
-    },
-    five: {
-      done: false,
-      started: false,
-      title: 'Approved'
-    }
-  };
+  // const states = {
+  //   one: {
+  //     done: false,
+  //     started: true,
+  //     title: 'Submitted'
+  //   },
+  //   two: {
+  //     done: false,
+  //     started: false,
+  //     title: 'Received'
+  //   },
+  //   three: {
+  //     done: false,
+  //     started: false,
+  //     title: 'Under Review'
+  //   },
+  //   four: {
+  //     done: false,
+  //     started: false,
+  //     title: 'In Clearance'
+  //   },
+  //   five: {
+  //     done: false,
+  //     started: false,
+  //     title: 'Approved'
+  //   }
+  // };
 
   let actionButtonText = 'View';
   let actionButtonIcon = faArrowRight;
 
   switch (status) {
     case 'draft':
-      states.one.title = 'Drafting';
       actionButtonText = 'Open';
       actionButtonIcon = faEdit;
       break;
 
-    case 'in review':
-      states.one.done = true;
-      states.two.done = true;
-      states.three.started = true;
-      break;
-
-    case 'awaiting state response':
-      states.one.done = true;
-      states.two.done = true;
-      states.three.started = true;
-      states.three.title = 'Pending State Response to CMS Comments';
-      break;
-
     case 'approved':
-      states.one.done = true;
-      states.two.done = true;
-      states.three.done = true;
-      states.four.done = true;
-      states.five.done = true;
       actionButtonText = 'Approval letter';
       break;
 
     case 'disapproved':
-      states.one.done = true;
-      states.two.done = true;
-      states.three.done = true;
-      states.four.done = true;
-      states.five.done = true;
-      states.five.title = 'Disapproved';
       actionButtonText = 'Disapproval letter?';
-      break;
-
-    case 'withdrawn':
-      states.one.title = 'Drafting';
-      states.five.done = true;
-      states.five.title = 'Withdrawn';
       break;
 
     default:
       break;
   }
 
+  const apdPipelineSteps = [
+    {
+      text: status === 'draft' ? 'Draft' : 'Submitted',
+      started: true,
+      done: [
+        'in review',
+        'awaiting state response',
+        'in clearance',
+        'approved',
+        'disapproved'
+      ].includes(status)
+    }
+  ];
+
+  if (status !== 'withdrawn') {
+    apdPipelineSteps.push(JSON.parse(JSON.stringify(apdPipelineSteps[0])));
+    apdPipelineSteps[1].text = 'Received';
+    apdPipelineSteps[1].started = false;
+
+    apdPipelineSteps.push({
+      text:
+        status === 'awaiting state response' ? 'Pending Comments' : 'Review',
+      started: ['in review', 'awaiting state response'].includes(status),
+      done: ['in clearance', 'approved', 'disapproved'].includes(status)
+    });
+    apdPipelineSteps.push({
+      text: 'Clearance',
+      started: status === 'in clearance',
+      done: ['approved', 'disapproved'].includes(status)
+    });
+  }
+
+  const decisionTexts = {
+    approved: 'Approved',
+    disapproved: 'Disapproved',
+    withdrawn: 'Withdrawn'
+  };
+
+  apdPipelineSteps.push({
+    text: ['withdrawn', 'approved', 'disapproved'].includes(status)
+      ? decisionTexts[status]
+      : 'Decision',
+    started: false,
+    done: ['withdrawn', 'approved', 'disapproved'].includes(status)
+  });
+
   return (
     <Fragment>
       <div className="sm-flex items-center justify-between mb4 pb4">
         <div className="bold sm-col-2">{name}</div>
         <div className="sm-col-7 flex items-center">
-          <ProgressDot
-            started={states.one.started}
-            done={states.one.done}
-            text={states.one.title}
-          />
-          <ProgressLine done={states.one.done} />
-          <ProgressDot
-            started={states.two.started}
-            done={states.two.done}
-            text={states.two.title}
-          />
-          <ProgressLine done={states.two.done} />
-          <ProgressDot
-            started={states.three.started}
-            done={states.three.done}
-            text={states.three.title}
-          />
-          <ProgressLine done={states.four.done} />
-          <ProgressDot
-            started={states.four.started}
-            done={states.four.done}
-            text={states.four.title}
-          />
-          <ProgressLine done={states.five.done} />
-          <ProgressDot
-            started={states.five.started}
-            done={states.five.done}
-            text={states.five.title}
-          />
+          {apdPipelineSteps.map((step, i) => (
+            <Fragment key={step.name}>
+              {i > 0 && <ProgressLine done={step.started || step.done} />}
+              <ProgressDot {...step} />
+            </Fragment>
+          ))}
         </div>
         <div className="sm-col-2 sm-right-align">
           <Btn
@@ -227,11 +222,16 @@ class CurrentDocuments extends Component {
             buttonClick={this.open(apd.id)}
           />
         ))}
-        <DocumentItem name="Example one" status="in review" />
-        <DocumentItem name="Example two" status="awaiting state response" />
-        <DocumentItem name="Example three" status="approved" />
-        <DocumentItem name="Example three" status="disapproved" />
-        <DocumentItem name="Example three" status="withdrawn" />
+        <DocumentItem name="Ex: APD in draft" status="draft" />
+        <DocumentItem name="Ex: APD in review" status="in review" />
+        <DocumentItem
+          name="Ex: Reviewed, with comments"
+          status="awaiting state response"
+        />
+        <DocumentItem name="Ex: In clearance" status="in clearance" />
+        <DocumentItem name="Ex: Approved" status="approved" />
+        <DocumentItem name="Ex: Disapproved" status="disapproved" />
+        <DocumentItem name="Ex: Withdrawn" status="withdrawn" />
       </div>
     );
   }
