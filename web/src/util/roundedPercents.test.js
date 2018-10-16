a4c550da1e2aec0ce8206c7cfaf0afeac460363c
Greg Walker <michael.walker@gsa.gov>
2018-10-16 14:03:27 -0500

    new tests

---
+++ web/src/util/roundedPercents.test.js
@@ -0,0 +1,18 @@
+import roundedPercents from './roundedPercents';
+
+describe('rounding percentages util', () => {
+  test('spreads a number across a set of percentages', () => {
+    expect(roundedPercents(224, [0.15, 0.19, 0.3, 0.36])).toEqual([
+      34,
+      42,
+      67,
+      81
+    ]);
+
+    expect(roundedPercents(100, [0.3333, 0.3333, 0.33333])).toEqual([
+      33,
+      33,
+      34
+    ]);
+  });
+});
