dad45b889ef595cea91f1840ba4b1ed1b4b2a691
Greg Walker <michael.walker@gsa.gov>
2018-10-23 12:45:44 -0500

    switch library, bang on things a bit

--- web/src/components/Inputs.js
+++ web/src/components/Inputs.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Editor } from 'react-draft-wysiwyg';
+import NumberFormat from 'react-number-format';
 import TextMask from 'react-text-mask';
 
 import { EDITOR_CONFIG, htmlToEditor, editorToHtml } from '../util/editor';
@@ -63,15 +64,14 @@ const Textarea = props => (
   <textarea className="m0 textarea" spellCheck="true" {...props} />
 );
 
-const makeMaskedInput = ({ mask, unmask }) => {
-  // TODO: split some of these components into their own file
-  // eslint-disable-next-line react/no-multi-comp
-  class MaskedInput extends Component {
+const makeNumberInput = formatProps => {
+  class Numbo extends Component {
     handleChange = e => {
       if (!this.props.onChange) return;
 
-      const { value } = e.target;
-      const newEvent = { target: { value: unmask(value), masked: value } };
+      const newEvent = {
+        target: { value: e.floatValue || 0, masked: e.formattedValue }
+      };
 
       this.props.onChange(newEvent);
     };
@@ -80,26 +80,56 @@ const makeMaskedInput = ({ mask, unmask }) => {
       const { onChange, ...rest } = this.props;
 
       return (
-        <TextMask
-          mask={mask}
+        <NumberFormat
           className="m0 input mono"
-          onChange={this.handleChange}
+          onValueChange={this.handleChange}
+          {...formatProps}
           {...rest}
         />
       );
     }
   }
 
-  MaskedInput.propTypes = {
-    onChange: PropTypes.func
-  };
-
-  MaskedInput.defaultProps = {
-    onChange: null
-  };
-
-  return MaskedInput;
+  return Numbo;
 };
+
+// const makeMaskedInput = ({ mask, unmask }) => {
+//   // TODO: split some of these components into their own file
+//   // eslint-disable-next-line react/no-multi-comp
+//   class MaskedInput extends Component {
+//     handleChange = e => {
+//       if (!this.props.onChange) return;
+
+//       const { value } = e.target;
+//       const newEvent = { target: { value: unmask(value), masked: value } };
+
+//       this.props.onChange(newEvent);
+//     };
+
+//     render() {
+//       const { onChange, ...rest } = this.props;
+
+//       return (
+//         <TextMask
+//           mask={mask}
+//           className="m0 input mono"
+//           onChange={this.handleChange}
+//           {...rest}
+//         />
+//       );
+//     }
+//   }
+
+//   MaskedInput.propTypes = {
+//     onChange: PropTypes.func
+//   };
+
+//   MaskedInput.defaultProps = {
+//     onChange: null
+//   };
+
+//   return MaskedInput;
+// };
 
 // a HOC for Text / Textarea input that includes
 // div wrapper and accompanying label
@@ -146,8 +176,12 @@ makeInput.propTypes = {
 
 const InputHolder = makeInput(Input);
 const TextareaHolder = makeInput(Textarea);
-const DollarInputHolder = makeInput(makeMaskedInput(MASKS.dollar));
-const PercentInputHolder = makeInput(makeMaskedInput(MASKS.percent));
+// const DollarInputHolder = makeInput(makeMaskedInput(MASKS.dollar));
+const DollarInputHolder = makeInput(
+  makeNumberInput({ decimalScale: 2, fixedDecimalScale: true, prefix: '$' })
+);
+const PercentInputHolder = makeInput(makeNumberInput({ suffix: '%' }));
+// const PercentInputHolder = makeInput(makeMaskedInput(MASKS.percent));
 
 export {
   InputHolder as Input,
