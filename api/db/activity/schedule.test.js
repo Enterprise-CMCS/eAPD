8add92eb73505493008ad1d0bb6ee759a43fe90b
Greg Walker <michael.walker@gsa.gov>
2018-10-24 10:48:57 -0500

    backend updates

--- api/db/activity/schedule.test.js
+++ api/db/activity/schedule.test.js
@@ -11,14 +11,7 @@ tap.test('schedule data model', async scheduleModelTests => {
       {
         tableName: 'activity_schedule',
         static: {
-          updateableFields: [
-            'actualEnd',
-            'actualStart',
-            'milestone',
-            'plannedEnd',
-            'plannedStart',
-            'status'
-          ]
+          updateableFields: ['endDate', 'milestone', 'status']
         }
       },
       'get the expected model definitions'
@@ -52,11 +45,8 @@ tap.test('schedule data model', async scheduleModelTests => {
     const attributes = {
       id: 1,
       activity_id: 2,
-      actualEnd: '2000-01-01',
-      actualStart: '2000-01-02',
+      endDate: '2000-01-01',
       milestone: 5,
-      plannedEnd: '2000-01-03',
-      plannedStart: '2000-01-04',
       status: 8
     };
 
@@ -65,44 +55,28 @@ tap.test('schedule data model', async scheduleModelTests => {
     test.match(output, {
       id: 1,
       activity_id: 2,
-      actual_end: '2000-01-01',
-      actual_start: '2000-01-02',
+      end_date: '2000-01-01',
       milestone: 5,
-      planned_end: '2000-01-03',
-      planned_start: '2000-01-04',
       status: 8
     });
   });
 
   scheduleModelTests.test('validation', async test => {
     await Promise.all(
-      ['actualEnd', 'actualStart', 'plannedEnd', 'plannedStart'].map(
-        async attr => {
-          await Promise.all(
-            [7, 'bob', 'January 3, 1947', '14 October 1066', '2014-3-9'].map(
-              async invalidValue => {
-                try {
-                  const self = { attributes: { [attr]: invalidValue } };
-                  await schedule.validate.bind(self)();
-                  test.fail(`rejects if ${attr} is "${invalidValue}"`);
-                } catch (e) {
-                  test.pass(`rejects if ${attr} is "${invalidValue}"`);
-                }
-              }
-            )
-          );
+      [7, 'bob', 'January 3, 1947', '14 October 1066', '2014-3-9'].map(
+        async invalidValue => {
+          try {
+            const self = { attributes: { endDate: invalidValue } };
+            await schedule.validate.bind(self)();
+            test.fail(`rejects if endDate is "${invalidValue}"`);
+          } catch (e) {
+            test.pass(`rejects if endDate is "${invalidValue}"`);
+          }
         }
       )
     );
 
-    const self = {
-      attributes: {
-        actualEnd: '2000-01-01',
-        actualStart: '2000-01-01',
-        plannedEnd: '2000-01-01',
-        plannedStart: '2000-01-01'
-      }
-    };
+    const self = { attributes: { endDate: '2000-01-01' } };
 
     try {
       await schedule.validate.bind(self)();
@@ -115,22 +89,16 @@ tap.test('schedule data model', async scheduleModelTests => {
   scheduleModelTests.test('overrides toJSON method', async jsonTests => {
     const self = { get: sinon.stub() };
     self.get.withArgs('id').returns('passport');
-    self.get.withArgs('actual_end').returns(moment('2001-01-01').toDate());
-    self.get.withArgs('actual_start').returns(moment('2002-02-02').toDate());
+    self.get.withArgs('end_date').returns(moment('2001-01-01').toDate());
     self.get.withArgs('milestone').returns('a really big rock');
-    self.get.withArgs('planned_end').returns(moment('2003-03-03').toDate());
-    self.get.withArgs('planned_start').returns(moment('2004-04-04').toDate());
     self.get.withArgs('status').returns('test is passing, hopefully');
 
     const output = schedule.toJSON.bind(self)();
 
     jsonTests.match(output, {
       id: 'passport',
-      actualEnd: '2001-01-01',
-      actualStart: '2002-02-02',
+      endDate: '2001-01-01',
       milestone: 'a really big rock',
-      plannedEnd: '2003-03-03',
-      plannedStart: '2004-04-04',
       status: 'test is passing, hopefully'
     });
   });
