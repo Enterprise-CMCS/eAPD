191de5f80ca380755a96c1516105a8c48ac59f29
Greg Walker <michael.walker@gsa.gov>
2018-10-29 16:49:31 -0500

    delete unused stuff

--- web/src/routes.js
+++ web/src/routes.js
@@ -4,16 +4,10 @@ import Login from './containers/Login';
 
 import StateDash from './components/StateDashboard';
 
-// temp pages for more targeted development & iteration
-import MiscPage from './components/temp/MiscPage';
-
 const routes = [
   { path: '/', component: StateDash, exact: true, nonPrivate: false },
   { path: '/apd', component: ApdApplication, exact: true, nonPrivate: false },
   { path: '/login', component: Login, nonPrivate: true },
-  // { path: '/dash', component: StateDash, nonPrivate: true },
-
-  { path: '/temp/misc', component: MiscPage, nonPrivate: true },
 
   { component: NoMatch, nonPrivate: true }
 ];
