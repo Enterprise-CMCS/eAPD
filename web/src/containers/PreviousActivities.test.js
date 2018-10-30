885fc4feaf7653328e16282bd6a19d8cf0a64f3e
Greg Walker <mgwalker@gmail.com>
2018-10-29 20:59:52 -0500

    previous activity summary

---
+++ web/src/containers/PreviousActivities.test.js
@@ -0,0 +1,52 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import sinon from 'sinon';
+
+import { updateApd } from '../actions/apd';
+
+import {
+  raw as PreviousActivities,
+  mapStateToProps,
+  mapDispatchToProps
+} from './PreviousActivities';
+
+describe('previous activities component', () => {
+  test('renders correctly if data is not loaded', () => {
+    const component = shallow(
+      <PreviousActivities
+        previousActivitySummary="previous"
+        updateApd={() => {}}
+      />
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  test('updates on text change', () => {
+    const updateApdProps = sinon.spy();
+    const component = shallow(
+      <PreviousActivities
+        previousActivitySummary="previous"
+        updateApd={updateApdProps}
+      />
+    );
+    component.find('RichText').prop('onSync')('this is html');
+
+    expect(
+      updateApdProps.calledWith({ previousActivitySummary: 'this is html' })
+    ).toBeTruthy();
+  });
+
+  test('maps state to props', () => {
+    const state = {
+      apd: { data: { previousActivitySummary: 'moop moop' } }
+    };
+
+    expect(mapStateToProps(state)).toEqual({
+      previousActivitySummary: 'moop moop'
+    });
+  });
+
+  test('maps dispatch to props', () => {
+    expect(mapDispatchToProps).toEqual({ updateApd });
+  });
+});
