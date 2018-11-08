197c3a99a16e1189cd2d1bdb8eb7c597c81eabc3
Greg Walker <michael.walker@gsa.gov>
2018-11-08 11:03:34 -0600

    test the dashboard button does a thing

--- web/src/containers/TopBtns.test.js
+++ web/src/containers/TopBtns.test.js
@@ -13,28 +13,60 @@ import { logout } from '../actions/auth';
 describe('Top buttons component', () => {
   test('renders correctly if logged out', () => {
     const component = shallow(
-      <TopBtns authenticated={false} logout={() => {}} />
+      <TopBtns authenticated={false} logout={() => {}} pushRoute={() => {}} />
     );
     expect(component).toMatchSnapshot();
   });
 
   test('renders correctly if logged in', () => {
-    const component = shallow(<TopBtns authenticated logout={() => {}} />);
+    const component = shallow(
+      <TopBtns authenticated logout={() => {}} pushRoute={() => {}} />
+    );
     expect(component).toMatchSnapshot();
   });
 
   test('renders correctly with dashboard hidden', () => {
     const component = shallow(
-      <TopBtns authenticated hideDashboard logout={() => {}} />
+      <TopBtns
+        authenticated
+        hideDashboard
+        logout={() => {}}
+        pushRoute={() => {}}
+      />
     );
     expect(component).toMatchSnapshot();
+  });
+
+  test('goes to the dashboard', () => {
+    const event = { preventDefault: sinon.spy() };
+    const logoutProp = sinon.spy();
+    const pushRouteProp = sinon.spy();
+
+    const component = shallow(
+      <TopBtns authenticated logout={logoutProp} pushRoute={pushRouteProp} />
+    );
+
+    component
+      .find('Btn')
+      .at(0)
+      .simulate('click', event);
+
+    expect(pushRouteProp.calledWith('/')).toBeTruthy();
+    expect(event.preventDefault.called).toBeTruthy();
   });
 
   test('calls logout prop', () => {
     const event = { preventDefault: sinon.spy() };
     const logoutProp = sinon.spy();
+    const pushRouteProp = sinon.spy();
+
     const component = shallow(
-      <TopBtns authenticated hideDashboard logout={logoutProp} />
+      <TopBtns
+        authenticated
+        hideDashboard
+        logout={logoutProp}
+        pushRoute={pushRouteProp}
+      />
     );
 
     component.find('Btn').simulate('click', event);
