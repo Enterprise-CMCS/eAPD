071e4adbaec698722cd160953ea87f6aca9c7e23
Heather Battaglia <heather.battaglia@gsa.gov>
2019-02-25 09:35:31 -0700

    update styling

--- web/src/containers/StateDashboardSidebar.js
+++ web/src/containers/StateDashboardSidebar.js
@@ -13,10 +13,10 @@ class Sidebar extends Component {
     const { apds, place } = this.props;
 
     return (
-      <div className="ds-l-col--3">
+      <div className="ds-l-col--3 bg-white">
         <aside className="site-sidebar">
           <div className="xs-hide sm-hide">
-            <div className="px2 py3 lg-px3 lg-py4 bg-white flex items-center">
+            <div className="flex items-center">
               <img
                 src={`/static/img/states/${place.id}.svg`}
                 alt={place.name}
@@ -24,7 +24,7 @@ class Sidebar extends Component {
                 width="40"
                 height="40"
               />
-              <h1 className="m0 blue h3 light caps line-height-2">
+              <h1 className="text-xl">
                 {place.name} <br />
                 {t('title', { year: '' })}
               </h1>
