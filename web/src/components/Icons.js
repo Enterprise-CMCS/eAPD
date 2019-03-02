921d537379ee7fb1cb0563c90e4f6a4b0e474301
Greg Walker <mgwalker@users.noreply.github.com>
2019-03-01 18:42:49 -0600

    get rid of old code

--- web/src/components/Icons.js
+++ web/src/components/Icons.js
@@ -20,24 +20,6 @@ import {
 
 import { faHelp, faCircle, faClock } from '@fortawesome/free-regular-svg-icons';
 
-// import faArrowRight from '@fortawesome/free-solid-svg-icons/faLongArrowAltRight';
-// import faHelp from '@fortawesome/free-regular-svg-icons/faQuestionCircle';
-// import faBell from '@fortawesome/free-solid-svg-icons/faBell';
-// import faCheckCircle from '@fortawesome/free-solid-svg-icons/faCheckCircle';
-// import faChevronDown from '@fortawesome/free-solid-svg-icons/faChevronDown';
-// import faChevronUp from '@fortawesome/free-solid-svg-icons/faChevronUp';
-// import faCircle from '@fortawesome/free-regular-svg-icons/faCircle';
-// import faClock from '@fortawesome/free-regular-svg-icons/faClock';
-// import faCode from '@fortawesome/free-solid-svg-icons/faCode';
-// import faCog from '@fortawesome/free-solid-svg-icons/faCog';
-// import faEdit from '@fortawesome/free-solid-svg-icons/faEdit';
-// import faExclamationTriangle from '@fortawesome/free-solid-svg-icons/faExclamationTriangle';
-// import faPlusCircle from '@fortawesome/free-solid-svg-icons/faPlusCircle';
-// import faPrint from '@fortawesome/free-solid-svg-icons/faPrint';
-// import faSpinner from '@fortawesome/free-solid-svg-icons/faSpinner';
-// import faHelpSolid from '@fortawesome/free-solid-svg-icons/faQuestionCircle';
-// import faSignOut from '@fortawesome/free-solid-svg-icons/faSignOutAlt';
-
 export {
   faArrowRight,
   faHelp,
