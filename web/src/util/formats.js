b3a4b4326f59dc44da0dce89c2aa6c34c0ae51d8
Greg Walker <michael.walker@gsa.gov>
2019-03-06 12:38:52 -0600

    update last component; remvoe old formatter

--- web/src/util/formats.js
+++ web/src/util/formats.js
@@ -1,7 +1,5 @@
 import { format } from 'd3-format';
 
-const BIG_NUMBER_THRESHOLD = 1000000;
-
 // eslint-disable-next-line no-restricted-globals
 export const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
 
@@ -11,19 +9,3 @@ export const fmt = (val, spec = ',.2f') =>
 export const formatNum = x => fmt(x, `,.0f`);
 export const formatDec = (x, n = 2) => fmt(x, `,.${n}~f`);
 export const formatPerc = (x, n = 0) => fmt(x, `+.${n}%`);
-
-export const formatMoney = (x, smN = 0, lgN = 3) => {
-  if (!isNumeric(x)) return '--';
-  const spec = +x < BIG_NUMBER_THRESHOLD ? `$,.${smN}f` : `$,.${lgN}~s`;
-
-  // d3-format uses SI prefixes, so replace the prefix
-  // with one that makes more sense for currency.
-  //  IN                        OUT
-  //   M -> mega, million -----> M
-  //   G -> giga, billion -----> B
-  //   T -> tera, trillion ----> T
-  //   P -> peta, quadrillion -> Q
-  return format(spec)(x)
-    .replace(/G$/, 'B')
-    .replace(/P$/, 'Q');
-};
