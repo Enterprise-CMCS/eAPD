0fb35d19e571712874df1d35d0a2d07a6e07e249
Greg Walker <michael.walker@gsa.gov>
2019-03-12 16:16:10 -0500

    remove unused code

--- web/src/components/Section.js
+++ web/src/components/Section.js
@@ -1,4 +1,3 @@
-import { Badge } from '@cmsgov/design-system-core';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -22,7 +21,6 @@ SectionDesc.propTypes = {
 
 const Section = ({ children, id, number = 0, resource }) => {
   const title = t([resource, 'title'], { defaultValue: false });
-  const subheader = t([resource, 'subheader'], { defaultValue: false });
   const helptext = t([resource, 'helpText'], { defaultValue: false });
 
   return (
@@ -43,12 +41,6 @@ const Section = ({ children, id, number = 0, resource }) => {
       </div>
       {children}
     </section>
-    // <section id={id} className="py2 border-bottom border-grey border-width-3">
-    //   {title && <SectionTitle>{title}</SectionTitle>}
-    //   {subheader && <div>{subheader}</div>}
-    //   {helptext && <SectionDesc>{helptext}</SectionDesc>}
-    //   {children}
-    // </section>
   );
 };
 
@@ -69,14 +61,6 @@ const Subsection = ({ children, id, nested, open, resource }) => {
   const title = t([resource, 'title'], { defaultValue: '' });
 
   return (
-    // <section
-    //   id={id}
-    //   className="ds-u-border-top--1 ds-u-margin-y--2 ds-u-padding-top--2"
-    // >
-    //   <h3 className="ds-h3">{title}</h3>
-    //   <Instruction source={`${resource}.instruction`} />
-    //   {children}
-    // </section>
     <Collapsible id={id} title={title} open={open} nested={nested}>
       <Instruction source={`${resource}.instruction`} />
       {children}
