3bc3cd3e5eeab878a857a6f00a043016a311be98
Heather Battaglia <heather.battaglia@gsa.gov>
2019-04-02 10:44:52 -0600

    init dashboard

---
+++ web/src/containers/admin/AdminDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AdminDashboard = () => {
+  return (
+    <div className="site-body ds-l-container">
+      <div className="ds-l-row ds-u-margin--0">
+        o hai
+      </div>
+    </div>
+  )
+};
+
+export default AdminDashboard;
