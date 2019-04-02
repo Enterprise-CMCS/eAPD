d19abe1a7ecd21e106c2153b38162ad5e32bac51
Greg Walker <michael.walker@gsa.gov>
2019-04-02 17:59:29 -0500

    hide files endpoints and ux

--- api/routes/files/index.js
+++ api/routes/files/index.js
@@ -1,18 +1,19 @@
 const logger = require('../../logger')('files route index');
-const del = require('./delete');
-const get = require('./get');
-const post = require('./post');
+// const del = require('./delete');
+// const get = require('./get');
+// const post = require('./post');
 
-module.exports = (
-  app,
-  delEndpoint = del,
-  getEndpoint = get,
-  postEndpoint = post
-) => {
-  logger.silly('setting up DELETE endpoint');
-  delEndpoint(app);
-  logger.silly('setting up GET endpoint');
-  getEndpoint(app);
-  logger.silly('setting up POST endpoint');
-  postEndpoint(app);
-};
+module.exports = () =>
+  // app,
+  // delEndpoint = del,
+  // getEndpoint = get,
+  // postEndpoint = post
+  {
+    logger.warn('files endpoints are commented out');
+    // logger.silly('setting up DELETE endpoint');
+    // delEndpoint(app);
+    // logger.silly('setting up GET endpoint');
+    // getEndpoint(app);
+    // logger.silly('setting up POST endpoint');
+    // postEndpoint(app);
+  };
