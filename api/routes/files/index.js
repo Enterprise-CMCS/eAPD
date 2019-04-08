d329f95dc4bd2d4b77ecd6ff7d13d1bfb5f764d0
Greg Walker <michael.walker@gsa.gov>
2019-04-08 16:14:45 -0500

    add comment with link to PR explaining comments

--- api/routes/files/index.js
+++ api/routes/files/index.js
@@ -1,3 +1,10 @@
+// These files commented out for eAPD Builder "Layer 1" pilot.
+//
+// See https://github.com/18F/cms-hitech-apd/pull/1415
+//
+// We opted to leave all the code here so it'd be faster to get this
+// functionalityturned back on in the future.
+
 const logger = require('../../logger')('files route index');
 // const del = require('./delete');
 // const get = require('./get');
