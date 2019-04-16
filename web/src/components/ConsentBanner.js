b1824c71fe8f9fc449ebb12471f3010a8e3567c5
Greg Walker <michael.walker@gsa.gov>
2019-04-16 14:57:37 -0500

    use card styles

--- web/src/components/ConsentBanner.js
+++ web/src/components/ConsentBanner.js
@@ -1,6 +1,6 @@
 import { Button } from '@cmsgov/design-system-core';
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 
 let cookie = name => {
   const cookieMap = (document.cookie || '').split(';').reduce((c, s) => {
@@ -41,82 +41,86 @@ class ConsentBanner extends PureComponent {
     return (
       <div className="card--container">
         <div className="ds-l-container">
-          <div className="ds-l-row">
-            <div className={`ds-l-col--${showDetails ? 2 : 4}`} />
-            {showDetails ? (
-              <div className="ds-l-col--8 ds-u-fill--white ds-u-padding--3 ds-u-radius">
-                <p>
-                  This warning banner provides privacy and security notices
-                  consistent with applicable federal laws, directives, and other
-                  federal guidance for accessing this Government system, which
-                  includes (1) this computer network, (2) all computers
-                  connected to this network, and (3) all devices and storage
-                  media attached to this network or to a computer on this
-                  network.
-                </p>
-                <p>
-                  This system is provided for Government authorized use only.
-                </p>
-                <p>
-                  Unauthorized or improper use of this system is prohibited and
-                  may result in disciplinary action and/or civil and criminal
-                  penalties.
-                </p>
-                <p>
-                  Personal use of social media and networking sites on this
-                  system is limited as to not interfere with official work
-                  duties and is subject to monitoring.
-                </p>
-                <p>
-                  By using this system, you understand and consent to the
-                  following:
-                </p>
-                <ul>
-                  <li>
-                    The Government may monitor, record, and audit your system
-                    usage, including usage of personal devices and email systems
-                    for official duties or to conduct HHS business. Therefore,
-                    you have no reasonable expectation of privacy regarding any
-                    communication or data transiting or stored on this system.
-                    At any time, and for any lawful Government purpose, the
-                    government may monitor, intercept, and search and seize any
-                    communication or data transiting or stored on this system.
-                  </li>
-                  <li>
-                    Any communication or data transiting or stored on this
-                    system may be disclosed or used for any lawful Government
-                    purpose. To continue, you must accept the terms and
-                    conditions. If you decline, your login will automatically be
-                    cancelled.
-                  </li>
-                </ul>
-                <div className="ds-u-text-align--right">
-                  <Button variation="primary" onClick={this.toggleDetails}>
-                    Hide details
-                  </Button>
-                </div>
-              </div>
-            ) : (
-              <div className="ds-l-col--4 ds-u-fill--white ds-u-padding--3 ds-u-radius">
-                <p>
-                  This is a U.S. government service. Your use indicates your
-                  consent to monitoring, recording, and no expectation of
-                  privacy. Misuse is subject to criminal and civil penalties.{' '}
-                  <Button
-                    size="small"
-                    variation="transparent"
-                    onClick={this.toggleDetails}
-                  >
-                    Read more details
-                  </Button>
-                </p>
-                <div className="ds-u-text-align--center">
-                  <Button variation="primary" onClick={this.agreeAndContinue}>
-                    Agree and Continue
-                  </Button>
-                </div>
-              </div>
-            )}
+          <div className="ds-l-row card">
+            <div className="ds-l-col--1 ds-u-margin-left--auto" />
+            <div className="ds-l-col--12 ds-l-sm-col--10 ds-l-lg-col--6">
+              {showDetails ? (
+                <Fragment>
+                  <p>
+                    This warning banner provides privacy and security notices
+                    consistent with applicable federal laws, directives, and
+                    other federal guidance for accessing this Government system,
+                    which includes (1) this computer network, (2) all computers
+                    connected to this network, and (3) all devices and storage
+                    media attached to this network or to a computer on this
+                    network.
+                  </p>
+                  <p>
+                    This system is provided for Government authorized use only.
+                  </p>
+                  <p>
+                    Unauthorized or improper use of this system is prohibited
+                    and may result in disciplinary action and/or civil and
+                    criminal penalties.
+                  </p>
+                  <p>
+                    Personal use of social media and networking sites on this
+                    system is limited as to not interfere with official work
+                    duties and is subject to monitoring.
+                  </p>
+                  <p>
+                    By using this system, you understand and consent to the
+                    following:
+                  </p>
+                  <ul>
+                    <li>
+                      The Government may monitor, record, and audit your system
+                      usage, including usage of personal devices and email
+                      systems for official duties or to conduct HHS business.
+                      Therefore, you have no reasonable expectation of privacy
+                      regarding any communication or data transiting or stored
+                      on this system. At any time, and for any lawful Government
+                      purpose, the government may monitor, intercept, and search
+                      and seize any communication or data transiting or stored
+                      on this system.
+                    </li>
+                    <li>
+                      Any communication or data transiting or stored on this
+                      system may be disclosed or used for any lawful Government
+                      purpose. To continue, you must accept the terms and
+                      conditions. If you decline, your login will automatically
+                      be cancelled.
+                    </li>
+                  </ul>
+                  <div className="ds-u-text-align--right">
+                    <Button variation="primary" onClick={this.toggleDetails}>
+                      Hide details
+                    </Button>
+                  </div>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <p>
+                    This is a U.S. government service. Your use indicates your
+                    consent to monitoring, recording, and no expectation of
+                    privacy. Misuse is subject to criminal and civil penalties.{' '}
+                    <Button
+                      size="small"
+                      variation="transparent"
+                      onClick={this.toggleDetails}
+                    >
+                      Read more details
+                    </Button>
+                  </p>
+                  <div className="ds-u-text-align--center">
+                    <Button variation="primary" onClick={this.agreeAndContinue}>
+                      Agree and Continue
+                    </Button>
+                  </div>
+                </Fragment>
+              )}
+            </div>
+            <div className="ds-l-col--1 ds-u-margin-right--auto" />
           </div>
         </div>
       </div>
