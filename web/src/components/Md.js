ed490127b300ae7b83d32e17f1e2be2ab6464794
Greg Walker <michael.walker@gsa.gov>
2019-05-01 09:11:54 -0500

    update apd summary

--- web/src/components/Md.js
+++ web/src/components/Md.js
@@ -3,12 +3,43 @@ import React from 'react';
 
 import md from '../util/md';
 
-const Md = ({ content, wrapper: Wrapper, ...rest }) => (
-  <Wrapper
-    dangerouslySetInnerHTML={{ __html: md.renderInline(content) }}
-    {...rest}
-  />
-);
+const formatTags = {
+  ol: 'ol',
+  ul: 'ul'
+};
+
+const Md = ({ content, wrapper: Wrapper, ...rest }) => {
+  const formatRegex = /\[([^\]]+)\]([^]*?)\[\/\1\]/g;
+  let formattedString = content;
+
+  let match = formatRegex.exec(content);
+  while (match !== null) {
+    const type = match[1];
+    if (formatTags[type]) {
+      const tag = formatTags[type];
+      const formatted = match[2]
+        .split('\n')
+        .map(value => value.trim())
+        .filter(value => !!value)
+        .map(value => `<li>${value}</li>`)
+        .join('');
+
+      formattedString = formattedString.replace(
+        `[${type}]${match[2]}[/${type}]`,
+        `<${tag}>${formatted}</${tag}>`
+      );
+    }
+
+    match = formatRegex.exec(content);
+  }
+
+  return (
+    <Wrapper
+      dangerouslySetInnerHTML={{ __html: md.renderInline(formattedString) }}
+      {...rest}
+    />
+  );
+};
 
 Md.propTypes = {
   content: PropTypes.string.isRequired,
