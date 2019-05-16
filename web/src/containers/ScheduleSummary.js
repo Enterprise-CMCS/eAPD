d7c58c382bdd0cd4532172de8db77877417dde8c
Greg Walker <michael.walker@gsa.gov>
2019-05-16 10:31:21 -0500

    restructure activity schedule table

--- web/src/containers/ScheduleSummary.js
+++ web/src/containers/ScheduleSummary.js
@@ -1,29 +1,44 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
-import ReactTable from 'react-table';
 
 import { t } from '../i18n';
 import Waypoint from './ConnectedWaypoint';
 import { Section, Subsection } from '../components/Section';
 import { selectActivitySchedule } from '../reducers/activities.selectors';
 
-const ScheduleSummary = ({ tableData }) => (
+const ScheduleSummary = ({ activities }) => (
   <Waypoint id="schedule-summary">
     <Section isNumbered id="schedule-summary" resource="scheduleSummary">
       <Subsection id="schedule-summary-table" resource="scheduleSummary.main">
-        {tableData.data.length === 0 ? (
+        {activities.length === 0 ? (
           <div className="p1 h6 alert">
             {t('scheduleSummary.noDataMessage')}
           </div>
         ) : (
-          <ReactTable
-            showPagination={false}
-            resizable={false}
-            minRows={0}
-            className="h6 -striped"
-            {...tableData}
-          />
+          <table className="table-cms">
+            <tbody>
+              {activities.map(({ name: activityName, milestones }, i) => (
+                <Fragment key={activityName}>
+                  <tr>
+                    <th colSpan="2" className="ds-u-font-weight--bold">
+                      Activity {i + 1}: {activityName}
+                    </th>
+                  </tr>
+                  {milestones.map(({ end, name: milestoneName, start }) => (
+                    <tr>
+                      <td className="ds-u-padding-left--3 ds-u-border-right--0">
+                        {milestoneName}
+                      </td>
+                      <td className="ds-u-padding-right--3 ds-u-border-left--0">
+                        {start || 'N/A'} - {end || 'N/A'}
+                      </td>
+                    </tr>
+                  ))}
+                </Fragment>
+              ))}
+            </tbody>
+          </table>
         )}
       </Subsection>
     </Section>
@@ -31,44 +46,12 @@ const ScheduleSummary = ({ tableData }) => (
 );
 
 ScheduleSummary.propTypes = {
-  tableData: PropTypes.shape({
-    data: PropTypes.array,
-    columns: PropTypes.array,
-    defaultSorted: PropTypes.array
-  }).isRequired
+  activities: PropTypes.array.isRequired
 };
 
-const Cell = row => row.value || 'N/A';
-
-const mapStateToProps = state => {
-  const data = selectActivitySchedule(state);
-
-  const columns = [
-    {
-      accessor: 'activityName',
-      Header: t('scheduleSummary.main.table.activity')
-    },
-    {
-      accessor: 'milestone',
-      Header: t('scheduleSummary.main.table.milestone'),
-      Cell
-    },
-    {
-      accessor: 'startDate',
-      Header: t('scheduleSummary.main.table.start'),
-      Cell
-    },
-    {
-      accessor: 'endDate',
-      Header: t('scheduleSummary.main.table.end'),
-      Cell
-    }
-  ];
-
-  const defaultSorted = [{ id: 'plannedStart', desc: false }];
-
-  return { tableData: { data, columns, defaultSorted } };
-};
+const mapStateToProps = state => ({
+  activities: selectActivitySchedule(state)
+});
 
 export default connect(mapStateToProps)(ScheduleSummary);
 
--- web/src/containers/ScheduleSummary.test.js
+++ web/src/containers/ScheduleSummary.test.js
@@ -4,38 +4,59 @@ import React from 'react';
 import { plain as ScheduleSummary, mapStateToProps } from './ScheduleSummary';
 
 describe('schedule summary component', () => {
-  const tableData = {
-    columns: [
-      { accessor: 'accessor 1', header: 'Header 1', cell: () => {} },
-      { accessor: 'accessor 2', header: 'Header 2', cell: () => {} },
-      { accessor: 'accessor 3', header: 'Header 3', cell: () => {} }
-    ],
-    data: [
-      {
-        'accessor 1': 'value 1.1',
-        'accessor 2': 'value 1.2',
-        'accessor 3': 'value 1.3'
-      },
-      {
-        'accessor 1': 'value 2.1',
-        'accessor 2': 'value 2.2',
-        'accessor 3': 'value 2.3'
-      }
-    ],
-    defaultSorted: [{ id: 'accessor 2', desc: false }]
-  };
+  const activities = [
+    {
+      name: 'Number One',
+      milestones: [
+        // Mariah Carey and Boz II Men's "One Sweet Day" sits at #1 on the
+        // Billboard Hot 100 for a record 16 consecutive weeks.
+        { end: '1996/03/16', name: 'one-one', start: '1995/12/02' },
+        // The Beatles begin their record-holding streak of #1 hits on
+        // Billboard with "Love Me Do" and wrap things up with "The Long and
+        // Winding Road"
+        { end: '1970/05/23', name: 'one-two', start: '1962/10/05' }
+      ],
+      // For the first time, Captain Picard gives Commander Riker his most
+      // famous order:  "Make it so, Number One."
+      start: '1988/12/10'
+    },
+    {
+      name: 'Number 💩',
+      milestones: [
+        // The Squatty Potty commercial hits YouTube and instantly goes viral.
+        { end: '2016/10/06', name: 'three-one', start: '2016/10/06' },
+        // National Geographic publishes a story about a 50,000-year old poop
+        // discovered in Spain.
+        { end: '2014/06/25', name: 'two-two', start: '2014/06/25' },
+        // The Gemini V mission marks the first time humans pooped in space.
+        // During the 8-day mission, Gordon Cooper and Pete Conrad pooped a
+        // total of 4 times.
+        { end: '1965/08/21', name: 'two-three', start: '1965/08/29' }
+      ],
+      // First flush toilet installed in the White House during the Millard
+      // Filmore administration, possibly its only noteworthy contribution.
+      start: '1853/02/18'
+    },
+    {
+      name: 'Number Three',
+      // The horse Justify wins the Triple Crown, 99 years after it was first
+      // won by the horse Sir Barton.
+      milestones: [
+        { end: '2018/06/09', name: 'three-one', start: '2018/05/05' }
+      ],
+      // Dale Earnhardt wins his 3rd NASCAR championship driving car #3.
+      start: '1987/11/22'
+    }
+  ];
 
   test('renders correctly', () => {
     expect(
-      shallow(<ScheduleSummary tableData={tableData} />)
+      shallow(<ScheduleSummary activities={activities} />)
     ).toMatchSnapshot();
   });
 
   test('renders correctly with no data', () => {
-    tableData.data.length = 0;
-    expect(
-      shallow(<ScheduleSummary tableData={tableData} />)
-    ).toMatchSnapshot();
+    expect(shallow(<ScheduleSummary activities={[]} />)).toMatchSnapshot();
   });
 
   test('maps state to props', () => {
@@ -44,13 +65,35 @@ describe('schedule summary component', () => {
         byKey: {
           key1: {
             name: 'activity 1',
-            plannedStartDate: 'planned start 1',
-            schedule: [{ a: 1 }, { a: 2 }, { a: 3 }]
+            // The Centennial Light, a 60-watt lightbulb was installed at a
+            // firehouse in Livermore, California, where it's still lit.
+            plannedStartDate: '1901-01-01',
+            schedule: [
+              // Sandra Day O'Connor, first woman to serve on the US Supreme
+              // Court, retires.
+              { endDate: '2006-01-31', milestone: '1-one' },
+              // Thurgood Marshall, first African-American to serve on the US
+              // Supreme Court, retires.
+              { endDate: '1991-10-01', milestone: '1-two' },
+              // Samuel Nelson, first person to retire from the US Supreme
+              // Court, retires.
+              { endDate: '1872-11-28', milestone: '1-three' }
+            ]
           },
           key2: {
             name: 'activity 2',
-            plannedStartDate: 'planned start 2',
-            schedule: [{ a: 'a' }, { a: 'b' }, { a: 'c' }]
+            // The United States Camel Corps is created. Seriously, look it up.
+            plannedStartDate: '1855-03-03',
+            schedule: [
+              // The Army sells its camels at auction, giving up the project.
+              { endDate: '1864-06-06', milestone: '2-one' },
+              // The last of the Army camels is sighted for the last time,
+              // wandering the desert. Nobody knows what happened to it after
+              // that, but through at least 1883, stories pop up from time to
+              // time of a giant camel overturning wagons and trampling people
+              // to death. They're probably not entirely true, though.
+              { endDate: '1873-08-08', milestone: '2-two' }
+            ]
           }
         }
       }
@@ -59,41 +102,25 @@ describe('schedule summary component', () => {
     const props = mapStateToProps(state);
 
     expect(props).toMatchObject({
-      tableData: {
-        columns: [
-          {
-            accessor: 'activityName',
-            Header: expect.any(String)
-          },
-          {
-            accessor: 'milestone',
-            Header: expect.any(String),
-            Cell: expect.any(Function)
-          },
-          {
-            accessor: 'startDate',
-            Header: expect.any(String),
-            Cell: expect.any(Function)
-          },
-          {
-            accessor: 'endDate',
-            Header: expect.any(String),
-            Cell: expect.any(Function)
-          }
-        ],
-        data: [
-          { a: 1, activityName: 'activity 1', startDate: 'planned start 1' },
-          { a: 2, activityName: 'activity 1', startDate: 'planned start 1' },
-          { a: 3, activityName: 'activity 1', startDate: 'planned start 1' },
-          { a: 'a', activityName: 'activity 2', startDate: 'planned start 2' },
-          { a: 'b', activityName: 'activity 2', startDate: 'planned start 2' },
-          { a: 'c', activityName: 'activity 2', startDate: 'planned start 2' }
-        ],
-        defaultSorted: [{ id: 'plannedStart', desc: false }]
-      }
+      activities: [
+        {
+          name: 'activity 1',
+          milestones: [
+            { end: '1872/11/28', name: '1-three' },
+            { end: '1991/10/01', name: '1-two' },
+            { end: '2006/01/31', name: '1-one' }
+          ],
+          start: '1901/01/01'
+        },
+        {
+          name: 'activity 2',
+          milestones: [
+            { end: '1864/06/06', name: '2-one' },
+            { end: '1873/08/08', name: '2-two' }
+          ],
+          start: '1855/03/03'
+        }
+      ]
     });
-
-    expect(props.tableData.columns[1].Cell({ value: 72 })).toEqual(72);
-    expect(props.tableData.columns[1].Cell({})).toEqual('N/A');
   });
 });
