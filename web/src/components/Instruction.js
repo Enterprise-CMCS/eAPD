995eecfe45b45bd8d12541b9d00b26f325b27eb3
Heather Battaglia <heather.battaglia@gsa.gov>
2019-05-22 11:51:16 -0600

    change how default heading is applied; fix lint errors

--- web/src/components/Instruction.js
+++ web/src/components/Instruction.js
@@ -9,12 +9,11 @@ const Instruction = ({ args, reverse, source, headingDisplay }) => {
   const short = t([source, 'short'], { defaultValue: false, ...args });
   const detail = t([source, 'detail'], { defaultValue: false, ...args });
   const helpText = t([source, 'helpText'], { defaultValue: false, ...args });
-  const Tag = headingDisplay !== undefined ? headingDisplay.level : 'h3';
-  const headingClass = headingDisplay !== undefined ? headingDisplay.className : 'ds-h3';
+  const Tag = headingDisplay.level;
 
   return (
     <div>
-      {heading && <Tag className={headingClass}>{heading}</Tag>}
+      {heading && <Tag className={headingDisplay.className}>{heading}</Tag>}
       {(short || detail || helpText) && (
         <div className="visibility--screen">
           {reverse && detail && <Md content={detail} wrapper="p" />}
@@ -36,12 +35,17 @@ const Instruction = ({ args, reverse, source, headingDisplay }) => {
 Instruction.propTypes = {
   args: PropTypes.object,
   reverse: PropTypes.bool,
-  source: PropTypes.string.isRequired
+  source: PropTypes.string.isRequired,
+  headingDisplay: PropTypes.object
 };
 
 Instruction.defaultProps = {
   args: null,
-  reverse: false
+  reverse: false,
+  headingDisplay: {
+    level: 'h3',
+    className: 'ds-h3'
+  }
 };
 
 export default Instruction;
