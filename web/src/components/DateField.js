68678d1d2766c31a56ff40c344f95e17a6a3575d
Greg Walker <michael.walker@gsa.gov>
2019-06-06 10:03:26 -0500

    some notes about date formats

--- web/src/components/DateField.js
+++ web/src/components/DateField.js
@@ -2,12 +2,18 @@ import { DateField as DSDateField } from '@cmsgov/design-system-core';
 import PropTypes from 'prop-types';
 import React, { Fragment, useMemo } from 'react';
 
+// Dates are stored internally as YYYY-MM-DD. The design system date field
+// expects separate day, month, and year values. So split the incoming
+// date up into its pieces.
 const splitDate = date =>
   useMemo(() => {
     const [year, month, day] = date.split('-');
     return { day: +day || '', month: +month || '', year: +year || '' };
   }, [date]);
 
+// The design system will call our formatter before calling our change event
+// handlers. This formatter puts the date pieces back into our internal date
+// string format.
 const joinDate = ({ day, month, year }) => `${year}-${month}-${day}`;
 
 const DateField = ({ value, onChange, ...rest }) => {
