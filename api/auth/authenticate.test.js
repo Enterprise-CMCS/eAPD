11874f004ed7a3f2a9a575b15de4814a7e4bc6f6
Greg Walker <michael.walker@gsa.gov>
2019-06-17 14:07:13 -0500

    limit failed logons

--- api/auth/authenticate.test.js
+++ api/auth/authenticate.test.js
@@ -10,6 +10,8 @@ const userModel = {
   fetch: sandbox.stub()
 };
 const get = sandbox.stub();
+const save = sandbox.stub();
+const set = sandbox.spy();
 
 const hash = {
   compare: sandbox.stub()
@@ -162,14 +164,80 @@ tap.test('local authentication', async authTest => {
     noUserTest.ok(doneCallback.calledWith(null, false), 'got a false user');
   });
 
+  authTest.test('with locked account', async invalidTest => {
+    get.withArgs('locked_until').returns(Date.now() + 5000);
+    userModel.fetch.resolves({ get, save, set });
+
+    await auth(lib.getNonce('user'), 'password', doneCallback);
+
+    invalidTest.ok(
+      userModel.query.calledOnce,
+      'one set of QUERY clauses is added'
+    );
+    invalidTest.ok(
+      userModel.query.calledWith('whereRaw', 'LOWER(email) = ?', ['user']),
+      'it is the QUERY we expect'
+    );
+    invalidTest.ok(
+      userModel.fetch.calledOnce,
+      'the query is executed one time'
+    );
+
+    invalidTest.ok(set.notCalled, 'user data model is not updated');
+    invalidTest.ok(hash.compare.notCalled, 'password is never checked');
+    invalidTest.equal(doneCallback.callCount, 1, 'called done callback once');
+    invalidTest.ok(doneCallback.calledWith(null, false), 'got a false user');
+  });
+
+  authTest.test('with locked account', async invalidTest => {
+    get.withArgs('email').returns('hello@world');
+    get.withArgs('password').returns('test-password');
+    get.withArgs('id').returns(57);
+    get.withArgs('locked_until').returns(500);
+    userModel.fetch.resolves({ get, save, set });
+
+    await auth(lib.getNonce('user'), 'password', doneCallback);
+
+    invalidTest.ok(
+      userModel.query.calledOnce,
+      'one set of QUERY clauses is added'
+    );
+    invalidTest.ok(
+      userModel.query.calledWith('whereRaw', 'LOWER(email) = ?', ['user']),
+      'it is the QUERY we expect'
+    );
+    invalidTest.ok(
+      userModel.fetch.calledOnce,
+      'the query is executed one time'
+    );
+
+    invalidTest.ok(
+      set.calledWith('failed_logons', null),
+      'failed logon data is reset'
+    );
+    invalidTest.ok(
+      set.calledWith('locked_until', null),
+      'account lock is reset'
+    );
+    invalidTest.ok(save.calledAfter(set), 'data model changes are saved');
+    invalidTest.equal(doneCallback.callCount, 1, 'called done callback once');
+    invalidTest.ok(doneCallback.calledWith(null, false), 'got a false user');
+  });
+
   authTest.test('with invalid password', async invalidTest => {
     get.withArgs('email').returns('hello@world');
     get.withArgs('password').returns('test-password');
     get.withArgs('id').returns(57);
-    userModel.fetch.resolves({ get });
+    get.withArgs('locked_until').returns(0);
+    userModel.fetch.resolves({ get, save, set });
     hash.compare.resolves(false);
 
+    const clock = sinon.useFakeTimers();
+    clock.tick(54321012345);
+
     await auth(lib.getNonce('user'), 'password', doneCallback);
+
+    clock.restore();
 
     invalidTest.ok(
       userModel.query.calledOnce,
@@ -190,9 +258,135 @@ tap.test('local authentication', async authTest => {
       'password is compared to database value'
     );
 
+    invalidTest.ok(
+      set.calledWith('failed_logons', [54321012345]),
+      'saves the failed logon'
+    );
+    invalidTest.ok(save.calledAfter(set), 'data model changes are saved');
+
     invalidTest.equal(doneCallback.callCount, 1, 'called done callback once');
     invalidTest.ok(doneCallback.calledWith(null, false), 'got a false user');
   });
+
+  authTest.test(
+    'with invalid password and several previous failed attempts',
+    async invalidTest => {
+      get.withArgs('email').returns('hello@world');
+      get.withArgs('password').returns('test-password');
+      get.withArgs('id').returns(57);
+      get
+        .withArgs('failed_logons')
+        .returns([54321012341, 54321012342, 54321012343, 54321012344]);
+      get.withArgs('locked_until').returns(0);
+      userModel.fetch.resolves({ get, save, set });
+      hash.compare.resolves(false);
+
+      process.env.AUTH_LOCK_FAILED_ATTEMPTS_COUNT = 5;
+      process.env.AUTH_LOCK_FAILED_ATTEMPTS_DURATION_MINUTES = 30;
+      process.env.AUTH_LOCK_FAILED_ATTEMPTS_WINDOW_TIME_MINUTES = 1;
+      const clock = sinon.useFakeTimers();
+      clock.tick(54321012345);
+
+      await auth(lib.getNonce('user'), 'password', doneCallback);
+
+      clock.restore();
+
+      invalidTest.ok(
+        userModel.query.calledOnce,
+        'one set of QUERY clauses is added'
+      );
+      invalidTest.ok(
+        userModel.query.calledWith('whereRaw', 'LOWER(email) = ?', ['user']),
+        'it is the QUERY we expect'
+      );
+      invalidTest.ok(
+        userModel.fetch.calledOnce,
+        'the query is executed one time'
+      );
+
+      invalidTest.ok(hash.compare.calledOnce, 'password is compared one time');
+      invalidTest.ok(
+        hash.compare.calledWith('password', 'test-password'),
+        'password is compared to database value'
+      );
+
+      invalidTest.ok(
+        set.calledWith('failed_logons', [
+          54321012341,
+          54321012342,
+          54321012343,
+          54321012344,
+          54321012345
+        ]),
+        'saves the failed logon'
+      );
+      invalidTest.ok(
+        set.calledWith('locked_until', 54321012345 + 1800000),
+        'account is marked as locked'
+      );
+      invalidTest.ok(save.calledAfter(set), 'data model changes are saved');
+
+      invalidTest.equal(doneCallback.callCount, 1, 'called done callback once');
+      invalidTest.ok(doneCallback.calledWith(null, false), 'got a false user');
+    }
+  );
+
+  authTest.test(
+    'with invalid password and several outdated previous failed attempts',
+    async invalidTest => {
+      get.withArgs('email').returns('hello@world');
+      get.withArgs('password').returns('test-password');
+      get.withArgs('id').returns(57);
+      get
+        .withArgs('failed_logons')
+        .returns([44321012341, 44321012342, 44321012343, 44321012344]);
+      get.withArgs('locked_until').returns(0);
+      userModel.fetch.resolves({ get, save, set });
+      hash.compare.resolves(false);
+
+      process.env.AUTH_LOCK_FAILED_ATTEMPTS_COUNT = 5;
+      process.env.AUTH_LOCK_FAILED_ATTEMPTS_DURATION_MINUTES = 30;
+      process.env.AUTH_LOCK_FAILED_ATTEMPTS_WINDOW_TIME_MINUTES = 1;
+      const clock = sinon.useFakeTimers();
+      clock.tick(54321012345);
+
+      await auth(lib.getNonce('user'), 'password', doneCallback);
+
+      clock.restore();
+
+      invalidTest.ok(
+        userModel.query.calledOnce,
+        'one set of QUERY clauses is added'
+      );
+      invalidTest.ok(
+        userModel.query.calledWith('whereRaw', 'LOWER(email) = ?', ['user']),
+        'it is the QUERY we expect'
+      );
+      invalidTest.ok(
+        userModel.fetch.calledOnce,
+        'the query is executed one time'
+      );
+
+      invalidTest.ok(hash.compare.calledOnce, 'password is compared one time');
+      invalidTest.ok(
+        hash.compare.calledWith('password', 'test-password'),
+        'password is compared to database value'
+      );
+
+      invalidTest.ok(
+        set.calledWith('failed_logons', [54321012345]),
+        'saves the failed logon'
+      );
+      invalidTest.notOk(
+        set.calledWith('locked_until'),
+        'account is not marked as locked'
+      );
+      invalidTest.ok(save.calledAfter(set), 'data model changes are saved');
+
+      invalidTest.equal(doneCallback.callCount, 1, 'called done callback once');
+      invalidTest.ok(doneCallback.calledWith(null, false), 'got a false user');
+    }
+  );
 
   authTest.test('with a valid user', async validTest => {
     get.withArgs('email').returns('hello@world');
@@ -203,7 +397,9 @@ tap.test('local authentication', async authTest => {
 
     const model = {
       activities: sinon.stub().resolves('play fetch'),
-      get
+      get,
+      save,
+      set
     };
     userModel.fetch.resolves(model);
     hash.compare.resolves(true);
@@ -225,6 +421,13 @@ tap.test('local authentication', async authTest => {
       hash.compare.calledWith('password', 'test-password'),
       'password is compared to database value'
     );
+
+    validTest.ok(
+      set.calledWith('failed_logons', null),
+      'failed logon data is reset'
+    );
+    validTest.ok(set.calledWith('locked_until', null), 'account lock is reset');
+    validTest.ok(save.calledAfter(set), 'data model changes are saved');
 
     validTest.equal(doneCallback.callCount, 1, 'called done callback once');
     validTest.ok(
