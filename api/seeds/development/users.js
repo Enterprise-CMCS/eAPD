7af1a2f293d29a39b8a14eb9267a67025469cb64
Greg Walker <michael.walker@gsa.gov>
2019-06-20 15:46:40 -0500

    send fully-populated user data back on login; tests

--- api/seeds/development/users.js
+++ api/seeds/development/users.js
@@ -4,13 +4,17 @@ exports.seed = async knex => {
   await knex('users').insert([
     {
       email: 'em@il.com',
+      name: 'Regular User',
       password: hash.hashSync('password'),
+      position: 'sticky',
       auth_role: 'state coordinator',
       state_id: 'mo'
     },
     {
       email: 'admin',
+      name: 'Admin McAdminersen',
       password: hash.hashSync('password'),
+      position: 'eAPD Administrator',
       auth_role: 'admin'
     }
   ]);
