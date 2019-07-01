bb08cb2d3943bf69c335492c6d029f514013b27a
Ron Bronson <ronaldbronson@f05hdj-j1y5hv2f.domain>
2019-07-01 16:08:02 -0700

    Update save error message
    Added some clarity to the save error message.

--- web/src/reducers/errors.js
+++ web/src/reducers/errors.js
@@ -66,7 +66,7 @@ const errorActions = {
   [ADMIN_CREATE_USER_ERROR]: ['addAccount', 'Unknown error creating account'],
   [ADMIN_EDIT_ACCOUNT_ERROR]: ['editAccount', 'Unknown error editing account'],
   [ADMIN_EDIT_ME_ERROR]: ['editOwnAccount', 'Unknown error editing account'],
-  [SAVE_APD_FAILURE]: ['saveApd', 'Save failed. Try saving changes again.']
+  [SAVE_APD_FAILURE]: ['saveApd', 'Save failed. Check the section & try saving again.']
 };
 
 const reducer = (state = initialState, action) => {
