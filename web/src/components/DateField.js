eb9b658f58b70620eee274f0e4a2969ff6947dd4
Greg Walker <michael.walker@gsa.gov>
2019-07-17 14:11:46 -0500

    lightweight date validation

--- web/src/components/DateField.js
+++ web/src/components/DateField.js
@@ -1,4 +1,5 @@
 import { DateField as DSDateField } from '@cmsgov/design-system-core';
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import React, { useMemo } from 'react';
 
@@ -19,9 +20,54 @@ const joinDate = ({ day, month, year }) => `${year}-${month}-${day}`;
 const DateField = ({ value, onChange, ...rest }) => {
   const dateParts = splitDate(value);
 
+  const errorInfo = useMemo(() => {
+    const date = moment(value, 'YYYY-M-D', true);
+    if (date.isValid()) {
+      return null;
+    }
+
+    const message = [];
+    let invalidDay = false;
+    let invalidMonth = false;
+    let invalidYear = false;
+
+    if (!dateParts.month) {
+      message.push('Month is required.');
+      invalidMonth = true;
+    }
+    if (dateParts.month > 12) {
+      message.push('Month must be between 1 and 12.');
+      invalidMonth = true;
+    }
+
+    if (!dateParts.day) {
+      message.push('Day is required.');
+      invalidDay = true;
+    }
+
+    if (`${dateParts.year}`.length !== 4) {
+      message.push('Year must be 4 digits.');
+      invalidYear = true;
+    }
+
+    if (message.length === 0) {
+      message.push(
+        'Invalid date - is the day number too high for the provided month and year?'
+      );
+    }
+
+    return {
+      errorMessage: message.join(' '),
+      invalidDay,
+      invalidMonth,
+      invalidYear
+    };
+  }, [value]);
+
   return (
     <DSDateField
       {...rest}
+      {...errorInfo}
       dayValue={dateParts.day}
       monthValue={dateParts.month}
       yearValue={dateParts.year}
