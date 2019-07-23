f7b94d2d3536f119c3f19c1a039cbe8b822d322d
Greg Walker <michael.walker@gsa.gov>
2019-07-23 12:55:56 -0500

    switch previous activity table

--- web/src/containers/ApdPreviousActivityTable.js
+++ web/src/containers/ApdPreviousActivityTable.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
+import DollarField from '../components/DollarField';
 import Dollars from '../components/Dollars';
 import { DollarInput } from '../components/Inputs';
 import { updateApd } from '../actions/apd';
@@ -24,23 +25,14 @@ const ApdPreviousActivityTable = ({
 
   return (
     <table className="budget-table">
-      <caption className="ds-h4">
-        HIT + HIE
-      </caption>
+      <caption className="ds-h4">HIT + HIE</caption>
       <thead>
         <tr>
           <th id="prev_act_hit_header_ffy">
-            <span className="ds-u-visibility--screen-reader">
-              Year
-            </span>
+            <span className="ds-u-visibility--screen-reader">Year</span>
           </th>
-          <th id="prev_act_hithie_total">
-            {TABLE_HEADERS.total}
-          </th>
-          <th
-            colSpan="2"
-            id="prev_act_hithie_federal"
-          >
+          <th id="prev_act_hithie_total">{TABLE_HEADERS.total}</th>
+          <th colSpan="2" id="prev_act_hithie_federal">
             {TABLE_HEADERS.federal()}
           </th>
         </tr>
@@ -69,57 +61,56 @@ const ApdPreviousActivityTable = ({
       <tbody>
         {years.map(year => {
           const federalApproved =
-              previousActivityExpenses[year].hithie.totalApproved * 0.9;
+            previousActivityExpenses[year].hithie.totalApproved * 0.9;
 
-            return (
-              <tr key={year}>
-                <th id={`prev_act_hithie_row_${year}`} headers="prev_act_hit_header_ffy">
-                  {TABLE_HEADERS.ffy(year)}
-                </th>
+          return (
+            <tr key={year}>
+              <th
+                id={`prev_act_hithie_row_${year}`}
+                headers="prev_act_hit_header_ffy"
+              >
+                {TABLE_HEADERS.ffy(year)}
+              </th>
 
-                <td
-                  headers={`prev_act_hithie_row_${year} prev_act_hithie_total prev_act_hithie_total_approved`}
-                >
-                  <DollarInput
-                    name={`hithie-approved-total-${year}`}
-                    label={`total approved funding for HIT and HIE for FFY ${year}, state plus federal`}
-                    hideLabel
-                    wrapperClass="budget-table--input-holder"
-                    className="budget-table--input__number"
-                    value={
-                      previousActivityExpenses[year].hithie.totalApproved
-                    }
-                    onChange={handleChange(year, 'hithie', 'totalApproved')}
-                  />
-                </td>
+              <td
+                headers={`prev_act_hithie_row_${year} prev_act_hithie_total prev_act_hithie_total_approved`}
+              >
+                <DollarField
+                  className="budget-table--input-holder"
+                  fieldClassName="budget-table--input__number"
+                  label={`total approved funding for HIT and HIE for FFY ${year}, state plus federal`}
+                  labelClassName="sr-only"
+                  name={`hithie-approved-total-${year}`}
+                  value={previousActivityExpenses[year].hithie.totalApproved}
+                  onChange={handleChange(year, 'hithie', 'totalApproved')}
+                />
+              </td>
 
-                <td
-                  headers={`prev_act_hithie_row_${year} prev_act_hithie_federal prev_act_hithie_federal_approved`}
-                  className="budget-table--number"
-                >
-                  <Dollars>{federalApproved}</Dollars>
-                </td>
+              <td
+                headers={`prev_act_hithie_row_${year} prev_act_hithie_federal prev_act_hithie_federal_approved`}
+                className="budget-table--number"
+              >
+                <Dollars>{federalApproved}</Dollars>
+              </td>
 
-                <td
-                  headers={`prev_act_hithie_row_${year} prev_act_hithie_federal prev_act_hithie_federal_actual`}
-                >
-                  <DollarInput
-                    name={`hithie-actual-federal-${year}`}
-                    label={`actual federal share for HIT and HIE for FFY ${year}`}
-                    hideLabel
-                    wrapperClass="budget-table--input-holder"
-                    className="budget-table--input__number"
-                    value={
-                      previousActivityExpenses[year].hithie.federalActual
-                    }
-                    onChange={handleChange(year, 'hithie', 'federalActual')}
-                  />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+              <td
+                headers={`prev_act_hithie_row_${year} prev_act_hithie_federal prev_act_hithie_federal_actual`}
+              >
+                <DollarField
+                  className="budget-table--input-holder"
+                  fieldClassName="budget-table--input__number"
+                  label={`actual federal share for HIT and HIE for FFY ${year}`}
+                  labelClassName="sr-only"
+                  name={`hithie-actual-federal-${year}`}
+                  value={previousActivityExpenses[year].hithie.federalActual}
+                  onChange={handleChange(year, 'hithie', 'federalActual')}
+                />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
