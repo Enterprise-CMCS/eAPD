764110e212e78415e4f238c1824a9cf9a5a1fc56
Heather Battaglia <heather.battaglia@gsa.gov>
2019-07-28 17:54:32 -0500

    wip componentDidUpdate actions

--- web/src/actions/aria.js
+++ web/src/actions/aria.js
@@ -1,4 +1,12 @@
+export const ARIA_QUEUE_ANNOUNCEMENT = 'ARIA_QUEUE_ANNOUNCEMENT';
 export const ARIA_ANNOUNCE_CHANGE = 'ARIA_ANNOUNCE_CHANGE';
+
+export const ariaQueueAnnouncement = change => dispatch => {
+    dispatch({
+        type: ARIA_QUEUE_ANNOUNCEMENT,
+        change
+    });
+}
 
 export const ariaAnnounce = message => dispatch => {
     dispatch({
--- web/src/reducers/aria.js
+++ web/src/reducers/aria.js
@@ -1,11 +1,17 @@
-import { ARIA_ANNOUNCE_CHANGE } from '../actions/aria';
+import {
+    ARIA_QUEUE_ANNOUNCEMENT,
+    ARIA_ANNOUNCE_CHANGE
+} from '../actions/aria';
 
 const initialState = {
+    ariaQueuedAnnouncement: '',
     ariaRegionMessage: ''
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case ARIA_QUEUE_ANNOUNCEMENT:
+            return { ariaQueuedAnnouncement: action.change };
         case ARIA_ANNOUNCE_CHANGE:
             return { ariaRegionMessage: action.message };
         default:
@@ -13,6 +19,7 @@ const reducer = (state = initialState, action) => {
     }
 }
 
+export const getAriaQueuedAnnouncement = (state) => state.aria.ariaQueuedAnnouncement;
 export const getAriaAnnouncement = (state) => state.aria.ariaRegionMessage;
 
 export default reducer;
