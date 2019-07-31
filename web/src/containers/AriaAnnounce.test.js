6809e3e6da28c9f31f35569e562ba08020457c29
Heather Battaglia <heather.battaglia@gsa.gov>
2019-07-31 17:52:51 -0600

    test aria snap with a message

--- web/src/containers/AriaAnnounce.test.js
+++ web/src/containers/AriaAnnounce.test.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { plain as AriaAnnounce } from './AriaAnnounce';
 
 describe('aria announce component', () => {
-    const props = {
-        ariaMessage: 'No. No. No. No.'
-    };
-
     it('renders correctly', () => {
         const component = shallow(<AriaAnnounce />);
+        expect(component).toMatchSnapshot();
+    });
+
+    it('renders a message', () => {
+        const props = {
+            ariaMessage: 'No. No. No. No.'
+        };
+        const component = shallow(<AriaAnnounce props={ props } />)
         expect(component).toMatchSnapshot();
     });
 });
