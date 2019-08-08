da25822bc8f3b28265bd641bf8a96709d2a8c973
Greg Walker <michael.walker@gsa.gov>
2019-08-08 10:26:13 -0500

    round numbers on dollar field blur

--- web/src/components/DollarField.test.js
+++ web/src/components/DollarField.test.js
@@ -5,6 +5,21 @@ import { act } from 'react-dom/test-utils';
 import DollarField from './DollarField';
 
 describe('DollarField component', () => {
+  it('renders correctly for non-numeric initial values', () => {
+    expect(
+      mount(
+        <DollarField
+          label="test label"
+          name="test name"
+          size="medium"
+          className="stuff"
+          value="abc"
+          onChange={jest.fn()}
+        />
+      )
+    ).toMatchSnapshot();
+  });
+
   it('renders correctly, does not add commas to small numbers, passing props down', () => {
     expect(
       mount(
@@ -57,6 +72,33 @@ describe('DollarField component', () => {
     component.update();
 
     expect(onChange).toHaveBeenCalledWith({ target: { value: 123456 } });
+    expect(component).toMatchSnapshot();
+  });
+
+  it('rounds numbers when the component loses focus, but still calls onBlur handler', () => {
+    const onBlur = jest.fn();
+    const onChange = jest.fn();
+
+    const component = mount(
+      <DollarField
+        label="test label"
+        name="test name"
+        size="medium"
+        className="stuff"
+        value="12332"
+        onBlur={onBlur}
+        onChange={onChange}
+      />
+    );
+
+    act(() => {
+      component.find('NumberField').prop('onBlur')({
+        target: { value: '123,456.78' }
+      });
+    });
+
+    expect(onBlur).toHaveBeenCalled();
+    expect(onChange).toHaveBeenCalledWith({ target: { value: 123457 } });
     expect(component).toMatchSnapshot();
   });
 });
