147edee3f6d1f69639b1c50de6cb291edae0ee71
Greg Walker <michael.walker@gsa.gov>
2019-09-24 13:50:13 -0500

    add actions for key personnel

--- web/src/actions/editApd.js
+++ web/src/actions/editApd.js
@@ -1,4 +1,70 @@
+import { updateBudget } from './apd';
+
+export const ADD_APD_ITEM = Symbol('add apd item');
 export const EDIT_APD = Symbol('edit apd');
+export const REMOVE_APD_ITEM = Symbol('remove apd item');
+
+export const addKeyPerson = () => ({
+  type: ADD_APD_ITEM,
+  path: '/keyPersonnel/-'
+});
+
+export const removeKeyPerson = (
+  index,
+  { global = window } = {}
+) => dispatch => {
+  if (global.confirm('Do you really want to delete this key person?')) {
+    dispatch({
+      type: REMOVE_APD_ITEM,
+      path: `/keyPersonnel/${index}`
+    });
+    dispatch(updateBudget());
+  }
+};
+
+export const setKeyPersonName = (name, index) => ({
+  type: EDIT_APD,
+  path: `/keyPersonnel/${index}/name`,
+  value: name
+});
+
+export const setKeyPersonEmail = (email, index) => ({
+  type: EDIT_APD,
+  path: `/keyPersonnel/${index}/email`,
+  value: email
+});
+
+export const setKeyPersonRole = (role, index) => ({
+  type: EDIT_APD,
+  path: `/keyPersonnel/${index}/position`,
+  value: role
+});
+
+export const setKeyPersonPercentTime = (percent, index) => ({
+  type: EDIT_APD,
+  path: `/keyPersonnel/${index}/percentTime`,
+  value: percent
+});
+
+// needs to trigger budget calculation
+export const setKeyPersonHasCosts = (hasCosts, index) => dispatch => {
+  dispatch({
+    type: EDIT_APD,
+    path: `/keyPersonnel/${index}/hasCosts`,
+    value: hasCosts
+  });
+  dispatch(updateBudget());
+};
+
+// needs to trigger budget calculation
+export const setKeyPersonCost = (cost, index, year) => dispatch => {
+  dispatch({
+    type: EDIT_APD,
+    path: `/keyPersonnel/${index}/costs/${year}`,
+    value: cost
+  });
+  dispatch(updateBudget());
+};
 
 export const setMedicaidDirectorEmail = email => {
   return {
