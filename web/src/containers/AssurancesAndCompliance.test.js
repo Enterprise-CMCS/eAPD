1aeec784e0658608ae5b636153ed44192625f749
Greg Walker <michael.walker@gsa.gov>
2019-09-30 11:01:28 -0500

    update component

--- web/src/containers/AssurancesAndCompliance.test.js
+++ web/src/containers/AssurancesAndCompliance.test.js
@@ -1,107 +1,101 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import React from 'react';
-import sinon from 'sinon';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
-import {
-  plain as AssurancesAndCompliance,
-  mapStateToProps,
-  mapDispatchToProps,
-  LinkOrText
-} from './AssurancesAndCompliance';
-import { updateApd } from '../actions/apd';
+import AssurancesAndCompliance, { LinkOrText } from './AssurancesAndCompliance';
+import { EDIT_APD } from '../actions/editApd';
+
+const mockStore = configureStore();
 
 describe('assurances and compliance component', () => {
   describe('main component', () => {
-    const sandbox = sinon.createSandbox();
-
-    const props = {
-      sections: {
-        procurement: [
-          { title: '42 CFR Part 495.348', checked: false, explanation: '' },
-          { title: 'SMM Section 11267', checked: true, explanation: '' },
-          { title: '45 CFR Part 95.615', checked: '', explanation: '' },
-          { title: '45 CFR Part 75.326', checked: true, explanation: '' }
-        ],
-        recordsAccess: [
-          {
-            title: '42 CFR Part 495.350',
-            checked: false,
-            explanation: 'some words'
-          },
-          { title: '42 CFR Part 495.352', checked: true, explanation: '' },
-          { title: '42 CFR Part 495.346', checked: true, explanation: '' },
-          {
-            title: '42 CFR Part 433.112(b)(5) - (9)',
-            checked: true,
-            explanation: ''
-          },
-          {
-            title: '45 CFR Part 95.615',
-            checked: false,
-            explanation: 'other words'
-          },
-          { title: 'SMM Section 11267', checked: true, explanation: '' }
-        ],
-        security: [
-          {
-            title: '45 CFR 164 Securities and Privacy',
-            checked: true,
-            explanation: ''
+    const state = {
+      apd: {
+        data: {
+          federalCitations: {
+            procurement: [
+              { title: '42 CFR Part 495.348', checked: true, explanation: '' },
+              { title: 'SMM Section 11267', checked: true, explanation: '' },
+              { title: '45 CFR Part 95.615', checked: '', explanation: '' },
+              { title: '45 CFR Part 75.326', checked: false, explanation: '' }
+            ],
+            recordsAccess: [
+              {
+                title: '42 CFR Part 495.350',
+                checked: false,
+                explanation: 'some words'
+              },
+              { title: '42 CFR Part 495.352', checked: true, explanation: '' },
+              { title: '42 CFR Part 495.346', checked: true, explanation: '' },
+              {
+                title: '42 CFR Part 433.112(b)(5) - (9)',
+                checked: true,
+                explanation: ''
+              },
+              {
+                title: '45 CFR Part 95.615',
+                checked: false,
+                explanation: 'other words'
+              },
+              { title: 'SMM Section 11267', checked: true, explanation: '' }
+            ],
+            security: [
+              {
+                title: '45 CFR 164 Securities and Privacy',
+                checked: true,
+                explanation: ''
+              }
+            ],
+            softwareRights: [
+              { title: '42 CFR Part 495.360', checked: true, explanation: '' },
+              { title: '45 CFR Part 95.617', checked: true, explanation: '' },
+              { title: '42 CFR Part 431.300', checked: true, explanation: '' },
+              { title: '42 CFR Part 433.112', checked: true, explanation: '' }
+            ]
           }
-        ],
-        softwareRights: [
-          { title: '42 CFR Part 495.360', checked: true, explanation: '' },
-          { title: '45 CFR Part 95.617', checked: true, explanation: '' },
-          { title: '42 CFR Part 431.300', checked: true, explanation: '' },
-          { title: '42 CFR Part 433.112', checked: true, explanation: '' }
-        ]
-      },
-      updateApd: sandbox.spy()
+        }
+      }
     };
 
+    const store = mockStore(state);
+
     beforeEach(() => {
-      sandbox.resetHistory();
+      store.clearActions();
     });
 
     test('renders correctly', () => {
-      expect(shallow(<AssurancesAndCompliance {...props} />)).toMatchSnapshot();
+      expect(
+        mount(
+          <Provider store={store}>
+            <AssurancesAndCompliance />
+          </Provider>
+        )
+      ).toMatchSnapshot();
     });
 
     test('dispatches when text is changed', () => {
-      const component = shallow(<AssurancesAndCompliance {...props} />);
+      const component = mount(
+        <Provider store={store}>
+          <AssurancesAndCompliance />
+        </Provider>
+      );
+
+      // Based on the state above, the first textarea should correspond to
+      // the 4th procurement citation. 4 = 3 because 0-based indexes.
 
       component
-        .find('ChoiceComponent')
-        .at(1) // choice 0 is yes, choice 1 is no
-        .dive()
-        .find('Choice')
-        .dive()
         .find('TextArea')
-        .simulate('change', { target: { value: 'new text' } });
+        .at(0)
+        .prop('onChange')({ target: { value: 'new text' } });
 
-      expect(
-        props.updateApd.calledWith({
-          federalCitation: { procurement: { 0: { explanation: 'new text' } } }
-        })
-      );
-    });
-
-    test('maps state to props', () => {
-      const state = {
-        apd: {
-          data: {
-            federalCitations: 'this is here'
-          }
+      expect(store.getActions()).toEqual([
+        {
+          type: EDIT_APD,
+          path: '/federalCitations/procurement/3/explanation',
+          value: 'new text'
         }
-      };
-
-      expect(mapStateToProps(state)).toEqual({
-        sections: 'this is here'
-      });
-    });
-
-    test('maps dispatch to props', () => {
-      expect(mapDispatchToProps).toEqual({ updateApd });
+      ]);
     });
   });
 
