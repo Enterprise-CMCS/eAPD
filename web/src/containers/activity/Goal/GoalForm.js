6fcfe0299fe56e4675d2766db182620b74b99c92
Greg Walker <michael.walker@gsa.gov>
2019-10-09 10:17:32 -0500

    json patchify goals

--- web/src/containers/activity/Goal/GoalForm.js
+++ web/src/containers/activity/Goal/GoalForm.js
@@ -1,53 +1,89 @@
 import { TextField } from '@cmsgov/design-system-core';
 
 import PropTypes from 'prop-types';
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useMemo } from 'react';
+import { connect } from 'react-redux';
+import { ActivityContext } from '../ActivityContext';
 import { t } from '../../../i18n';
 import TextArea from '../../../components/TextArea';
+import {
+  setGoalDescription,
+  setGoalObjective
+} from '../../../actions/editActivity';
 
 const GoalForm = ({
   item: { description, objective },
   index,
-  handleChange
-}) => (
-  <Fragment>
-    <TextField
-      autoFocus="true"
-      name="name"
-      className="data-entry-box"
-      label={t('activities.goals.description.input.label', {
-        defaultValue: ''
-      })}
-      hint={t('activities.goals.description.input.hint', {
-        defaultValue: ''
-      })}
-      value={description}
-      onChange={handleChange(index, 'description')}
-    />
+  setDescription,
+  setObjective
+}) => {
+  const { index: activityIndex } = useContext(ActivityContext);
 
-    <TextArea
-      name="milestones"
-      className="data-entry-box"
-      label={t('activities.goals.objective.input.label', {
-        defaultValue: 'Benchmarks'
-      })}
-      rows={6}
-      hint={t('activities.goals.objective.input.hint', {
-        defaultValue: ''
-      })}
-      value={objective}
-      onChange={handleChange(index, 'objective')}
-    />
-  </Fragment>
-);
+  const changeDescription = useMemo(
+    () => ({ target: { value } }) => {
+      setDescription(activityIndex, index, value);
+    },
+    [index]
+  );
+
+  const changeObjective = useMemo(
+    () => ({ target: { value } }) => {
+      setObjective(activityIndex, index, value);
+    },
+    [index]
+  );
+
+  return (
+    <Fragment>
+      <TextField
+        autoFocus="true"
+        name="name"
+        className="data-entry-box"
+        label={t('activities.goals.description.input.label', {
+          defaultValue: ''
+        })}
+        hint={t('activities.goals.description.input.hint', {
+          defaultValue: ''
+        })}
+        value={description}
+        onChange={changeDescription}
+      />
+
+      <TextArea
+        name="milestones"
+        className="data-entry-box"
+        label={t('activities.goals.objective.input.label', {
+          defaultValue: 'Benchmarks'
+        })}
+        rows={6}
+        hint={t('activities.goals.objective.input.hint', {
+          defaultValue: ''
+        })}
+        value={objective}
+        onChange={changeObjective}
+      />
+    </Fragment>
+  );
+};
 
 GoalForm.propTypes = {
-  handleChange: PropTypes.func.isRequired,
   index: PropTypes.number.isRequired,
   item: PropTypes.shape({
     description: PropTypes.string,
     objective: PropTypes.string
-  }).isRequired
+  }).isRequired,
+  setDescription: PropTypes.func.isRequired,
+  setObjective: PropTypes.func.isRequired
 };
 
-export default GoalForm;
+const mapDispatchToProps = {
+  setDescription: setGoalDescription,
+  setObjective: setGoalObjective
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(GoalForm);
+
+export { GoalForm as plain, mapDispatchToProps };
