a345eda89d085b8f367daec89b9750e914cd7a39
Greg Walker <mgwalker@users.noreply.github.com>
2019-10-10 14:36:29 -0500

    JSON patchify activity overview/summary section (#1888)
    * add actions
    
    * update overview component to use new actions

---
+++ web/src/actions/editActivity/activityList.test.js
@@ -0,0 +1,76 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { UPDATE_BUDGET } from '../budget';
+
+import { ADD_APD_ITEM, EDIT_APD, REMOVE_APD_ITEM } from '../editApd/symbols';
+
+import {
+  addActivity,
+  removeActivity,
+  setActivityName,
+  setActivityFundingSource
+} from './activityList';
+
+const mockStore = configureStore([thunk]);
+
+describe('APD activity edit actions for overview section', () => {
+  const store = mockStore('test state');
+
+  beforeEach(() => {
+    store.clearActions();
+  });
+
+  it('dispatches an action for adding an activity', () => {
+    store.dispatch(addActivity());
+
+    expect(store.getActions()).toEqual([
+      {
+        type: ADD_APD_ITEM,
+        path: '/activities/-',
+        state: 'test state'
+      },
+      { type: UPDATE_BUDGET, state: 'test state' }
+    ]);
+  });
+
+  it('dispatches an action for removing an activity if confirmed', () => {
+    const global = { confirm: jest.fn().mockReturnValue(true) };
+
+    store.dispatch(removeActivity(3, { global }));
+
+    expect(store.getActions()).toEqual([
+      { type: REMOVE_APD_ITEM, path: '/activities/3' },
+      { type: UPDATE_BUDGET, state: 'test state' }
+    ]);
+  });
+
+  it('does not dispatch an action for removing an activity if denied', () => {
+    const global = { confirm: jest.fn().mockReturnValue(false) };
+
+    store.dispatch(removeActivity(3, { global }));
+
+    expect(store.getActions()).toEqual([]);
+  });
+
+  it('dispatches an action for setting an activity name', () => {
+    expect(setActivityName(53, 'new name')).toEqual({
+      type: EDIT_APD,
+      path: '/activities/53/name',
+      value: 'new name'
+    });
+  });
+
+  it('dispatches an action for setting an activity funding source', () => {
+    store.dispatch(setActivityFundingSource(13, 'funding source'));
+
+    expect(store.getActions()).toEqual([
+      {
+        type: EDIT_APD,
+        path: '/activities/13/fundingSource',
+        value: 'funding source'
+      },
+      { type: UPDATE_BUDGET, state: 'test state' }
+    ]);
+  });
+});
--- web/src/actions/editActivity/overview.js
+++ web/src/actions/editActivity/overview.js
@@ -1,52 +1,19 @@
-import { ADD_APD_ITEM, EDIT_APD, REMOVE_APD_ITEM } from '../editApd/symbols';
-import { updateBudget } from '../budget';
+import { EDIT_APD } from '../editApd/symbols';
 
-/**
- * Add a new activity to the current APD
- */
-export const addActivity = () => (dispatch, getState) => {
-  dispatch({
-    type: ADD_APD_ITEM,
-    path: '/activities/-',
-    state: getState()
-  });
-  dispatch(updateBudget());
-};
-
-/**
- * Remove an activity from the current APD
- * @param {Number} index The index of the activity to be removed
- * @param {Object} di Dependency injection object
- * @param {Object} di.global The window object, which provides window.confirm
- */
-export const removeActivity = (index, { global = window } = {}) => dispatch => {
-  if (global.confirm('Do you really want to delete this activity?')) {
-    dispatch({ type: REMOVE_APD_ITEM, path: `/activities/${index}` });
-    dispatch(updateBudget());
-  }
-};
-
-/**
- * Rename an activity
- * @param {Number} index The index of the activity to rename
- * @param {String} name The new activity name
- */
-export const setActivityName = (index, name) => ({
+export const setActivityOverview = (activityIndex, overview) => ({
   type: EDIT_APD,
-  path: `/activities/${index}/name`,
-  value: name
+  path: `/activities/${activityIndex}/summary`,
+  value: overview
 });
 
-/**
- * Change an activity's Medicaid program funding source
- * @param {Number} index The index of the activity to change
- * @param {String} source The funding source to switch to - 'HIE', 'HIT', or 'MMIS'
- */
-export const setActivityFundingSource = (index, source) => dispatch => {
-  dispatch({
-    type: EDIT_APD,
-    path: `/activities/${index}/fundingSource`,
-    value: source
-  });
-  dispatch(updateBudget());
-};
+export const setActivityDescription = (activityIndex, description) => ({
+  type: EDIT_APD,
+  path: `/activities/${activityIndex}/description`,
+  value: description
+});
+
+export const setActivityAlternatives = (activityIndex, alternatives) => ({
+  type: EDIT_APD,
+  path: `/activities/${activityIndex}/alternatives`,
+  value: alternatives
+});
