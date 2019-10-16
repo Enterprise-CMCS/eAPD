2b59673d782203c0af7374d82ae2d8c12d899bff
Greg Walker <michael.walker@gsa.gov>
2019-10-16 09:57:42 -0500

    actions

--- web/src/actions/editActivity/costAllocate.js
+++ web/src/actions/editActivity/costAllocate.js
@@ -17,28 +17,58 @@ export const setCostAllocationFFPFundingSplit = (
   index,
   year,
   federal,
-  state) => dispatch => {
-    dispatch({
-      type: EDIT_APD,
-      path: `/activities/${index}/costAllocation/${year}/ffp/federal`,
-      value: federal
-    });
-    dispatch({
-      type: EDIT_APD,
-      path: `/activities/${index}/costAllocation/${year}/ffp/state`,
-      value: state
-    });
-    dispatch(updateBudget());
-  };
+  state
+) => dispatch => {
+  dispatch({
+    type: EDIT_APD,
+    path: `/activities/${index}/costAllocation/${year}/ffp/federal`,
+    value: federal
+  });
+  dispatch({
+    type: EDIT_APD,
+    path: `/activities/${index}/costAllocation/${year}/ffp/state`,
+    value: state
+  });
+  dispatch(updateBudget());
+};
 
 export const setCostAllocationFFPOtherFunding = (
   index,
   year,
-  otherFunding) => dispatch => {
-    dispatch({
-      type: EDIT_APD,
-      path: `/activities/${index}/costAllocation/${year}/other`,
-      value: otherFunding
-    });
-    dispatch(updateBudget());
-  };
+  otherFunding
+) => dispatch => {
+  dispatch({
+    type: EDIT_APD,
+    path: `/activities/${index}/costAllocation/${year}/other`,
+    value: otherFunding
+  });
+  dispatch(updateBudget());
+};
+
+export const setFFPForInHouseCostsForFiscalQuarter = (
+  activityIndex,
+  year,
+  quarter,
+  ffpPercent
+) => dispatch => {
+  dispatch({
+    type: EDIT_APD,
+    path: `/activities/${activityIndex}/quarterlyFFP/${year}/${quarter}/state`,
+    value: ffpPercent
+  });
+  dispatch(updateBudget());
+};
+
+export const setFFPForContractorCostsForFiscalQuarter = (
+  activityIndex,
+  year,
+  quarter,
+  ffpPercent
+) => dispatch => {
+  dispatch({
+    type: EDIT_APD,
+    path: `/activities/${activityIndex}/quarterlyFFP/${year}/${quarter}/contractors`,
+    value: ffpPercent
+  });
+  dispatch(updateBudget());
+};
