57d0a7c3632772b210523409aa792c279a65101d
Greg Walker <michael.walker@gsa.gov>
2019-11-18 09:22:51 -0600

    executive summary

--- web/src/containers/ExecutiveSummaryBudget.js
+++ web/src/containers/ExecutiveSummaryBudget.js
@@ -1,87 +1,101 @@
-import PropTypes from 'prop-types';
-import React, { Fragment } from 'react';
-import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
 
-import Dollars from '../components/Dollars';
-import { t } from '../i18n';
+import Dollars from "../components/Dollars";
+import { t } from "../i18n";
 
 const thId = (program, share) =>
-  `program-budget-table-${program}${share ? `-${share}` : ''}`;
+  `program-budget-table-${program}${share ? `-${share}` : ""}`;
 const tdHdrs = (program, share) =>
   `program-budget-table-${program} program-budget-table-${program}-${share}`;
 
-const DollarCell = ({ headers, value }) => (
+const DollarCell = ({ headers, long, value }) => (
   <td className="budget-table--number" headers={headers}>
-    <Dollars>{value}</Dollars>
+    <Dollars long={long}>{value}</Dollars>
   </td>
 );
 
 DollarCell.propTypes = {
   headers: PropTypes.string,
+  long: PropTypes.bool,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired
 };
 
-DollarCell.defaultProps = { headers: '' };
+DollarCell.defaultProps = { headers: "", long: false };
 
-const ExecutiveSummaryBudget = ({ budget }) => {
+const ExecutiveSummaryBudget = ({ budget, isViewOnly }) => {
   const { hit, hie, hitAndHie, mmisByFFP, years } = budget;
 
   if (!years.length) return null;
 
   const rowKeys = [
-    ...years.map(year => ({ year, display: t('ffy', { year }) })),
-    { year: 'total', display: 'Total' }
+    ...years.map(year => ({ year, display: t("ffy", { year }) })),
+    { year: "total", display: "Total" }
   ];
 
   return (
     <Fragment>
       <table className="budget-table">
         <caption className="ds-h4">
-          HIT + HIE <span className="ds-u-visibility--screen-reader">executive summary</span>
+          HIT + HIE{" "}
+          <span className="ds-u-visibility--screen-reader">
+            executive summary
+          </span>
         </caption>
         <thead>
           <tr>
             <th id="program-budget-table-null1" />
-            <th colSpan="2" id={thId('hit')}>
-              {t('executiveSummary.budgetTable.hit')}
+            <th colSpan="2" id={thId("hit")}>
+              {t("executiveSummary.budgetTable.hit")}
             </th>
-            <th colSpan="2" id={thId('hie')}>
-              {t('executiveSummary.budgetTable.hie')}
+            <th colSpan="2" id={thId("hie")}>
+              {t("executiveSummary.budgetTable.hie")}
             </th>
-            <th colSpan="3" id={thId('combined')}>
-              {t('executiveSummary.budgetTable.hitHie')}
+            <th colSpan="3" id={thId("combined")}>
+              {t("executiveSummary.budgetTable.hitHie")}
             </th>
           </tr>
           <tr>
             <th id="program-budget-table-null2" />
-            <th className="ds-u-text-align--right" id={thId('hit', 'fed')}>
-              {t('executiveSummary.budgetTable.fedShare')}
+            <th className="ds-u-text-align--right" id={thId("hit", "fed")}>
+              {t("executiveSummary.budgetTable.fedShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('hit', 'state')}>
-              {t('executiveSummary.budgetTable.stateShare')}
+            <th className="ds-u-text-align--right" id={thId("hit", "state")}>
+              {t("executiveSummary.budgetTable.stateShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('hie', 'fed')}>
-              {t('executiveSummary.budgetTable.fedShare')}
+            <th className="ds-u-text-align--right" id={thId("hie", "fed")}>
+              {t("executiveSummary.budgetTable.fedShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('hie', 'state')}>
-              {t('executiveSummary.budgetTable.stateShare')}
+            <th className="ds-u-text-align--right" id={thId("hie", "state")}>
+              {t("executiveSummary.budgetTable.stateShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('combined', 'fed')}>
-              {t('executiveSummary.budgetTable.fedShare')}
+            <th className="ds-u-text-align--right" id={thId("combined", "fed")}>
+              {t("executiveSummary.budgetTable.fedShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('combined', 'state')}>
-              {t('executiveSummary.budgetTable.stateShare')}
+            <th
+              className="ds-u-text-align--right"
+              id={thId("combined", "state")}
+            >
+              {t("executiveSummary.budgetTable.stateShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('combined', 'total')}>
-              {t('executiveSummary.budgetTable.grandTotal')}
+            <th
+              className="ds-u-text-align--right"
+              id={thId("combined", "total")}
+            >
+              {t("executiveSummary.budgetTable.grandTotal")}
             </th>
           </tr>
         </thead>
         <tbody>
-         {rowKeys.map(({ year, display }) => (
+          {rowKeys.map(({ year, display }) => (
             <tr
               key={year}
-              className={display==='Total' ? 'budget-table--total budget-table--row__highlight' : ''}
+              className={
+                display === "Total"
+                  ? "budget-table--total budget-table--row__highlight"
+                  : ""
+              }
             >
               <th
                 headers="program-budget-table-null1 program-budget-table-null2"
@@ -91,31 +105,38 @@ const ExecutiveSummaryBudget = ({ budget }) => {
               </th>
               <DollarCell
                 value={hit.combined[year].federal}
-                headers={tdHdrs('hit', 'fed')}
+                long={isViewOnly}
+                headers={tdHdrs("hit", "fed")}
               />
               <DollarCell
                 value={hit.combined[year].state}
-                headers={tdHdrs('hit', 'state')}
+                long={isViewOnly}
+                headers={tdHdrs("hit", "state")}
               />
               <DollarCell
                 value={hie.combined[year].federal}
-                headers={tdHdrs('hie', 'fed')}
+                long={isViewOnly}
+                headers={tdHdrs("hie", "fed")}
               />
               <DollarCell
                 value={hie.combined[year].state}
-                headers={tdHdrs('hie', 'state')}
+                long={isViewOnly}
+                headers={tdHdrs("hie", "state")}
               />
               <DollarCell
                 value={hitAndHie.combined[year].federal}
-                headers={tdHdrs('combined', 'fed')}
+                long={isViewOnly}
+                headers={tdHdrs("combined", "fed")}
               />
               <DollarCell
                 value={hitAndHie.combined[year].state}
-                headers={tdHdrs('combined', 'state')}
+                long={isViewOnly}
+                headers={tdHdrs("combined", "state")}
               />
               <DollarCell
                 value={hitAndHie.combined[year].medicaid}
-                headers={tdHdrs('combined', 'total')}
+                long={isViewOnly}
+                headers={tdHdrs("combined", "total")}
               />
             </tr>
           ))}
@@ -124,52 +145,64 @@ const ExecutiveSummaryBudget = ({ budget }) => {
 
       <table className="budget-table">
         <caption className="ds-h4">
-          MMIS <span className="ds-u-visibility--screen-reader">executive summary</span>
+          MMIS{" "}
+          <span className="ds-u-visibility--screen-reader">
+            executive summary
+          </span>
         </caption>
         <thead>
           <tr>
             <th id="program-budget-table-null3" />
-            <th colSpan="2" id={thId('mmis90')}>
-              {t('executiveSummary.budgetTable.mmis90')}
+            <th colSpan="2" id={thId("mmis90")}>
+              {t("executiveSummary.budgetTable.mmis90")}
             </th>
-            <th colSpan="2" id={thId('mmis75')}>
-              {t('executiveSummary.budgetTable.mmis75')}
+            <th colSpan="2" id={thId("mmis75")}>
+              {t("executiveSummary.budgetTable.mmis75")}
             </th>
-            <th colSpan="2" id={thId('mmis50')}>
-              {t('executiveSummary.budgetTable.mmis50')}
+            <th colSpan="2" id={thId("mmis50")}>
+              {t("executiveSummary.budgetTable.mmis50")}
             </th>
-            <th colSpan="3" id={thId('mmisTotal')}>
-              {t('executiveSummary.budgetTable.mmisTotal')}
+            <th colSpan="3" id={thId("mmisTotal")}>
+              {t("executiveSummary.budgetTable.mmisTotal")}
             </th>
           </tr>
           <tr>
             <th id="program-budget-table-null4" />
-            <th className="ds-u-text-align--right" id={thId('mmis90', 'fed')}>
-              {t('executiveSummary.budgetTable.fedShare')}
+            <th className="ds-u-text-align--right" id={thId("mmis90", "fed")}>
+              {t("executiveSummary.budgetTable.fedShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('mmis90', 'state')}>
-              {t('executiveSummary.budgetTable.stateShare')}
+            <th className="ds-u-text-align--right" id={thId("mmis90", "state")}>
+              {t("executiveSummary.budgetTable.stateShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('mmis75', 'fed')}>
-              {t('executiveSummary.budgetTable.fedShare')}
+            <th className="ds-u-text-align--right" id={thId("mmis75", "fed")}>
+              {t("executiveSummary.budgetTable.fedShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('mmis75', 'state')}>
-              {t('executiveSummary.budgetTable.stateShare')}
+            <th className="ds-u-text-align--right" id={thId("mmis75", "state")}>
+              {t("executiveSummary.budgetTable.stateShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('mmis50', 'fed')}>
-              {t('executiveSummary.budgetTable.fedShare')}
+            <th className="ds-u-text-align--right" id={thId("mmis50", "fed")}>
+              {t("executiveSummary.budgetTable.fedShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('mmis50', 'state')}>
-              {t('executiveSummary.budgetTable.stateShare')}
+            <th className="ds-u-text-align--right" id={thId("mmis50", "state")}>
+              {t("executiveSummary.budgetTable.stateShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('mmisTotal', 'fed')}>
-              {t('executiveSummary.budgetTable.fedShare')}
+            <th
+              className="ds-u-text-align--right"
+              id={thId("mmisTotal", "fed")}
+            >
+              {t("executiveSummary.budgetTable.fedShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('mmisTotal', 'state')}>
-              {t('executiveSummary.budgetTable.stateShare')}
+            <th
+              className="ds-u-text-align--right"
+              id={thId("mmisTotal", "state")}
+            >
+              {t("executiveSummary.budgetTable.stateShare")}
             </th>
-            <th className="ds-u-text-align--right" id={thId('mmisTotal', 'total')}>
-              {t('executiveSummary.budgetTable.grandTotal')}
+            <th
+              className="ds-u-text-align--right"
+              id={thId("mmisTotal", "total")}
+            >
+              {t("executiveSummary.budgetTable.grandTotal")}
             </th>
           </tr>
         </thead>
@@ -177,46 +210,62 @@ const ExecutiveSummaryBudget = ({ budget }) => {
           {rowKeys.map(({ year, display }) => (
             <tr
               key={year}
-              className={display==='Total' ? 'budget-table--total budget-table--row__highlight' : ''}
+              className={
+                display === "Total"
+                  ? "budget-table--total budget-table--row__highlight"
+                  : ""
+              }
             >
-              <th headers="program-budget-table-null3 program-budget-table-null4" scope="row">
+              <th
+                headers="program-budget-table-null3 program-budget-table-null4"
+                scope="row"
+              >
                 {display}
               </th>
               <DollarCell
-                value={mmisByFFP['90-10'][year].federal}
-                headers={tdHdrs('mmis90', 'fed')}
+                value={mmisByFFP["90-10"][year].federal}
+                long={isViewOnly}
+                headers={tdHdrs("mmis90", "fed")}
               />
               <DollarCell
-                value={mmisByFFP['90-10'][year].state}
-                headers={tdHdrs('mmis90', 'state')}
+                value={mmisByFFP["90-10"][year].state}
+                long={isViewOnly}
+                headers={tdHdrs("mmis90", "state")}
               />
               <DollarCell
-                value={mmisByFFP['75-25'][year].federal}
-                headers={tdHdrs('mmis75', 'fed')}
+                value={mmisByFFP["75-25"][year].federal}
+                long={isViewOnly}
+                headers={tdHdrs("mmis75", "fed")}
               />
               <DollarCell
-                value={mmisByFFP['75-25'][year].state}
-                headers={tdHdrs('mmis75', 'state')}
+                value={mmisByFFP["75-25"][year].state}
+                long={isViewOnly}
+                headers={tdHdrs("mmis75", "state")}
               />
               <DollarCell
-                value={mmisByFFP['50-50'][year].federal}
-                headers={tdHdrs('mmis50', 'fed')}
+                value={mmisByFFP["50-50"][year].federal}
+                long={isViewOnly}
+                headers={tdHdrs("mmis50", "fed")}
               />
               <DollarCell
-                value={mmisByFFP['50-50'][year].state}
-                headers={tdHdrs('mmis50', 'state')}
+                value={mmisByFFP["50-50"][year].state}
+                long={isViewOnly}
+                headers={tdHdrs("mmis50", "state")}
               />
               <DollarCell
                 value={mmisByFFP.combined[year].federal}
-                headers={tdHdrs('mmisTotal', 'fed')}
+                long={isViewOnly}
+                headers={tdHdrs("mmisTotal", "fed")}
               />
               <DollarCell
                 value={mmisByFFP.combined[year].state}
-                headers={tdHdrs('mmisTotal', 'state')}
+                long={isViewOnly}
+                headers={tdHdrs("mmisTotal", "state")}
               />
               <DollarCell
                 value={mmisByFFP.combined[year].medicaid}
-                headers={tdHdrs('mmisTotal', 'total')}
+                long={isViewOnly}
+                headers={tdHdrs("mmisTotal", "total")}
               />
             </tr>
           ))}
@@ -227,8 +276,11 @@ const ExecutiveSummaryBudget = ({ budget }) => {
 };
 
 ExecutiveSummaryBudget.propTypes = {
-  budget: PropTypes.object.isRequired
+  budget: PropTypes.object.isRequired,
+  isViewOnly: PropTypes.bool
 };
+
+ExecutiveSummaryBudget.defaultProps = { isViewOnly: false };
 
 const mapStateToProps = ({ budget }) => ({ budget });
 
---
+++ web/src/containers/viewOnly/ExecutiveSummary.js
@@ -0,0 +1,96 @@
+import PropTypes from "prop-types";
+import React, { Fragment, PureComponent } from "react";
+import { connect } from "react-redux";
+
+import ExecutiveSummaryBudget from "../ExecutiveSummaryBudget";
+import Dollars from "../../components/Dollars";
+import Review from "../../components/Review";
+import { t } from "../../i18n";
+
+import { selectApdYears } from "../../reducers/apd.selectors";
+import {
+  selectBudgetExecutiveSummary,
+  selectBudgetGrandTotal
+} from "../../reducers/budget.selectors";
+
+class ExecutiveSummary extends PureComponent {
+  render() {
+    const { data, total, years } = this.props;
+    return (
+      <div>
+        <h2>Executive Summary</h2>
+        <h3>Activities Summary</h3>
+        {data.map((activity, i) => (
+          <Review
+            key={activity.key}
+            heading={`Activity ${i + 1}: ${activity.name ||
+              t("activities.noNameYet")}`}
+            headingLevel={4}
+            className={i === data.length - 1 ? "ds-u-border-bottom--0" : ""}
+          >
+            {activity.summary && <p>{activity.summary}</p>}
+
+            <ul className="ds-c-list--bare">
+              <li>
+                <strong>Date:</strong> {activity.dateRange}
+              </li>
+              <li>
+                <strong>Total cost of activity:</strong>{" "}
+                <Dollars long>{activity.combined}</Dollars>
+              </li>
+              <li>
+                <strong>Medicaid share:</strong>{" "}
+                <Dollars long>{activity.medicaid}</Dollars> (
+                <Dollars long>{activity.federal}</Dollars> Federal share)
+              </li>
+            </ul>
+          </Review>
+        ))}
+        <hr className="ds-u-border--dark ds-u-margin--0" />
+        <Review
+          heading="Total cost"
+          headingLevel={4}
+          className="ds-u-border--0"
+        >
+          <ul className="ds-c-list--bare">
+            <li>
+              <strong>Federal Fiscal Years requested:</strong> FFY{" "}
+              {years.join(", ")}
+            </li>
+            <li>
+              <strong>Medicaid share:</strong>{" "}
+              <Dollars long>{total.medicaid}</Dollars> (
+              <Dollars long>{total.federal}</Dollars> Federal share)
+            </li>
+            <li>
+              <strong>Total funding request:</strong>{" "}
+              <Dollars long>{total.combined}</Dollars>
+            </li>
+          </ul>
+        </Review>
+
+        <h3>Program Budget Tables</h3>
+        <ExecutiveSummaryBudget isViewOnly />
+      </div>
+    );
+  }
+}
+
+ExecutiveSummary.propTypes = {
+  data: PropTypes.array.isRequired,
+  total: PropTypes.object.isRequired,
+  years: PropTypes.array.isRequired
+};
+
+const mapStateToProps = state => ({
+  data: selectBudgetExecutiveSummary(state),
+  total: selectBudgetGrandTotal(state),
+  years: selectApdYears(state)
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(ExecutiveSummary);
+
+export { ExecutiveSummary as plain, mapStateToProps };
