15cc609a9690ff94207fa3373cc81bad64d950e4
Heather <heather.battaglia@gsa.gov>
2019-11-18 18:05:51 -0700

    finish out activity with cost allocation and standards and conditions

--- web/src/containers/viewOnly/activities/Activity.js
+++ web/src/containers/viewOnly/activities/Activity.js
@@ -3,6 +3,8 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import Dollars from '../../../components/Dollars';
 
+import CostAllocateFFP from "../../activity/CostAllocateFFP";
+
 const Activity = ({activity}) => {
   const buildGoal = (goal) => {
     return (
@@ -55,6 +57,88 @@ const Activity = ({activity}) => {
     )
   }
 
+  const buildContractor = (contractor, index) => {
+    const contractTerm = () => {
+      if (!contractor.start && !contractor.end) {
+        return "Dates not specified";
+      } else {
+        const start = contractor.start === "" ? 'unspecified' : contractor.start;
+        const end = contractor.end === "" ? 'unspecified' : contractor.end;
+        return `${start} — ${end}`;
+      }
+    }
+
+    return (
+      <Fragment>
+        <p><strong>{index + 1}. {contractor.name}</strong>{contractor.hourly.useHourly === true && ' (hourly resource)'}</p>
+        <p>{contractor.description}</p>
+        <p><strong>Contract term: </strong>{contractTerm()}</p>
+        <ul className="ds-c-list--bare">
+          {Object.entries(contractor.years).map(([year, cost ]) => (
+            <li key={year}>
+              <strong>{year} Costs:</strong> <Dollars>{cost}</Dollars> 
+              {contractor.hourly.useHourly === true &&
+                <>
+                  <p>Number of hours: {contractor.hourly.data[year].hours}</p>
+                  <p>Hourly rate: <Dollars>{contractor.hourly.data[year].rate}</Dollars></p>
+                </>
+              }
+            </li>
+          ))}
+        </ul>
+        <p><strong>Total cost: </strong><Dollars>{contractor.totalCost}</Dollars></p>
+      </Fragment>
+    )
+  }
+
+  const buildStandardsAndConditions = (key, description) => {
+        let title;
+        switch(key) {
+          case 'modularity':
+            title = 'Modularity';
+            break;
+          case 'mita':
+            title = 'Medicaid Information Technology Architecture (MITA)';
+            break;
+          case 'industryStandards':
+            title = 'Industry Standards';
+            break;
+          case 'leverage':
+            title = 'Leverage';
+            break;
+          case 'businessResults':
+            title = 'Business Results';
+            break;
+          case 'reporting':
+            title = 'Reporting';
+            break;
+          case 'interoperability':
+            title = 'Interoperability';
+            break;
+          case 'mitigationStrategy':
+            title = 'Mitigation Strategy';
+            break;
+          case 'keyPersonnel':
+            title = 'Key Personnel';
+            break;
+          case 'documentation':
+            title = 'Documentation';
+            break;
+          case 'minimizeCost':
+            title = 'Strategies to Minimize Cost and Difficulty on Alternative Hardware or Operating System';
+            break;
+          default:
+            console.log('no match found');
+        } 
+        return (
+          <Fragment>
+            <p><strong>{title}</strong></p>
+            <p>{description === '' ? 'No description provided' : description}</p>
+          </Fragment>
+        )
+      ;
+  }
+
   return (
     <Fragment>
       <h3>{activity.name}</h3>
@@ -84,9 +168,26 @@ const Activity = ({activity}) => {
       <h4>In-House Cost Categories: Non-Personnel</h4>
       {activity.expenses.map((expense, index) => buildExpense(expense, index))}
 
-      <h4>In-House Cost Categories: Private Contractor Costs</h4>
-
+      <h4>Private Contractor Costs</h4>
+      {activity.contractorResources.map((contractor, index) => buildContractor(contractor, index))}
       <hr />
+
+      <h4>Cost Allocation</h4>
+      <h5>Description of Cost Allocation Methodology</h5>
+      <div dangerouslySetInnerHTML={{__html: activity.costAllocationNarrative.methodology}} />
+      <h5>Description of Other Funding</h5>
+      <div dangerouslySetInnerHTML={{__html: activity.costAllocationNarrative.otherSources}} />
+
+      <h4>Federal Financial Participation (FFP) and Cost Allocation</h4>
+      <CostAllocateFFP
+        aKey={activity.key}
+        activityIndex={0}
+        costAllocation={activity.costAllocation}
+        isViewOnly
+      />
+  
+      <h4>Standards and Conditions</h4>
+      {Object.entries(activity.standardsAndConditions).map(([key, description]) => buildStandardsAndConditions(key, description))}
     </Fragment>
   )
 };
