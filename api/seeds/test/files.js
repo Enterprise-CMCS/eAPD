fad24e9eeae015d4afdf491b55f1e8d8d37dc323
Greg Walker <michael.walker@gsa.gov>
2019-12-04 12:40:11 -0600

    update openapi; fix test seeds

--- api/seeds/test/files.js
+++ api/seeds/test/files.js
@@ -4,15 +4,11 @@ exports.seed = async knex => {
   // 42xx IDs refer to goals
   // 43xx IDs refer to objectives
 
-  await knex('files').insert([
+  await knex('apd_files').insert([
     {
-      id: 5000,
-      key: 'download.txt',
+      apd_id: 4000,
+      id: 'bf520e38-9b63-4e8b-82c1-da8fb58f5668',
       metadata: '{"some":"metadata","in":"here"}'
-    },
-    { id: 5001, key: 'download.txt' },
-    { id: 5002, key: 'download.txt' },
-    { id: 5003, key: 'download.txt' },
-    { id: 5004, key: 'not real' }
+    }
   ]);
 };
