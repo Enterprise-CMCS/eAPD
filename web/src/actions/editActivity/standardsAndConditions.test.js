a5186acbf5711ee8e6163cfcb665cdb2a5c564b1
Greg Walker <michael.walker@gsa.gov>
2019-12-12 16:55:48 -0600

    the frontend bits

--- web/src/actions/editActivity/standardsAndConditions.test.js
+++ web/src/actions/editActivity/standardsAndConditions.test.js
@@ -1,106 +1,30 @@
 import {
-  setActivityStandardsBusinessResults,
-  setActivityStandardsDocumentation,
-  setActivityStandardsIndustryStandards,
-  setActivityStandardsInteroperability,
-  setActivityStandardsKeyPersonnel,
-  setActivityStandardsLeverage,
-  setActivityStandardsMITA,
-  setActivityStandardsMinimizeCost,
-  setActivityStandardsMitigationStrategy,
-  setActivityStandardsModularity,
-  setActivityStandardsReporting
+  setActivityStandardAndConditionDoesNotSupportExplanation,
+  setActivityStandardAndConditionSupportExplanation
 } from './standardsAndConditions';
 import { EDIT_APD } from '../editApd/symbols';
 
 describe('APD activity edit actions for activity standards and conditions section', () => {
-  it('dispatches an action for setting an activity business result standard', () => {
-    expect(setActivityStandardsBusinessResults(37, 'new result')).toEqual({
-      type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/businessResults',
-      value: 'new result'
-    });
-  });
-
-  it('dispatches an action for setting an activity documentation standard', () => {
-    expect(setActivityStandardsDocumentation(37, 'new doc')).toEqual({
-      type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/documentation',
-      value: 'new doc'
-    });
-  });
-
-  it('dispatches an action for setting an activity industry standards standard', () => {
-    expect(setActivityStandardsIndustryStandards(37, 'new standards')).toEqual({
-      type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/industryStandards',
-      value: 'new standards'
-    });
-  });
-
-  it('dispatches an action for setting an activity interoperability standard', () => {
-    expect(setActivityStandardsInteroperability(37, 'new interop')).toEqual({
-      type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/interoperability',
-      value: 'new interop'
-    });
-  });
-
-  it('dispatches an action for setting an activity key personnel standard', () => {
-    expect(setActivityStandardsKeyPersonnel(37, 'new people')).toEqual({
-      type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/keyPersonnel',
-      value: 'new people'
-    });
-  });
-
-  it('dispatches an action for setting an activity leverage standard', () => {
-    expect(setActivityStandardsLeverage(37, 'new leverage')).toEqual({
-      type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/leverage',
-      value: 'new leverage'
-    });
-  });
-
-  it('dispatches an action for setting an activity minimize-cost standard', () => {
-    expect(setActivityStandardsMinimizeCost(37, 'new minimum')).toEqual({
-      type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/minimizeCost',
-      value: 'new minimum'
-    });
-  });
-
-  it('dispatches an action for setting an activity MITA standard', () => {
-    expect(setActivityStandardsMITA(37, 'new mita')).toEqual({
-      type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/mita',
-      value: 'new mita'
-    });
-  });
-
-  it('dispatches an action for setting an activity mitigation strategy standard', () => {
+  it('dispatches an action for setting an activity explanation for not supporting the standards and conditions', () => {
     expect(
-      setActivityStandardsMitigationStrategy(37, 'new mitigation')
+      setActivityStandardAndConditionDoesNotSupportExplanation(
+        37,
+        'new non-support'
+      )
     ).toEqual({
       type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/mitigationStrategy',
-      value: 'new mitigation'
+      path: '/activities/37/standardsAndConditions/doesNotSupport',
+      value: 'new non-support'
     });
   });
 
-  it('dispatches an action for setting an activity modularity standard', () => {
-    expect(setActivityStandardsModularity(37, 'new modules')).toEqual({
+  it('dispatches an action for setting an activity explanation for supporting the standards and conditions', () => {
+    expect(
+      setActivityStandardAndConditionSupportExplanation(37, 'new support')
+    ).toEqual({
       type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/modularity',
-      value: 'new modules'
-    });
-  });
-
-  it('dispatches an action for setting an activity reporting standard', () => {
-    expect(setActivityStandardsReporting(37, 'new report')).toEqual({
-      type: EDIT_APD,
-      path: '/activities/37/standardsAndConditions/reporting',
-      value: 'new report'
+      path: '/activities/37/standardsAndConditions/supports',
+      value: 'new support'
     });
   });
 });
--- web/src/containers/activity/StandardsAndConditions.js
+++ web/src/containers/activity/StandardsAndConditions.js
@@ -3,227 +3,64 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import {
-  setActivityStandardsBusinessResults,
-  setActivityStandardsDocumentation,
-  setActivityStandardsIndustryStandards,
-  setActivityStandardsInteroperability,
-  setActivityStandardsKeyPersonnel,
-  setActivityStandardsLeverage,
-  setActivityStandardsMinimizeCost,
-  setActivityStandardsMITA,
-  setActivityStandardsMitigationStrategy,
-  setActivityStandardsModularity,
-  setActivityStandardsReporting
+  setActivityStandardAndConditionDoesNotSupportExplanation,
+  setActivityStandardAndConditionSupportExplanation
 } from '../../actions/editActivity';
 
 import RichText from '../../components/RichText';
-import Instruction from '../../components/Instruction';
 import { Subsection } from '../../components/Section';
+import TextArea from '../../components/TextArea';
 import { selectActivityByIndex } from '../../reducers/activities.selectors';
 
 const StandardsAndConditions = ({
   activity,
   activityIndex,
-  setBusinessResults,
-  setDocumentation,
-  setIndustryStandards,
-  setInteroperability,
-  setKeyPersonnel,
-  setLeverage,
-  setMinimizeCost,
-  setMITA,
-  setMitigationStrategy,
-  setModularity,
-  setReporting
-}) => {
-  const getHandler = action => html => {
-    action(activityIndex, html);
-  };
+  setDoesNotSupport,
+  setSupport
+}) => (
+  <Subsection resource="activities.standardsAndConditions" nested>
+    <div className="ds-u-margin-bottom--6">
+      <h5 className="ds-h5 ds-u-margin-bottom--3">
+        Include a description about how this activity will support the Medicaid
+        standards and conditions{' '}
+        <a href="https://www.ecfr.gov/cgi-bin/text-idx?node=se42.4.433_1112">
+          42 CFR 433.112
+        </a>
+        .
+      </h5>
 
-  return (
-    <Subsection resource="activities.standardsAndConditions" nested>
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.modularity.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.modularity}
-          onSync={getHandler(setModularity)}
-          editorClassName="rte-textarea-l"
+      <RichText
+        content={activity.standardsAndConditions.supports}
+        onSync={html => setSupport(activityIndex, html)}
+        editorClassName="rte-textarea-1"
+      />
+
+      <div className="ds-c-choice__checkedChild ds-u-margin-top--3">
+        <h5 className="ds-h5">
+          If this activity does not support the Medicaid standards and
+          conditions, please explain.
+        </h5>
+
+        <TextArea
+          label=""
+          name="activity-set-standards-and-conditions-non-support"
+          onChange={({ target: { value } }) =>
+            setDoesNotSupport(activityIndex, value)
+          }
+          rows={6}
+          style={{ maxWidth: 'initial' }}
+          value={activity.standardsAndConditions.doesNotSupport}
         />
       </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.mita.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.mita}
-          onSync={getHandler(setMITA)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.industryStandards.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.industryStandards}
-          onSync={getHandler(setIndustryStandards)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.leverage.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.leverage}
-          onSync={getHandler(setLeverage)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.businessResults.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.businessResults}
-          onSync={getHandler(setBusinessResults)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.reporting.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.reporting}
-          onSync={getHandler(setReporting)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.interoperability.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.interoperability}
-          onSync={getHandler(setInteroperability)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.mitigationStrategy.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.mitigationStrategy}
-          onSync={getHandler(setMitigationStrategy)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.keyPersonnel.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.keyPersonnel}
-          onSync={getHandler(setKeyPersonnel)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.documentation.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.documentation}
-          onSync={getHandler(setDocumentation)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-
-      <div className="ds-u-margin-bottom--6">
-        <Instruction
-          source="activities.standardsAndConditions.minimizeCost.instruction"
-          headingDisplay={{
-            level: 'h6',
-            className: 'ds-h5'
-          }}
-        />
-        <RichText
-          content={activity.standardsAndConditions.minimizeCost}
-          onSync={getHandler(setMinimizeCost)}
-          editorClassName="rte-textarea-l"
-        />
-      </div>
-    </Subsection>
-  );
-};
+    </div>
+  </Subsection>
+);
 
 StandardsAndConditions.propTypes = {
   activity: PropTypes.object.isRequired,
   activityIndex: PropTypes.number.isRequired,
-  setBusinessResults: PropTypes.func.isRequired,
-  setDocumentation: PropTypes.func.isRequired,
-  setIndustryStandards: PropTypes.func.isRequired,
-  setInteroperability: PropTypes.func.isRequired,
-  setKeyPersonnel: PropTypes.func.isRequired,
-  setLeverage: PropTypes.func.isRequired,
-  setMinimizeCost: PropTypes.func.isRequired,
-  setMITA: PropTypes.func.isRequired,
-  setMitigationStrategy: PropTypes.func.isRequired,
-  setModularity: PropTypes.func.isRequired,
-  setReporting: PropTypes.func.isRequired
+  setSupport: PropTypes.func.isRequired,
+  setDoesNotSupport: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state, props) => ({
@@ -231,17 +68,8 @@ const mapStateToProps = (state, props) => ({
 });
 
 const mapDispatchToProps = {
-  setBusinessResults: setActivityStandardsBusinessResults,
-  setDocumentation: setActivityStandardsDocumentation,
-  setIndustryStandards: setActivityStandardsIndustryStandards,
-  setInteroperability: setActivityStandardsInteroperability,
-  setKeyPersonnel: setActivityStandardsKeyPersonnel,
-  setLeverage: setActivityStandardsLeverage,
-  setMinimizeCost: setActivityStandardsMinimizeCost,
-  setMITA: setActivityStandardsMITA,
-  setMitigationStrategy: setActivityStandardsMitigationStrategy,
-  setModularity: setActivityStandardsModularity,
-  setReporting: setActivityStandardsReporting
+  setDoesNotSupport: setActivityStandardAndConditionDoesNotSupportExplanation,
+  setSupport: setActivityStandardAndConditionSupportExplanation
 };
 
 export default connect(
