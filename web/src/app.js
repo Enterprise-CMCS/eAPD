cdb26510c0056de4b8a7a1fb3c9ef8d8100130e9
Greg Walker <mgwalker@gmail.com>
2019-12-16 16:42:42 -0600

    enable this in prod too, how about?

--- web/src/app.js
+++ web/src/app.js
@@ -1,23 +1,24 @@
-import "@babel/polyfill";
+import '@babel/polyfill';
 
-import { createBrowserHistory } from "history";
-import React from "react";
-import { render } from "react-dom";
-import { routerMiddleware } from "connected-react-router";
-import { createStore, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger"; // eslint-disable-line import/no-extraneous-dependencies
-import thunk from "redux-thunk";
+import { createBrowserHistory } from 'history';
+import React from 'react';
+import { render } from 'react-dom';
+import { routerMiddleware } from 'connected-react-router';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger'; // eslint-disable-line import/no-extraneous-dependencies
+import thunk from 'redux-thunk';
 
-import { initI18n } from "./i18n";
-import reducer from "./reducers";
-import Root from "./components/Root";
-import { browserIsRed } from "./util/browser";
-import { html } from "./components/UpgradeBrowser";
+import { initI18n } from './i18n';
+import reducer from './reducers';
+import Root from './components/Root';
+import saveMiddleware from './saveMiddleware';
+import { browserIsRed } from './util/browser';
+import { html } from './components/UpgradeBrowser';
 
 if (browserIsRed) {
   // For browsers we don't support at all, use native DOM APIs to add the
   // warning box since we can't be certain that React will work.
-  document.getElementById("app").innerHTML = `
+  document.getElementById('app').innerHTML = `
   <div style="margin: 30px;">
     <div class="ds-col-4 ds-c-alert ds-c-alert--error">${html}</div>
   </div>`;
@@ -29,8 +30,8 @@ if (browserIsRed) {
 
   const history = createBrowserHistory();
 
-  const middleware = [thunk, routerMiddleware(history)];
-  if (process.env.NODE_ENV !== "production") {
+  const middleware = [thunk, routerMiddleware(history), saveMiddleware];
+  if (process.env.NODE_ENV !== 'production') {
     middleware.push(createLogger());
   }
 
@@ -38,6 +39,6 @@ if (browserIsRed) {
 
   render(
     <Root history={history} store={store} />,
-    document.getElementById("app")
+    document.getElementById('app')
   );
 }
