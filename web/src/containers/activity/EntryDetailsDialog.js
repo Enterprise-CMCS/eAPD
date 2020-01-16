1edeff51557c5c1d1ef5c7aba32ea1fcf8a9f2ba
Heather <heather.battaglia@gsa.gov>
2020-01-16 15:11:34 -0700

    fix up tests

--- web/src/containers/activity/EntryDetailsDialog.js
+++ web/src/containers/activity/EntryDetailsDialog.js
@@ -1,6 +1,6 @@
 import { Dialog, Tabs, TabPanel } from '@cmsgov/design-system-core';
 
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import ContractorResources from './ContractorResources';
@@ -13,56 +13,52 @@ import StandardsAndConditions from './StandardsAndConditions';
 
 const ActivityDialog = props => {
   const { title, activityIndex, closeModal } = props;
-  const [showModal, setShowModal] = useState(true);
 
   const handleOnExit = () => {
-    setShowModal(false);
     closeModal();
   };
 
   return (
-    showModal && (
-      <Dialog
-        ariaCloseLabel={`Close modal for ${title}`}
-        className="ds-c-dialog--full"
-        closeButtonVariation="transparent"
-        closeText="Save and close"
-        onExit={() => handleOnExit()}
-        title={title}
-      >
-        <Tabs>
-          <TabPanel
-            id={`#activity-overview-${activityIndex}`}
-            tab="Activity overview"
-          >
-            <Overview activityIndex={activityIndex} />
-            <StandardsAndConditions activityIndex={activityIndex} />
-          </TabPanel>
-          <TabPanel id={`#activity-goals-${activityIndex}`} tab="Goals">
-            <Goals activityIndex={activityIndex} />
-            <Schedule activityIndex={activityIndex} />
-          </TabPanel>
-          <TabPanel
-            id={`#activity-cost-categories-${activityIndex}`}
-            tab="In-house cost categories"
-          >
-            <Costs activityIndex={activityIndex} />
-          </TabPanel>
-          <TabPanel
-            id={`#activity-contractor-costs-${activityIndex}`}
-            tab="Private contractor costs"
-          >
-            <ContractorResources activityIndex={activityIndex} />
-          </TabPanel>
-          <TabPanel
-            id={`#activity-cost-allocation-${activityIndex}`}
-            tab="Cost allocation"
-          >
-            <CostAllocate activityIndex={activityIndex} />
-          </TabPanel>
-        </Tabs>
-      </Dialog>
-    )
+    <Dialog
+      ariaCloseLabel={`Close modal for ${title}`}
+      className="ds-c-dialog--full"
+      closeButtonVariation="transparent"
+      closeText="Save and close"
+      onExit={() => handleOnExit()}
+      title={title}
+    >
+      <Tabs>
+        <TabPanel
+          id={`activity-overview-${activityIndex}-tab`}
+          tab="Activity overview"
+        >
+          <Overview activityIndex={activityIndex} />
+          <StandardsAndConditions activityIndex={activityIndex} />
+        </TabPanel>
+        <TabPanel id={`activity-goals-${activityIndex}-tab`} tab="Goals">
+          <Goals activityIndex={activityIndex} />
+          <Schedule activityIndex={activityIndex} />
+        </TabPanel>
+        <TabPanel
+          id={`activity-cost-categories-${activityIndex}-tab`}
+          tab="In-house cost categories"
+        >
+          <Costs activityIndex={activityIndex} />
+        </TabPanel>
+        <TabPanel
+          id={`activity-contractor-costs-${activityIndex}-tab`}
+          tab="Private contractor costs"
+        >
+          <ContractorResources activityIndex={activityIndex} />
+        </TabPanel>
+        <TabPanel
+          id={`activity-cost-allocation-${activityIndex}-tab`}
+          tab="Cost allocation"
+        >
+          <CostAllocate activityIndex={activityIndex} />
+        </TabPanel>
+      </Tabs>
+    </Dialog>
   );
 };
 
