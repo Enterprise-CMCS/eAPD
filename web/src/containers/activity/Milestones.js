259400e23c6f04be6cd3997061a303b9b13832af
Greg Walker <michael.walker@gsa.gov>
2020-01-24 15:46:42 -0600

    move milestones into their own component

---
+++ web/src/containers/activity/Milestones.js
@@ -0,0 +1,66 @@
+import PropTypes from 'prop-types';
+import React, { Fragment, useCallback } from 'react';
+import { connect } from 'react-redux';
+
+import FormAndReviewList from '../../components/FormAndReviewList';
+import { MilestoneForm, MilestoneReview } from './Milestone';
+
+import { addMilestone, removeMilestone } from '../../actions/editActivity';
+import { Subsection } from '../../components/Section';
+import { t } from '../../i18n';
+import { selectActivityByIndex } from '../../reducers/activities.selectors';
+
+const Milestone = ({ activity, activityIndex, add, remove }) => {
+  const handleAdd = useCallback(() => {
+    add(activityIndex);
+  });
+
+  const handleDelete = useCallback(key => {
+    activity.schedule.forEach(({ key: milestoneKey }, i) => {
+      if (milestoneKey === key) {
+        remove(activityIndex, i);
+      }
+    });
+  });
+
+  return (
+    <Subsection resource="activities.milestones">
+      <Fragment>
+        <div className="mb3">
+          <hr />
+
+          <FormAndReviewList
+            activityIndex={activityIndex}
+            addButtonText={t('activities.milestones.addMilestoneButtonText')}
+            list={activity.schedule}
+            collapsed={MilestoneReview}
+            expanded={MilestoneForm}
+            noDataMessage={t('activities.milestones.noMilestonesNotice')}
+            onAddClick={handleAdd}
+            onDeleteClick={handleDelete}
+          />
+        </div>
+      </Fragment>
+    </Subsection>
+  );
+};
+
+Milestone.propTypes = {
+  activity: PropTypes.object.isRequired,
+  activityIndex: PropTypes.number.isRequired,
+  add: PropTypes.func.isRequired,
+  remove: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state, { activityIndex }) => ({
+  activity: selectActivityByIndex(state, { activityIndex })
+});
+
+const mapDispatchToProps = {
+  add: addMilestone,
+  remove: removeMilestone
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Milestone);
+
+export { Milestone as plain, mapStateToProps, mapDispatchToProps };
