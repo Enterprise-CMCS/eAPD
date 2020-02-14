e6746161ab118d27258a9040d18a27e4043fe59e
Greg Walker <michael.walker@gsa.gov>
2020-02-14 16:49:00 -0600

    update deps; fix lint

--- web/src/containers/activity/CostAllocate.js
+++ web/src/containers/activity/CostAllocate.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CostAllocateFFP from './CostAllocateFFP';
 import {
   setCostAllocationMethodology,
   setCostAllocationOtherFunding
