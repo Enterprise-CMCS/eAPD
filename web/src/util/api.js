36f0126bfd0a36835443e1692a2e704968e7c7ec
Greg Walker <michael.walker@gsa.gov>
2020-02-19 14:56:00 -0600

    action test 1

--- web/src/util/api.js
+++ web/src/util/api.js
@@ -1,6 +1,6 @@
-import axiosClient from 'axios';
-
 export const apiUrl = process.env.API_URL || 'http://localhost:8000';
+
+import axiosClient from 'axios';
 
 const axios = axiosClient.create({
   baseURL: apiUrl,
