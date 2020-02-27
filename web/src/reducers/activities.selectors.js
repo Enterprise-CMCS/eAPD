d981864d2238dcb27927e6275ce5b1f6eeebdbc4
Greg Walker <michael.walker@gsa.gov>
2020-02-27 11:42:27 -0600

    copy a bunch of stuff from the prototype; hook up the data; add some tests

--- web/src/reducers/activities.selectors.js
+++ web/src/reducers/activities.selectors.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect';
-import { selectApdData } from './apd.selectors';
+import { selectApdData, selectApdYears } from './apd.selectors';
 import { stateDateRangeToDisplay, stateDateToDisplay } from '../util';
 
 export const selectActivityByIndex = (
@@ -30,31 +30,131 @@ export const selectAllActivities = ({
 const selectBudgetForActivity = ({ budget }, { aKey }) =>
   budget.activities[aKey];
 
-export const makeSelectCostAllocateFFP = () =>
-  createSelector(
-    [selectActivityByIndex, selectBudgetForActivity],
-    (activity, budget) => {
-      const { costAllocation } = activity;
-      const { costsByFFY } = budget;
+export const selectCostAllocationForActivityByIndex = createSelector(
+  selectActivityByIndex,
+  ({ costAllocation }) => costAllocation
+);
 
-      const byYearData = Object.entries(costsByFFY)
-        .filter(([year]) => year !== 'total')
-        .map(([year, costs]) => {
-          const { ffp } = costAllocation[year];
-          const ffpSelectVal = `${ffp.federal}-${ffp.state}`;
+export const selectActivityCostSummary = createSelector(
+  selectApdYears,
+  selectActivityByIndex,
+  (
+    // This intermediate selector gets the budget for an activity by index
+    // rather than by key.
+    {
+      apd: {
+        data: { activities }
+      },
+      budget
+    },
+    { activityIndex }
+  ) => budget.activities[activities[activityIndex].key],
+  (
+    // This intermediate selector maps key personnel into the same data
+    // structure as activity state personnel IF AND ONLY IF we are working
+    // on the first activity. Key personnel get rolled into the first activity,
+    // Program Administration. For other activities, just returns empty arrays.
+    {
+      apd: {
+        data: { keyPersonnel, years }
+      }
+    },
+    { activityIndex }
+  ) =>
+    years.reduce(
+      (o, ffy) => ({
+        ...o,
+        [ffy]:
+          activityIndex === 0
+            ? keyPersonnel.map(kp => ({
+                description: `${kp.name} (APD Key Personnel)`,
+                totalCost: kp.hasCosts ? kp.costs[ffy] : 0,
+                unitCost: null,
+                units: `${kp.percentTime}% time`
+              }))
+            : []
+      }),
+      []
+    ),
+  (years, activity, budget, keyPersonnel) => {
+    const total = {
+      federalShare: 0,
+      medicaidShare: 0,
+      otherFunding: 0,
+      stateShare: 0,
+      totalCost: 0
+    };
 
-          return {
-            year,
-            total: costs.total,
-            medicaidShare: costs.medicaidShare,
-            ffpSelectVal,
-            allocations: { federal: costs.federal, state: costs.state }
-          };
-        });
+    const summary = years.reduce(
+      (o, year) => ({
+        ...o,
+        [year]: {
+          contractorResources: activity.contractorResources.map(c => ({
+            description: c.name,
+            totalCost: c.years[year],
+            unitCost: c.hourly.useHourly ? c.hourly.data[year].rate : null,
+            units: c.hourly.useHourly
+              ? `${c.hourly.data[year].hours} hours`
+              : null
+          })),
+          contractorResourcesTotal: activity.contractorResources.reduce(
+            (sum, contractor) => sum + contractor.years[year],
+            0
+          ),
+          federalPercent: 0,
+          federalShare: budget.costsByFFY[year].federal,
+          keyPersonnel: keyPersonnel[year],
+          medicaidShare: budget.costsByFFY[year].medicaidShare,
+          nonPersonnel: activity.expenses.map(e => ({
+            description: e.category,
+            totalCost: e.years[year],
+            unitCost: null,
+            units: null
+          })),
+          nonPersonnelTotal: activity.expenses.reduce(
+            (sum, expense) => sum + expense.years[year],
+            0
+          ),
+          otherFunding: activity.costAllocation[year].other,
+          statePercent: 0,
+          statePersonnel: activity.statePersonnel.map(p => ({
+            description: p.title,
+            totalCost: p.years[year].amt * p.years[year].perc,
+            unitCost: p.years[year].amt,
+            units: `${p.years[year].perc} FTE`
+          })),
+          statePersonnelTotal:
+            activity.statePersonnel.reduce(
+              (sum, personnel) =>
+                sum + personnel.years[year].amt * personnel.years[year].perc,
+              0
+            ) + keyPersonnel[year].reduce((sum, kp) => sum + kp.totalCost, 0),
+          stateShare: budget.costsByFFY[year].state,
+          totalCost: budget.costsByFFY[year].total
+        }
+      }),
+      {}
+    );
 
-      return { byYearData, costAllocation };
-    }
-  );
+    Object.values(summary).forEach(
+      ({
+        federalShare,
+        medicaidShare,
+        otherFunding,
+        stateShare,
+        totalCost
+      }) => {
+        total.federalShare += federalShare;
+        total.medicaidShare += medicaidShare;
+        total.otherFunding += otherFunding;
+        total.stateShare += stateShare;
+        total.totalCost += totalCost;
+      }
+    );
+
+    return { years: summary, total };
+  }
+);
 
 export const makeSelectCostAllocateFFPBudget = () =>
   createSelector([selectApdData, selectBudgetForActivity], (apd, budget) => ({
@@ -87,10 +187,31 @@ export const selectActivitySchedule = createSelector(
         });
 
         milestones.sort(({ end: endA }, { end: endB }) => {
-          if (endA === endB) {
-            return 0;
+          const [monthA, dayA, yearA] = endA.split('/');
+          const [monthB, dayB, yearB] = endB.split('/');
+
+          if (+yearA > +yearB) {
+            return 1;
           }
-          return endA > endB ? 1 : -1;
+          if (+yearA < +yearB) {
+            return -1;
+          }
+
+          if (+monthA > +monthB) {
+            return 1;
+          }
+          if (+monthA < +monthB) {
+            return -1;
+          }
+
+          if (+dayA > +dayB) {
+            return 1;
+          }
+          if (+dayA < +dayB) {
+            return -1;
+          }
+
+          return 0;
         });
       }
     );
