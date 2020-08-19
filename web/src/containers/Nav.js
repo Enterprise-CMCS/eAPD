1a0b9cda2ff40ef6a1db298f70b108bf0a09997d
Richard Davis <rdavis.bacs@gmail.com>
2020-08-19 15:25:10 -0400

    Refactor Navigation (#2389)
    * update nav on APD_ACTIVITIES_CHANGE
    * add Nav component tests
    * refactor Continue and Previous Buttons
    * add ScrollToElement component and tests
    * refactor SecondaryNav
    * refactor all the navigation!
    * fix waypoint scrolling
    * fix Continue/Previous buttons
    * fix continue/previous links on Activity List view
    * Fix 'Clicking Export and Submit skips the review step.'
    * Configure ConnectedWaypoint not to fire when window.scrollTo is called
    * Rename ScrollToElement to AutoScrollToElement
    * update waypoint ids
    * Move nav reducer, add tests
    * Refactor <AutoScrollToElement/> and <ConnectedWaypoint/> to use connected-react-router
    * Use URL to determine Nav selectedId
    * update changelog
    * remove storybook cloudfoundry app manifest
    * remove ux cloudfoundry app manifest (not using cloudfoundry)
    * remove cloudfoundry manifest file (not using cloudfoundry)

---
+++ web/src/containers/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { VerticalNav } from '@cmsgov/design-system-core';
+import { connect } from 'react-redux';
+import NavLink from '../components/NavLink';
+
+const Nav = ({ links, selectedId }) => (
+  <VerticalNav
+    component={NavLink}
+    items={links}
+    selectedId={selectedId} />
+);
+
+Nav.propTypes = {
+  links: PropTypes.array.isRequired,
+  selectedId: PropTypes.string.isRequired
+};
+
+const mapStateToProps = ({ nav }) => ({
+  links: nav.links,
+  selectedId: nav.selectedId
+});
+
+export default connect(mapStateToProps)(Nav);
+
+export { Nav as plain, NavLink, mapStateToProps };
