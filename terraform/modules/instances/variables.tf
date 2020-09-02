7b68d620d03cd6ee505d33e714a28df4238810c6
cthulhuplus <brookswm@gmail.com>
2020-09-02 13:58:31 -0400

    Cleaning up code

--- terraform/modules/instances/variables.tf
+++ terraform/modules/instances/variables.tf
@@ -1,13 +1,3 @@
-/*
-variable "eapd_jumpbox_ami" {}
-variable "eapd_jumpbox_instance_type" {}
-variable "eapd_jumpbox_key_name" {}
-variable "eapd_jumpbox_vpc_security_group_ids" {}
-variable "eapd_jumpbox_subnet_id" {}
-variable "eapd_jumpbox_associate_public_ip_address" {}
-*/
-//variable "eapd_jumpbox_tags_name" {}
-//variable "eapd_jumpbox_tags_provisioner" {}
 
 variable "eapd_jumpbox_ami" {}
 variable "eapd_jumpbox_instance_type" {}
