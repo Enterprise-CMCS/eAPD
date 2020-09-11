0cbc95d7894d9d256e664868fe810131eef64869
Richard Davis <rdavis.bacs@gmail.com>
2020-09-11 16:31:56 -0400

    refactor apd.json schema

--- api/schemas/index.js
+++ api/schemas/index.js
@@ -1,5 +1,22 @@
 const Ajv = require('ajv');
 
+// all schemas, except the base apd document schema
+const schemas = [
+  "activity.json",
+  "contractorResource.json",
+  "costAllocation.json",
+  "costAllocationNarrative.json",
+  "definitions.json",
+  "expense.json",
+  "federalCitations.json",
+  "incentivePayments.json",
+  "keyPersonnel.json",
+  "objective.json",
+  "person.json",
+  "previousActivityExpenses.json",
+  "stateProfile.json"
+].map(schema => require(`./${schema}`));
+
 const apdSchema = require('./apd.json');
 
 const ajv = new Ajv({
@@ -9,6 +26,8 @@ const ajv = new Ajv({
   // have any additional or extraneous fields. Ideally, this would be false, always.
   removeAdditional: process.env.NODE_ENV !== 'test'
 });
+
+schemas.forEach(schema => ajv.addSchema(schema));
 
 const validateApd = ajv.compile({
   ...apdSchema,
