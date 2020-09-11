e39d1d54db2ddec15efd4126beb34e967a697beb
Richard Davis <rdavis.bacs@gmail.com>
2020-09-11 17:14:28 -0400

    remove vcap junk

--- api/env.js
+++ api/env.js
@@ -23,25 +23,7 @@ const defaults = {
   STORE_TYPE: 'null' // default to using the /dev/null store
 };
 
-// Check if there are any CloudFoundry user-provided services
-// offering up environment variables.  If there are, extract
-// them and merge them into our environment.
-const upsEnv = {};
-if (process.env.VCAP_SERVICES) {
-  try {
-    const vcap = JSON.parse(process.env.VCAP_SERVICES);
-    if (Array.isArray(vcap['user-provided'])) {
-      vcap['user-provided'].forEach(ups => {
-        Object.entries(ups.credentials).forEach(([name, value]) => {
-          upsEnv[name] = value;
-        });
-      });
-    }
-  } catch (e) {} // eslint-disable-line no-empty
-}
-
 dotenv.config();
-process.env = { ...defaults, ...upsEnv, ...process.env };
 
 // Don't require this until process.env is finished setting up, since that
 // defines how the logger works.
