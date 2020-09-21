7529ec2f1b25fde0ee848bd8ba359f1fa6c3eb36
Tiffany Forkner <thetif@gmail.com>
2020-09-21 12:09:07 -0400

    added knex migrations

--- api/migrations/20200921100859_change-role-names.js
+++ api/migrations/20200921100859_change-role-names.js
@@ -1,27 +1,27 @@
 exports.up = async knex => {
   await knex('auth_roles')
     .where({ name: 'admin' })
-    .update({ name: 'eAPD Admin' });
+    .update({ name: 'eAPD Admin', isActive: true });
 
   await knex('auth_roles')
     .where({ name: 'federal analyst' })
-    .update({ name: 'eAPD Federal Analyst' });
+    .update({ name: 'eAPD Federal Analyst', isActive: true });
 
   await knex('auth_roles')
     .where({ name: 'federal leadership' })
-    .update({ name: 'eAPD Federal Leadership' });
+    .update({ name: 'eAPD Federal Leadership', isActive: false });
 
   await knex('auth_roles')
     .where({ name: 'federal' })
-    .update({ name: 'eAPD Federal' });
+    .update({ name: 'eAPD Federal', isActive: false });
 
   await knex('auth_roles')
     .where({ name: 'state coordinator' })
-    .update({ name: 'eAPD State Coordinator' });
+    .update({ name: 'eAPD State Coordinator', isActive: false });
 
   await knex('auth_roles')
     .where({ name: 'state' })
-    .update({ name: 'eAPD State' });
+    .update({ name: 'eAPD State', isActive: false });
 };
 
 exports.down = async knex => {
