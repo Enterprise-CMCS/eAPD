e8c842d1f659bb5651657605a410237c5656c129
Ty Bolchoz <tybolt@gmail.com>
2020-10-21 17:21:20 -0400

    build custom component for locked accounts

--- web/src/containers/LoginLocked.js
+++ web/src/containers/LoginLocked.js
@@ -4,17 +4,19 @@ import React, { useState, Fragment } from 'react';
 const LoginLocked = () => {
 
 	return (
-		<div className="card--container" id="start-main-content">
+		<div id="start-main-content">
 			<div className="ds-l-container">
-				<div className="ds-l-row card">
-					<div className="ds-l-col--1 ds-u-margin-left--auto" />
-					<div className="ds-l-col--12 ds-l-sm-col--10 ds-l-lg-col--6">
-						<img src="/static/icons/error.svg" />
-						<h1>Verify Your Identity</h1>
-						<p>Please contact the System Administrator for account reset.</p>
-						<p>Reset will occur in 60 minutes 00 seconds.</p>			
-					</div>					
-					<div className="ds-l-col--1 ds-u-margin-right--auto" />
+				<div className="login-card">					
+					<h1 className="ds-u-display--flex ds-u-justify-content--start ds-u-align-items--center ds-u-margin--0">
+						<img src="/static/icons/error.svg" height="40" />
+						<span>Verify Your Identity</span>
+					</h1>
+					<h2 className="ds-h4 ds-u-margin-y--3">Account Locked</h2>
+					<p>Please contact the <a>System Administrator</a> for account reset.</p>
+					<p>Reset will occur in <span className="ds-u-color--error">60 minutes 00 seconds.</span></p>		
+					<div className="ds-u-display--flex ds-u-justify-content--end ds-u-margin-top--3 ds-u-padding-top--2 ds-u-border-top--2">
+						<a href="/" className="ds-c-button ds-c-button--transparent">Cancel</a>
+					</div>	
 				</div>
 			</div>
 		</div>
