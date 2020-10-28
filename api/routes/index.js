ad1d7d16f0c6b914f46f4a8a45910a27b4e80d75
Richard Davis <rdavis.bacs@gmail.com>
2020-10-28 15:15:18 -0400

    Add routes for requesting and approving access (#2600)
    * add create, read, and update routes for affiliations
    * add OpenAPI documentation for /states routes, and affiliations routes
    * use owasp/zap2docker-stable image, adjust config
    * respond with 404 status when an undefined route is requested (e.g. - GET /foobar)
    * respond with 400 status when an undefined resource is requested (e.g. - GET /states/zz)
    * update changelog

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,24 +1,32 @@
 const logger = require('../logger')('routes index');
-const auth = require('./auth');
+const affiliations = require('./affiliations');
 const apds = require('./apds');
+const auth = require('./auth');
 const me = require('./me');
+const states = require('./states')
 const users = require('./users');
 const openAPI = require('./openAPI');
 
 module.exports = (
   app,
+  affiliationsEndpoint = affiliations,
   apdsEndpoint = apds,
   authEndpoint = auth,
   meEndpoint = me,
+  statesEndpoint = states,
   usersEndpoint = users,
   openAPIdoc = openAPI
 ) => {
+  logger.silly('setting up routes for affilitions');
+  affiliationsEndpoint(app);
   logger.silly('setting up routes for apds');
   apdsEndpoint(app);
   logger.silly('setting up routes for auth');
   authEndpoint(app);
   logger.silly('setting up routes for me');
   meEndpoint(app);
+  logger.silly('setting up routes for states');
+  statesEndpoint(app);
   logger.silly('setting up routes for users');
   usersEndpoint(app);
 
