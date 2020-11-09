656ab6605df17754cf487fc1aed938915951054e
Richard Davis <rdavis.bacs@gmail.com>
2020-11-09 12:53:30 -0500

    site sources

--- api/logger/morgan.js
+++ api/logger/morgan.js
@@ -6,6 +6,7 @@ morgan.token('id', req => req.id)
 morgan.token('ip', req => req.ip)
 morgan.token('uid', req => req.user && req.user.id)
 
+// https://www.loggly.com/ultimate-guide/node-logging-basics/#:~:text=Logging%20in%20JSON%20Format
 const tokenString = [
   'id',
   'ip',
