e636e555a7d0b4a658da787d26672ec73587574a
Ty Bolchoz <tybolt@gmail.com>
2020-11-16 10:52:29 -0500

    adds new icons, creates views

--- web/src/containers/StateDashboard.js
+++ web/src/containers/StateDashboard.js
@@ -31,6 +31,42 @@ const PendingApproval = () => (
   </div>
 );
 
+const ApprovalDenied = () => (
+  <div className="ds-u-display--flex ds-u-flex-direction--column ds-u-justify-content--center ds-u-align-items--center ds-u-margin-y--4">
+    <img alt="Puzzle Piece Icon" src="../static/icons/alert.svg" height="51" />
+    <h3 className="ds-u-margin-bottom--1">
+      Approval Has Been Denied
+    </h3>
+    <p className="ds-u-margin--0">
+      Please contact State Administrator for more information.
+    </p>
+  </div>
+);
+
+const ApprovalRevoked = () => (
+  <div className="ds-u-display--flex ds-u-flex-direction--column ds-u-justify-content--center ds-u-align-items--center ds-u-margin-y--4">
+    <img alt="Puzzle Piece Icon" src="../static/icons/alert.svg" height="51" />
+    <h3 className="ds-u-margin-bottom--1">
+      Approval Permissions Revoked
+    </h3>
+    <p className="ds-u-margin--0">
+      Please contact State Administrator for more information.
+    </p>
+  </div>
+);
+
+const Approved = () => (
+  <div className="ds-u-display--flex ds-u-flex-direction--column ds-u-justify-content--center ds-u-align-items--center ds-u-margin-y--4">
+    <img alt="Puzzle Piece Icon" src="../static/icons/thumbs-up.svg" width="57" />
+    <h3 className="ds-u-margin-bottom--1">
+      Approved
+    </h3>
+    <p className="ds-u-margin--0">
+      Congratulations! You may now create an APD.
+    </p>
+  </div>
+);
+
 const StateDashboard = (
   {
     apds,
@@ -108,7 +144,7 @@ const StateDashboard = (
           </div>
         </div>
       </div>
-      {pending ? <PendingApproval /> : null}
+      {pending ? <Approved /> : null}
       {fetching ? <Loading>Loading APDs</Loading> : null}
       {!fetching && !pending && apds.length === 0 ? (
         <div className="ds-l-row">
