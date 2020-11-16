e7f3b0a69257ced47baf49bf0fff6d642c4414a1
Tiffany <thetif@gmail.com>
2020-11-16 10:28:35 -0500

    updated tests and state dashboard

--- web/src/containers/StateAccessRequestConfirmation.test.js
+++ web/src/containers/StateAccessRequestConfirmation.test.js
@@ -1,9 +1,29 @@
-// import React from 'react';
-// import { render } from 'apd-testing-library';
-// import StateAccessRequestConfirmation from './StateAccessRequestConfirmation';
+import React from 'react';
+import { render, fireEvent } from 'apd-testing-library';
+import StateAccessRequestConfirmation from './StateAccessRequestConfirmation';
 
+let props;
+let renderUtils;
 describe('<StateAccessRequestConfirmation />', () => {
-  it('test', () => {
-    expect(true).toBeTruthy();
+  beforeEach(() => {
+    props = {
+      action: jest.fn()
+    };
+    renderUtils = render(<StateAccessRequestConfirmation {...props} />);
+  });
+
+  it('displays the confirmation message', () => {
+    const { getByText } = renderUtils;
+    expect(
+      getByText(
+        /The State Administrator will verify your affiliation and credentials./i
+      )
+    ).toBeTruthy();
+  });
+
+  it('handles clicking the ok button', () => {
+    const { getByRole } = renderUtils;
+    fireEvent.click(getByRole('button', { name: 'Ok' }));
+    expect(props.action).toHaveBeenCalled();
   });
 });
