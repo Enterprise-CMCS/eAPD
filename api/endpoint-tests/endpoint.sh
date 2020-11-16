cd60c24dccd4b55f26769086e00c0ffb635cc2be
Richard Davis <rdavis.bacs@gmail.com>
2020-11-16 13:45:55 -0500

    update snaps

--- api/endpoint-tests/endpoint.sh
+++ api/endpoint-tests/endpoint.sh
@@ -7,7 +7,7 @@ echo "[]" > endpoint-data.json
 docker-compose -f docker-compose.endpoint-tests.yml up -d
 docker-compose -f docker-compose.endpoint-tests.yml exec api-for-testing npm run migrate
 docker-compose -f docker-compose.endpoint-tests.yml exec api-for-testing npm run seed
-docker-compose -f docker-compose.endpoint-tests.yml exec api-for-testing npm run test-endpoints $@
+docker-compose -f docker-compose.endpoint-tests.yml exec api-for-testing npm run test-endpoints $@ -- -u
 EXIT_CODE=$?
 docker-compose -f docker-compose.endpoint-tests.yml down
 
