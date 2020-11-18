8c9abee6dd6d60c4cf4df991621e6e68cb05659d
Tiffany <thetif@gmail.com>
2020-11-18 17:37:32 -0500

    more changes to CSP

--- bin/headers-staging.lambda.js
+++ bin/headers-staging.lambda.js
@@ -1,12 +1,15 @@
 const api = 'https://staging-eapd-api.cms.gov';
 const okta = 'https://impl.idp.idm.cms.gov';
+const ga = 'https://www.google-analytics.com';
+const gtm = 'https://www.googletagmanager.com';
+const gf = 'https://font.googleapis.com';
 
 const securityHeaders = {
-  'Content-Security-Policy': `default-src 'self' 'unsafe-inline'; connect-src 'self' ${api} ${okta}; frame-src 'self' ${okta}; frame-ancestors 'self' ${okta}; img-src 'self' data: ${api} https://www.google-analytics.com; object-src none; script-src 'self' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;`,
+  'Content-Security-Policy': `default-src 'self' 'unsafe-inline'; connect-src ${api} ${okta}; child-src ${okta}; frame-ancestors ${okta}; img-src 'self' ${api} ${okta} ${ga}; object-src 'none'; script-src ${gtm} ${ga} ${okta}; style-src ${gf}`,
   'Strict-Transport-Security': 'max-age=63072000',
-  'X-Content-Type-Options': 'nosniff',
-  'X-Frame-Options': 'SAMEORIGIN',
+  'X-Frame-Options': 'deny',
   'X-XSS-Protection': '1; mode=block',
+  'X-Content-Type-Options': 'nosniff',
 };
 
 exports.handler = async (event, context, callback) => {
