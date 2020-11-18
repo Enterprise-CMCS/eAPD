6e26f6d0bbbfc7f7a507833e4c5e4cc560042054
Tiffany <thetif@gmail.com>
2020-11-18 12:49:22 -0500

    changes to CSP string; upgraded okta-sdk

--- bin/headers-staging.lambda.js
+++ bin/headers-staging.lambda.js
@@ -2,11 +2,18 @@ const api = 'https://staging-eapd-api.cms.gov';
 const okta = 'https://impl.idp.idm.cms.gov';
 
 const securityHeaders = {
-  'Content-Security-Policy': `default-src 'self' 'unsafe-inline'; connect-src ${api} ${okta}; frame-ancestors none; img-src 'self' data: ${api} https://www.google-analytics.com; object-src none; script-src 'self' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;`,
-  'Strict-Transport-Security': 'max-age=63072000',
-  'X-Frame-Options': 'deny',
-  'X-XSS-Protection': '1; mode=block',
+  // 'Content-Security-Policy': `default-src 'self' 'unsafe-inline'; connect-src 'self' ${api} ${okta}; frame-ancestors none; img-src 'self' data: ${api} ${okta} https://www.google-analytics.com; object-src none; script-src 'self' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;`,
+  // 'Strict-Transport-Security': 'max-age=63072000',
+  // 'X-Content-Type-Options': 'nosniff',
+  // 'X-Frame-Options': 'deny',
+  // 'X-XSS-Protection': '1; mode=block',
+
+  'Content-Security-Policy': `default-src 'self'; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: ${api} ${okta} https://www.google-analytics.com; font-src 'self' data:; frame-ancestors 'none'; connect-src 'self' ${api} ${okta}`,
+  'Referrer-Policy': 'no-referrer, strict-origin-when-cross-origin',
+  'Strict-Transport-Security': 'max-age=63072000; includeSubDomains',
   'X-Content-Type-Options': 'nosniff',
+  'X-Frame-Options': 'DENY',
+  'X-XSS-Protection': '1; mode=block',
 };
 
 exports.handler = async (event, context, callback) => {
