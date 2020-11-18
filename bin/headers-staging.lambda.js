58c489dd790a7acb4d41fd7d4cea0862ae7aa723
Tiffany <thetif@gmail.com>
2020-11-18 14:44:35 -0500

    added okta to frame options

--- bin/headers-staging.lambda.js
+++ bin/headers-staging.lambda.js
@@ -2,10 +2,10 @@ const api = 'https://staging-eapd-api.cms.gov';
 const okta = 'https://impl.idp.idm.cms.gov';
 
 const securityHeaders = {
-  'Content-Security-Policy': `default-src 'self' 'unsafe-inline'; connect-src 'self' ${api} ${okta}; frame-ancestors none; img-src 'self' data: ${api} https://www.google-analytics.com; object-src none; script-src 'self' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;`,
+  'Content-Security-Policy': `default-src 'self' 'unsafe-inline'; connect-src 'self' ${api} ${okta}; frame-src 'self' ${okta}; frame-ancestors 'self' ${okta}; img-src 'self' data: ${api} https://www.google-analytics.com; object-src none; script-src 'self' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;`,
   'Strict-Transport-Security': 'max-age=63072000',
   'X-Content-Type-Options': 'nosniff',
-  'X-Frame-Options': 'deny',
+  'X-Frame-Options': 'SAMEORIGIN',
   'X-XSS-Protection': '1; mode=block',
 };
 
