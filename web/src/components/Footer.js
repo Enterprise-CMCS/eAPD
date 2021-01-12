20a21deb1931a27a308436e28dec9111be8b2205
Ty Bolchoz <tybolt@gmail.com>
2021-01-12 14:56:25 -0500

    formatting

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -5,40 +5,35 @@ const Footer = () => {
     <footer className="visibility--screen">
       <div className="ds-l-container">
         <div className="ds-l-row ds-u-display--flex ds-u-justify-content--between ds-u-align-items--center">
-            <img
-              src="/static/img/dhhs_logo_black.png"
-              alt="Health and Human Services logo"
-              className="hhs-logo"
-              width="80px"
-            />
-
-            <img
-              src="/static/img/cms-logo.png"
-              alt="Centers for Medicare & Medicaid Services Logo"
-              className="cms-logo"
-              width="150px"
-            />
-
-            <img
-              src="/static/img/macpro_logo_transparent.png"
-              alt="MACPro logo"
-              className="macpro-logo"
-              width="100px"
-            />
-
-            <img
-              src="/static/img/eAPDLogoSVG:ICO/SVG/eAPDColVarSVG.svg"
-              alt="eAPD logo"
-              className="eAPD-logo"
-              width="115px"
-            />
-
-            <p className="ds-u-measure--narrow">
-              A federal government website managed and paid for by the U.S.
-              Centers for Medicare and Medicaid Services and part of the MACPro
-              suite.
-            </p>
-
+          <img
+            src="/static/img/dhhs_logo_black.png"
+            alt="Health and Human Services logo"
+            className="hhs-logo"
+            width="80px"
+          />
+          <img
+            src="/static/img/cms-logo.png"
+            alt="Centers for Medicare & Medicaid Services Logo"
+            className="cms-logo"
+            width="150px"
+          />
+          <img
+            src="/static/img/macpro_logo_transparent.png"
+            alt="MACPro logo"
+            className="macpro-logo"
+            width="100px"
+          />
+          <img
+            src="/static/img/eAPDLogoSVG:ICO/SVG/eAPDColVarSVG.svg"
+            alt="eAPD logo"
+            className="eAPD-logo"
+            width="115px"
+          />
+          <p className="ds-u-measure--narrow">
+            A federal government website managed and paid for by the U.S.
+            Centers for Medicare and Medicaid Services and part of the MACPro
+            suite.
+          </p>
         </div>
       </div>
       <div className="footer--banner">
