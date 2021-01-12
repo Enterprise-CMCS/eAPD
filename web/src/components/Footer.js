3cab2f167ad039a30ce980bc21857fd857ba09dc
Ty Bolchoz <tybolt@gmail.com>
2021-01-12 10:14:03 -0500

    lint, changelog

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="visibility--screen">
       <div className="ds-l-container">
         <div className="ds-l-row ds-u-display--flex ds-u-align-items--center">
-          <div class="ds-l-col--6 ds-l-lg-col--2 ds-u-display--flex ds-u-align-items--center ds-u-flex-direction--column">
+          <div className="ds-l-col--6 ds-l-lg-col--2 ds-u-display--flex ds-u-align-items--center ds-u-flex-direction--column">
             <img
               src="/static/img/dhhs_logo_black.png"
               alt="Health and Human Services logo"
@@ -13,7 +13,7 @@ const Footer = () => {
               width="80px"
             />
           </div>
-          <div class="ds-l-col--6 ds-l-lg-col--2 ds-u-display--flex ds-u-align-items--center ds-u-flex-direction--column">
+          <div className="ds-l-col--6 ds-l-lg-col--2 ds-u-display--flex ds-u-align-items--center ds-u-flex-direction--column">
             <img
               src="/static/img/cms-logo.png"
               alt="Centers for Medicare & Medicaid Services Logo"
@@ -21,15 +21,15 @@ const Footer = () => {
               width="150px"
             />
           </div>
-          <div class="ds-l-col--6 ds-l-lg-col--2 ds-u-display--flex ds-u-align-items--center ds-u-flex-direction--column">
+          <div className="ds-l-col--6 ds-l-lg-col--2 ds-u-display--flex ds-u-align-items--center ds-u-flex-direction--column">
             <img
               src="/static/img/macpro_logo_transparent.png"
               alt="MACPro logo"
               className="macpro-logo"
               width="100px"
-              />
+            />
           </div>
-          <div class="ds-l-col--6 ds-l-lg-col--2 ds-u-display--flex ds-u-align-items--center ds-u-flex-direction--column">
+          <div className="ds-l-col--6 ds-l-lg-col--2 ds-u-display--flex ds-u-align-items--start ds-u-flex-direction--column">
             <img
               src="/static/img/eAPDLogoSVG:ICO/SVG/eAPDColVarSVG.svg"
               alt="eAPD logo"
@@ -37,7 +37,7 @@ const Footer = () => {
               width="115px"
             />
           </div>
-          <div class="ds-l-col--12 ds-l-lg-col--4">
+          <div className="ds-l-col--12 ds-l-lg-col--4">
             <p>
               A federal government website managed and paid for by the U.S.
               Centers for Medicare and Medicaid Services and part of the MACPro
@@ -50,7 +50,8 @@ const Footer = () => {
         <div className="ds-l-container">
           <div className="ds-l-row">
             <p className="ds-l-md-col--6">
-              Email <a href="mailto:CMS-EAPD@cms.hhs.gov">CMS-EAPD@cms.hhs.gov</a> for
+              Email{' '}
+              <a href="mailto:CMS-EAPD@cms.hhs.gov">CMS-EAPD@cms.hhs.gov</a> for
               help or feedback.
             </p>
             <p className="ds-l-md-col--6 ds-u-text-align--right">
