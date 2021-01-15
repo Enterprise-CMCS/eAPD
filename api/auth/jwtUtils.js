555861cc364a3762e9b367f30267f1077cee8c3b
Tiffany <thetif@gmail.com>
2021-01-15 17:58:53 -0500

    added the ability to get the accessToken from the cookie in the instance of img requests

--- api/auth/jwtUtils.js
+++ api/auth/jwtUtils.js
@@ -28,10 +28,28 @@ const verifyWebToken = async (token, { verifier = verifyJWT } = {}) => {
  * @returns {(String|null)} JWT string or null
  */
 const jwtExtractor = req => {
-  const token = req.get('Authorization');
-  if (!token || !token.toLowerCase().match(/^bearer\s/)) return null;
-  const [temp, result] = token.split(' '); // eslint-disable-line no-unused-vars
-  return result;
+  if (req.get('Authorization')) {
+    const token = req.get('Authorization');
+    if (!token || !token.toLowerCase().match(/^bearer\s/)) return null;
+    const [temp, result] = token.split(' '); // eslint-disable-line no-unused-vars
+    return result;
+  }
+  if (req.url.match(/^\/apds\/(\d+)\/files/) && req.get('Cookie')) {
+    // because our image files within the RTE are just img tags
+    // we cannot append our authorization header, but because
+    // we are storing our access token in a cookie, we can read
+    // the access token from there in this instance
+    const cookies = req.get('Cookie'); // get the cookies
+    const tokens = cookies.split('; '); // split the cookie string into individual cookies
+    const accessTokenObj = tokens.find(token =>
+      token.match(/^okta-token-storage_accessToken/)
+    ); // find the cookie that stores the access token
+    // eslint-disable-next-line no-unused-vars
+    const [key, value] = accessTokenObj.split('='); // get the value
+    const valueObj = JSON.parse(unescape(value)); // the value is an encoded string, convert it to a json object
+    return valueObj.accessToken; // return the access token
+  }
+  return null;
 };
 
 module.exports = {
