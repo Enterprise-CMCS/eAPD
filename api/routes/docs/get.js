d8d0e46c91fe52b7b1c440d45b18520da6675415
Tiffany <thetif@gmail.com>
2021-01-25 15:04:08 -0500

    updated path and added headers to name the file to download

--- api/routes/docs/get.js
+++ api/routes/docs/get.js
@@ -4,11 +4,13 @@ const { getFile: get } = require('../../files');
 const HELP_DOC = 'EUAAccountRegistration.pdf';
 
 module.exports = (app, { getFile = get } = {}) => {
-  logger.silly('setting up GET /docs/help route');
+  logger.silly('setting up GET /docs/account-registration route');
 
-  app.get('/docs/help', async (req, res) => {
+  app.get('/docs/account-registration', async (req, res) => {
     try {
       const file = await getFile(HELP_DOC);
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', `attachment; filename=${HELP_DOC}`);
       res.send(file).end();
     } catch (e) {
       logger.error({ id: req.id, message: 'error fetching file' });
