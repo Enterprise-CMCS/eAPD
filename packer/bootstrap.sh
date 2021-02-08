fb3581d5b879e12915879d9d6993c4029c90ff22
cthulhuplus <brookswm@gmail.com>
2021-02-08 14:36:42 -0500

    Attempting to address Git Branch issue

--- packer/bootstrap.sh
+++ packer/bootstrap.sh
@@ -122,7 +122,7 @@ cat <<CWAGENTCONFIG > /opt/aws/amazon-cloudwatch-agent/doc/cwagent.json
 
 CWAGENTCONFIG
 
-# Nginx is preview only
+# Nginx is test/preview only
 touch /opt/aws/amazon-cloudwatch-agent/doc/var-log.json
 cat <<CWVARLOGCONFIG > /opt/aws/amazon-cloudwatch-agent/doc/var-log.json
 {
@@ -132,51 +132,51 @@ cat <<CWVARLOGCONFIG > /opt/aws/amazon-cloudwatch-agent/doc/var-log.json
         "collect_list": [
           {
             "file_path": "/var/log/aide/aide.log*",
-            "log_group_name": "preview/var/log/aide/aide.log"
+            "log_group_name": "test/var/log/aide/aide.log"
           },
           {
             "file_path": "/var/log/audit/audit.log*",
-            "log_group_name": "preview/var/log/audit/audit.log"
+            "log_group_name": "test/var/log/audit/audit.log"
           },
           {
             "file_path": "/var/log/awslogs.log*",
-            "log_group_name": "preview/var/log/awslogs.log"
+            "log_group_name": "test/var/log/awslogs.log"
           },
           {
             "file_path": "/var/log/cloud-init.log*",
-            "log_group_name": "preview/var/log/cloud-init.log"
+            "log_group_name": "test/var/log/cloud-init.log"
           },
           {
             "file_path": "/var/log/cloud-init-output.log*",
-            "log_group_name": "preview/var/log/cloud-init-output.log"
+            "log_group_name": "test/var/log/cloud-init-output.log"
           },
           {
             "file_path": "/var/log/cron*",
-            "log_group_name": "preview/var/log/cron"
+            "log_group_name": "test/var/log/cron"
           },
           {
             "file_path": "/var/log/dmesg*",
-            "log_group_name": "preview/var/log/dmesg"
+            "log_group_name": "test/var/log/dmesg"
           },
           {
             "file_path": "/var/log/maillog*",
-            "log_group_name": "preview/var/log/maillog"
+            "log_group_name": "test/var/log/maillog"
           },
           {
             "file_path": "/var/log/messages*",
-            "log_group_name": "preview/var/log/messages"
+            "log_group_name": "test/var/log/messages"
           },
           {
             "file_path": "/var/log/nginx/access_log*",
-            "log_group_name": "preview/var/log/nginx/access_log"
+            "log_group_name": "test/var/log/nginx/access_log"
           },
           {
             "file_path": "/var/log/nginx/error_log*",
-            "log_group_name": "preview/var/log/nginx/error_log"
+            "log_group_name": "test/var/log/nginx/error_log"
           },
           {
             "file_path": "/var/log/secure*",
-            "log_group_name": "preview/var/log/secure"
+            "log_group_name": "test/var/log/secure"
           }
         ]
       }
@@ -195,15 +195,15 @@ cat <<CWVAROPTCONFIG > /opt/aws/amazon-cloudwatch-agent/doc/var-opt.json
         "collect_list": [
           {
             "file_path": "/var/opt/ds_agent/diag/ds_agent.log*",
-            "log_group_name": "preview/var/opt/ds_agent/diag/ds_agent.log"
+            "log_group_name": "test/var/opt/ds_agent/diag/ds_agent.log"
           },
           {
             "file_path": "/var/opt/ds_agent/diag/ds_agent-err.log*",
-            "log_group_name": "preview/var/opt/ds_agent/diag/ds_agent-err.log"
+            "log_group_name": "test/var/opt/ds_agent/diag/ds_agent-err.log"
           },
           {
             "file_path": "/var/opt/ds_agent/diag/ds_am.log*",
-            "log_group_name": "preview/var/opt/ds_agent/diag/ds_am.log"
+            "log_group_name": "test/var/opt/ds_agent/diag/ds_am.log"
           }
         ]
       }
@@ -223,43 +223,43 @@ cat <<CWAPPLOGCONFIG > /opt/aws/amazon-cloudwatch-agent/doc/app-logs.json
         "collect_list": [
           {
             "file_path": "/app/api/logs/eAPD-API-error-0.log*",
-            "log_group_name": "preview/app/api/logs/eAPD-API-error-0.log"
+            "log_group_name": "test/app/api/logs/eAPD-API-error-0.log"
           },
           {
             "file_path": "/app/api/logs/eAPD-API-out-0.log*",
-            "log_group_name": "preview/app/api/logs/eAPD-API-out-0.log"
+            "log_group_name": "test/app/api/logs/eAPD-API-out-0.log"
           },
           {
             "file_path": "/app/api/logs/eAPD-API-*",
-            "log_group_name": "preview/app/api/logs/eAPD-API-combined-0.log"
+            "log_group_name": "test/app/api/logs/eAPD-API-combined-0.log"
           },          
           {
             "file_path": "/app/api/logs/Database-migration-error.log*",
-            "log_group_name": "preview/app/api/logs/Database-migration-error.log"
+            "log_group_name": "test/app/api/logs/Database-migration-error.log"
           },
           {
             "file_path": "/app/api/logs/Database-migration-out.log*",
-            "log_group_name": "preview/app/api/logs/Database-migration-out.log"
+            "log_group_name": "test/app/api/logs/Database-migration-out.log"
           },
           {
             "file_path": "/app/api/logs/Database-migration-*",
-            "log_group_name": "preview/app/api/logs/Database-migration-combined.log"
+            "log_group_name": "test/app/api/logs/Database-migration-combined.log"
           },          
           {
             "file_path": "/app/api/logs/Database-seeding-error.log*",
-            "log_group_name": "preview/app/api/logs/Database-seeding-error.log"
+            "log_group_name": "test/app/api/logs/Database-seeding-error.log"
           },
           {
             "file_path": "/app/api/logs/Database-seeding-out.log*",
-            "log_group_name": "preview/app/api/logs/Database-seeding-out.log"
+            "log_group_name": "test/app/api/logs/Database-seeding-out.log"
           },
           {
             "file_path": "/app/api/logs/Database-seeding-*",
-            "log_group_name": "preview/app/api/logs/Database-seeding-combined.log"
+            "log_group_name": "test/app/api/logs/Database-seeding-combined.log"
           },                                           
           {
             "file_path": "/app/api/logs/cms-hitech-apd-api.logs*",
-            "log_group_name": "preview/app/api/logs/cms-hitech-apd-api.logs"              
+            "log_group_name": "test/app/api/logs/cms-hitech-apd-api.logs"              
           }    
         ]
       }
