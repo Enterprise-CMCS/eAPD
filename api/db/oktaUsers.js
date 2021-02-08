b5e6c6b04bc8e1181510bf2e367acf2dd67352cd
Richard Davis <rdavis.bacs@gmail.com>
2021-02-08 13:02:32 -0500

    create or update okta_users

---
+++ api/db/oktaUsers.js
@@ -0,0 +1,42 @@
+const logger = require('../logger')('db/oktaUsers');
+const knex = require('./knex');
+
+const getOktaUser = (user_id, { db = knex } = {}) => {
+  return db('okta_users').where({ user_id }).first();
+}
+
+const createOktaUser = (user_id, email, metadata, { db = knex } = {}) => (
+  db('okta_users')
+  .insert({
+    user_id,
+    email,
+    metadata
+  })
+)
+
+const updateOktaUser = (user_id, email, metadata, { db = knex } = {}) => (
+  db('okta_users')
+  .where({ user_id })
+  .update({ email, metadata })
+)
+
+const createOrUpdateOktaUser = (user_id, email, metadata) => (
+  getOktaUser(user_id).then(user => {
+    if (!user) {
+      return createOktaUser(user_id, email, metadata)
+    } else {
+      return updateOktaUser(user_id, email, metadata)
+    }
+  })
+  .catch(e => logger.error({
+     message: `could not create or update user_id: ${user_id}`,
+     e
+  }))
+)
+
+module.exports = {
+  createOktaUser,
+  createOrUpdateOktaUser,
+  getOktaUser,
+  updateOktaUser
+}
