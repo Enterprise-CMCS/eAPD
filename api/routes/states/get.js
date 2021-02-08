f12f8ad28aa744bc7526a202a8f3db779b8115f7
Richard Davis <rdavis.bacs@gmail.com>
2021-02-08 15:36:13 -0500

    add getStateAdmins

--- api/routes/states/get.js
+++ api/routes/states/get.js
@@ -1,9 +1,9 @@
-const { raw: knex } = require('../../db');
+const db = require('../../db');
 const { loggedIn } = require('../../middleware/auth');
 
 module.exports = app => {
   app.get('/states', (request, response, next) => {
-    knex('states')
+    db.raw('states')
       .select('id', 'name')
       .then(rows => response.status(200).json(rows))
       .catch(next);
@@ -11,12 +11,13 @@ module.exports = app => {
 
   app.get('/states/:id', loggedIn, (request, response, next) => {
     const { id } = request.params;
-    knex('states')
+    db.raw('states')
       .where({ id })
       .first()
       .then(row => {
         if (row) {
-          response.status(200).json(row);
+          stateAdmins = db.getStateAdmins(id);
+          response.status(200).json({ ...row, stateAdmins });
         } else {
           response.status(400).end();
         }
