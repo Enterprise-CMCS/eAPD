33b6c41230554e34de9e8f1a8ef2412a055160aa
Tiffany <thetif@gmail.com>
2021-02-10 15:30:48 -0500

    updated cloudwatch settings

--- api/auth/jwtMiddleware.js
+++ api/auth/jwtMiddleware.js
@@ -22,8 +22,8 @@ const jwtMiddleware = async (
     verifyToken = verifyWebToken
   } = {}
 ) => {
-  const jwt = extractor(req);
   try {
+    const jwt = extractor(req);
     const claims = jwt ? await verifyToken(jwt) : false;
     if (!claims) return next();
 
@@ -38,6 +38,8 @@ const jwtMiddleware = async (
       req.user = user;
     }
   } catch (err) {
+    console.log({ err });
+    logger.info('error', err.message);
     logger.error(`error message: ${err.message}`);
   }
   return next();
