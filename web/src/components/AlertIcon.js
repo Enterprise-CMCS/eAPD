60351ffe190585d0443fd0f35d45fe685a44ec41
Richard Davis <rdavis.bacs@gmail.com>
2021-02-12 11:44:06 -0500

    save point

--- web/src/components/AlertIcon.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const AlertIcon = () =>
-  <img alt="Alert Icon" src="../static/icons/alert.svg" height="51" />;
-
-export default AlertIcon;
--- web/src/components/PuzzlePieceIcon.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const PuzzlePieceIcon = () =>
-  <img alt="Puzzle Piece Icon" src="../static/icons/alert.svg" height="51" />;
-
-export default PuzzlePieceIcon;
