3c9ab8e41f53202aca9fd84863054098e1589a52
Tiffany <thetif@gmail.com>
2021-02-23 12:57:44 -0500

    changed wording

--- web/src/components/EnableCookies.js
+++ web/src/components/EnableCookies.js
@@ -1,7 +1,7 @@
 const html = `<div class="ds-c-alert__body">
-  <h3 class="ds-c-alert__heading">Please enable cookies in your browser.</h3>
+  <h3 class="ds-c-alert__heading">Cookies are not enabled in your browser.</h3>
   <p class="ds-c-alert__text ds-u-measure--wide">
-    It looks like you have blocked all cookies. Please enable at least 
+    It looks like you have blocked all cookies. Enable at least 
     first-party cookies to use the Centers for Medicare & Medicaid Services 
     (CMS) eAPD site.
   </p>
