6df6cd36dc02ed5d7dcabf7cc1b3deada48ec6bb
Ty Bolchoz <tybolt@gmail.com>
2021-02-23 13:51:03 -0500

    updates copy on unexpected error modal

--- web/src/containers/UnexpectedError.js
+++ web/src/containers/UnexpectedError.js
@@ -21,11 +21,9 @@ const UnexpectedError = ({ hasError, isSaving, save }) => {
           onExit={closeErrorAlert}
         >
           <p>
-            Your changes aren&apos;t being saved. Try saving your changes again
-            in a few minutes by clicking on the save button. If you refresh your
-            browser without saving backing up your changes, changes made after
-            this message appeared will be lost.
+            Your changes aren&apos;t being saved. Verify your internet connection and try saving your changes again in a few minutes by clicking on the save button. If you refresh your browser without saving, changes made after this message appeared will be lost.
           </p>
+          <p>If your internet connection is working and this message persists for more than 5 minutes, please contact us at <a href="mailto:CMS-EAPD@cms.hhs.gov">CMS-EAPD@cms.hhs.gov</a>.</p>
           <p className="ds-u-text-align--right ds-u-margin-bottom--0">
             <Button variation="primary" onClick={save}>
               {isSaving && <Spinner />}
