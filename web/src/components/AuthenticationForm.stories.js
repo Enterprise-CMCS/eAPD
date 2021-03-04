2400b1a23f9865560398bc1612f71a73b53256a3
Tiffany <thetif@gmail.com>
2021-03-04 17:40:41 -0500

    removed examples

--- web/src/components/AuthenticationForm.stories.js
+++ web/src/components/AuthenticationForm.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { plain as AuthenticationForm } from './AuthenticationForm';
+import * as CardFormStories from './CardForm.stories';
 
 export default {
   title: 'AuthenticationForm',
@@ -19,46 +20,42 @@ export default {
 export const Basic = args => <AuthenticationForm {...args} />;
 
 Basic.args = {
-  cancelable: true,
-  canSubmit: true,
-  success: false,
-  error: false,
-  primaryButtonText: ['Save changes', 'Working'],
-  sectionName: '',
-  working: false,
-  hasEverLoggedOn: false,
-  onSave: () => {}
+  ...CardFormStories.Basic.args
 };
 
 Basic.parameters = {
   jest: ['AuthenticationForm.test.js']
 };
 
+const Form = () => (
+  <div className="ds-u-margin-bottom--4">
+    <label
+      htmlFor="textfield"
+      id="textfield-label"
+      className="ds-c-label ds-u-margin-y--2 ds-u-font-weight--normal"
+    >
+      Enter some text in this field
+    </label>
+    <input
+      width="200px"
+      aria-labelledby="textfield-label"
+      className="ds-c-field ds-c-field--medium"
+      id="textfield"
+      type="text"
+      name="textfield"
+      defaultValue="Some Text"
+    />
+  </div>
+);
+
 export const TextField = args => (
   <AuthenticationForm {...args}>
-    <div className="ds-u-margin-bottom--4">
-      <label
-        htmlFor="textfield"
-        id="textfield-label"
-        className="ds-c-label ds-u-margin-y--2 ds-u-font-weight--normal"
-      >
-        Enter some text in this field
-      </label>
-      <input
-        width="200px"
-        aria-labelledby="textfield-label"
-        className="ds-c-field ds-c-field--medium"
-        id="textfield"
-        type="text"
-        name="textfield"
-        defaultValue="Some Text"
-      />
-    </div>
+    <Form />
   </AuthenticationForm>
 );
 
 TextField.args = {
-  ...Basic.args
+  ...CardFormStories.Basic.args
 };
 
 TextField.parameters = {
@@ -67,29 +64,12 @@ TextField.parameters = {
 
 export const TextFieldWithFooter = args => (
   <AuthenticationForm {...args}>
-    <div className="ds-u-margin-bottom--4">
-      <label
-        htmlFor="textfield"
-        id="textfield-label"
-        className="ds-c-label ds-u-margin-y--2 ds-u-font-weight--normal"
-      >
-        Enter some text in this field
-      </label>
-      <input
-        width="200px"
-        aria-labelledby="textfield-label"
-        className="ds-c-field ds-c-field--medium"
-        id="textfield"
-        type="text"
-        name="textfield"
-        defaultValue="Some Text"
-      />
-    </div>
+    <Form />
   </AuthenticationForm>
 );
 
 TextFieldWithFooter.args = {
-  ...Basic.args,
+  ...CardFormStories.Basic.args,
   footer: <p className="ds-u-padding-top--2">This is a footer</p>
 };
 
