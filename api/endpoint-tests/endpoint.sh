b9effc8d12be1f826afff8bd3aab67f12dce074e
Richard Davis <rdavis.bacs@gmail.com>
2021-03-05 12:57:49 -0500

    refactor endpoint script

--- api/endpoint-tests/endpoint.sh
+++ api/endpoint-tests/endpoint.sh
@@ -3,16 +3,16 @@
 rm ./endpoint-tests/endpoint-data.json
 echo "[]" > endpoint-data.json
 
-alias dc="docker-compose"
 compose_file="docker-compose.endpoint-tests.yml"
+alias dc="docker-compose -f $compose_file"
 
 ENDPOINT_COVERAGE_CAPTURE=true \
-  dc -f "$compose_file" up -d
-dc -f "$compose_file" exec api-for-testing npm run migrate
-dc -f "$compose_file" exec api-for-testing npm run seed
-dc -f "$compose_file" exec api-for-testing npm run test-endpoints $@
+  dc up -d
+dc exec api-for-testing npm run migrate
+dc exec api-for-testing npm run seed
+dc exec api-for-testing npm run test-endpoints $@ # -- --update-snapshot
 EXIT_CODE=$?
-dc -f "$compose_file" down
+dc down
 
 mv endpoint-data.json ./endpoint-tests
 node endpoint-tests/endpoint-coverage.js
