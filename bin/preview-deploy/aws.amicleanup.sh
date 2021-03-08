3f257e71edcccdf6e8cbf78443c5b095d1190877
cthulhuplus <brookswm@gmail.com>
2021-03-08 12:19:51 -0500

    Updated AMI cleanup script to check for instances in motion before cleaning up AMIs

--- bin/preview-deploy/aws.amicleanup.sh
+++ bin/preview-deploy/aws.amicleanup.sh
@@ -10,14 +10,16 @@ set -e
 function cleanupPlatinumAMI() {
   # Configure AWS CLI with defaults
   configureAWS
-
-  echo "• Finding available Platinum AMIs"
+  
+  printf "• Finding state of AWS environment\n"
+  waitForAtRest
+  printf "• Finding available Platinum AMIs\n"
   getAvailableAMIs
-  echo "• Checking for Platinum AMIs currently in use"
+  printf "• Checking for Platinum AMIs currently in use\n"
   checkAMIForUsage
-  echo "• Checking for the most recently created Platinum AMI"
+  printf "• Checking for the most recently created Platinum AMI\n"
   getMostRecentAMI
-  echo "• These AMIs have been deregistered because they are not most recent or in use"
+  printf "• AMI actions being taken\n"
   deregisterIfNotUsed
 }
 
@@ -33,22 +35,22 @@ function getAvailableAMIs() {
     | jq -rc '.Images[].ImageId' 
 }
 
-# Finds all existing preview instances
-#function findExistingInstances() {
-#  aws ec2 describe-instances \
-#    --filter "Name=tag:environment,Values=preview" \
-#    | jq -rc '.Reservations[].Instances[] | [.InstanceId, (.Tags[] | (select(.Key == "github-pr") | .Value))] | @csv'
-#}
 function checkAMIForUsage() {
   AVAIL_AMIS=$(getAvailableAMIs)
   for AMI_ID in $AVAIL_AMIS
   do 
-    aws ec2 describe-instances --filter Name=image-id,Values="$AMI_ID" | jq -rc '.Reservations[].Instances[] | .ImageId'
+    aws ec2 describe-instances \
+      --filter Name=image-id,Values="$AMI_ID" \
+      | jq -rc '.Reservations[].Instances[] | .ImageId'
   done
 }
 
 function getMostRecentAMI() {
-  aws ec2 describe-images --owners self --filters "Name=name,Values=eAPD Platinum AMI - *" --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text
+  aws ec2 describe-images \
+    --owners self \
+    --filters "Name=name,Values=eAPD Platinum AMI - *" \
+    --query 'sort_by(Images, &CreationDate)[-1].ImageId' \
+    --output text
 }
 
 function deregisterIfNotUsed() {
@@ -58,13 +60,32 @@ function deregisterIfNotUsed() {
   for AMI_ID in $AVAIL_AMIS
   do
     if [[ $AMI_ID == $IN_USE_AMIS ]] || [[ $AMI_ID == $MOST_RECENT_AMI ]]; then
-      #:
-      print "Keeping $AMI_ID"
+      printf "Keeping $AMI_ID\n"
     else
-      #aws ec2 deregister-image --image-id $AMI_ID
-      print "Deregistering $AMI_ID"
+      aws ec2 deregister-image --image-id $AMI_ID
+      printf "Deregistering $AMI_ID\n"
     fi
   done
+}
+
+# Gets State of Instances"
+function getInMotionInstanceIds() {
+  aws ec2 describe-instances \
+    --filter "Name=instance-state-name,Values=pending,rebooting,stopping,shutting-down" \
+    | jq -rc '.Reservations[].Instances[].State.Name'
+}
+
+# This function is to make sure the environment is at rest
+# No pending, stopping, rebooting Instances
+function waitForAtRest() {
+  INMOTION_INSTANCES=$(getInMotionInstanceIds)
+  sleep 15
+  while [[ $INMOTION_INSTANCES == "pending" ]] || [[ $INMOTION_INSTANCES == "rebooting" ]] || [[ $INMOTION_INSTANCES == "stopping" ]] || [[ $INMOTION_INSTANCES == "shutting-down" ]]; do
+    printf "There are in motion Instances\n"
+    sleep 300
+  done
+
+  printf "There are no Instances in motion\n"
 }
 
 cleanupPlatinumAMI
