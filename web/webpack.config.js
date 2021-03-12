92bf3d554ae4fe0b155088b38ee1a2508afda481
Tiffany <thetif@gmail.com>
2021-03-12 18:05:44 -0500

    fixed webpack

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -49,13 +49,15 @@ const config = {
           // together
           MiniCssExtractPlugin.loader,
 
-          // Creates `style` nodes from JS strings
-          'style-loader',
-
           // Interprets any url() and @import statements and resolves them to
           // their full path on the local disk.
           'css-loader',
 
+          // Handles resolving and importing all the CSS files, so we end up
+          // with one nice, big file to deal with. Also adds vendor prefixes
+          // as necessary for CSS rules that aren't yet widely supported.
+          'postcss-loader',
+          
           {
             // Parse the Sass into CSS.
             loader: 'sass-loader',
