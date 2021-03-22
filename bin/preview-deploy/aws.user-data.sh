3a89ad42bdbb174ccadd90f215aec26d86f84e74
cthulhuplus <brookswm@gmail.com>
2021-03-22 14:46:08 -0400

    Removed executible permissions on service file

--- bin/preview-deploy/aws.user-data.sh
+++ bin/preview-deploy/aws.user-data.sh
@@ -7,6 +7,7 @@ sudo yum install -y gcc-c++
 
 # Set up the pm2 systemd service for the backend application
 mv /home/ec2-user/eapd-backend.service.tpl  /usr/lib/systemd/system/eapd-backend.service
+chmod 644 /usr/lib/systemd/system/eapd-backend.service
 ln -s  /usr/lib/systemd/system/eapd-backend.service /etc/systemd/system/multi-user.target.wants
 
 # Test to see the command that is getting built for pulling the Git Branch
