f27a74a526ed39da9c890ac6033af932c64c1f38
Tiffany <tforkner@fearless.tech>
2021-03-26 13:56:14 -0400

    Update EnableCookies.js
    Updated the text to tell the user to update their browser after they enable cookies

--- web/src/components/EnableCookies.js
+++ web/src/components/EnableCookies.js
@@ -1,9 +1,8 @@
 const html = `<div class="ds-c-alert__body">
   <h3 class="ds-c-alert__heading">Cookies are not enabled in your browser.</h3>
   <p class="ds-c-alert__text ds-u-measure--wide">
-    It looks like you have blocked all cookies. Enable at least 
-    first-party cookies to use the Centers for Medicare & Medicaid Services 
-    (CMS) eAPD site.
+    It looks like you have blocked all cookies. Enable at least first-party cookies 
+    and refresh your browser to use the Centers for Medicare & Medicaid Services (CMS) eAPD site.
   </p>
 </div>`;
 
