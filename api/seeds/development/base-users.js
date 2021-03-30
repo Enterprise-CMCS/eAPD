7e4fbbf45eef20ae1b02ab0c1d71de747644a721
knollfear <mknoll@fearless.tech>
2021-03-30 12:39:48 -0400

    switching tasks so just saving my place.

--- api/seeds/development/base-users.js
+++ api/seeds/development/base-users.js
@@ -3,7 +3,18 @@ const { oktaClient } = require('../../auth/oktaAuth');
 const { createUsersToAdd } = require('../shared/set-up-users');
 
 exports.seed = async knex => {
-  const oktaAffiliations = await createUsersToAdd(knex, oktaClient);
+  const {oktaAffiliations, stateCertifications} = await createUsersToAdd(knex, oktaClient);
   await knex('auth_affiliations').insert(oktaAffiliations);
   logger.info('Completed adding affiliations');
+  await knex('state_admin_certifications').insert(stateCertifications)
+  const auditEntries = stateCertifications.map(certification =>{
+    return {
+      uid:certification.uid,
+      changeDate:Date.now(),
+      changedBy: 'seeds'
+    }
+  })
+
+  await knex('state_admin_certifications_audit').insert(auditEntries)
+
 };
