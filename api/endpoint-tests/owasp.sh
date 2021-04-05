7167b1ed72811e254661bf81755074eebd00f7f5
Tiffany <thetif@gmail.com>
2021-04-05 14:52:18 -0400

    meh

--- api/endpoint-tests/owasp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-ENDPOINT_COVERAGE_CAPTURE=true docker-compose -f docker-compose.endpoint-tests.yml up -d
-docker-compose -f docker-compose.endpoint-tests.yml exec api-for-testing npm run migrate
-docker-compose -f docker-compose.endpoint-tests.yml exec api-for-testing npm run seed
-docker pull owasp/zap2docker-stable
-docker run --name zap --network api_default -v "$(pwd)/../owasp":/zap/wrk -t owasp/zap2docker-stable zap-api-scan.py -c api-scan.conf -t http://api-for-testing:8000/open-api -f openapi
-docker container rm zap
-docker-compose -f docker-compose.endpoint-tests.yml down
