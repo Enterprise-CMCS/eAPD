561bb4320ed4449eb1a26d84033a215d2fa31643
Ty Bolt <tybolt@gmail.com>
2021-04-16 08:12:37 -0400

    wip sends federal users to admin dashboard

--- web/src/containers/Dashboard.js
+++ web/src/containers/Dashboard.js
@@ -1,19 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { getIsAdmin } from '../reducers/user.selector';
+import { getIsAdmin, getIsFederal } from '../reducers/user.selector';
 import AdminDashboard from './admin/AdminDashboard';
 import StateDashboard from './StateDashboard';
 
-const Dashboard = ({ isAdmin, ...rest }) =>
-  isAdmin ? <AdminDashboard {...rest} /> : <StateDashboard {...rest} />;
+const Dashboard = ({ isAdmin, isFederal, ...rest }) =>
+  isAdmin || isFederal ? <AdminDashboard {...rest} /> : <StateDashboard {...rest} />;
 
 Dashboard.propTypes = {
-  isAdmin: PropTypes.bool.isRequired
+  isAdmin: PropTypes.bool.isRequired,
+  isFederal: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
-  isAdmin: getIsAdmin(state)
+  isAdmin: getIsAdmin(state),
+  isFederal: getIsFederal(state)
 });
 
 export default connect(mapStateToProps)(Dashboard);
