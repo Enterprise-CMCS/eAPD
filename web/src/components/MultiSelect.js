c6c215ea9cbe1d7f82fdec8ac8ce8ef53b273c2f
Ty Bolt <tybolt@gmail.com>
2021-04-28 16:24:38 -0400

    uses filter for autocomplete

--- web/src/components/MultiSelect.js
+++ web/src/components/MultiSelect.js
@@ -1,34 +1,50 @@
-import React, { useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Autocomplete, Badge, TextField } from '@cmsgov/design-system';
 
-import Icon, { faTimesCircle } from '../components/Icons';
+import Icon, { faTimesCircle } from "./Icons";
 
-const MultiSelect = ({ options, handleSelection }) => {
+const MultiSelect = ({ options }) => {
   
+  const [itemList, setItemList] = useState(options);
+
   const [selectedOptions, setSelectedOptions] = useState([]);
 
-  const addOption = (selectedItem) => {
-    console.log("selected option:", selectedItem);
+  const handleOnChange = (item) => {
+    console.log(item);
+    const newSelection = selectedOptions;
+    newSelection.push(item);
+    setSelectedOptions(newSelection);
   };
-  console.log("options", options);
 
-  const removeItemFromList = (item) => {
-    console.log("item to be removed", item.target.parentElement)
+  const removeItemFromSelection = (item) => {
+    console.log(selectedOptions);
+    console.log("item to be removed", item.target.dataset.id);
   };
+
+  const filterItems = query => {
+    setItemList(options.filter(el => el.name.toLowerCase().indexOf(query.toLowerCase()) !== -1));
+  }
 
   return (
     <Autocomplete
-      items={options}
-      label="Select from the options below:"
-      onChange={(selectedItem) => console.log(selectedItem)}
-      onInputValueChange={(inputVal) => console.log('[Autocomplete]: ' + inputVal)}
+      items={itemList}
+      onChange={handleOnChange}
+      onInputValueChange={filterItems}
+      clearSearchButton={false}
     >
       {/* render a collection of badges here with a button to remove them */}
-      <Badge variation="info">Maryland {' '} <Icon icon={faTimesCircle} onClick={removeItemFromList} /></Badge>
+      {selectedOptions.map(el => {
+        return (
+          <Badge key={el.id} variation="info">
+            {el.name} {' '} 
+            <Icon data-id={el.id} icon={faTimesCircle} onClick={removeItemFromSelection} />
+          </Badge>          
+        )
+      })}
       <TextField
         hint=""
-        label=""
+        label="Search state or affiliation"
         labelClassName="ds-u-margin-top--0"
         name="Downshift_autocomplete"
       />
@@ -41,7 +57,7 @@ MultiSelect.defaultProps = {
 };
 
 MultiSelect.propTypes = {
-
+  options: PropTypes.array.isRequired
 };
 
 export default MultiSelect;
