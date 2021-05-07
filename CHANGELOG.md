19ee1805e85c746336b3098edf240f12f60599ad
Tiffany <thetif@gmail.com>
2021-05-07 13:48:50 -0400

    added styling on ol and ul lists too

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,22 +1,20 @@
 # Next release
 
-Anticipated release: April 26, 2021
+Anticipated release: May 17, 2021
 
 #### 🚀 New features
 
 #### 🐛 Bugs fixed
-- Shows a "password expired" alert instead of "invalid mfa code" alert when logging in with MFA and an expired password ([#2988])
+
+- Investigate why rich text editor is losing formatting after page reload ([#2961])
 
 - Refreshing Page Traps the User on Validating the Session ([#2976])
 
 #### ⚙️ Behind the scenes
 
-- Upgraded modules ([#2947])
-
 # Previous releases
 
 See our [release history](https://github.com/CMSgov/eAPD/releases)
 
-[#2988]: https://github.com/CMSgov/eAPD/issues/2988
-[#2947]: https://github.com/CMSgov/eAPD/issues/2947
+[#2961]: https://github.com/CMSgov/eAPD/issues/2961
 [#2976]: https://github.com/CMSgov/eAPD/issues/2976
--- api/util/sanitize.test.js
+++ api/util/sanitize.test.js
@@ -4,7 +4,7 @@ const sanitize = require('./sanitize');
 tap.test('sanitize input', async sanitizeTests => {
   sanitizeTests.test('it should sanitize dangerous input', async test => {
     const dirty = '<a xlink:href="javascript:alert(document.domain)">XSS</a>';
-    test.equal(
+    test.equals(
       sanitize(dirty),
       '<a>XSS</a>',
       'strips out dangerous characters'
@@ -13,13 +13,30 @@ tap.test('sanitize input', async sanitizeTests => {
 
   sanitizeTests.test('it should allow allowed characters', async test => {
     const dirty = '<h1>I am allowed</h1>';
-    test.equal(sanitize(dirty), dirty, 'leaves characters that are allowed');
+    test.equals(sanitize(dirty), dirty, 'leaves characters that are allowed');
   });
 
   sanitizeTests.test('it should allow attributes for links', async test => {
     const dirty =
       '<p><a title="Google" href="http://google.com" target="_blank" rel="noopener">google.com</a></p>';
-    test.equal(sanitize(dirty), dirty, 'allows link attributes');
+    test.equals(sanitize(dirty), dirty, 'allows link attributes');
+  });
+
+  sanitizeTests.test('it should allow style for spans', async test => {
+    const dirty =
+      '<span style="color:#3598db;text-decoration:line-through">styling</span>';
+    test.equals(sanitize(dirty), dirty, 'alows style attributes');
+  });
+
+  sanitizeTests.test('it should allow style for p', async test => {
+    const dirty = '<p style="padding-left:200px">big indent</p>';
+    test.equals(sanitize(dirty), dirty, 'allows style attributes');
+  });
+
+  sanitizeTests.test('it should allow different types of list', async test => {
+    const dirty =
+      '<ol style="list-style-type:upper-roman"><li>First<ol style="list-style-type:upper-alpha"><li>Second<ol style="list-style-type:lower-roman"><li>Third<ol style="list-style-type:lower-alpha"><li>Fourth<ol><li>Fifth<ol style="list-style-type:lower-greek"><li>Sixth<ul><li>Seventh<ul style="list-style-type:circle"><li>Eighth</li></ul></li></ul></li></ol></li></ol></li></ol></li></ol></li></ol></li></ol>';
+    test.equals(sanitize(dirty), dirty, 'allows list type attributes');
   });
 
   sanitizeTests.test(
@@ -27,7 +44,7 @@ tap.test('sanitize input', async sanitizeTests => {
     async test => {
       const dirty =
         '<a download hreflang media ping referrerpolicy type>link</a>';
-      test.equal(
+      test.equals(
         sanitize(dirty),
         '<a>link</a>',
         'prevents unallowed link attributes'
