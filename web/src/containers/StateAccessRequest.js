dedede54bda113490ceeaaa80d0e090e1b37aaec
Ty Bolt <tybolt@gmail.com>
2021-05-14 11:14:50 -0400

    use reducer hook to manage autocomplete logic, fix css issue with search icon

--- web/src/containers/StateAccessRequest.js
+++ web/src/containers/StateAccessRequest.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
 import { Autocomplete, Badge, TextField } from '@cmsgov/design-system';
 
@@ -7,65 +7,68 @@ import AuthenticationForm from '../components/AuthenticationForm';
 import { STATES } from '../util/states';
 
 const StateAccessRequest = ({ saveAction, errorMessage, fetching }) => {
-  // We maintain 4 objects to support the multiple state selection
-  // 1. initialStates - full list of states that can be filtered against. as
-  // states/items are selected by the user, they are removed from this list
-  const [initialStates, setInitialStates] = useState(STATES);
 
-  // 2. filteredStates - list of states that match search query
-  const [filteredStates, setFilteredStates] = useState([]);
-
-  // 3. selectedStates - array of chosen states. Used for both form submission
-  // and displaying the badges on the page
-  const [selectedStates, setSelectedStates] = useState([]);
+  const initialState = {
+    fullStateList: STATES,
+    filteredStates: STATES,
+    selectedStates: [],
+    inputValue: ''
+  };
   
-  // 4. queryValue - controlled value for what is displayed in the input field
-  const [inputValue, setInputValue] = useState('');
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'filter':
+        return {
+          ...state,
+          filteredStates: state.fullStateList.filter(el => el.name.toLowerCase().startsWith(action.payload.toLowerCase())),
+          inputValue: action.payload
+        }
+      case 'addSelectedState':
+      console.log("fullstates list", state.fullStateList)
+        return {
+          selectedStates: [...state.selectedStates, action.payload],
+          fullStateList: state.fullStateList.filter(item => item.id !== action.payload.id),
+          filteredStates: state.fullStateList.filter(item => item.id !== action.payload.id),
+          inputValue: ''
+        }
+      case 'removeSelectedState':
+        const newFullStatesList = state.fullStateList;
+        const addBackState = STATES.find(item => item.id === action.payload);
+        newFullStatesList.push(addBackState);
+        newFullStatesList.sort( (a, b) => a.name.localeCompare(b.name) );
+        return {
+          ...state,
+          fullStateList: newFullStatesList,
+          selectedStates: state.selectedStates.filter(item => item.id !== action.payload ),
+          inputValue: ''
+        }
+      default:
+        throw new Error();
+    }
+  }
+  
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleOnChange = selection => {
-    setInputValue('');
-    
-    // onChange can be triggered by hitting escape so we need to
-    // return when that happens if nothing is entered
+    // onChange can be triggered by hitting escape which will 
+    // pass a null value and make the subsequent actions fail
     if(selection === null) {
       return;
-    }
-
-    // add state to selectedStates
-    const newSelection = selectedStates;
-    newSelection.push(selection);
-    setSelectedStates(newSelection);
-    
-    // remove state from initialState list
-    setInitialStates(initialStates.filter(item => item.id !== selection.id));
-
-    // reset filteredStates
-    setFilteredStates(initialStates.filter(item => item.id !== selection.id));
+    }  
+    dispatch({type: 'addSelectedState', payload: selection})
   };
   
   const handleRemoveItem = element => {
-    setInputValue('');
-
-    // remove from selectedStates
-    const newSelection = selectedStates.filter(item => item.id !== element.target.dataset.id );
-    setSelectedStates(newSelection);
-    
-    // add back to initialStates list
-    const newInitialStates = initialStates;
-    const addBackState = STATES.find(item => item.id === element.target.dataset.id);
-    newInitialStates.push(addBackState);
-    newInitialStates.sort( (a, b) => a.name.localeCompare(b.name) );
-    setInitialStates(newInitialStates);
+    dispatch({type: 'removeSelectedState', payload: element.target.dataset.id});
   };
   
   const handleInputChange = query => {
-    setInputValue(query);
-    setFilteredStates(initialStates.filter(el => el.name.toLowerCase().indexOf(query.toLowerCase()) !== -1));
+    dispatch({type: 'filter', payload: query})
   }
 
   const handleSubmit = e => {
     e.preventDefault();
-    saveAction(selectedStates);
+    saveAction(state.selectedStates);
   };
 
   return (
@@ -76,7 +79,7 @@ const StateAccessRequest = ({ saveAction, errorMessage, fetching }) => {
         legend="Verify Your Identity"
         cancelable={false}
         className="ds-u-margin-top--7"
-        canSubmit={(selectedStates.length > 0)}
+        canSubmit={(state.selectedStates.length > 0)}
         error={errorMessage}
         success={null}
         working={fetching}
@@ -92,14 +95,14 @@ const StateAccessRequest = ({ saveAction, errorMessage, fetching }) => {
           </label>
 
           <Autocomplete
-            items={filteredStates}
+            items={state.filteredStates}
             onChange={handleOnChange}
             onInputValueChange={handleInputChange}
             clearSearchButton={false}
-            inputValue={inputValue}
+            inputValue={state.inputValue}
             id='state-selection'
           >
-            {selectedStates.map(el => {
+            {state.selectedStates.map(el => {
               return (
                 <Badge className="ds-u-margin-bottom--1" key={el.id} variation="info">
                   {el.name} {' '} 
