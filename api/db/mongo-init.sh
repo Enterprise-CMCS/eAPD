07da1e9dacb14dd7a24af122eaefdb56f24fb434
Tiffany <thetif@gmail.com>
2021-05-21 16:56:15 -0400

    added mongo to local dev

---
+++ api/db/mongo-init.sh
@@ -0,0 +1,1 @@
+mongo --eval "db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD'); db = db.getSiblingDB('$DB_NAME'); db.createUser({ user: '$DB_USER', pwd: '$DB_PASSWORD', roles: [{ role: 'dbOwner', db: '$DB_NAME' }] }); db.createCollection('apd'); db.createCollection('apd_history');"
