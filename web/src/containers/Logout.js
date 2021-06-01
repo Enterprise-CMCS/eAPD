c2fbae56f5ff97d1346d28cfd393923b5515a1a4
Ty Bolt <tybolt@gmail.com>
2021-06-01 10:31:10 -0400

    remove logout failure markup from Logout component

--- web/src/containers/Logout.js
+++ web/src/containers/Logout.js
@@ -6,7 +6,7 @@ import { useHistory } from 'react-router-dom';
 
 import { logout as dispatchLogout } from '../actions/auth';
 
-const Logout = ({ authenticated, failedLogout, logout, error }) => {
+const Logout = ({ authenticated, logout, error }) => {
   const history = useHistory();
   useEffect(() => {
     logout();
@@ -18,29 +18,11 @@ const Logout = ({ authenticated, failedLogout, logout, error }) => {
     }
   }, [authenticated]);
 
-  // Since we are still removing cookies we don't have to show this 
-  // message. Leaving this failedLogout condition just in case
-  return (
-    <div>
-    {failedLogout && (
-      <div className="site-body ds-l-container">
-        <main id="start-main-content">
-          <div className="ds-u-padding-top--2">
-            <h1>Unable to successfully logout</h1>
-            <p>There was an error attempting to logout. Please try again.</p>
-            <p>{error}</p>
-          </div>
-        </main>
-      </div>
-    )}
-    <span />
-    </div>
-  );
+  return <span />
 };
 
 Logout.propTypes = {
   authenticated: PropTypes.bool.isRequired,
-  failedLogout: PropTypes.bool.isRequired,
   logout: PropTypes.func.isRequired,
   error: PropTypes.string
 };
@@ -52,12 +34,10 @@ Logout.defaultProps = {
 const mapStateToProps = ({
   auth: {
     authenticated,
-    failedLogout,
     error
   }
 }) => ({
   authenticated,
-  failedLogout,
   error
 });
 
