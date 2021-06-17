dc6c31a1332b89d459a0785b50f9bdb7a15ce410
knollfear <mknoll@fearless.tech>
2021-06-17 16:04:15 -0400

    updating view only for assurances and compliance to match figma designs.

--- web/src/containers/viewOnly/AssurancesAndCompliance.js
+++ web/src/containers/viewOnly/AssurancesAndCompliance.js
@@ -1,43 +1,43 @@
 import PropTypes from 'prop-types';
-import React, { Fragment } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-import { CheckCircle, TimesCircle } from '../../components/Icons';
 import regLinks from '../../data/assurancesAndCompliance.yaml';
 import { selectFederalCitations } from '../../reducers/apd.selectors';
 import { t } from '../../i18n';
+import {LinkOrText} from '../AssurancesAndCompliance'
 
 const AssurancesAndCompliance = ({ citations }) => {
   return (
     <div>
       <h2>Assurances and Compliance</h2>
-      {Object.entries(regLinks).map(([name]) => (
+      {Object.entries(regLinks).map(([name, regulations]) => (
         <div key={name} className="ds-u-margin-bottom--3">
           <h4 className="ds-h4">
             {t(`assurancesAndCompliance.headings.${name}`)}
           </h4>
           <ul className="ds-c-list--bare">
             {citations[name].map(({ title, checked, explanation }) => (
-              <li key={title}>
-                {checked === true && (
-                  <span style={{ fontSize: '20px', paddingRight: '8px' }}>
-                    <CheckCircle />
-                  </span>
-                )}
-                {checked === false && (
-                  <span style={{ fontSize: '20px', paddingRight: '8px' }}>
-                    <TimesCircle />
-                  </span>
-                )}
-                {checked === '' && <div className="fake-radio-button" />}
-                {title}
-                {checked === false && (
-                  <Fragment>
-                    <br />
-                    <strong>Explanation:</strong>{' '}
-                    {explanation || <em>No explanation given</em>}
-                  </Fragment>
-                )}
+              <li key={title} style={{"page-break-inside": "avoid"}} className="ds-u-margin--1">
+                <div className="ds-u-padding-bottom--1">
+                  Does this APD comply with <LinkOrText link={regulations[title]} title={title} />?
+                </div>
+                {checked && <div><strong>Yes </strong></div> }
+
+                {checked === false &&
+                  <div className="ds-l-container ds-u-margin-left--0">
+                    <div className="ds-l-row">
+                      <div className="ds-l-col--1">
+                        <strong>No</strong>
+                      </div>
+                      <div className="ds-l-col--11" style={{ borderLeft:"3px solid #0071BC" }}>
+                        {explanation || <em>No response was provided</em>}
+                      </div>
+                    </div>
+                  </div>
+                }
+                {checked === '' && <div><strong>No response was provided</strong></div>}
+                <br/>
               </li>
             ))}
           </ul>
