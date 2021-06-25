c90a2624f2f60c7739d7b73b453ec82c186bc118
cthulhuplus <brookswm@gmail.com>
2021-06-25 14:36:52 -0400

    Hardcoded RedHat version into Mongo repo URL

--- packer/bootstrap.sh
+++ packer/bootstrap.sh
@@ -20,7 +20,7 @@ touch /etc/yum.repos.d/mongodb-org-4.4.repo
 echo "
 [mongodb-org-4.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.4/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/4.4/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
