d5c65fcd5da8b1042544130089ad33b492e271a1
cthulhuplus <brookswm@gmail.com>
2021-07-21 13:57:09 -0400

    Updating license populating method

--- bin/preview-deploy/aws.user-data.sh
+++ bin/preview-deploy/aws.user-data.sh
@@ -16,7 +16,7 @@ export OKTA_API_KEY="__OKTA_API_KEY__"
 export JWT_SECRET="__JWT_SECRET__"
 export MONGO_DATABASE="__MONGO_DATABASE__"
 export MONGO_URL="__MONGO_URL__"
-export NEW_RELIC_LICENSE_KEY="__NEW_RELIC_LICENSE_KEY__"
+echo "license_key: __NEW_RELIC_LICENSE_KEY__" >> /etc/newrelic-infra.yml
 cd ~
 mkdir -p /app/api/logs
 touch /app/api/logs/eAPD-API-error-0.log
