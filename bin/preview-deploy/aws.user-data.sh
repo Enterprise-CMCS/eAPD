1687032b4f22c35e2ca993d1ab1aadaee1fcfb7e
cthulhuplus <brookswm@gmail.com>
2021-07-22 09:48:25 -0400

    added a missing closing quote

--- bin/preview-deploy/aws.user-data.sh
+++ bin/preview-deploy/aws.user-data.sh
@@ -120,7 +120,7 @@ systemctl start newrelic-infra
 # configuration to be restored when it starts
 su - ec2-user -c '~/.bash_profile; sudo env PATH=$PATH:/home/ec2-user/.nvm/versions/node/v14.16.0/bin /home/ec2-user/.nvm/versions/node/v14.16.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user'
 su - ec2-user -c 'pm2 save'
-su - ec2-user -c 'pm2 restart "eAPD API'
+su - ec2-user -c 'pm2 restart "eAPD API"'
 
 # These logs aren't created until after the Instance is provisioned and need to be added to CloudWatch as a last step
 #touch /opt/aws/amazon-cloudwatch-agent/doc/launch-logs.json
