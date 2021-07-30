710a38273ad1c6d75aa4d241928748df53ae7cf7
Ty Bolt <tybolt@gmail.com>
2021-07-30 15:59:50 -0400

    update migration

--- api/migrations/20210729160006_add-expiration-date-to-affiliations.js
+++ api/migrations/20210729160006_add-expiration-date-to-affiliations.js
@@ -1,7 +1,44 @@
-exports.up = knex => {
-  return knex.schema.table('auth_affiliations', table => {
-    table.date('expires_at').comment('expiration date');
-  })
+/* eslint-disable camelcase */
+exports.up = async knex => {
+  await knex.schema.table('auth_affiliations', table => {
+    table
+      .date('expires_at')
+      .comment('expiration date');
+  });
+
+  const rolesQuery = await knex('auth_roles').where({isActive: true});
+
+  const roles = Object.fromEntries(
+    rolesQuery.map(item => [item.name, item.id])
+  )
+
+  knex('auth_affiliations')
+    .then(affiliations =>
+      Promise.all(
+        affiliations.map(({ id, role_id, created_at }) => {
+          if(role_id !== roles['eAPD System Admin'] 
+          && role_id !== roles['eAPD Federal Admin'] 
+          && role_id !== roles['eAPD State Admin']) {
+            return knex('auth_affiliations')
+              .where('status', 'approved')
+              .where('id', id)
+              .update({
+                expires_at: new Date(created_at.getFullYear() + 1, created_at.getMonth(), created_at.getDate())
+              })
+            }
+          if(role_id === roles['eAPD State Admin']) {
+            return knex('auth_affiliations')
+              .where('status', 'approved')
+              .where('id', id)
+              .update({
+                expires_at: new Date('06-30-2022')
+              })
+          }
+          return null;
+        })
+      )
+    )
+
 };
 
 exports.down = knex => {
