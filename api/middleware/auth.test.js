edb61697eed1b4ef4063a6fac446cb5155f0c76e
knollfear <mknoll@fearless.tech>
2021-08-02 16:11:27 -0400

    allowing can middleware to accept a list of permissions instead of a single permission.

--- api/middleware/auth.test.js
+++ api/middleware/auth.test.js
@@ -99,6 +99,48 @@ tap.test('"can" middleware', async canMiddlewareTest => {
       validTest.ok(next.called, 'endpoint handling chain is continued');
     }
   );
+
+
+  canMiddlewareTest.test(
+    'continues if the user has one of the expected activities',
+    async validTest => {
+      can(['foo', 'bar'])({ user: { activities: ['foo'] } }, res, next);
+
+      validTest.ok(res.send.notCalled, 'no body is sent');
+      validTest.ok(res.status.notCalled, 'HTTP status not set');
+      validTest.ok(res.end.notCalled, 'response is not terminated');
+      validTest.ok(next.called, 'endpoint handling chain is continued');
+    }
+  );
+
+  canMiddlewareTest.test(
+    'continues if the user has one of the expected activities',
+    async validTest => {
+      can(['foo', 'bar'])({ user: { activities: ['bar'] } }, res, next);
+
+      validTest.ok(res.send.notCalled, 'no body is sent');
+      validTest.ok(res.status.notCalled, 'HTTP status not set');
+      validTest.ok(res.end.notCalled, 'response is not terminated');
+      validTest.ok(next.called, 'endpoint handling chain is continued');
+    }
+  );
+
+  canMiddlewareTest.test(
+    'rejects if the user does not have the expected activity',
+    async invalidTest => {
+      can(['activity', 'quidditch'])({ user: { activities: ['foo', 'bar', 'baz', 'qux'] } }, res, next);
+
+      invalidTest.ok(
+        res.status.calledWith(403),
+        'HTTP status set to 403 Forbidden'
+      );
+      invalidTest.ok(res.end.called, 'response is terminated');
+      invalidTest.ok(
+        next.notCalled,
+        'endpoint handling chain is not continued'
+      );
+    }
+  );
 });
 
 tap.test('"validForState" middleware', async validForStateMiddlewareTest => {
