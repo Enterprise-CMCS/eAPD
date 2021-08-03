ab30c5281983565c232dece98e045baf4f7fc5aa
cthulhuplus <brookswm@gmail.com>
2021-08-03 09:25:27 -0400

    Creating mongo-init.sh to populate Mongo with users

--- bin/preview-deploy/mongo-init.sh
+++ bin/preview-deploy/mongo-init.sh
@@ -1,4 +1,9 @@
-echo 'Creating application user and db'
+echo 'Creating Mongo Admin User'
+
+mongo admin \
+        --eval "db.runCommand({'createUser' : '${PREVIEW_MONGO_INITDB_ROOT_USERNAME}','pwd' : '${PREVIEW_MONGO_INITDB_ROOT_PASSWORD}', 'roles' : [{'role' : 'root','db' : 'admin'}]});"
+
+echo 'Creating eAPD Application Eser and DB'
 
 mongo ${PREVIEW_MONGO_INITDB_DATABASE} \
         -u ${PREVIEW_MONGO_INITDB_ROOT_USERNAME} \
