9db6f1ec1b8869ee33d3ed60730d4459cd6d6dfc
cthulhuplus <brookswm@gmail.com>
2021-08-04 17:06:51 -0400

    Redoing the way I'm seeding mongo users

--- bin/preview-deploy/aws.user-data.sh
+++ bin/preview-deploy/aws.user-data.sh
@@ -111,14 +111,9 @@ sed -i "1 s|^|require('newrelic');\n|" main.js
 
 #Preparing Mongo DB Users
 cd ~
-cat <<MONGOUSERCONFIG > /home/ec2-user/mongo-init.sh
-echo 'Creating Mongo Admin User'
 mongo admin --eval "db.runCommand({'createUser' : '${PREVIEW_MONGO_INITDB_ROOT_USERNAME}','pwd' : '${PREVIEW_MONGO_INITDB_ROOT_PASSWORD}', 'roles' : [{'role' : 'root','db' : 'admin'}]});"
-
-echo 'Creating eAPD Application Eser and DB'
 mongo ${PREVIEW_MONGO_INITDB_DATABASE} -u ${PREVIEW_MONGO_INITDB_ROOT_USERNAME} -p ${PREVIEW_MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin --eval "db.createUser({user: '${PREVIEW_MONGO_DATABASE_USERNAME}', pwd: '${PREVIEW_MONGO_DATABASE_PASSWORD}', roles:[{role:'readWrite', db: '${PREVIEW_MONGO_INITDB_DATABASE}'}]});"
-MONGOUSERCONFIG
-sh /home/ec2-user/mongo-init.sh
+touch mongo-ran.txt
 E_USER
 
 sudo yum remove -y gcc-c++
