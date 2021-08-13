6c14603bf8877ed50610c074ef767e8ce55afd23
Tiffany <thetif@gmail.com>
2021-08-13 10:11:31 -0400

    removed expires_at from migration because it's already in Ty's PR

--- api/migrations/20210804195502_auth_affiliation_audit.js
+++ api/migrations/20210804195502_auth_affiliation_audit.js
@@ -1,45 +1,32 @@
-
-exports.up = async (knex) => {
+exports.up = async knex => {
   await knex.schema.createTable('auth_affiliation_audit', table => {
     table.comment('Audit log for changes to auth affiliations');
 
-    table
-      .increments('id')
+    table.increments('id');
 
-    table.timestamps(true, true)
+    table.timestamps(true, true);
 
-    table
-      .string('changed_by')
+    table.string('changed_by');
 
-    table
-      .integer('original_role_id')
+    table.integer('original_role_id');
 
-    table
-      .integer('new_role_id')
+    table.integer('new_role_id');
 
-    table
-      .string('original_status')
+    table.string('original_status');
 
-    table
-      .string('new_status')
-
-  })
+    table.string('new_status');
+  });
 
   await knex.schema.table('auth_affiliations', table => {
-    table.dropColumn('updated_by')
-    table
-      .dateTime('expires_at')
-  })
+    table.dropColumn('updated_by');
+  });
 };
 
-exports.down = async (knex) => {
-
-  await knex.schema.dropTable('auth_affiliation_audit')
+exports.down = async knex => {
+  await knex.schema.dropTable('auth_affiliation_audit');
   await knex.schema.table('auth_affiliations', table => {
     table
       .string('updated_by')
       .comment('id of user who last updated the record');
-
-    table.dropColumn('expires_at')
-  })
+  });
 };
