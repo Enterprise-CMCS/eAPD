d9830b44c56c46de3d2e352b7ebcd112f1064190
Ty Bolt <tybolt@gmail.com>
2021-08-16 09:23:35 -0400

    update migration

--- api/migrations/20210813145811_update-state-admin-certifications.js
+++ api/migrations/20210813145811_update-state-admin-certifications.js
@@ -1,21 +1,27 @@
 exports.up = async knex => {
   await knex.schema.table('state_admin_certifications', table => {
     table
-      .uuid('file_id')
+      .uuid('fileId')
       .comment(
         'unique identifier shared with the storage system; this value could be a foreign key, depending on the storage mechanism being used'
       );
-    table.integer('file_size').comment('size of the file, in bytes');
-    table.json('file_metadata').comment('any user-provided metadata about the file');
+    table.integer('fileSize').comment('size of the file, in bytes');
+    table.json('fileMetadata').comment('any user-provided metadata about the file');
     table.string('email').comment('email of the user being certified');
+  });
+  await knex.schema.table('state_admin_certifications_audit', table => {
+    table.enu('changeType', ['add', 'remove']).comment('type of change to certification; either add or remove');
   });
 };
 
 exports.down = async knex => {
   await knex.schema.table('state_admin_certifications', table => {
-    table.dropColumn('file_id');
-    table.dropColumn('file_size');
-    table.dropColumn('file_metadata');
+    table.dropColumn('fileId');
+    table.dropColumn('fileSize');
+    table.dropColumn('fileMetadata');
     table.dropColumn('email');
   })
+  await knex.schema.table('state_admin_certifications_audit', table => {
+    table.dropColumn('changeType');
+  });
 };
