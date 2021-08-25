4019bbe8e3ab7d36cb35ea0c7bf04a31cf9e3b35
chris-miraflor <87026821+chris-miraflor@users.noreply.github.com>
2021-08-25 09:50:40 -0700

    cmiraflor/3390 e2e tests for assurances and compliance (#3409)
    * added tests
    
    * moved tests
    
    * merged with development
    
    Co-authored-by: Tiffany <thetif@gmail.com>

---
+++ integrationTests/cypress/page-objects/assurances-compliance-page.js
@@ -0,0 +1,48 @@
+/* eslint class-methods-use-this: "off" */
+
+class AssurancesCompliancePage {
+  // assert if a link with the given text and URL reference exists here
+  assertLink = (category, name, ref) => {
+    cy.findByRole('heading', { name: category }).parent()
+      .find('a')
+      .contains(name)
+      .invoke('attr', 'href')
+      .then(href => {
+        cy.wrap(href).should('eq', ref);
+      });
+  };
+
+  // Returns the div containing the regulation + response fields
+  getRegulationDiv = (category, regulation) => {
+    cy.findByRole('heading', { name: category }).parent()
+      .contains('fieldset', regulation)
+  }
+
+  // Given a regulation, fills in an appropriate response.
+  // A null response results in checking "yes" for following the regulation
+  // A string response checks "no" and enters the string into the 
+  fillRegulation = (category, regulation, response) => {
+    cy.findByRole('heading', { name: category }).parent()
+      .contains('fieldset', regulation)
+      .as('regulationDiv');
+
+    if(response === null) {
+      cy.get('@regulationDiv')
+        .find('[value="yes"]')
+        .check({force: true});
+    } else {
+      cy.get('@regulationDiv')
+        .find('[value="no"]')
+        .check({force: true});
+
+      if(response.length > 0) {
+        cy.get('@regulationDiv')
+          .find('textarea')
+          .type(response);
+      }
+      
+    }
+  }
+}
+
+export default AssurancesCompliancePage;
