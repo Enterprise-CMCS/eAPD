952a16224561db5428116279c0d6eec087834139
knollfear <mknoll@fearless.tech>
2021-08-31 15:23:42 -0400

    finished up connecting tests to DB.

--- api/unit-test.sh
+++ api/unit-test.sh
@@ -1,9 +1,21 @@
+#!/usr/bin/env sh
+
 export NODE_ENV=test
-export TEST_DB_HOST=localhost
-export TEST_DB_PORT=55432
+export TEST_DB_HOST=db
+export TEST_DB_PORT=5432
+
 
 docker-compose -f docker-compose.endpoint-tests.yml up -d db
 
-npm run test
+docker-compose -f docker-compose.endpoint-tests.yml exec db sh -c 'PGPASSWORD=cms psql -U postgres -tc "DROP DATABASE IF EXISTS hitech_apd_test;"'
+docker-compose -f docker-compose.endpoint-tests.yml exec db sh -c 'PGPASSWORD=cms psql -U postgres -tc "CREATE DATABASE hitech_apd_test;"'
 
+docker-compose -f docker-compose.endpoint-tests.yml run api-for-testing npm run migrate
+docker-compose -f docker-compose.endpoint-tests.yml run api-for-testing npm run seed
 
+docker-compose -f docker-compose.endpoint-tests.yml run api-for-testing npm run test $@
+EXIT_CODE=$?
+
+docker-compose -f docker-compose.endpoint-tests.yml down
+
+exit $EXIT_CODE
