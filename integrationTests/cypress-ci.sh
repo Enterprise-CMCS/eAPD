543da303fb9f741ad435339791f751e4aaa02541
cthulhuplus <brookswm@gmail.com>
2021-09-02 13:35:28 -0400

    Created Cypress-CI script and update GHA to run it

---
+++ integrationTests/cypress-ci.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+export NODE_ENV=development
+export API_URL=http://localhost:8081
+export CYPRESS_TESTS=true
+
+docker-compose -f ../docker-compose.yml up -d
+docker-compose exec api npm run migrate
+docker-compose exec api npm run seed
+
+npx cypress run --headless
+
+docker-compose down
