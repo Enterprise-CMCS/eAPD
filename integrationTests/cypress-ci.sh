939b595e4af0a6be1e0f0cf58716112d7665c56b
cthulhuplus <brookswm@gmail.com>
2021-09-03 11:55:30 -0400

    Attempting to get Cypress running

--- integrationTests/cypress-ci.sh
+++ integrationTests/cypress-ci.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
 export NODE_ENV=development
-export API_URL=http://localhost:8081
-export CYPRESS_TESTS=true
+#export API_URL=http://localhost:8081
+#export CYPRESS_TESTS=true
 
 docker-compose -f ../docker-compose.yml up -d
 docker-compose exec api npm run migrate
 docker-compose exec api npm run seed
-docker-compose -f docker-compose.cypress.yml up -d $@
+#docker-compose -f docker-compose.cypress.yml up $@
 
-#npx cypress run --headless $@
+npx cypress run --headless $@
 EXIT_CODE=$?
 docker-compose down
 exit $EXIT_CODE
