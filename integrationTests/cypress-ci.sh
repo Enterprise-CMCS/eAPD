c72a58f6f439bc7bd6a95f7ee4e5931e2d86c08d
cthulhuplus <brookswm@gmail.com>
2021-09-07 14:55:03 -0400

    Latest attempts

--- integrationTests/cypress-ci.sh
+++ integrationTests/cypress-ci.sh
@@ -4,13 +4,15 @@ export NODE_ENV=development
 #export API_URL=http://localhost:8081
 export CYPRESS_TESTS=true
 
-docker-compose -f ../docker-compose.yml up --build -d --verbose
-docker-compose exec -t api npm run migrate
-docker-compose exec -t api npm run seed
-docker-compose exec -t api npm run cy:run:ci $@
+#docker-compose -f ../docker-compose.yml up --build -d --verbose
+#docker-compose exec -t api npm run migrate
+#docker-compose exec -t api npm run seed
+#docker-compose exec -t api npm run cy:run:ci $@
 #docker-compose -f docker-compose.cypress.yml up $@
 
 #npx cypress run --headless $@
+
+docker-compose -f ./docker-compose.cypress.yml up $@
 
 EXIT_CODE=$?
 docker-compose down
