9cee4365b3a4484febd9e057e660e1e960dc3399
cthulhuplus <brookswm@gmail.com>
2021-09-07 10:05:07 -0400

    Added verbose flag

--- integrationTests/cypress-ci.sh
+++ integrationTests/cypress-ci.sh
@@ -4,7 +4,7 @@ export NODE_ENV=development
 #export API_URL=http://localhost:8081
 export CYPRESS_TESTS=true
 
-docker-compose -f ../docker-compose.yml up --build -t -d && \
+docker-compose -f ../docker-compose.yml up --build -d --verbose && \
     docker-compose exec -t api npm run migrate && \
     docker-compose exec -t api npm run seed && \
     docker-compose exec -t api npm run cy:run:ci $@
