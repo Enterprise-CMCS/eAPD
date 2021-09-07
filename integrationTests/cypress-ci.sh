34775033d9c619f092c18a5c862c99ddc416b032
cthulhuplus <brookswm@gmail.com>
2021-09-07 10:20:50 -0400

    verbose OG style

--- integrationTests/cypress-ci.sh
+++ integrationTests/cypress-ci.sh
@@ -4,10 +4,10 @@ export NODE_ENV=development
 #export API_URL=http://localhost:8081
 export CYPRESS_TESTS=true
 
-docker-compose -f ../docker-compose.yml up --build -d --verbose && \
-    docker-compose exec -t api npm run migrate && \
-    docker-compose exec -t api npm run seed && \
-    docker-compose exec -t api npm run cy:run:ci $@
+docker-compose -f ../docker-compose.yml up --build -d --verbose
+docker-compose exec -t api npm run migrate
+docker-compose exec -t api npm run seed
+docker-compose exec -t api npm run cy:run:ci $@
 #docker-compose -f docker-compose.cypress.yml up $@
 
 #npx cypress run --headless $@
