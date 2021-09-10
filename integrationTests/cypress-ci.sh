f9535316a0cce123582b8c5cdbada2c690adba0e
cthulhuplus <brookswm@gmail.com>
2021-09-10 17:39:55 -0400

    More debuging and differnt copy attempt

--- integrationTests/cypress-ci.sh
+++ integrationTests/cypress-ci.sh
@@ -14,26 +14,23 @@ echo "Running migrate on API container"
 docker-compose exec -e OKTA_DOMAIN="$OKTA_DOMAIN" -e OKTA_API_KEY="$OKTA_API_KEY" -t api npm run migrate
 echo "Running seed on API container"
 docker-compose exec -e OKTA_DOMAIN="$OKTA_DOMAIN" -e OKTA_API_KEY="$OKTA_API_KEY" -t api npm run seed
-echo "Copying tokens from API container method 1"
+echo "Copying test tokens from API container method 1"
 docker cp $(docker ps -aqf "name=eapd_api_1"):./seeds/test/tokens.json ../api/seeds/test/
+echo "Copying development tokens from API container method 1"
+docker cp $(docker ps -aqf "name=eapd_api_1"):./seeds/development/tokens.json ../api/seeds/test/
 echo "Print API Container home directory contents"
 docker exec $(docker ps -aqf "name=eapd_api_1") ls -la
 echo "Print API Container seeds directory contents"
 docker exec $(docker ps -aqf "name=eapd_api_1") ls -la seeds
-echo "Print API Container home directory contents"
+echo "Print API Container seeds/test directory contents"
 docker exec $(docker ps -aqf "name=eapd_api_1") ls -la seeds/test
-echo "Print API Container test directory contents"
+echo "Print API Container seeds/test/tokens.json directory contents"
+docker exec $(docker ps -aqf "name=eapd_api_1") ls -la seeds/test/tokens.json
+echo "Print API Container seeds/development working directory"
+docker exec $(docker ps -aqf "name=eapd_api_1") ls -la seeds/test
+echo "Print API Container seeds/development/tokens.json directory contents"
 docker exec $(docker ps -aqf "name=eapd_api_1") ls -la seeds/test/tokens.json
 echo "Print API Container present working directory"
-docker exec $(docker ps -aqf "name=eapd_api_1") pwd
-echo "Print API Container seeds location from root"
-docker exec $(docker ps -aqf "name=eapd_api_1") ls -la /app/seeds/test/tokens.json
-echo "Print API Container seeds location from current"
-docker exec $(docker ps -aqf "name=eapd_api_1") ls -la ./seeds/test/tokens.json
-echo "Print running Docker containers"
-docker ps
-
-echo "NODE_ENV=$NODE_ENV"
 
 echo "Starting Cypress E2E Tests"
 npx cypress run $@
