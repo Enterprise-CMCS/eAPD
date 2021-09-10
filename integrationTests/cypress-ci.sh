ef2f5778ea09d2eec91a7a008ae18751a1eb3bbd
cthulhuplus <brookswm@gmail.com>
2021-09-10 14:27:52 -0400

    Fixed path for seeds and fixed fail/success status

--- integrationTests/cypress-ci.sh
+++ integrationTests/cypress-ci.sh
@@ -3,11 +3,6 @@ export OKTA_SERVER_ID="$OKTA_SERVER_ID"
 export OKTA_CLIENT_ID="$OKTA_CLIENT_ID"
 export OKTA_API_KEY="$OKTA_API_KEY"
 export JWT_SECRET="$JWT_SECRET"
-echo "OKTA_DOMAIN=$OKTA_DOMAIN"
-echo "OKTA_SERVER_ID=$OKTA_SERVER_ID"
-echo "OKTA_CLIENT_ID=$OKTA_CLIENT_ID"
-echo "OKTA_API_KEY=$OKTA_API_KEY"
-echo "JWT_SECRET=$JWT_SECRET"
 
 echo "Startng and backgrounding the App"
 docker-compose -f ../docker-compose.yml up -d
@@ -20,12 +15,14 @@ docker-compose exec -e OKTA_DOMAIN="$OKTA_DOMAIN" -e OKTA_API_KEY="$OKTA_API_KEY
 echo "Running seed on API container"
 docker-compose exec -e OKTA_DOMAIN="$OKTA_DOMAIN" -e OKTA_API_KEY="$OKTA_API_KEY" api npm run seed
 echo "Copying tokens from API container"
-docker cp eapd_api_1:/app/seeds/test/tokens.json ../../../api/seeds/test/tokens.json
+docker cp eapd_api_1:/app/seeds/test/tokens.json ../api/seeds/test/tokens.json
 
 echo "NODE_ENV=$NODE_ENV"
 
 echo "Starting Cypress E2E Tests"
-npx cypress run
+npx cypress run $@
 
+EXIT_CODE=$?
 echo "Shutting down the App"
 docker-compose -f ../docker-compose.yml down
+exit $EXIT_CODE
