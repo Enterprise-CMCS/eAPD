5d68a1d403168bf0709a093f065859daa514f0a9
Ty Bolt <tybolt@gmail.com>
2021-09-23 10:31:35 -0400

    fixes bugs, adds pagination

--- web/src/containers/admin/StateAdminLetters.js
+++ web/src/containers/admin/StateAdminLetters.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useMemo, useState } from 'react';
 import { useHistory } from "react-router-dom";
-import { useTable, useFilters, useGlobalFilter, useSortBy, useAsyncDebounce } from 'react-table';
+import { useTable, useFilters, useGlobalFilter, useSortBy, usePagination, useAsyncDebounce } from 'react-table';
 
 import {
   Button,
@@ -71,7 +71,117 @@ const mockapi = [
       "file": "/auth/certifications/09123098",
       "affiliationId": null,
       "potentialMatches": "0"
-  }
+  },
+  {
+      "id": 47,
+      "name": "State Admin",
+      "email": "stateadmin@fearless.tech",
+      "phone": "4105555555",
+      "state": "ak",
+      "ffy": "2020",
+      "file": "/auth/certifications/09e2109ei",
+      "affiliationId": null,
+      "potentialMatches": "1"
+  },
+  {
+      "id": 48,
+      "name": "Test Okta",
+      "email": "testokta@fearless.tech",
+      "phone": "4105554444",
+      "state": "md",
+      "ffy": "2019",
+      "file": "/auth/certifications/09e2109ei",
+      "affiliationId": null,
+      "potentialMatches": "1"
+  },
+  {
+      "id": 49,
+      "name": "State Contractor",
+      "email": "statecontractor@fearless.tech",
+      "phone": "4105553333",
+      "state": "ga",
+      "ffy": "2021",
+      "file": "/auth/certifications/09e2109ei",
+      "affiliationId": null,
+      "potentialMatches": "1"
+  },
+  {
+      "id": 50,
+      "name": "State Staff",
+      "email": "statestaff@fearless.tech",
+      "phone": "4105552222",
+      "state": "ak",
+      "ffy": "2021",
+      "file": "/auth/certifications/09e2109ei",
+      "affiliationId": null,
+      "potentialMatches": "1"
+  },
+  {
+      "id": 51,
+      "name": "Reese Set",
+      "email": "resetmfa@fearless.tech",
+      "phone": "4105551111",
+      "state": "tn",
+      "ffy": "2021",
+      "file": "/auth/certifications/09123098",
+      "affiliationId": null,
+      "potentialMatches": "0"
+  },
+  {
+      "id": 47,
+      "name": "State Admin",
+      "email": "stateadmin@fearless.tech",
+      "phone": "4105555555",
+      "state": "ak",
+      "ffy": "2020",
+      "file": "/auth/certifications/09e2109ei",
+      "affiliationId": null,
+      "potentialMatches": "1"
+  },
+  {
+      "id": 48,
+      "name": "Test Okta",
+      "email": "testokta@fearless.tech",
+      "phone": "4105554444",
+      "state": "md",
+      "ffy": "2019",
+      "file": "/auth/certifications/09e2109ei",
+      "affiliationId": null,
+      "potentialMatches": "1"
+  },
+  {
+      "id": 49,
+      "name": "State Contractor",
+      "email": "statecontractor@fearless.tech",
+      "phone": "4105553333",
+      "state": "ga",
+      "ffy": "2021",
+      "file": "/auth/certifications/09e2109ei",
+      "affiliationId": null,
+      "potentialMatches": "1"
+  },
+  {
+      "id": 50,
+      "name": "State Staff",
+      "email": "statestaff@fearless.tech",
+      "phone": "4105552222",
+      "state": "ak",
+      "ffy": "2021",
+      "file": "/auth/certifications/09e2109ei",
+      "affiliationId": null,
+      "potentialMatches": "1"
+  },
+  {
+      "id": 51,
+      "name": "Reese Set",
+      "email": "resetmfa@fearless.tech",
+      "phone": "4105551111",
+      "state": "tn",
+      "ffy": "2021",
+      "file": "/auth/certifications/09123098",
+      "affiliationId": null,
+      "potentialMatches": "0"
+  },
 ];
 
 // Using React-Table we will need to format the data before
@@ -99,6 +209,69 @@ function makeData(payload) {
   });
 };
 
+
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length
+  const [value, setValue] = React.useState(globalFilter)
+  
+  const onChange = useAsyncDebounce(value => {
+    console.log("whats happening here");
+    setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+    <span>
+      <TextField
+        name="globalFilter"
+        value={value || ""}
+        onChange={e => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        label={`Search all`}
+      />
+    </span>
+  )
+};
+
+function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id, Header },
+}) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set()
+    preFilteredRows.forEach(row => {
+      options.add(row.values[id])
+    })
+    return [...options.values()]
+  }, [id, preFilteredRows])
+
+  // Render a multi-select box
+  return (
+    <Dropdown
+      name="dropdownFilter"
+      label={Header}
+      value={filterValue}
+      options={[]}
+      onChange={e => {
+        setFilter(e.target.value || undefined)
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </Dropdown>
+  )
+};
+
 const StateAdminLetters = () => {  
   const history = useHistory();
   
@@ -119,6 +292,12 @@ const StateAdminLetters = () => {
       {
         Header: 'Phone Number',
         accessor: 'phone',
+        disableSortBy: true,
+      },
+      {
+        Header: 'FFY',
+        accessor: 'ffy',
+        disableSortBy: true,
       },
       {
         Header: 'State',
@@ -127,26 +306,21 @@ const StateAdminLetters = () => {
         filter: 'includes',
       },
       {
-        Header: 'FFY',
-        accessor: 'ffy',
-      },
-      {
         Header: 'Status',
         accessor: 'status',
         Filter: SelectColumnFilter,
         filter: 'includes',
+        disableSortBy: true,
       },
       {
         Header: 'View',
         accessor: 'file',
-        disableFilters: true,
-        disableSortBy: true
+        disableSortBy: true,
       },
       {
         Header: 'Actions',
         accessor: 'actions',
         disableSortBy: true,
-        disableFilters: true
       },
     ],
     []
@@ -154,120 +328,47 @@ const StateAdminLetters = () => {
   
   const data = React.useMemo(() => makeData(mockapi), [])
   
-  function GlobalFilter({
-    preGlobalFilteredRows,
-    globalFilter,
-    setGlobalFilter,
-  }) {
-    const count = preGlobalFilteredRows.length
-    const [value, setValue] = React.useState(globalFilter)
-    
-    const onChange = useAsyncDebounce(value => {
-      setGlobalFilter(value || undefined)
-    }, 200)
-  
-    return (
-      <span>
-        <TextField
-          value={value || ""}
-          onChange={e => {
-            setValue(e.target.value);
-            onChange(e.target.value);
-          }}
-          label={`Search all`}
-        />
-      </span>
-    )
-  };
-  
-  function SelectColumnFilter({
-    column: { filterValue, setFilter, preFilteredRows, id, Header },
-  }) {
-    // Calculate the options for filtering
-    // using the preFilteredRows
-    const options = React.useMemo(() => {
-      const options = new Set()
-      preFilteredRows.forEach(row => {
-        options.add(row.values[id])
-      })
-      return [...options.values()]
-    }, [id, preFilteredRows])
-  
-    // Render a multi-select box
-    return (
-      <Dropdown
-        label={Header}
-        value={filterValue}
-        options={options}
-        onChange={e => {
-          setFilter(e.target.value || undefined)
-        }}
-      >
-        <option value="">All</option>
-        {options.map((option, i) => (
-          <option key={i} value={option}>
-            {option}
-          </option>
-        ))}
-      </Dropdown>
-    )
-  }
-  
-  // Define a default UI for filtering
-  function DefaultColumnFilter({
-    column: { filterValue, preFilteredRows, setFilter },
-  }) {
-    const count = preFilteredRows.length
-  
-    return (
-      <TextField
-        value={filterValue || ''}
-        onChange={e => {
-          setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
-        }}
-      />
-    )
-  }  
-  
-  const defaultColumn = React.useMemo(
-    () => ({
-      // Let's set up our default Filter UI
-      Filter: DefaultColumnFilter,
-    }),
-    []
-  )
-  
   const {
    getTableProps,
    getTableBodyProps,
    headerGroups,
-   rows,
    prepareRow,
-   state,
+   state: { globalFilter, pageIndex, pageSize },
+   page,
+   canPreviousPage,
+   canNextPage,
+   pageOptions,
+   pageCount,
+   gotoPage,
+   nextPage,
+   previousPage,
+   setPageSize,
+   
    preGlobalFilteredRows,
    setGlobalFilter,
   } = useTable(
    { 
      columns, 
-     data,
-     defaultColumn
+     data
    },
    useFilters,
    useGlobalFilter,
-   useSortBy
+   useSortBy,
+   usePagination
   )  
   
   return (
     <div>
+     
       <Button onClick={handleAddStateButton} variation="primary">Add State Admin Letter</Button>
       
       <div className="ds-u-display--flex ds-u-justify-content--between" style={{maxWidth: '30rem'}}>
         {headerGroups[0].headers.find(item => item.Header === 'Status').render('Filter')}
         {headerGroups[0].headers.find(item => item.Header === 'State').render('Filter')}
         <GlobalFilter
-         preGlobalFilteredRows={preGlobalFilteredRows}
-         globalFilter={state.globalFilter}
-         setGlobalFilter={setGlobalFilter}
+           preGlobalFilteredRows={preGlobalFilteredRows}
+           globalFilter={globalFilter}
+           setGlobalFilter={setGlobalFilter}
         />
       </div>
       
@@ -286,9 +387,9 @@ const StateAdminLetters = () => {
                    {column.canSort ?
                       column.isSorted
                        ? column.isSortedDesc
-                         ? ' ▼'
-                         : ' ▲'
-                       : '⬍'
+                         ? ' ▾'
+                         : ' ▴'
+                       : ' ⬍'
                      : ''}
                  </span>
                </TableCell>
@@ -297,7 +398,7 @@ const StateAdminLetters = () => {
          ))}
        </TableHead>
        <TableBody {...getTableBodyProps()}>
-         {rows.map(row => {
+         {page.map((row, i) => {
            prepareRow(row)
            return (
              <TableRow {...row.getRowProps()}>
@@ -328,6 +429,39 @@ const StateAdminLetters = () => {
          })}
        </TableBody>
       </Table>
+      
+      <div className="ds-u-display--flex ds-u-justify-content--end ds-u-padding-y--2">
+        <label htmlFor="rowSizeSelect">Rows per page:</label>
+        <select
+          id="rowSizeSelect"
+          value={pageSize}
+          className="ds-u-margin-left--1 ds-u-margin-right--2"
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              {pageSize}
+            </option>
+          ))}
+        </select>
+        
+        <span className="ds-u-padding-x--1">
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        
+        <button style={{border: "none", background: "transparent"}} onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button style={{border: "none", background: "transparent"}} onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+      </div>
+      
     </div>
   )
 }
