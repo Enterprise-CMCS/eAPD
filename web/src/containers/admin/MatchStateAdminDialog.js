424e69c59f1982cc8949a3190466aea44b988d0f
Ty Bolt <tybolt@gmail.com>
2021-10-04 14:59:13 -0400

    updates state admin match dialog

--- web/src/containers/admin/MatchStateAdminDialog.js
+++ web/src/containers/admin/MatchStateAdminDialog.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Dialog, Dropdown, Button } from '@cmsgov/design-system';
 
+import axios from '../../util/api';
 import { STATES } from '../../util/states';
 
 const MatchStateAdminDialog = ({
   certification,
   hideModal
 }) => {
+  const [stateAffiliations, setStateAffiliations] = useState([]);
+  const [selectedUser, setSelectedUser] = useState({});
   
-  const handleDropdownUpdate = event => {
-    console.log("handleDropdownUpdate, event:", event);
+  useEffect(() => {
+    const state = certification.state.toLowerCase();
+    
+    async function fetchDataAndSetDefault() {
+      const affiliations = await axios.get(`/states/${state}/affiliations`);
+      setStateAffiliations(affiliations.data);
+      {/* Compare the certification email/name to the affiliations and make it the default */}
+      const match = affiliations.data.find(affiliation => {
+        return affiliation.email === certification.email || affiliation.displayName === certification.name;
+      });
+      setSelectedUser(match);
+    };
+    fetchDataAndSetDefault();
+  },[]);
+  
+  const handleUserSelect = async event => {
+    const userInfo = stateAffiliations.find(affiliation => affiliation.id == event.target.value);
+    setSelectedUser(userInfo);
   };
   
+  const handleMatch = event => {
+    console.log("submit the match");
+  }
+  
   const stateName = STATES.find(state => state.id === certification.state.toLowerCase()).name;
+  
+  const dropdownOptions = stateAffiliations.map(item => {
+    return {
+      label: item.displayName,
+      value: item.id
+    }
+  })
   
   return (
     <Dialog
@@ -21,45 +51,46 @@ const MatchStateAdminDialog = ({
       heading="Match State Admin Letter to User"
       className="ds-u-left-border-warning"
       size="full"
+      actionsClassName="ds-u-display--flex ds-u-justify-content--end ds-u-align-items--center"
       actions={[
+        <Button variation="transparent" onClick={hideModal} key="action2">
+          Cancel
+        </Button>,
         <Button
-          className="ds-u-margin-right--3 ds-u-margin-top--2"
-          onClick={() => {}}
+          onClick={handleMatch}
           key="action1"
           variation="primary"
         >
           Match and Approve Access
-        </Button>,
-        <Button variation="transparent" onClick={hideModal} key="action2">
-          Cancel
         </Button>
       ]}
       >
       <h3>All {stateName} Users</h3>
       <hr />
       <Dropdown
-        options={[]}
+        options={dropdownOptions}
         size="medium"
-        label={`Choose a ${stateName} user`}
+        label={`Select User`}
         name="selectedPermission"
-        value={"yep"}
-        onChange={() => {}}
+        onChange={handleUserSelect}
+        value={selectedUser.id}
       />
-      <div className="ds-l-row">
+      <div className="ds-l-row ds-u-padding-y--3">
         <div className="ds-l-col">
           <h4>EUA User Account</h4>
           <div className="ds-u-border--2 ds-u-padding--2">
-            <ul class="ds-c-list ds-c-list--bare" aria-labelledby="unstyled-list-id">
-              <li><strong>Email:</strong></li>
-              <li><strong>Phone:</strong></li>
-              <li><strong>State:</strong></li>
+            <ul className="ds-c-list ds-c-list--bare" aria-labelledby="unstyled-list-id">
+              <li><strong>{selectedUser.displayName}</strong></li>
+              <li><strong>Email:</strong> <span>{selectedUser.email}</span></li>
+              <li><strong>Phone:</strong> <span>{selectedUser.primaryPhone}</span></li>
+              <li><strong>State:</strong> <span>{selectedUser.stateId ? selectedUser.stateId.toUpperCase() : ''}</span></li>
             </ul>
           </div>
         </div>
         <div className="ds-l-col">
           <h4>State Admin Letter</h4>
           <div className="ds-u-border--2 ds-u-padding--2">
-            <ul class="ds-c-list ds-c-list--bare" aria-labelledby="unstyled-list-id">
+            <ul className="ds-c-list ds-c-list--bare" aria-labelledby="unstyled-list-id">
               <li><strong>{certification.name}</strong></li>
               <li><strong>Email:</strong> <span>{certification.email}</span></li>
               <li><strong>Phone:</strong> <span>{certification.phone}</span></li>
@@ -72,4 +103,4 @@ const MatchStateAdminDialog = ({
   )
 }
 
-export default MatchStateAdminDialog
+export default MatchStateAdminDialog;
