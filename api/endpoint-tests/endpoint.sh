f89905240fe88a88a3137ce3e5f2f5a45e479166
cthulhuplus <brookswm@gmail.com>
2021-10-04 16:02:51 -0400

    Reworking debugging

--- api/endpoint-tests/endpoint.sh
+++ api/endpoint-tests/endpoint.sh
@@ -3,18 +3,19 @@
 rm ./endpoint-tests/endpoint-data.json
 
 export NODE_ENV=test
+export NODE_DEBUG='net,tls,http,https'
 export API_URL=http://localhost:8081
 export CYPRESS_TESTS=false
 unset DEV_DB_NAME
 
 echo "[]" > endpoint-data.json
 
-ENDPOINT_COVERAGE_CAPTURE=true docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml up -d
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml exec api-for-testing npm run migrate
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml exec api-for-testing npm run seed
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml exec api-for-testing npm run test-endpoints $@
+ENDPOINT_COVERAGE_CAPTURE=true docker-compose -f docker-compose.endpoint-tests.yml up -d
+docker-compose -f docker-compose.endpoint-tests.yml exec -e NODE_DEBUG='net,tls,http,https' api-for-testing npm run migrate
+docker-compose -f docker-compose.endpoint-tests.yml exec -e NODE_DEBUG='net,tls,http,https' api-for-testing npm run seed
+docker-compose -f docker-compose.endpoint-tests.yml exec -e NODE_DEBUG='net,tls,http,https' api-for-testing npm run test-endpoints $@
 EXIT_CODE=$?
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml down
+docker-compose -f docker-compose.endpoint-tests.yml down
 
 mv endpoint-data.json ./endpoint-tests
 
--- api/unit-test.sh
+++ api/unit-test.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
 
 export NODE_ENV=test
+export NODE_DEBUG='net,tls,http,https'
 
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml up -d db
+docker-compose -f docker-compose.endpoint-tests.yml up -d db
 
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml exec db sh -c 'PGPASSWORD=cms psql -U postgres -tc "DROP DATABASE IF EXISTS hitech_apd_test;"'
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml exec db sh -c 'PGPASSWORD=cms psql -U postgres -tc "CREATE DATABASE hitech_apd_test;"'
+docker-compose -f docker-compose.endpoint-tests.yml exec -e NODE_DEBUG='net,tls,http,https' db sh -c 'PGPASSWORD=cms psql -U postgres -tc "DROP DATABASE IF EXISTS hitech_apd_test;"'
+docker-compose -f docker-compose.endpoint-tests.yml exec -e -e NODE_DEBUG='net,tls,http,https' db sh -c 'PGPASSWORD=cms psql -U postgres -tc "CREATE DATABASE hitech_apd_test;"'
 
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml run api-for-testing npm run migrate
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml run api-for-testing npm run seed
+docker-compose -f docker-compose.endpoint-tests.yml run -e NODE_DEBUG='net,tls,http,https' api-for-testing npm run migrate
+docker-compose -f docker-compose.endpoint-tests.yml run -e NODE_DEBUG='net,tls,http,https' api-for-testing npm run seed
 
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml run api-for-testing npm run test $@
+docker-compose -f docker-compose.endpoint-tests.yml run -e NODE_DEBUG='net,tls,http,https' api-for-testing npm run test $@
 EXIT_CODE=$?
 
-docker-compose -e NODE_DEBUG='net,tls,http,https' -f docker-compose.endpoint-tests.yml down
+docker-compose -f docker-compose.endpoint-tests.yml down
 
 exit $EXIT_CODE
