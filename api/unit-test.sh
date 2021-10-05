22f727de97124d5e37f42da716ab96e36c780ed9
cthulhuplus <brookswm@gmail.com>
2021-10-05 17:31:43 -0400

    Removing hardcoded versions and debugging information

--- api/unit-test.sh
+++ api/unit-test.sh
@@ -2,12 +2,6 @@
 
 export NODE_ENV=test
 
-npm cache clean --force
-#nvm use --delete-prefix v6.1.0 --silent
-nvm install 14
-nvm alias default v14
-which npm
-
 docker-compose -f docker-compose.endpoint-tests.yml up -d db
 
 docker-compose -f docker-compose.endpoint-tests.yml exec db sh -c 'PGPASSWORD=cms psql -U postgres -tc "DROP DATABASE IF EXISTS hitech_apd_test;"'
