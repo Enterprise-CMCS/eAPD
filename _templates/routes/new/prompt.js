2f0ed1b9f26f07d4b2ad8cc266ade7451fe6cbde
Michael Knoll <42681520+knollfear@users.noreply.github.com>
2021-10-14 13:04:02 -0400

    Hygen template for adding in an API endpoint (#3489)
    Co-authored-by: Tiffany <tforkner@fearless.tech>

---
+++ _templates/routes/new/prompt.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    type: 'select',
+    choices: ["get", "post", "put", "patch", "delete"],
+    name: 'httpVerb',
+    message: "What http verb is this route for?"
+  },
+  {
+    type: 'input',
+    name: 'httpPath',
+    message: "What is the path for this endpoint (foo/bar)"
+  },
+
+]
