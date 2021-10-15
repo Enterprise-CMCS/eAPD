ec47a8501ccee87f08dd78001eefd7589644c089
cthulhuplus <brookswm@gmail.com>
2021-10-15 14:01:26 -0400

    Cleaning up tests

--- integrationTests/cypress-ci.sh
+++ integrationTests/cypress-ci.sh
@@ -4,20 +4,6 @@ export OKTA_CLIENT_ID="$OKTA_CLIENT_ID"
 export OKTA_API_KEY="$OKTA_API_KEY"
 export JWT_SECRET="$JWT_SECRET"
 
-# Launch Dbus Session manager
-dbus-launch
-#echo "Startng and backgrounding the App"
-#docker-compose -f ../docker-compose.yml up -d
-
-#echo "Waiting for the App to start"
-#sleep 30
-
-#echo "Running migrate"
-#docker-compose exec -e OKTA_DOMAIN="$OKTA_DOMAIN" -e OKTA_API_KEY="$OKTA_API_KEY" -T api npm run migrate
-
-#echo "Running seed"
-#docker-compose exec -e OKTA_DOMAIN="$OKTA_DOMAIN" -e OKTA_API_KEY="$OKTA_API_KEY" -T api npm run seed
-
 echo "Creatng TOKEN_LOC env var"
 export TOKEN_LOC=$(docker exec eapd_api_1 find /app -type f -name tokens.json)
 
