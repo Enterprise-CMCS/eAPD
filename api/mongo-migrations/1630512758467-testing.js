09c960ff907f35feea0c604e9f4fa06ca298350a
Tiffany <tforkner@fearless.tech>
2021-10-19 14:52:51 -0400

    tforkner/3117-migrate-updates (#3455)

---
+++ api/mongo-migrations/1630512758467-testing.js
@@ -0,0 +1,15 @@
+/**
+ * Make any changes you need to make to the database here
+ */
+async function up () {
+  // Write migration here
+}
+
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+async function down () {
+  // Write migration here
+}
+
+module.exports = { up, down };
