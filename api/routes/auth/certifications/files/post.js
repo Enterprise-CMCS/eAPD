57498fe19c34fecd0baa3beb63025383e3744e8e
Ty Bolt <tybolt@gmail.com>
2021-10-19 10:00:25 -0400

    fix issue with get request being in post file

--- api/routes/auth/certifications/files/post.js
+++ api/routes/auth/certifications/files/post.js
@@ -18,31 +18,6 @@ module.exports = (
 ) => {
   logger.silly('setting up POST /auth/certifications/files route');
 
-  app.get(
-    '/auth/certifications/files/:fileID',
-    loggedIn,
-    can('view-state-certifications'),
-    async (req, res, next) => {
-      try {
-        const file = await getFile(req.params.fileID);
-        if(file) {
-          res.setHeader('Content-Type', 'application/octet-stream');
-          res.setHeader(
-            'Content-Disposition',
-            `attachment; filename=upload.pdf` // Todo: Capture metadata when file is uploaded and return it back
-          );
-          res.send(file).end();
-        } else {
-          res.status(400).end();
-        }
-      } catch (e) {
-        logger.error({ id: req.id, message: 'error fetching file' });
-        logger.error({ id: req.id, message: e });
-        next(e);
-      }
-    }
-  );
-
   app.post(
     '/auth/certifications/files',
     loggedIn,
