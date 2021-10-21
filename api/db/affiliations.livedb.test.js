611b3c1c55a9b9af525effd29b69df2675128514
Ty Bolt <tybolt@gmail.com>
2021-10-21 19:31:36 -0400

    adds db test

---
+++ api/db/affiliations.livedb.test.js
@@ -0,0 +1,120 @@
+const tap = require('tap');
+const knex = require('./knex')
+
+const { getAffiliationMatches } = require('./affiliations');
+
+const setupDB = async (oktaUsers, authAffiliations) =>{
+  await knex('okta_users').insert(oktaUsers);
+  await knex('auth_affiliations').insert(authAffiliations);
+}
+
+tap.test('database wrappers / affiliations with live db', async affiliations => {
+//   console.log("test started!");
+//   let stateStaffRoleId
+//   let stateAdminRoleId
+// 
+//   affiliations.before(async () => {
+//  
+//   });
+  
+  affiliations.test('only returns pending state admins and approved state staff', async test => {
+    let stateStaffRoleId = await knex('auth_roles')
+      .where({ name: 'eAPD State Staff' })
+      .first()
+      .then(role => role.id);
+  
+    let stateAdminRoleId = await knex('auth_roles')
+      .where({ name: 'eAPD State Admin' })
+      .first()
+      .then(role => role.id);
+      
+    const OKTA_USERS = {
+      stateAdminApproved: {
+        user_id: 'stateadmin1',
+        email: 'stateAdminApproved@email.com',
+        displayName:'should not be returned',
+        secondEmail: '',
+        primaryPhone: '',
+        mobilePhone: '',
+        login: 'test1'
+      },
+      stateAdminPending: {
+        user_id: 'stateadmin2',
+        email: 'stateAdminPending@email.com',
+        displayName:'should be returned',
+        secondEmail: '',
+        primaryPhone: '',
+        mobilePhone: '',
+        login: 'test2'
+      },
+      stateStaffRevoked: {
+        user_id: 'statestaff1',
+        email: 'stateStaffRevoked@email.com',
+        displayName:'shoould not be returned',
+        secondEmail: '',
+        primaryPhone: '',
+        mobilePhone: '',
+        login: 'test3'
+      },
+      stateStaffApproved: {
+        user_id: 'statestaff2',
+        email: 'stateStaffApproved@email.com',
+        displayName:'should be returned',
+        secondEmail: '',
+        primaryPhone: '',
+        mobilePhone: '',
+        login: 'test4'
+      }
+    };
+    
+    const AUTH_AFFILIATIONS = {
+      stateAdminApproved: {
+        user_id: 'stateadmin1',
+        state_id: 'ak',
+        role_id: stateAdminRoleId,
+        status: 'approved',
+        username: 'test1'
+      },
+      stateAdminPending: {
+        user_id: 'stateadmin2',
+        state_id: 'ak',
+        role_id: stateAdminRoleId,
+        status: 'requested',
+        username: 'test2'
+      },
+      stateStaffRevoked: {
+        user_id: 'statestaff1',
+        state_id: 'ak',
+        role_id: stateStaffRoleId,
+        status: 'revoked',
+        username: 'test3'
+      },
+      stateStaffApproved: {
+        user_id: 'statestaff2',
+        state_id: 'ak',
+        role_id: stateStaffRoleId,
+        status: 'approved',
+        username: 'test4'
+      }
+    };
+    
+    await knex('auth_affiliations').delete();
+    await knex('okta_users').delete();
+    
+    const oktaUsers = [OKTA_USERS.stateAdminApproved, OKTA_USERS.stateAdminPending, OKTA_USERS.stateStaffRevoked, OKTA_USERS.stateStaffApproved];
+    const authAffiliations = [AUTH_AFFILIATIONS.stateAdminApproved, AUTH_AFFILIATIONS.stateAdminPending, AUTH_AFFILIATIONS.stateStaffRevoked, AUTH_AFFILIATIONS.stateStaffApproved];
+    
+    await setupDB(oktaUsers, authAffiliations);
+  
+    const results = await getAffiliationMatches({stateId: 'ak'});
+    
+    test.same(results.length, 2);
+    test.notHas(AUTH_AFFILIATIONS.stateAdminApproved);
+    test.notHas(AUTH_AFFILIATIONS.stateStaffRevoked);
+  });
+  
+  affiliations.teardown(() =>{
+    knex.destroy()
+    return null
+  });
+});
