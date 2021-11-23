571ccf97fd958d589f4ef2c15c924cb51e6f01a3
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-11-23 10:24:47 -0500

    Add styling and  edit button

--- web/src/containers/ApdHeader.js
+++ web/src/containers/ApdHeader.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { Button } from '@cmsgov/design-system';
 import React from 'react';
 import { connect } from 'react-redux';
 import EditableLabel from 'react-inline-editing';
@@ -6,19 +7,33 @@ import EditableLabel from 'react-inline-editing';
 import { getAPDCreation, getAPDName, getAPDYearRange } from '../reducers/apd';
 import { setApdName } from '../actions/editApd';
 
+import Icon, { faEdit } from '../components/Icons';
+
 const ApdHeader = ({ apdName, setName, year }) => {
+
   return (
     <div>
-      <EditableLabel
-        text={apdName}
-        value={apdName}
-        labelClassName='ds-h1 apd--title'
-        inputClassName='ds-h1 apd--title'
-        inputWidth='250px'
-        inputHeight='46px'
-        onFocusOut={text => setName(text)}
-      />
-      <h1 className="ds-h4">
+      <div id='apd-title-container' className='flex-align-row'>
+        <div id='editable-label'>
+          <EditableLabel
+            text={apdName}
+            value={apdName}
+            labelClassName='ds-h1 apd--title'
+            labelWidth='250px'
+            inputClassName='ds-h1 apd--title'
+            inputWidth='250px'
+            inputHeight='46px'
+            onFocusOut={text => setName(text)}
+          />
+        </div>
+        <Button id='title-edit-link' class='ds-c-button ds-c-button--transparent' onClick={() => {
+          const titleEdit = document.getElementById('editable-label').firstChild;
+          titleEdit.click();
+        }}>
+          <Icon icon={faEdit} style={{ width: '14px' }} /> Edit
+        </Button>
+      </div>
+      <h1 className="ds-h5 ds-u-margin-top--1 ds-u-margin-bottom--3">
         HITECH IAPD | FFY {year}
       </h1>
     </div>
