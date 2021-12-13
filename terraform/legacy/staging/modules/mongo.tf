01b7a72f07caeb60bd7c204c8847729b10e458c8
cthulhuplus <brookswm@gmail.com>
2021-12-13 09:42:20 -0500

    Updated terraform ami for mongo

--- terraform/legacy/staging/modules/mongo.tf
+++ terraform/legacy/staging/modules/mongo.tf
@@ -13,7 +13,7 @@ data "aws_ami" "latest_golden_image" {
 
 resource "aws_instance" "eapd_mongo_staging" {
 #    ami                         = data.aws_ami.latest_golden_image.id
-    ami                         = "ami-0f2164e06ea15152d"
+    ami                         = "ami-0f5a45f74f4baa173"
     instance_type               = "m3.medium"
     vpc_security_group_ids      = ["sg-01e01435dbbe6ce32", aws_security_group.eapd-staging-mongo-ec2.id]
     subnet_id                   = "subnet-07e1b9ed6ed5fb8c7"
