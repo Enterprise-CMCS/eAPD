7cce766b6e5aab35d13adb97a9cbee644efbead5
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-12-14 12:57:23 -0500

    Comment out whole test

--- web/src/containers/ApdSummary.test.js
+++ web/src/containers/ApdSummary.test.js
@@ -1,131 +1,131 @@
-import React from 'react';
-import { renderWithConnection, screen } from 'apd-testing-library';
-import userEvent from '@testing-library/user-event';
+// import React from 'react';
+// import { renderWithConnection, screen } from 'apd-testing-library';
+// import userEvent from '@testing-library/user-event';
 
-import ApdSummary from './ApdSummary';
+// import ApdSummary from './ApdSummary';
 
-jest.mock('../util/api', () => ({
-  get: jest.fn(),
-  post: jest.fn(),
-  patch: jest.fn(),
-  delete: jest.fn()
-}));
+// jest.mock('../util/api', () => ({
+//   get: jest.fn(),
+//   post: jest.fn(),
+//   patch: jest.fn(),
+//   delete: jest.fn()
+// }));
 
-const defaultProps = {
-  addApdYear: jest.fn(),
-  removeApdYear: jest.fn(),
-  setHIE: jest.fn(),
-  setHIT: jest.fn(),
-  setMMIS: jest.fn(),
-  setOverview: jest.fn()
-};
+// const defaultProps = {
+//   addApdYear: jest.fn(),
+//   removeApdYear: jest.fn(),
+//   setHIE: jest.fn(),
+//   setHIT: jest.fn(),
+//   setMMIS: jest.fn(),
+//   setOverview: jest.fn()
+// };
 
-const setup = (props = {}) => {
-  return renderWithConnection(<ApdSummary {...defaultProps} {...props} />, {
-    initialState: {
-      apd: {
-        data: {
-          id: '123',
-          activities: [],
-          keyPersonnel: [],
-          incentivePayments: {
-            ehAmt: {
-              2020: {
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0
-              },
-              2021: {
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0
-              }
-            },
-            ehCt: {
-              2020: {
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0
-              },
-              2021: {
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0
-              }
-            },
-            epAmt: {
-              2020: {
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0
-              },
-              2021: {
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0
-              }
-            },
-            epCt: {
-              2020: {
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0
-              },
-              2021: {
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0
-              }
-            }
-          },
-          previousActivityExpenses: {
-            2020: {
-              hithie: {
-                federalActual: 0,
-                totalApproved: 0
-              },
-              mmis: {
-                90: { federalActual: 0, totalApproved: 0 },
-                75: { federalActual: 0, totalApproved: 0 },
-                50: { federalActual: 0, totalApproved: 0 }
-              }
-            },
-            2021: {
-              hithie: {
-                federalActual: 0,
-                totalApproved: 0
-              },
-              mmis: {
-                90: { federalActual: 0, totalApproved: 0 },
-                75: { federalActual: 0, totalApproved: 0 },
-                50: { federalActual: 0, totalApproved: 0 }
-              }
-            }
-          },
-          narrativeHIE: 'about hie',
-          narrativeHIT: 'about hit',
-          narrativeMMIS: 'about mmis',
-          programOverview: 'about the program',
-          years: ['2020', '2021'],
-          yearOptions: ['2020', '2021', '2022']
-        },
-        byId: {
-          123: {
-            name: 'test apd'
-          }
-        }
-      }
-    }
-  });
-};
+// const setup = (props = {}) => {
+//   return renderWithConnection(<ApdSummary {...defaultProps} {...props} />, {
+//     initialState: {
+//       apd: {
+//         data: {
+//           id: '123',
+//           activities: [],
+//           keyPersonnel: [],
+//           incentivePayments: {
+//             ehAmt: {
+//               2020: {
+//                 1: 0,
+//                 2: 0,
+//                 3: 0,
+//                 4: 0
+//               },
+//               2021: {
+//                 1: 0,
+//                 2: 0,
+//                 3: 0,
+//                 4: 0
+//               }
+//             },
+//             ehCt: {
+//               2020: {
+//                 1: 0,
+//                 2: 0,
+//                 3: 0,
+//                 4: 0
+//               },
+//               2021: {
+//                 1: 0,
+//                 2: 0,
+//                 3: 0,
+//                 4: 0
+//               }
+//             },
+//             epAmt: {
+//               2020: {
+//                 1: 0,
+//                 2: 0,
+//                 3: 0,
+//                 4: 0
+//               },
+//               2021: {
+//                 1: 0,
+//                 2: 0,
+//                 3: 0,
+//                 4: 0
+//               }
+//             },
+//             epCt: {
+//               2020: {
+//                 1: 0,
+//                 2: 0,
+//                 3: 0,
+//                 4: 0
+//               },
+//               2021: {
+//                 1: 0,
+//                 2: 0,
+//                 3: 0,
+//                 4: 0
+//               }
+//             }
+//           },
+//           previousActivityExpenses: {
+//             2020: {
+//               hithie: {
+//                 federalActual: 0,
+//                 totalApproved: 0
+//               },
+//               mmis: {
+//                 90: { federalActual: 0, totalApproved: 0 },
+//                 75: { federalActual: 0, totalApproved: 0 },
+//                 50: { federalActual: 0, totalApproved: 0 }
+//               }
+//             },
+//             2021: {
+//               hithie: {
+//                 federalActual: 0,
+//                 totalApproved: 0
+//               },
+//               mmis: {
+//                 90: { federalActual: 0, totalApproved: 0 },
+//                 75: { federalActual: 0, totalApproved: 0 },
+//                 50: { federalActual: 0, totalApproved: 0 }
+//               }
+//             }
+//           },
+//           narrativeHIE: 'about hie',
+//           narrativeHIT: 'about hit',
+//           narrativeMMIS: 'about mmis',
+//           programOverview: 'about the program',
+//           years: ['2020', '2021'],
+//           yearOptions: ['2020', '2021', '2022']
+//         },
+//         byId: {
+//           123: {
+//             name: 'test apd'
+//           }
+//         }
+//       }
+//     }
+//   });
+// };
 
 // describe('APD overview component', () => {
 //   test('dispatches on text change', () => {
