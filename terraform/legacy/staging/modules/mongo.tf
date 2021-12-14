ff9956c96219731101a4f5eaa119d3a34112965e
cthulhuplus <brookswm@gmail.com>
2021-12-14 17:01:09 -0500

    Changed Node environment to development

--- terraform/legacy/staging/modules/mongo.tf
+++ terraform/legacy/staging/modules/mongo.tf
@@ -13,7 +13,7 @@ data "aws_ami" "latest_golden_image" {
 
 resource "aws_instance" "eapd_mongo_staging" {
 #    ami                         = data.aws_ami.latest_golden_image.id
-    ami                         = "ami-0ff37aee2ab20fc99"
+    ami                         = "ami-02e55e7ca2265016c"
     instance_type               = "m3.medium"
     vpc_security_group_ids      = ["sg-01e01435dbbe6ce32", aws_security_group.eapd-staging-mongo-ec2.id]
     subnet_id                   = "subnet-07e1b9ed6ed5fb8c7"
