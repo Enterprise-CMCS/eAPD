991e787a9c4ef0aa3adbdd57d56a24e11cdd19fe
Tiffany <thetif@gmail.com>
2021-12-20 13:55:16 -0500

    adding more envs

--- integrationTests/cypress-ci-setup.sh
+++ integrationTests/cypress-ci-setup.sh
@@ -1,9 +1,3 @@
-export OKTA_DOMAIN="$OKTA_DOMAIN"
-export OKTA_SERVER_ID="$OKTA_SERVER_ID"
-export OKTA_CLIENT_ID="$OKTA_CLIENT_ID"
-export OKTA_API_KEY="$OKTA_API_KEY"
-export JWT_SECRET="$JWT_SECRET"
-
 echo "Creatng TOKEN_LOC env var"
 export TOKEN_LOC=$(docker exec eapd_api_1 find /app -type f -name tokens.json)
 
