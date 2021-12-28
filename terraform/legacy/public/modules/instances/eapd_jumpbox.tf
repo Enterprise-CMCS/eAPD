d7570704451357b16a255ba7aa55332e441eac93
cthulhuplus <brookswm@gmail.com>
2021-12-28 14:52:48 -0500

    Updating Docker login and Jumpbox tagging

--- terraform/legacy/public/modules/instances/eapd_jumpbox.tf
+++ terraform/legacy/public/modules/instances/eapd_jumpbox.tf
@@ -21,6 +21,10 @@ resource "aws_instance" "eapd_jumpbox_bb" {
     associate_public_ip_address = false
     tags = {
         Name = "eAPD Jumpbox BB"
+        "Patch Group" = "RHEL7"
+        "cms-cloud-exempt:open-sg" = "CLDSPT-5877"
+        "Patch Window" = "ITOPS-Wave1-Non-Mktplc-Prod-MW"
+        Terraform = "True"        
     }   
 }
 
@@ -34,6 +38,10 @@ resource "aws_instance" "eapd_jumpbox_tf" {
     associate_public_ip_address = false
     tags = {
         Name = "eAPD Jumpbox TF"
+        "Patch Group" = "RHEL7"
+        "cms-cloud-exempt:open-sg" = "CLDSPT-5877"
+        "Patch Window" = "ITOPS-Wave1-Non-Mktplc-Prod-MW"
+        Terraform = "True"
     }
 }
 
@@ -47,6 +55,10 @@ resource "aws_instance" "eapd_jumpbox_tb" {
     associate_public_ip_address = false
     tags = {
         Name = "eAPD Jumpbox TB"
+        "Patch Group" = "RHEL7"
+        "cms-cloud-exempt:open-sg" = "CLDSPT-5877"
+        "Patch Window" = "ITOPS-Wave1-Non-Mktplc-Prod-MW"
+        Terraform = "True"
     }   
 }
 
@@ -54,11 +66,15 @@ resource "aws_instance" "eapd_jumpbox_nz" {
 
     ami                         = data.aws_ami.latest_silver_image.id
     instance_type               = "t3.medium"
-    key_name                    = "keyname-tbd"
+    key_name                    = "eapd_bbrooks" #Needs valid keypair, placeholder until NZ creates one
     vpc_security_group_ids      = [ "sg-084cef5d95e485fd4", "sg-0f3c6bfa62fcefa4a", "sg-02271e574628f901f"]
     subnet_id                   = "subnet-04c22c81fd7060c13"
     associate_public_ip_address = false
     tags = {
-        Name = "eAPD Jumpbox TF"
+        Name = "eAPD Jumpbox NZ"
+        "Patch Group" = "RHEL7"
+        "cms-cloud-exempt:open-sg" = "CLDSPT-5877"
+        "Patch Window" = "ITOPS-Wave1-Non-Mktplc-Prod-MW"
+        Terraform = "True"
     }
 }
