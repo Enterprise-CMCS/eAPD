2f5ff8d857d53a98427dd50a0dc1ae479280e462
cthulhuplus <brookswm@gmail.com>
2022-01-04 13:47:49 -0500

    Attempting to fix preview mongo deploys

--- bin/preview-deploy/aws.user-data.sh
+++ bin/preview-deploy/aws.user-data.sh
@@ -113,10 +113,25 @@ sed -i "1 s|^|require('newrelic');\n|" main.js
 
 #Preparing Mongo DB Users
 cd ~
-cat <<MONGOUSERSEED > mongo-init.sh
-mongo admin --eval "db.runCommand({'createUser' : '','pwd' : '', 'roles' : [{'role' : 'root','db' : 'admin'}]});"
-mongo  -u  -p  --authenticationDatabase admin --eval "db.createUser({user: '', pwd: '', roles:[{role:'readWrite', db: ''}]});"
+#cat <<MONGOUSERSEED > mongo-init.sh
+#mongo admin --eval "db.runCommand({'createUser' : '','pwd' : '', 'roles' : [{'role' : 'root','db' : 'admin'}]});"
+#mongo $MONGO_INITDB_DATABASE --eval "db.runCommand({'createUser' : '$MONGO_INITDB_ROOT_USERNAME','pwd' : '$MONGO_INITDB_ROOT_PASSWORD', 'roles' : [{'role' : 'root','db' : '$MONGO_INITDB_DATABASE'}]});"
+#MONGOROOTUSERSEED
+#cd ~/eAPD/api
+#sh ~/mongo-init.sh
+#mongo  -u  -p  --authenticationDatabase admin --eval "db.createUser({user: '', pwd: '', roles:[{role:'readWrite', db: ''}]});"
+#MONGOUSERSEED
+cat <<MONGOROOTUSERSEED > mongo-init.sh
+mongo $MONGO_INITDB_DATABASE --eval "db.runCommand({'createUser' : '$MONGO_INITDB_ROOT_USERNAME','pwd' : '$MONGO_INITDB_ROOT_PASSWORD', 'roles' : [{'role' : 'root','db' : '$MONGO_INITDB_DATABASE'}]});"
+MONGOROOTUSERSEED
+cd ~/eAPD/api
+sh ~/mongo-init.sh
+NODE_ENV=production MONGO_ADMIN_URL=$MONGO_ADMIN_URL DATABASE_URL=$DATABASE_URL OKTA_DOMAIN=$OKTA_DOMAIN OKTA_API_KEY=$OKTA_API_KEY npm run migrate
+cd ~
+cat <<MONGOUSERSEED > mongo-user.sh
+mongo $MONGO_INITDB_DATABASE --eval "db.runCommand({'createUser' : '$MONGO_DATABASE_USERNAME','pwd' : '$MONGO_DATABASE_PASSWORD', 'roles' : [{'role' : 'dbOwner', 'db' :'$MONGO_DATABASE'}]});"
 MONGOUSERSEED
+sh ~/mongo-user.sh
 E_USER
 
 sudo yum remove -y gcc-c++
