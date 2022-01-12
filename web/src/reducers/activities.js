716049b8378fd8563cef63c073a470a5cae996ce
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-01-12 13:11:10 -0500

    Restart

--- web/src/reducers/activities.js
+++ web/src/reducers/activities.js
@@ -1,5 +1,6 @@
 import { arrToObj, generateKey as defaultGenerateKey } from '../util';
 
+
 // Make this thing injectible for testing.
 let generateKey = defaultGenerateKey;
 export const setKeyGenerator = fn => {
