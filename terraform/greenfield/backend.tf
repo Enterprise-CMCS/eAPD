3fd136988e9d546846994aed827b09963be4bd80
cthulhuplus <brookswm@gmail.com>
2022-02-03 16:47:42 -0500

    Updating terraform for Test

--- terraform/greenfield/backend.tf
+++ terraform/greenfield/backend.tf
@@ -1,13 +1,15 @@
+# Declare the provider
+provider "aws" {
+    region = var.aws_region
+    profile = "eAPD-${terraform.workspace}"
+}
+
+# Terraform Version is controlled by .terraform-version and is set to 1.0.11 (02/02/2022)
 terraform {
     backend "s3"{
-        bucket = "terraform-state-bucket-name"
+        profile = "eAPD-Test" # Will update to Prod when it comes online
+        bucket = "eapd-terraform-state"
         key = "terraform.tfstate"
         region = "us-east-1"
     }
 }
-
-# Declare the provider
-provider "aws" {
-    region = var.aws_region
-}
-
--- terraform/greenfield/variables.tf
+++ terraform/greenfield/variables.tf
@@ -1,13 +1,3 @@
-variable "eAPD" {
-    type = "map"
-    description = "IaC for eAPD Enviornments"
-    default = {
-        test = "eapd-terraform-test"
-        impl = "eapd-terraform-impl"
-        prod = "eapd-terraform-prod"
-    }
-}
-
 variable "aws_region" {}
   
 variable "env" {
