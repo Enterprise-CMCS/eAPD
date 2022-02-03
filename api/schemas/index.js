25a266b5eed741a870a3567a67ecbfdc73e22cec
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-02-03 11:28:06 -0500

    Update ajv type

--- api/schemas/index.js
+++ api/schemas/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-dynamic-require, global-require */
-const Ajv = require("ajv").default
+const Ajv = require("ajv").default({ allErrors: true });
 
 // all schemas, except the base apd document schema
 const schemas = [
