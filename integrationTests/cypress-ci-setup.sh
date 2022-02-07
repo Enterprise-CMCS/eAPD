97a30e63b1aef56bf2bc1b54382939504a555e42
Tiffany <thetif@gmail.com>
2022-02-07 14:21:06 -0500

    updated docker-compose for cypress; updated commands for starting up cypress

--- integrationTests/cypress-ci-setup.sh
+++ integrationTests/cypress-ci-setup.sh
@@ -1,17 +1,16 @@
-echo "Creatng TOKEN_LOC env var"
-export TOKEN_LOC=$(docker exec eapd-api-1 find /app -type f -name tokens.json)
+docker-compose -f docker-compose.cypress-ci.yml up -d
+sleep 3
+
+docker-compose -f docker-compose.cypress-ci.yml exec api npm run migrate
+docker-compose -f docker-compose.cypress-ci.yml exec api npm run seed
+
+echo "Creating TOKEN_LOC env var"
+export TOKEN_LOC=$(docker exec integrationtests_api_1 find /app -type f -name tokens.json)
 
 echo "Copying token"
 docker cp \
-  $(docker ps -aqf "name=eapd-api-1"):$TOKEN_LOC \
+  $(docker ps -aqf "name=integrationtests_api_1"):$TOKEN_LOC \
   ./tokens.json
 
 echo "Creating Cypress Env File"
 echo $CYPRESS_ENV >> ./cypress.env.json
-
-npx cypress run -C cypress.json --parallel --record $1 $2 $@
-
-EXIT_CODE=$?
-echo $EXIT_CODE
-
-exit $EXIT_CODE
