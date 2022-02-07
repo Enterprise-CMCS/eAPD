9f339db0ca38e3c61ab1d7916dd7eece1860afe7
Tiffany <thetif@gmail.com>
2022-02-07 10:04:59 -0500

    changed api container name

--- integrationTests/cypress-ci-setup.sh
+++ integrationTests/cypress-ci-setup.sh
@@ -1,9 +1,9 @@
 echo "Creatng TOKEN_LOC env var"
-export TOKEN_LOC=$(docker exec eapd_api_1 find /app -type f -name tokens.json)
+export TOKEN_LOC=$(docker exec eapd-api-1 find /app -type f -name tokens.json)
 
 echo "Copying token"
 docker cp \
-  $(docker ps -aqf "name=eapd_api_1"):$TOKEN_LOC \
+  $(docker ps -aqf "name=eapd-api-1"):$TOKEN_LOC \
   ./tokens.json
 
 echo "Creating Cypress Env File"
