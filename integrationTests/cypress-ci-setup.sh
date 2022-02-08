f417db64ccb678a2f93b99dc4b05bfbf0f00ec7e
Tiffany <thetif@gmail.com>
2022-02-08 15:30:07 -0500

    change machine to docker in circle config e2e test

--- integrationTests/cypress-ci-setup.sh
+++
@@ -1,7 +0,0 @@
-export NODE_ENV=development
-
-docker-compose exec api npm run migrate
-docker-compose exec api npm run seed
-
-echo "Creating Cypress Env File"
-echo $CYPRESS_ENV >> ./cypress.env.json
