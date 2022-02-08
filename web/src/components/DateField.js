b3ce91286c55d632d3b327d983b2ff5b951907f7
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-02-08 14:00:19 -0500

    Remove joinDate

--- web/src/components/DateField.js
+++ web/src/components/DateField.js
@@ -3,22 +3,6 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 import React, { useState, useMemo } from 'react';
 
-// The design system will call our formatter before calling our change event
-// handlers. This formatter puts the date pieces back into our internal date
-// string format.
-const joinDate = ({ day, month, year }) => {
-  // If the date is empty, revert to an empty string so it can save
-  // successfully.
-  if (!day && !month && !year) {
-    return '';
-  }
-
-  // Make sure it's an ISO-8601 date, which uses 2-digit month and day
-  return `${year}-${month < 10 ? `0${month}` : month}-${
-    day < 10 ? `0${day}` : day
-  }`;
-};
-
 const DateField = ({ value, onChange, ...rest }) => {
   const [errorInfo, setErrorInfo] = useState({
     errorMessage: [],
@@ -114,7 +98,6 @@ const DateField = ({ value, onChange, ...rest }) => {
       dayValue={dateParts.day}
       monthValue={dateParts.month}
       yearValue={dateParts.year}
-      dateFormatter={joinDate}
       onChange={onChange}
       onComponentBlur={getErrorMsg}
       errorPlacement='bottom'
