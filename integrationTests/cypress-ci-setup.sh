c27cd2873d6323b1c950a8792931b85b9dc973e6
Tiffany <thetif@gmail.com>
2022-02-08 11:26:10 -0500

    updated config to use local cypress instead of docker cypress; removed cypress-ci docker-compose file; changed cypress-ci-setup back to original version

--- integrationTests/cypress-ci-setup.sh
+++ integrationTests/cypress-ci-setup.sh
@@ -1,15 +1,9 @@
-docker-compose -f docker-compose.cypress-ci.yml up -d
-sleep 3
-
-docker-compose -f docker-compose.cypress-ci.yml exec api npm run migrate
-docker-compose -f docker-compose.cypress-ci.yml exec api npm run seed
-
 echo "Creating TOKEN_LOC env var"
-export TOKEN_LOC=$(docker exec integrationtests_api_1 find /app -type f -name tokens.json)
+export TOKEN_LOC=$(docker exec eapd_api_1 find /app -type f -name tokens.json)
 
 echo "Copying token"
 docker cp \
-  $(docker ps -aqf "name=integrationtests_api_1"):$TOKEN_LOC \
+  $(docker ps -aqf "name=eapd_api_1"):$TOKEN_LOC \
   ./tokens.json
 
 echo "Creating Cypress Env File"
