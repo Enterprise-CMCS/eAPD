b82b1f9d7e4764c526230266f1025da085426062
Tiffany <thetif@gmail.com>
2022-02-16 13:33:08 -0500

    update mongo failure print statement

--- api/db/mongodb.js
+++ api/db/mongodb.js
@@ -28,6 +28,7 @@ const setup = () =>
       })
       .catch(err => {
         logger.error(`Error in MongoDB connection: ${err}`);
+        logger.error(`Full error: ${JSON.stringify(err)}`);
         reject(err);
       });
   });
