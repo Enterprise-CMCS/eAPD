a30beb9f9d009573a9c62c57ce6a4ad1c4fe893a
cthulhuplus <brookswm@gmail.com>
2022-02-23 09:35:08 -0500

    Update preview instance size to t3.large

--- bin/preview-deploy/aws.sh
+++ bin/preview-deploy/aws.sh
@@ -166,7 +166,7 @@ function configureUserData() {
 #   PR_NUM - Github pull request number associated with this instance
 function createNewInstance() {
   aws ec2 run-instances \
-    --instance-type t4g.medium \
+    --instance-type t3.large \
     --image-id $1 \
     --security-group-ids "$AWS_SECURITY_GROUP" \
     --subnet-id "$AWS_SUBNET" \
