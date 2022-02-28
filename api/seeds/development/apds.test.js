6fe62dcea61d8e03f7ff65ef23ee96bf738b3d42
Ty Bolt <tybolt@gmail.com>
2022-02-28 10:57:39 -0500

    updates dev seed test

--- api/seeds/development/apds.test.js
+++ api/seeds/development/apds.test.js
@@ -3,14 +3,13 @@ const { data } = require('./apds');
 const APD = require('../../models/apd');
 
 tap.test('development APD seed document', async t => {
-  let seedApd = new APD(data[0]);
-  console.log("seedApd", seedApd);
+  const apdDoc = new APD(data[0]);
+  const validateApd = await apdDoc.validate().then((errors) => {
+    if (errors) {
+      return `errors: ${errors}`;      
+    }
+    return 'is valid, according to mongoose schema';
+  });
   
-  let validateApd = seedApd.validateSync();
-  
-  // console.log("validateApd", validateApd);
-  // t.ok(validateApd(apd), 'is valid, according to apd.json schema');
-  // t.notOk(validateApd.errors, 'has no reported errors');
-
-  t.equal([], validateApd.errors, 'has empty array of errors');
+  t.same(validateApd, 'is valid, according to mongoose schema');
 });
