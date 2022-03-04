3867ed74b15c2b60df887047993c2fb59501ae48
cthulhuplus <brookswm@gmail.com>
2022-03-04 16:37:55 -0500

    Adding first security pass to backend S3 bucket

--- terraform/greenfield/bootstrap/main.tf
+++ terraform/greenfield/bootstrap/main.tf
@@ -6,17 +6,22 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform-state-bucket" {
     bucket = "terraform-state-bucket-${formatdate("YYYYMMDDhhmmss", "2022-02-11T14:45:30-05:00")}"
-#    acl    = "private"
-#    server_side_encryption_configuration {
-#        rule {
-#            apply_server_side_encryption_by_default {
-#                sse_algorithm = "AES256"
-#            }
-#        }
-#    }
+
     tags = {
         Name = "Terraform State Bucket"
         Environment = "Test" # Currently deployed into Test
         Terraform = "Bootstrap" # Terraform method used to create
+    }
+}
+
+resource "aws_s3_bucket_acl" "terraform-state-bucket-acl" {
+    bucket = aws_s3_bucket.terraform-state-bucket.id
+    acl    = "private"
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
     }
 }
