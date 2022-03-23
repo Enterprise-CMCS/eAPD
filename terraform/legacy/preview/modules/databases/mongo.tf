3046e2ffb258f4cc06c462b6f35c922e5fff7beb
cthulhuplus <brookswm@gmail.com>
2022-03-23 09:48:22 -0400

    Updated user data script path

--- terraform/legacy/preview/modules/databases/mongo.tf
+++ terraform/legacy/preview/modules/databases/mongo.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "eapd_mongo" {
     vpc_security_group_ids      = ["sg-0bc99618daf4cd5da"]
     subnet_id                   = "subnet-09af713f4e068ac99"
     key_name                    = "eapd_bbrooks"
-    user_data                   = "../../../bin/prod-deploy/aws.user-data.sh"
+    user_data                   = "../../../../../bin/preview-deploy/aws.user-data.sh"
     associate_public_ip_address = true #This is a preview resource, should be moved to preview Terraform
     tags = {
         Name = var.instance_name
