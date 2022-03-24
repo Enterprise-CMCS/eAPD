a103a2adc72bf3222828c903994f4d1512120c88
Darren Mirano <dmirano@fearless.tech>
2022-03-24 14:42:38 -0400

    fixed eslint issue

--- web/src/pages/apd/ApdPageRoutes.js
+++ web/src/pages/apd/ApdPageRoutes.js
@@ -7,7 +7,6 @@ import {
   useRouteMatch as actualUseRouteMatch
 } from 'react-router-dom';
 
-<<<<<<< HEAD:web/src/pages/apd/ApdPageRoutes.js
 import ApdHeader from '../../layout/header/ApdHeader';
 import Activities from '../../containers/activity/All';
 import EntryPage from '../../containers/activity/EntryPage';
@@ -19,19 +18,6 @@ import PreviousActivities from '../../containers/PreviousActivities';
 import ProposedBudget from '../../containers/ProposedBudget';
 import ScheduleSummary from '../../containers/ScheduleSummary';
 import StateProfile from '../../components/ApdStateProfile';
-=======
-import ApdHeader from '../layout/header/ApdHeader';
-import Activities from './activity/All';
-import EntryPage from './activity/EntryPage';
-import AssurancesAndCompliance from './AssurancesAndCompliance';
-import Export from './ApdExport';
-import ApdSummary from './ApdSummary';
-import ExecutiveSummary from './ExecutiveSummary';
-import PreviousActivities from './PreviousActivities';
-import ProposedBudget from './ProposedBudget';
-import ScheduleSummary from './ScheduleSummary';
-import StateProfile from '../components/ApdStateProfile';
->>>>>>> 886da3951acbe1d7ffdbd7d636ac4e66de9a1d0f:web/src/containers/ApdPageRoutes.js
 
 const ApdPageRoutes = ({ apdId, useRouteMatch }) => {
   const { path } = useRouteMatch();
