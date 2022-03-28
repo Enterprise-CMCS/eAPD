8a6f9473c9d77989c9222c1671482836f101b6e0
Tiffany <thetif@gmail.com>
2022-03-28 16:38:02 -0400

    audit table for logging in (#3829)

--- api/db/roles.js
+++ api/db/roles.js
@@ -1,15 +1,18 @@
 const knex = require('./knex');
 
 const getAllActiveRoles = (requestedRoles, { db = knex } = {}) => {
-  const query = db('auth_roles')
-    .select('id', 'name')
-    .where({ isActive: true })
-  if(requestedRoles) {
+  const query = db('auth_roles').select('id', 'name').where({ isActive: true });
+  if (requestedRoles) {
     return query.whereIn('name', requestedRoles);
   }
   return query;
 };
 
+const getAllActivities = ({ db = knex } = {}) => {
+  return db('auth_activities').select();
+};
+
 module.exports = {
-  getAllActiveRoles
+  getAllActiveRoles,
+  getAllActivities
 };
