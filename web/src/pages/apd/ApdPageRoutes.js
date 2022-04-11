a94f8f2c98f609a68e9c7ca1911c03e44af63463
mirano-darren <87026092+mirano-darren@users.noreply.github.com>
2022-04-11 15:46:54 -0400

    Dmirano/3787 activites dashboard restructure (#3959)
    * layout restructure
    
    * updated tests
    
    * dashboard refactor
    
    * update tests
    
    * added login refactor
    
    * updated tests
    
    * added admin refactor
    
    * updated tests
    
    * route refactor
    
    * route refactor to /pages/
    
    * updated tests (not finishes yet)
    
    * apd components refactor
    
    * updated tests (route still causing issues)
    
    * updated tests
    
    * update tests, all pass now
    
    * fixed merge conflicts
    
    * relocated apd overview components
    
    * updated tests and renamed to ApdOverview
    
    * key state refactor
    
    * updated tests
    
    * previous activity restructure
    
    * updated tests
    
    * schedule summary refactor
    
    * update tests
    
    * executive summary restructure
    
    * updated tests
    
    * proposed budget refactor
    
    * updated tests
    
    * assurance and compliance restructure
    
    * updated tests
    
    * update tests after merge with proposed budget refactor
    
    * export refactor
    
    * updated tests
    
    * activities refactor
    
    * update tests
    
    * activities dashboard refactor
    
    * updated tests
    
    * updated failing tests

--- web/src/pages/apd/ApdPageRoutes.js
+++ web/src/pages/apd/ApdPageRoutes.js
@@ -8,7 +8,7 @@ import {
 } from 'react-router-dom';
 
 import ApdHeader from '../../layout/header/ApdHeader';
-import Activities from '../../containers/activity/All';
+import Activities from './activities/activities-dashboard/ActivitiesDashboard';
 import EntryPage from '../../containers/activity/EntryPage';
 import AssurancesAndCompliance from './assurances-and-compliance/AssurancesAndCompliance';
 import Export from './export/ApdExport';
