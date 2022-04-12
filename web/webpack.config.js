ab98b9c169599b079b16d6e7c0185954db8bc0a3
Tiffany Forkner <thetif@gmail.com>
2022-04-12 17:55:43 -0400

    updating webpack

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -67,7 +67,7 @@ const config = {
             loader: 'sass-loader',
             options: {
               sassOptions: {
-                includePaths: [path.resolve(__dirname, '../node_modules')]
+                includePaths: ['node_modules']
               }
             }
           }
