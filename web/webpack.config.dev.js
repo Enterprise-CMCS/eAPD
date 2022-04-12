c1df3baef5c874e6c5989f56ac1be08e1c2807fe
Tiffany Forkner <thetif@gmail.com>
2022-04-12 17:29:53 -0400

    working on tinymce styling

--- web/webpack.config.dev.js
+++ web/webpack.config.dev.js
@@ -33,6 +33,21 @@ const config = {
           'style-loader',
           // Translates CSS into CommonJS
           'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      browsers: 'last 2 versions'
+                    }
+                  ]
+                ]
+              }
+            }
+          },
           // Compiles Sass to CSS
           {
             loader: 'sass-loader',
