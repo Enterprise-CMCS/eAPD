a562fc690d5d0f06822ecb0e9019136be67dc403
Darren Mirano <dmirano@fearless.tech>
2022-04-13 10:50:19 -0400

    fixed linting issues

---
+++ web/src/pages/apd/activities/oms/OutcomesAndMetrics/index.js
@@ -0,0 +1,4 @@
+import OutcomeAndMetricForm from './OutcomeAndMetricForm';
+import OutcomeAndMetricReview from './OutcomeAndMetricReview';
+
+export { OutcomeAndMetricForm, OutcomeAndMetricReview };
