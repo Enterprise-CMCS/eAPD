5523cbe3fbc9e07f66a4f55fd0997bf67f497cf1
Darren Mirano <dmirano@fearless.tech>
2022-04-14 10:05:24 -0400

    updated failing tests

--- web/src/containers/App.js
+++ web/src/containers/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import Routes from '../pages/Route';
-import Wrapper from './Wrapper';
-import Broadcast from './Broadcast';
-import AriaAnnounce from '../containers/AriaAnnounce';
-import AutoScrollToElement from './AutoScrollToElement';
+import Wrapper from '../components/Wrapper';
+import Broadcast from '../components/Broadcast';
+import AriaAnnounce from '../components/AriaAnnounce';
+import AutoScrollToElement from '../components/AutoScrollToElement';
 
 const App = () => (
   <Wrapper>
