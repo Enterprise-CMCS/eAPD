7d971ec4cc2eeb37c4d7666a198b4c86849e0a46
Tiffany Forkner <thetif@gmail.com>
2022-04-19 17:02:40 -0400

    removed console.logs

--- web/src/pages/apd/activities/contractor-costs/ContractorResource/ContractorResourceForm.js
+++ web/src/pages/apd/activities/contractor-costs/ContractorResource/ContractorResourceForm.js
@@ -47,18 +47,6 @@ const ContractorResourceForm = forwardRef(
       setFormValid(isValid);
     }, [isValid]); // eslint-disable-line react-hooks/exhaustive-deps
 
-    useEffect(() => {
-      console.log({ isValid, errors });
-      console.log(`getValues ${JSON.stringify(getValues(), null, 2)}`);
-      console.log(
-        `validation ${JSON.stringify(
-          validationSchema.validate(getValues()),
-          null,
-          2
-        )}`
-      );
-    }, [isValid, errors, getValues()]); // eslint-disable-line react-hooks/exhaustive-deps
-
     const apdFFYs = Object.keys(getValues('years'));
 
     const onSubmit = e => {
