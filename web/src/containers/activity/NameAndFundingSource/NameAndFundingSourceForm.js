79fb6dd1fe6fd9280c6be6f2e033ca5e4c689d44
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-04-19 09:04:16 -0400

    Name error added

--- web/src/containers/activity/NameAndFundingSource/NameAndFundingSourceForm.js
+++ web/src/containers/activity/NameAndFundingSource/NameAndFundingSourceForm.js
@@ -1,59 +1,105 @@
 import { ChoiceList, TextField } from '@cmsgov/design-system';
 import PropTypes from 'prop-types';
-import React, { Fragment, useCallback } from 'react';
+import React, { forwardRef, Fragment, useCallback, useEffect, useReducer } from 'react';
+import { useForm, useFieldArray, Controller } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
 import { connect } from 'react-redux';
 
+import Joi from 'joi';
 import {
   setActivityName,
   setActivityFundingSource
 } from '../../../actions/editActivity';
 
-const NameAndFundingSourceForm = ({
-  index,
-  item: { fundingSource, name },
-  setFundingSource,
-  setName
-}) => {
-  const changeName = useCallback(
-    ({ target: { value } }) => {
-      setName(index, value);
-    },
-    [index, setName]
-  );
+const schema = Joi.object({
+  name: Joi.string().required().messages({
+    'string.base': 'Activity Name is required',
+    'string.empty': 'Activity Name is required'
+  })
+})
 
-  const changeFundingSource = useCallback(
-    ({ target: { value } }) => {
-      setFundingSource(index, value);
-    },
-    [index, setFundingSource]
-  );
+const NameAndFundingSourceForm = forwardRef(
+  ({ 
+    index, 
+    item: { fundingSource, name }, 
+    setFundingSource, 
+    setFormValid, 
+    setName 
+  }, ref) => {
+    NameAndFundingSourceForm.displayName = 'NameAndFundingSourceForm';
 
-  const choices = ['HIT', 'HIE', 'MMIS'].map(choice => ({
-    checked: fundingSource === choice,
-    label: choice,
-    value: choice
-  }));
+    const {
+      control,
+      formState: { errors, isValid, isValidating }
+    } = useForm({
+      defaultValues: {
+        name: name,
+        fundingSource: fundingSource
+      },
+      mode: 'onBlur',
+      reValidateMode: 'onBlur',
+      resolver: joiResolver(schema)
+    });
 
-  return (
-    <Fragment>
-      <TextField
-        label="Activity name"
-        name="activity-name"
-        value={name}
-        onChange={changeName}
-        className="remove-clearfix"
-      />
-      <ChoiceList
-        choices={choices}
-        label="Program type"
-        labelClassName="ds-u-margin-bottom--1"
-        name="program-type"
-        onChange={changeFundingSource}
-        type="radio"
-      />
-    </Fragment>
-  );
-};
+    try {
+      schema.validateAsync({name})
+      console.log({fundingSource, name});
+    } catch(err) {
+      console.log(err);
+    }
+
+    const changeName = useCallback(
+      ({ target: { value } }) => {
+        setName(index, value);
+      },
+      [index, setName]
+    );
+
+    const changeFundingSource = useCallback(
+      ({ target: { value } }) => {
+        setFundingSource(index, value);
+      },
+      [index, setFundingSource]
+    );
+
+    const choices = ['HIT', 'HIE', 'MMIS'].map(choice => ({
+      checked: fundingSource === choice,
+      label: choice,
+      value: choice
+    }));
+
+    return (
+      <Fragment>
+        <Controller
+          name="name"
+          control={control}
+          render={({ field: {onChange, ...props} }) => (
+            <TextField
+              {...props}
+              label="Activity name"
+              value={name}
+              onChange={e => {
+                changeName(e);
+                onChange(e);
+              }}
+              className="remove-clearfix"
+              errorMessage={errors?.name?.message}
+              errorPlacement="bottom"
+            />
+          )}
+        />
+        <ChoiceList
+          choices={choices}
+          label="Program type"
+          labelClassName="ds-u-margin-bottom--1"
+          name="program-type"
+          onChange={changeFundingSource}
+          type="radio"
+        />
+      </Fragment>
+    );
+  }
+);
 
 NameAndFundingSourceForm.propTypes = {
   index: PropTypes.number.isRequired,
