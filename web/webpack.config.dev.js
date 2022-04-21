b21f2d5bf62d2291409443a00d6aca4fd6940dad
Tiffany Forkner <thetif@gmail.com>
2022-04-21 17:16:08 -0400

    set rule back to just sass

--- web/webpack.config.dev.js
+++ web/webpack.config.dev.js
@@ -27,7 +27,7 @@ const config = {
       // In dev, load our styles directly into the generated JS. That way
       // we got hot reloading on our Sass as well.
       {
-        test: /\.s?css$/,
+        test: /\.scss$/,
         use: [
           // Creates `style` nodes from JS strings
           'style-loader',
