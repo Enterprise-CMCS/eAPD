28b0136979098e91c906f381de4f9f40d7b64b0c
tbolt <tybolt@gmail.com>
2022-04-21 07:42:02 -0400

    update migration (#3994)

--- api/mongo-migrations/1647374289696-apd-schema.js
+++ api/mongo-migrations/1647374289696-apd-schema.js
@@ -22,48 +22,48 @@ async function up () {
   
   // Create new object by sections/new schema
   const updatedApds = apds.map(apd => {
-    return {
-      id: apd.id,
-      createdAt: apd.createdAt,
-      updatedAt: apd.updatedAt,
-      years: apd.years,
-      stateId: apd.stateId,
-      status: apd.status,
-      name: apd.name,
-      apdOverview: {
-        programOverview: apd.programOverview,
-        narrativeHIT: apd.narrativeHIT,
-        narrativeHIE: apd.narrativeHIE,
-        narrativeMMIS: apd.narrativeMMIS
-      },
-      keyStatePersonnel: {
-        medicaidDirector: apd.stateProfile.medicaidDirector,
-        medicaidOffice: apd.stateProfile.medicaidOffice,
-        keyPersonnel: apd.keyPersonnel
-      },
-      previousActivities: {
-        previousActivitySummary: apd.previousActivitySummary,
-        actualExpenditures: apd.previousActivityExpenses
-      },
-      activities: apd.activities.map(activity => ({
-        ...activity,
-        contractorResources: convertContractorResources(activity.contractorResources)
-      })),
-      proposedBudget: {
-        incentivePayments: apd.incentivePayments
-      },
-      assurancesAndCompliances: apd.federalCitations
+    if(apd.stateProfile) {
+      return {
+        id: apd._id,
+        createdAt: apd.createdAt,
+        updatedAt: apd.updatedAt,
+        years: apd.years,
+        stateId: apd.stateId,
+        status: apd.status,
+        name: apd.name,
+        apdOverview: {
+          programOverview: apd.programOverview,
+          narrativeHIT: apd.narrativeHIT,
+          narrativeHIE: apd.narrativeHIE,
+          narrativeMMIS: apd.narrativeMMIS
+        },
+        keyStatePersonnel: {
+          medicaidDirector: apd.stateProfile.medicaidDirector,
+          medicaidOffice: apd.stateProfile.medicaidOffice,
+          keyPersonnel: apd.keyPersonnel
+        },
+        previousActivities: {
+          previousActivitySummary: apd.previousActivitySummary,
+          actualExpenditures: apd.previousActivityExpenses
+        },
+        activities: apd.activities.map(activity => ({
+          ...activity,
+          contractorResources: convertContractorResources(activity.contractorResources)
+        })),
+        proposedBudget: {
+          incentivePayments: apd.incentivePayments
+        },
+        assurancesAndCompliances: apd.federalCitations
+      }
     }
   });
   
   // Update them into the database
-  try {
-    updatedApds.forEach( async apd => {
-      await this('APD').replaceOne(apd._id, { ...apd });
-    });
-  } catch (error) {
-    logger.error(error);
-  }
+  await Promise.all(updatedApds.map(async (apd) => {
+    await this('APD').replaceOne( { _id: apd.id }, { ...apd });
+  })).catch(err => {
+    logger.error(err)
+  });
 }
 
 /**
