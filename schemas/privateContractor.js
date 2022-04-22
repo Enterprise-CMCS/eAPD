0488b37a9e3dcbafdca7e87e438ea3e44014b7f5
Tiffany Forkner <thetif@gmail.com>
2022-04-22 16:40:09 -0400

    syncing privateContractor

--- schemas/privateContractor.js
+++ schemas/privateContractor.js
@@ -23,8 +23,8 @@ const schemas = Joi.object({
   }),
   end: Joi.date()
     .format('YYYY-MM-DD')
-    .min(Joi.ref('start'))
     .iso()
+    .min(Joi.ref('start'))
     .required()
     .messages({
       'date.base': 'Provide an end date.',
@@ -33,11 +33,12 @@ const schemas = Joi.object({
       'date.min': 'Provide an end date that is after the start date.',
       'any.ref': 'Provide an end date that is after the start date.'
     }),
-  totalCost: Joi.number().positive().required().messages({
+  totalCost: Joi.number().positive().allow(0).required().messages({
     'number.base': 'Provide a contract cost.',
     'number.empty': 'Provide a contract cost.',
     'number.format': 'Provide a contract cost greater than or equal to $0.',
-    'number.positive': 'Provide a contract cost greater than or equal to $0.'
+    'number.positive': 'Provide a contract cost greater than or equal to $0.',
+    'number.allow': 'Provide a contract cost greater than or equal to $0.'
   }),
   useHourly: Joi.string().required().messages({
     'string.base': 'Must select hourly or yearly.',
@@ -48,19 +49,24 @@ const schemas = Joi.object({
     then: Joi.object().pattern(
       /\d{4}/,
       Joi.object({
-        hours: Joi.number().positive().required().messages({
-          'number.base': 'Provide a number of hours.',
-          'number.empty': 'Provide a number of hours.',
+        hours: Joi.number().positive().allow(0).required().messages({
+          'number.base':
+            'Provide a number of hours greater than or equal to 0.',
           'number.format': 'Provide a valid number of hours.',
           'number.positive':
+            'Provide a number of hours greater than or equal to 0.',
+          'number.allow':
+            'Provide a number of hours greater than or equal to 0.',
+          'number.empty':
             'Provide a number of hours greater than or equal to 0.'
         }),
-        rate: Joi.number().positive().greater(0).required().messages({
-          'number.base': 'Provide an hourly rate.',
-          'number.empty': 'Provide an hourly rate.',
-          'number.format': 'Provide a valid dollar value.',
-          'number.positive': 'Provide an hourly rate greater than $0.',
-          'number.greater': 'Provide an hourly rate greater than $0.'
+        rate: Joi.number().positive().allow(0).required().messages({
+          'number.base': 'Provide an hourly rate greater than or equal to $0.',
+          'number.empty': 'Provide an hourly rate greater than or equal to $0.',
+          'number.positive':
+            'Provide an hourly rate greater than or equal to $0.',
+          'number.allow': 'Provide an hourly rate greater than or equal to $0.',
+          'number.format': 'Provide a valid dollar value.'
         })
       })
     ),
@@ -70,12 +76,13 @@ const schemas = Joi.object({
     is: 'no',
     then: Joi.object().pattern(
       /\d{4}/,
-      Joi.number().positive().greater(0).required().messages({
-        'number.base': 'Provide an annual cost.',
-        'number.empty': 'Provide an annual cost.',
-        'number.format': 'Provide a valid dollar value.',
-        'number.positive': 'Provide an annual cost greater than $0.',
-        'number.greater': 'Provide an annual cost greater than $0.'
+      Joi.number().positive().allow(0).required().messages({
+        'number.base': 'Provide an annual cost greater than or equal to $0.',
+        'number.empty': 'Provide an annual cost greater than or equal to $0.',
+        'number.positive':
+          'Provide an annual cost greater than or equal to $0.',
+        'number.allow': 'Provide an annual cost greater than or equal to $0.',
+        'number.format': 'Provide a valid dollar value.'
       })
     ),
     otherwise: Joi.any()
