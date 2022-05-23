1995d03d118ccfba0bbf03650f151705ea1f6541
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-05-23 16:08:01 -0400

    Key Person validation

--- web/src/pages/apd/key-state-personnel/ApdStateProfileMedicaidOffice.js
+++ web/src/pages/apd/key-state-personnel/ApdStateProfileMedicaidOffice.js
@@ -1,9 +1,10 @@
 import { Dropdown, TextField } from '@cmsgov/design-system';
 import PropTypes from 'prop-types';
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
 import { connect } from 'react-redux';
+
 import { titleCase } from 'title-case';
 import {
   setMedicaidDirectorEmail,
@@ -19,46 +20,47 @@ import { t } from '../../../i18n';
 import { selectKeyStatePersonnel } from '../../../redux/selectors/apd.selectors';
 import { selectState } from '../../../redux/reducers/user';
 import { STATES } from '../../../util';
-
 import Joi from 'joi';
+
+const medicaidDirectorSchema = Joi.object().keys({
+  name: Joi.string().required().messages({
+    'string.base': 'Provide the name of the State Medicaid Director.',
+    'string.empty': 'Provide the name of the State Medicaid Director.',
+    'string.min': 'Provide the name of the State Medicaid Director.',
+    'string.required': 'Provide the name of the State Medicaid Director.'
+  }),
+  email: Joi.string().required().messages({
+    'string.base': 'Provide the email address of the State Medicaid Director.',
+    'string.empty': 'Provide the email address of the State Medicaid Director.'
+  }),
+  phone: Joi.string().required().messages({
+    'string.base': 'Provide a valid phone number for the State Medicaid Director.',
+    'string.empty': 'Provide a valid phone number for the State Medicaid Director.'
+  })
+})
+
+const medicaidOfficeSchema = Joi.object({
+  address1: Joi.string().min(1).required().messages({
+    'string.base': 'Provide a mailing street address for the Medicaid office.',
+    'string.empty': 'Provide a mailing street address for the Medicaid office.'
+  }),
+  city: Joi.string().min(1).required().messages({
+    'string.base': 'Provide a city name.',
+    'string.empty': 'Provide a city name.'
+  }),
+  zip: Joi.string().required().messages({
+    'string.base': 'Provide a zip code.',
+    'string.empty': 'Provide a zip code.'
+  })
+});
+
+const keyStatePersonnelSchema = Joi.object({
+  medicaidDirector: medicaidDirectorSchema,
+  medicaidOffice: medicaidOfficeSchema
+})
 
 const dirTRoot = 'apd.stateProfile.directorAndAddress.director';
 const offTRoot = 'apd.stateProfile.directorAndAddress.address';
-
-const stateProfileSchema = Joi.object({
-  apdStateProfileName: Joi.string().trim().min(1).required().messages({
-    'string.base': 'Provide the name of the State Medicaid Director.',
-    'string.empty': 'Provide the name of the State Medicaid Director.',
-    'string.min': 'Provide the name of the State Medicaid Director.'
-  }),
-  apdStateProfileEmail: Joi.string().trim().min(1).required().messages({
-    'string.base': 'Provide the email address of the State Medicaid Director.',
-    'string.empty': 'Provide the email address of the State Medicaid Director.',
-    'string.min': 'Provide the email address of the State Medicaid Director.'
-  }),
-  apdStateProfilePhone: Joi.string().trim().min(1).required().messages({
-    'string.base': 'Provide a valid phone number for the State Medicaid Director.',
-    'string.empty': 'Provide a valid phone number for the State Medicaid Director.',
-    'string.min': 'Provide a valid phone number for the State Medicaid Director.'
-  }),
-  apdStateProfileAddr1: Joi.string().trim().min(1).required().messages({
-    'string.base': 'Provide a mailing street address for the Medicaid office.',
-    'string.empty': 'Provide a mailing street address for the Medicaid office.',
-    'string.min': 'Provide a mailing street address for the Medicaid office.'
-  }),
-  apdStateProfileAddr2: Joi.any(),
-  apdStateProfileCity: Joi.string().trim().min(1).required().messages({
-    'string.base': 'Provide a city name.',
-    'string.empty': 'Provide a city name.',
-    'string.min': 'Provide a city name.'
-  }),
-  apdStateProfileState: Joi.any(),
-  apdStateProfileZip: Joi.string().trim().min(1).required().messages({
-    'string.base': 'Provide a zip code.',
-    'string.empty': 'Provide a zip code.',
-    'string.min': 'Provide a zip code.'
-  })
-})
 
 const ApdStateProfile = ({
   defaultStateID,
@@ -72,10 +74,30 @@ const ApdStateProfile = ({
   setZip,
   keyStatePersonnel
 }) => {
-  ApdStateProfile.displayName = 'ApdStateProfile';
-
   const { medicaidDirector, medicaidOffice } = keyStatePersonnel;
 
+  const {
+    control,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      medicaidDirector: {
+        name: medicaidDirector.name,
+        email: medicaidDirector.email,
+        phone: medicaidDirector.phone
+      },
+      medicaidOffice: {
+        address1: medicaidOffice.address1,
+        address2: medicaidOffice.address2,
+        city: medicaidOffice.city,
+        state: medicaidOffice.state || defaultStateID,
+        zip: medicaidOffice.zip
+      }
+    },
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+    resolver: joiResolver(keyStatePersonnelSchema)
+  });
 
   const handleChange =
     action =>
@@ -83,56 +105,116 @@ const ApdStateProfile = ({
       action(value);
     };
 
+  console.log({errors})
+
   return (
     <Fragment>
       <fieldset>
         <legend className="ds-u-padding-bottom--1">
           {titleCase(t(`${dirTRoot}.title`))}
         </legend>
-        <TextField
-          name="apdStateProfileName"
-          label={t(`${dirTRoot}.labels.name`)}
-          value={medicaidDirector.name}
-          onChange={handleChange(setName)}
+        <Controller
+          name={`medicaidDirector.name`}
+          control={control}
+          render={({ field: { onChange, onBlur, ...props } }) => (
+            <TextField
+              {...props}
+              label={t(`${dirTRoot}.labels.name`)}
+              onBlur={onBlur}
+              onChange={({ target: { value } }) => {
+                onChange(value);
+                setName(value);
+              }}
+              errorMessage={errors?.medicaidDirector?.name?.message}
+              errorPlacement="bottom"
+            />
+          )}
         />
-        <TextField
-          name="apdStateProfileEmail"
-          label={t(`${dirTRoot}.labels.email`)}
-          value={medicaidDirector.email}
-          onChange={handleChange(setEmail)}
+        <Controller
+          name={`medicaidDirector.email`}
+          control={control}
+          render={({ field: { onChange, onBlur, ...props } }) => (
+            <TextField
+              {...props}
+              label={t(`${dirTRoot}.labels.email`)}
+              onBlur={onBlur}
+              onChange={({ target: { value } }) => {
+                onChange(value);
+                setEmail(value);
+              }}
+              errorMessage={errors?.medicaidDirector?.email?.message}
+              errorPlacement="bottom"
+            />
+          )}
         />
-        <TextField
-          name="apdStateProfilePhone"
-          label={t(`${dirTRoot}.labels.phone`)}
-          value={medicaidDirector.phone}
-          onChange={handleChange(setPhone)}
+        <Controller
+          name={`medicaidDirector.phone`}
+          control={control}
+          render={({ field: { onChange, onBlur, ...props } }) => (
+            <TextField
+              {...props}
+              label={t(`${dirTRoot}.labels.phone`)}
+              onBlur={onBlur}
+              onChange={({ target: { value } }) => {
+                onChange(value);
+                setPhone(value);
+              }}
+              errorMessage={errors?.medicaidDirector?.phone?.message}
+              errorPlacement="bottom"
+            />
+          )}
         />
       </fieldset>
 
       <fieldset>
-        <legend className="ds-u-padding-bottom--1">
+        <legend className="ds-u-padding-y--1">
           {titleCase(t(`${offTRoot}.title`))}
         </legend>
-        <TextField
-          name="apdStateProfileAddr1"
-          label={t(`${offTRoot}.labels.address1`)}
-          value={medicaidOffice.address1}
-          onChange={handleChange(setAddress1)}
+        <Controller
+          name={`medicaidOffice.address1`}
+          control={control}
+          render={({ field: { onChange, onBlur, ...props } }) => (
+            <TextField
+              {...props}
+              label={t(`${offTRoot}.labels.address1`)}
+              onBlur={onBlur}
+              onChange={({ target: { value } }) => {
+                onChange(value);
+                setAddress1(value);
+              }}
+              errorMessage={errors?.medicaidOffice?.address1?.message}
+              errorPlacement="bottom"
+            />
+          )}
         />
         <TextField
-          name="apdStateProfileAddr2"
+          name="apd-state-profile-addr2"
           label={t(`${offTRoot}.labels.address2`)}
           hint="Optional"
           value={medicaidOffice.address2}
-          onChange={handleChange(setAddress2)}
+          onChange={({ target: { value } }) => {
+            setAddress2(value)
+          }}
         />
         <div className="ds-l-row">
-          <TextField
-            name="apdStateProfileCity"
-            label={t(`${offTRoot}.labels.city`)}
-            value={medicaidOffice.city}
-            className="ds-l-col--6"
-            onChange={handleChange(setCity)}
+          <Controller
+            name={`medicaidOffice.city`}
+            control={control}
+            render={({ field: { onChange, onBlur, ...props } }) => (
+              <TextField
+                {...props}
+                label={t(`${offTRoot}.labels.city`)}
+                onBlur={onBlur}
+                value={medicaidOffice.city}
+                className="ds-l-col--6"
+                onChange={({ target: { value } }) => {
+                  onChange(value)
+                  setCity(value);
+                }}
+                errorMessage={errors?.medicaidOffice?.city?.message}
+                errorPlacement="bottom"
+              />
+            )}
           />
           <div className="ds-u-clearfix ds-l-col--6">
             <Dropdown
@@ -140,7 +222,9 @@ const ApdStateProfile = ({
               name="apd-state-profile-state"
               label={t(`${offTRoot}.labels.state`)}
               value={medicaidOffice.state || defaultStateID}
-              onChange={handleChange(setState)}
+              onChange={({ target: value }) => {
+                setState(value)
+              }}
               options={STATES.map(({ id, name }) => ({
                 label: name,
                 value: id.toUpperCase()
@@ -148,12 +232,24 @@ const ApdStateProfile = ({
             />
           </div>
         </div>
-        <TextField
-          name="apdStateProfileZip"
-          label={t(`${offTRoot}.labels.zip`)}
-          value={medicaidOffice.zip}
-          mask="zip"
-          onChange={handleChange(setZip)}
+        <Controller
+          name={`medicaidOffice.zip`}
+          control={control}
+          render={({ field: { onChange, onBlur, ...props } }) => (
+            <TextField
+              {...props}
+              label={t(`${offTRoot}.labels.zip`)}
+              onBlur={onBlur}
+              value={medicaidOffice.zip}
+              mask="zip"
+              onChange={({ target: { value } }) => {
+                onChange(value);
+                setZip(value);
+              }}
+              errorMessage={errors?.medicaidOffice?.zip?.message}
+              errorPlacement="bottom"
+            />
+          )}
         />
       </fieldset>
     </Fragment>
