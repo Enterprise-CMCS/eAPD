ed1f4c8aa9d33f01bdf1c07a1403e3258e3b0548
Ty Bolt <tybolt@gmail.com>
2022-05-25 10:12:41 -0400

    update snapshots, adds demo complete mode

--- web/src/components/AdminCheckPanel.js
+++ web/src/components/AdminCheckPanel.js
@@ -12,6 +12,7 @@ import Icon, { faExclamationTriangle, faArrowRight } from '../components/Icons';
 const AdminCheckPanel = ({showAdminCheck, metadata, toggleAdmin}) => {
   
   const [miniCheck, setMiniReview] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
   
   const handleClose = () => {
     toggleAdmin(false);
@@ -19,8 +20,12 @@ const AdminCheckPanel = ({showAdminCheck, metadata, toggleAdmin}) => {
   
   const toggleCollapse = () => {
     console.log("collapse the panel");
-    miniCheck === false ? setMiniReview(true) : setMiniReview(false);    
+    miniCheck ? setMiniReview(false) : setMiniReview(true);
     // toggleAdminCollapse(true);
+  };
+  
+  const toggleComplete = () => {
+    isComplete ? setIsComplete(false) : setIsComplete(true);
   };
 
   return (
@@ -48,77 +53,107 @@ const AdminCheckPanel = ({showAdminCheck, metadata, toggleAdmin}) => {
         >
           <div className="eapd-admin-check__header">
             <h2 className="ds-c-drawer__header-heading">Administrative Check</h2> {/* need to make this have a tabindex=0 at some point, the linter doesn't like it but we need it for initial focus */}
-            <Button variation="transparent" onClick={toggleCollapse}>{miniCheck ? "Expand" : "Collapse"}</Button>
-            {/* <Button size="small" onClick={handleClose} aria-label="Close help drawer">Close</Button> */}
+            {!isComplete && (
+              <Button variation="transparent" onClick={toggleCollapse}>{miniCheck ? "Expand" : "Collapse"}</Button>
+            )}
+            {isComplete && (
+              <Button variation="transparent" onClick={handleClose} aria-label="Close help drawer">Close</Button>
+            )}
           </div>
           {miniCheck && (
             <div className="eapd-admin-mini">
               <div className="ds-u-display--flex ds-u-justify-content--start ds-u-align-items--center">
-                <Badge size="big" variation="alert">3</Badge>
-                <span className="ds-h4 ds-u-margin-y--1 ds-u-padding-left--1 ds-u-color--error">Incomplete Required Fields Total</span>
+                <Badge size="big" variation={`${isComplete ? "success" : "alert"}`}>{isComplete ? "0" : "3"}</Badge>
+                <span className={`ds-h4 ds-u-margin-y--1 ds-u-padding-left--1 ${isComplete ? "ds-u-color--success" : "ds-u-color--error"}`}>Incomplete Required Fields Total</span>
               </div>
-              <h3 className="ds-u-font-size--base">
-                <strong>Key State Personnel: </strong> 
-                <span className="ds-u-font-weight--normal">2 field(s) to complete on page</span>
-              </h3>
-              <ol className="ds-u-padding-top--0 ds-u-padding-bottom--1 ds-c-list">
-                <li><strong>Email address: </strong>Provide the email address of the Medicaid Director.</li>
-                <li><strong>Phone number: </strong>Provide a valid phone number for the Medicaid Director.</li>
-              </ol>
-              <Button size="big" variation="primary">
-                Continue to Results of Previous Activities
-                <Icon className="ds-u-padding-left--1" icon={faArrowRight} />
-              </Button>
+              {isComplete && (
+                <Fragment>
+                  <p>Well Done! The Administrative Check is complete.</p>
+                  <p>Return to the <a href="#">Export and Submit page.</a></p>
+                </Fragment>
+              )}
+              {!isComplete && (
+                <Fragment>
+                  <h3 className="ds-u-font-size--base">
+                    <strong>Key State Personnel: </strong> 
+                    <span className="ds-u-font-weight--normal">2 field(s) to complete on page</span>
+                  </h3>
+                  <ol className="ds-u-padding-top--0 ds-u-padding-bottom--1 ds-c-list">
+                    <li><strong>Email address: </strong>Provide the email address of the Medicaid Director.</li>
+                    <li><strong>Phone number: </strong>Provide a valid phone number for the Medicaid Director.</li>
+                  </ol>
+                  <Button size="big" variation="primary">
+                    Continue to Results of Previous Activities
+                    <Icon className="ds-u-padding-left--1" icon={faArrowRight} />
+                  </Button>
+                </Fragment>
+              )}
+              <a className="ds-u-font-size--small" onClick={toggleComplete}> [Demo: Toggle Complete]</a>
             </div>
           )}
           {miniCheck === false && (
             <Fragment>
               <p>
-                Review the list below for any required fields in this APD which are missing content. These fields must be completed before submission to CMS.
+                Review the list below for any required fields in this APD which are missing content. These fields must be completed before submission to CMS. <a className="ds-u-font-size--small" onClick={toggleComplete}> [Demo: Toggle Complete]</a>
               </p>
               <div className="ds-u-border--2 ds-u-padding--2">
                 <div className="ds-u-display--flex ds-u-justify-content--end ds-u-text-align--right ds-u-align-items--center">
-                  <span className="ds-u-color--error ds-u-font-weight--bold ds-u-margin-right--2">Incomplete<br/>Required Fields</span>
-                  <span className="ds-u-fill--error ds-u-color--white ds-u-radius ds-u-padding-x--3 ds-u-padding-y--2 ds-u-font-size--2xl ds-u-font-weight--bold">{metadata.incomplete}</span>
+                  <span className={`${isComplete ? "ds-u-color--success" : "ds-u-color--error"} ds-u-font-weight--bold ds-u-margin-right--2`}>Incomplete<br/>Required Fields</span>
+                  <span className={`${isComplete ? "ds-u-fill--success" : "ds-u-fill--error"} ds-u-color--white ds-u-radius ds-u-padding-x--3 ds-u-padding-y--2 ds-u-font-size--2xl ds-u-font-weight--bold`}>{isComplete ? "0" : metadata.incomplete}</span>
                 </div>
                 <hr className="eapd-admin-check__divider" />
-                <h3>To Do</h3>
-                <Accordion bordered>
-                  {Object.keys(metadata.todo).map((key) => (
-                      <AccordionItem
-                        key={key}
-                        heading={
-                          <Fragment key={key}>
-                            <h3 className="ds-u-margin--0">{metadata.todo[key].name}: <span className="ds-u-font-weight--normal">{metadata.todo[key].incomplete} field(s) to complete</span></h3>
-                            <Link to={metadata.todo[key].link}>Go to the {metadata.todo[key].name} page.</Link>
-                          </Fragment>
-                        }
-                        defaultOpen={false}
-                      >
-                      <ol>
-                        {metadata.todo[key].fields.map((field) => (
-                          <li key={field.name} className="ds-h5">
-                            {field.name} <br />
-                            <span className="ds-u-font-weight--normal">{field.description}</span>
-                          </li>
-                        ))}
-                      </ol>
-                      </AccordionItem>
-                  ))}
-                </Accordion>
-                <h3>Recently Completed</h3>
-                {metadata.recents.length > 0 && (
-                  <div>
-                  {metadata.recents.map((item) => (
-                    <div key={item.section} className="ds-u-border--1 ds-u-margin-bottom--2 ds-u-padding--2">
-                      <h3 className="ds-u-margin--0">{item.page}</h3>
-                      <Link to={item.link}>Go to the {item.section} page.</Link>
+                {isComplete && (
+                  <Fragment>
+                    <h3 className="ds-u-text-align--center ds-text-heading--2xl ds-u-font-weight--normal ds-u-margin-top--1">Administrative Check is Complete</h3>
+                    <p className="ds-u-text-align--center ds-h4 ds-u-margin-top--1 ds-u-font-weight--normal">Return to the <a href="#">export and submit</a> page.</p>
+                    <div className="ds-u-display--flex ds-u-justify-content--center ds-u-padding--5">
+                      <svg id="complete-check" width="168" height="140" viewBox="0 0 168 140" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M161.993 5.00315L57.072 136.621L4.22056 15.7833L55.6805 62.9824L56.478 63.7139L57.4237 63.1877L161.993 5.00315Z" stroke="#FDB124" stroke-width="3"/>
+                      </svg>
                     </div>
-                  ))}
-                  </div>
+                  </Fragment>
                 )}
-                {metadata.recents.length === 0 && (
-                  <p>No sections recently completed</p>
+                {!isComplete && (
+                  <Fragment>
+                    <h3>To Do</h3>
+                    <Accordion bordered>
+                      {Object.keys(metadata.todo).map((key) => (
+                          <AccordionItem
+                            key={key}
+                            heading={
+                              <Fragment key={key}>
+                                <h3 className="ds-u-margin--0">{metadata.todo[key].name}: <span className="ds-u-font-weight--normal">{metadata.todo[key].incomplete} field(s) to complete</span></h3>
+                                <Link to={metadata.todo[key].link}>Go to the {metadata.todo[key].name} page.</Link>
+                              </Fragment>
+                            }
+                            defaultOpen={false}
+                          >
+                          <ol>
+                            {metadata.todo[key].fields.map((field) => (
+                              <li key={field.name} className="ds-h5">
+                                {field.name} <br />
+                                <span className="ds-u-font-weight--normal">{field.description}</span>
+                              </li>
+                            ))}
+                          </ol>
+                          </AccordionItem>
+                      ))}
+                    </Accordion>
+                    <h3>Recently Completed</h3>
+                    {metadata.recents.length > 0 && (
+                      <div>
+                      {metadata.recents.map((item) => (
+                        <div key={item.section} className="ds-u-border--1 ds-u-margin-bottom--2 ds-u-padding--2">
+                          <h3 className="ds-u-margin--0">{item.page}</h3>
+                          <Link to={item.link}>Go to the {item.section} page.</Link>
+                        </div>
+                      ))}
+                      </div>
+                    )}
+                    {metadata.recents.length === 0 && (
+                      <p>No sections recently completed</p>
+                    )}
+                  </Fragment>
                 )}
               </div>
             </Fragment>
