a666f84c46c8f15d481065c7467530088d658b19
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-05-31 14:15:45 -0400

    Change to static schema

--- web/src/pages/apd/key-state-personnel/ApdStateProfileMedicaidOffice.js
+++ web/src/pages/apd/key-state-personnel/ApdStateProfileMedicaidOffice.js
@@ -22,44 +22,8 @@ import { t } from '../../../i18n';
 import { selectKeyStatePersonnel } from '../../../redux/selectors/apd.selectors';
 import { selectState } from '../../../redux/reducers/user';
 import { STATES } from '../../../util';
-import Joi from 'joi';
 
-const medicaidDirectorSchema = Joi.object().keys({
-  name: Joi.string().required().messages({
-    'string.base': 'Provide the name of the State Medicaid Director.',
-    'string.empty': 'Provide the name of the State Medicaid Director.',
-    'string.min': 'Provide the name of the State Medicaid Director.',
-    'string.required': 'Provide the name of the State Medicaid Director.'
-  }),
-  email: Joi.string().required().messages({
-    'string.base': 'Provide the email address of the State Medicaid Director.',
-    'string.empty': 'Provide the email address of the State Medicaid Director.'
-  }),
-  phone: Joi.string().required().messages({
-    'string.base': 'Provide a valid phone number for the State Medicaid Director.',
-    'string.empty': 'Provide a valid phone number for the State Medicaid Director.'
-  })
-})
-
-const medicaidOfficeSchema = Joi.object({
-  address1: Joi.string().min(1).required().messages({
-    'string.base': 'Provide a mailing street address for the Medicaid office.',
-    'string.empty': 'Provide a mailing street address for the Medicaid office.'
-  }),
-  city: Joi.string().min(1).required().messages({
-    'string.base': 'Provide a city name.',
-    'string.empty': 'Provide a city name.'
-  }),
-  zip: Joi.string().required().messages({
-    'string.base': 'Provide a zip code.',
-    'string.empty': 'Provide a zip code.'
-  })
-});
-
-const keyStatePersonnelSchema = Joi.object({
-  medicaidDirector: medicaidDirectorSchema,
-  medicaidOffice: medicaidOfficeSchema
-})
+import medicaidDirectorSchema from '../../../static/schemas/medicaidDirector';
 
 const dirTRoot = 'apd.stateProfile.directorAndAddress.director';
 const offTRoot = 'apd.stateProfile.directorAndAddress.address';
@@ -99,7 +63,7 @@ const ApdStateProfile = ({
         zip: medicaidOffice.zip
       }
     },
-    resolver: joiResolver(keyStatePersonnelSchema)
+    resolver: joiResolver(medicaidDirectorSchema)
   });
 
   useEffect(() => {
