cc32991ecaa919d4f6ddd6ddd15320f1c43cc19a
Tiffany Forkner <thetif@gmail.com>
2022-06-02 14:15:28 -0400

    update oktaAuth to handle being null in storybook

--- web/src/layout/header/Header.stories.js
+++ web/src/layout/header/Header.stories.js
@@ -1,22 +1,44 @@
 import React from 'react';
-import { plain as Header } from './Header';
+import { renderWithProviderAndRouter } from 'apd-storybook-library';
+import Header from './Header';
 
 export default {
   title: 'Layout/Header/Header',
   component: Header,
+  includeStories: /.*Story$/,
   parameters: {
     jest: ['Header.test.js']
   }
 };
 
-export const Basic = args => <Header {...args} />;
-Basic.args = {
-  ariaExpanded: false,
-  showSiteTile: true,
-  authenticated: false,
-  currentUser: { username: 'User', displayName: 'Test User' },
-  isAdmin: false,
-  currentState: { id: 'AK' },
-  canViewStateAdmin: false,
-  pathname: '/'
-};
+export const Template = args => <Header {...args} />;
+
+export const UnauthenticatedUserStory = Template.bind({});
+UnauthenticatedUserStory.decorators = [
+  story =>
+    renderWithProviderAndRouter({
+      initialState: {
+        auth: {
+          authenticated: false
+        },
+        user: null,
+        router: {
+          location: {
+            pathname: '/'
+          }
+        }
+      },
+      story
+    })
+];
+
+// Basic.args = {
+//   ariaExpanded: false,
+//   showSiteTile: true,
+//   authenticated: false,
+//   currentUser: { username: 'User', displayName: 'Test User' },
+//   isAdmin: false,
+//   currentState: { id: 'AK' },
+//   canViewStateAdmin: false,
+//   pathname: '/'
+// };
