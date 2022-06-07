2fe456df8354d3404a133eb2cf495ac29d3f8f16
Tiffany Forkner <thetif@gmail.com>
2022-06-07 09:57:07 -0400

    moved hygen to main package; haven't written template yet

--- _templates/stories/new/prompt.js
+++ _templates/stories/new/prompt.js
@@ -1,7 +1,13 @@
 module.exports = [
   {
     type: 'input',
+    name: 'componentName',
+    message: 'What is the name of the component?'
+  },
+  {
+    type: 'input',
     name: 'componentPath',
-    message: 'What is the path of the component to write stories for?'
+    message:
+      'What is the path of the component? (i.e. components/ containers/ layout/ pages/...'
   }
 ];
