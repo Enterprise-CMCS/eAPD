0be9b5ee6e703d2802b40a698993e7cd74407f2c
cthulhuplus <brookswm@gmail.com>
2022-06-09 15:03:44 -0400

    Updating the Staging/Prod AMI creation process

--- bin/prod-deploy/aws.user-data.sh
+++ bin/prod-deploy/aws.user-data.sh
@@ -51,57 +51,13 @@ CWAGENTCONFIG
 
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/doc/cwagent.json
 
-# Add a user group for the default user, and make it the owner of the /app
-# directory, then give the directory group write permission
-groupadd eapd
-gpasswd -a ec2-user eapd
-mkdir /app
-chown -R :eapd /app
-chmod g+w /app
-
-# Oddly, EC2 images don't have git installed. Shruggy person.
-yum -y install git
-
-# Install New Relic Infrastructure Monitor
-curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/7/x86_64/newrelic-infra.repo
-yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
-yum install newrelic-infra -y
-
-# Become the default user. Everything between "<<E_USER" and "E_USER" will be
-# run in the context of this su command.
-su ec2-user <<E_USER
+# Prepare the environment
+sudo su - ec2-user <<E_USER
 # The su block begins inside the root user's home directory.  Switch to the
 # ec2-user home directory.
 cd ~
-
-# Add New Relic License Key to Infra Monitor config
 sudo sh -c "echo license_key: '__NEW_RELIC_LICENSE_KEY__' >> /etc/newrelic-infra.yml"
 
-# Create application logs
-mkdir -p /app/api/logs
-touch /app/api/logs/eAPD-API-error-0.log
-touch /app/api/logs/eAPD-API-out-0.log
-touch /app/api/logs/Database-migration-error.log
-touch /app/api/logs/Database-migration-out.log
-touch /app/api/logs/Database-seeding-error.log
-touch /app/api/logs/Database-seeding-out.log
-touch /app/api/logs/cms-hitech-apd-api.logs
-
-# Install nvm.  Do it inside the ec2-user home directory so that user will have
-# access to it forever, just in case we need to get into the machine and
-# manually do some stuff to it.
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-source ~/.bashrc
-
-# We're using Node 16.15.0, we care about minor/patch versions
-nvm install 16.15.0
-nvm alias default 16.15.0
-
-# Install pm2: https://www.npmjs.com/package/pm2
-# This is what'll manage running the API Node app. It'll keep it alive and make
-# sure it's running when the EC2 instance restarts.
-npm i -g pm2
-npm i -g yarn@1.22.18
 # Get the built API code
 cd /app
 echo __BUILDURL__ |tee /home/ec2-user/buildurl.txt
--- packer/backend20220608.sh
+++ packer/backend20220608.sh
@@ -3,6 +3,57 @@
 # Become root user to perform installation and configuration
 sudo su <<R_USER
 #!/bin/bash
+
+# Add a user group for the default user, and make it the owner of the /app
+# directory, then give the directory group write permission
+groupadd eapd
+gpasswd -a ec2-user eapd
+mkdir /app
+chown -R :eapd /app
+chmod g+w /app
+
+# Oddly, EC2 images don't have git installed.
+yum -y install git
+
+# Install New Relic Infrastructure Monitor
+curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/7/x86_64/newrelic-infra.repo
+yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
+yum install newrelic-infra -y
+
+# Become the default user. Everything between "<<E_USER" and "E_USER" will be
+# run in the context of this su command.
+su ec2-user <<E_USER
+# The su block begins inside the root user's home directory.  Switch to the
+# ec2-user home directory.
+cd ~
+
+# Create application logs
+mkdir -p /app/api/logs
+touch /app/api/logs/eAPD-API-error-0.log
+touch /app/api/logs/eAPD-API-out-0.log
+touch /app/api/logs/Database-migration-error.log
+touch /app/api/logs/Database-migration-out.log
+touch /app/api/logs/Database-seeding-error.log
+touch /app/api/logs/Database-seeding-out.log
+touch /app/api/logs/cms-hitech-apd-api.logs
+
+# Install nvm.  Do it inside the ec2-user home directory so that user will have
+# access to it forever, just in case we need to get into the machine and
+# manually do some stuff to it.
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+source ~/.bashrc
+
+# We're using Node 16.15.0, we care about minor/patch versions
+nvm install 16.15.0
+nvm alias default 16.15.0
+
+# Install pm2: https://www.npmjs.com/package/pm2
+# This is what'll manage running the API Node app. It'll keep it alive and make
+# sure it's running when the EC2 instance restarts.
+npm i -g pm2
+npm i -g yarn@1.22.18
+E_USER
+
 # Install New Relic Infrastructure Monitor
 curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/7/x86_64/newrelic-infra.repo
 yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
@@ -26,47 +77,47 @@ cat <<CWAPPLOGCONFIG > /opt/aws/amazon-cloudwatch-agent/doc/app-logs.json
         "collect_list": [
           {
             "file_path": "/app/api/logs/eAPD-API-error-0.log*",
-            "log_group_name": "preview/app/api/logs/eAPD-API-error-0.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/eAPD-API-error-0.log"
           },
           {
             "file_path": "/app/api/logs/eAPD-API-out-0.log*",
-            "log_group_name": "preview/app/api/logs/eAPD-API-out-0.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/eAPD-API-out-0.log"
           },
           {
             "file_path": "/app/api/logs/eAPD-API-*",
-            "log_group_name": "preview/app/api/logs/eAPD-API-combined-0.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/eAPD-API-combined-0.log"
           },
           {
             "file_path": "/var/log/mongodb/mongo.log*",
-            "log_group_name": "preview/app/api/logs/eAPD-API-combined-0.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/eAPD-API-combined-0.log"
           },              
           {
             "file_path": "/app/api/logs/Database-migration-error.log*",
-            "log_group_name": "preview/app/api/logs/Database-migration-error.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/Database-migration-error.log"
           },
           {
             "file_path": "/app/api/logs/Database-migration-out.log*",
-            "log_group_name": "preview/app/api/logs/Database-migration-out.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/Database-migration-out.log"
           },
           {
             "file_path": "/app/api/logs/Database-migration-*",
-            "log_group_name": "preview/app/api/logs/Database-migration-combined.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/Database-migration-combined.log"
           },          
           {
             "file_path": "/app/api/logs/Database-seeding-error.log*",
-            "log_group_name": "preview/app/api/logs/Database-seeding-error.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/Database-seeding-error.log"
           },
           {
             "file_path": "/app/api/logs/Database-seeding-out.log*",
-            "log_group_name": "preview/app/api/logs/Database-seeding-out.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/Database-seeding-out.log"
           },
           {
             "file_path": "/app/api/logs/Database-seeding-*",
-            "log_group_name": "preview/app/api/logs/Database-seeding-combined.log"
+            "log_group_name": "$ENVIRONMENT/app/api/logs/Database-seeding-combined.log"
           },                                           
           {
             "file_path": "/app/api/logs/cms-hitech-apd-api.logs*",
-            "log_group_name": "preview/app/api/logs/cms-hitech-apd-api.logs"              
+            "log_group_name": "$ENVIRONMENT/app/api/logs/cms-hitech-apd-api.logs"              
           }    
         ]
       }
@@ -85,51 +136,43 @@ cat <<CWVARLOGCONFIG > /opt/aws/amazon-cloudwatch-agent/doc/var-log.json
         "collect_list": [
           {
             "file_path": "/var/log/aide/aide.log*",
-            "log_group_name": "preview/var/log/aide/aide.log"
+            "log_group_name": "$ENVIRONMENT/var/log/aide/aide.log"
           },
           {
             "file_path": "/var/log/audit/audit.log*",
-            "log_group_name": "preview/var/log/audit/audit.log"
+            "log_group_name": "$ENVIRONMENT/var/log/audit/audit.log"
           },
           {
             "file_path": "/var/log/awslogs.log*",
-            "log_group_name": "preview/var/log/awslogs.log"
+            "log_group_name": "$ENVIRONMENT/var/log/awslogs.log"
           },
           {
             "file_path": "/var/log/cloud-init.log*",
-            "log_group_name": "preview/var/log/cloud-init.log"
+            "log_group_name": "$ENVIRONMENT/var/log/cloud-init.log"
           },
           {
             "file_path": "/var/log/cloud-init-output.log*",
-            "log_group_name": "preview/var/log/cloud-init-output.log"
+            "log_group_name": "$ENVIRONMENT/var/log/cloud-init-output.log"
           },
           {
             "file_path": "/var/log/cron*",
-            "log_group_name": "preview/var/log/cron"
+            "log_group_name": "$ENVIRONMENT/var/log/cron"
           },
           {
             "file_path": "/var/log/dmesg*",
-            "log_group_name": "preview/var/log/dmesg"
+            "log_group_name": "$ENVIRONMENT/var/log/dmesg"
           },
           {
             "file_path": "/var/log/maillog*",
-            "log_group_name": "preview/var/log/maillog"
+            "log_group_name": "$ENVIRONMENT/var/log/maillog"
           },
           {
             "file_path": "/var/log/messages*",
-            "log_group_name": "preview/var/log/messages"
-          },
-          {
-            "file_path": "/var/log/nginx/access.log*",
-            "log_group_name": "preview/var/log/nginx/access.log"
-          },
-          {
-            "file_path": "/var/log/nginx/error.log*",
-            "log_group_name": "preview/var/log/nginx/error.log"
+            "log_group_name": "$ENVIRONMENT/var/log/messages"
           },
           {
             "file_path": "/var/log/secure*",
-            "log_group_name": "preview/var/log/secure"
+            "log_group_name": "$ENVIRONMENT/var/log/secure"
           },
           {
             "file_path": "/var/log/mongodb/mongod.log*",
