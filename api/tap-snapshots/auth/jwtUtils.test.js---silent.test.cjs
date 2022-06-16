38970a4d1a49f55639caa63307e8e1c69df51baa
Tiffany Forkner <thetif@gmail.com>
2022-06-16 14:36:34 -0400

    added snapshots back

---
+++ api/tap-snapshots/auth/jwtUtils.test.js---silent.test.cjs
@@ -0,0 +1,16 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+
+exports[`auth/jwtUtils.test.js --silent TAP > output 1`] = `
+Object {
+  "algorithm": "HS256",
+  "audience": "eAPD-test",
+  "expiresIn": "12h",
+  "issuer": "eAPD",
+  "notBefore": "0",
+}
+`;
