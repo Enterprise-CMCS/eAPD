064fe51bb1bafdffc0139e1b4dcd898c451c874b
Tiffany Forkner <thetif@gmail.com>
2022-07-27 09:24:21 -0400

    setting adminCheck to false by default

--- web/src/redux/reducers/apd.js
+++ web/src/redux/reducers/apd.js
@@ -325,7 +325,7 @@ const initialState = {
   loaded: false,
   error: '',
   selectAPDOnLoad: false,
-  adminCheck: process.env.TEALIUM_ENV !== 'prod'
+  adminCheck: false
 };
 
 // eslint-disable-next-line default-param-last
