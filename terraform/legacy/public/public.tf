9c2e320654d4d9575ec34c2633143be215d65475
cthulhuplus <brookswm@gmail.com>
2022-07-29 11:50:00 -0400

    Fixed typo in Atlantis config

--- terraform/legacy/public/public.tf
+++ terraform/legacy/public/public.tf
@@ -40,4 +40,4 @@ module "instances" {
     eapd_jumpbox_subnet_id                    = var.eapd_jumpbox_subnet_id
 }
 
-# REreturn of the testing   
+# REreturn of the testing   Fixed typo in Atlanis confi
