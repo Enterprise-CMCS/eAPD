5e1e54a3cd164c839d7afaddf8106e2d7ef96843
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-01 11:07:25 -0400

    remove lint

--- web/src/pages/apd/proposed-budget/IncentivePayments.js
+++ web/src/pages/apd/proposed-budget/IncentivePayments.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { Fragment, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import { joiResolver } from '@hookform/resolvers/joi';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 
 import {
