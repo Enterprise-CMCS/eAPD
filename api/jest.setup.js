8213d4485cf4114ad7b91e3f681121860bf19075
Tiffany Forkner <thetif@gmail.com>
2022-08-03 18:14:34 -0400

    rolled back jest setup changes

--- api/jest.setup.js
+++ api/jest.setup.js
@@ -1,13 +1,3 @@
-const { setup, init } = require('./db/mongodb');
+const { setup } = require('./db/mongodb');
 
-module.exports = async () => {
-  // eslint-disable-next-line no-console
-  console.log('initializing MongoDB connection');
-  try {
-    await setup();
-    await init();
-    return 0;
-  } catch (err) {
-    return 1;
-  }
-};
+module.exports = async () => setup();
