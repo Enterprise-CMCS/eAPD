b303a8e7dc8399cdc0894d5d36230ac2fa27bfb7
Tiffany Forkner <thetif@gmail.com>
2022-08-11 13:35:11 -0400

    updated to tap tests

--- api/babel.config.js
+++ api/babel.config.js
@@ -1,10 +1,5 @@
 const defaultConfigs = require('../babel.config');
 
 module.exports = {
-  ...defaultConfigs,
-  env: {
-    test: {
-      plugins: ['istanbul']
-    }
-  }
+  ...defaultConfigs
 };
