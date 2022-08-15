19fabbb21f804e5dfeb6519fb41470fe1fa9f483
Tiffany Forkner <thetif@gmail.com>
2022-08-15 17:59:46 -0400

    moved builds to before everything in cypress-setup-ci

--- e2e/cypress-setup-ci.sh
+++ e2e/cypress-setup-ci.sh
@@ -5,12 +5,13 @@ export DEV_DB_HOST=localhost
 export DEV_DB_PORT=54325
 export TEALIUM_ENV="test"
 
+yarn workspace @cms-eapd/common build
+yarn workspace @cms-eapd/api build
+yarn workspace @cms-eapd/web build
+
 docker-compose up -d
 sleep 5
 
 docker-compose exec api yarn run migrate
 docker-compose exec api yarn run seed
 sleep 5
-
-yarn workspace @cms-eapd/common build
-yarn workspace @cms-eapd/api build
