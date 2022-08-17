2eb23061b4a41446b1e5e37a1f7c7708f193a4b1
Ty Bolt <tybolt@gmail.com>
2022-08-17 11:49:34 -0400

    adds snapshot test

--- web/src/pages/apd/activities/ffp/CostAllocateFFP.test.js
+++ web/src/pages/apd/activities/ffp/CostAllocateFFP.test.js
@@ -99,16 +99,16 @@ const initialState = {
       key123: {
         costsByFFY: {
           2022: {
-            federal: 2512373,
-            medicaidShare: 3349831,
-            state: 837458,
-            total: 3454831
+            federal: 0,
+            medicaidShare: 0,
+            state: 0,
+            total: 300
           },
           total: {
-            federal: 4316194,
-            medicaidShare: 5354076,
-            state: 1037882,
-            total: 5459076
+            federal: 0,
+            medicaidShare: 0,
+            state: 0,
+            total: 300
           }
         },
         quarterlyFFP: {
@@ -224,6 +224,20 @@ describe('the CostAllocateFFP component', () => {
     jest.resetAllMocks();
   });
 
+  it('renders tbale correctly', async () => {
+    await setup(
+      {},
+      {
+        initialState: initialState
+      }
+    );
+
+    const table = screen.getByRole('table', {
+      name: `Activity 1 Budget for FFY ${initialState.apd.data.years[0]}`
+    });
+    expect(table).toMatchSnapshot();
+  });
+
   it('renders page title', async () => {
     await setup(
       {},
@@ -311,188 +325,4 @@ describe('the CostAllocateFFP component', () => {
     const error = await screen.findByText('Select a federal-state split.');
     expect(error).toBeInTheDocument();
   });
-
-  //
-  //   it('handles updating federal-state split dropdown', async () => {
-  //     const { user } = await setup(
-  //       {},
-  //       {
-  //         initialState: initialStateWithAdminCheck
-  //       }
-  //     );
-  //
-  //     const dropdown = screen.getByRole('combobox', {
-  //       name: 'federal-state split'
-  //     });
-  //     // expect(dropdown.value).to.equal('0-100');
-  //     user.change(dropdown, { target: { value: '75-25' } });
-  //     expect(
-  //       screen.getByText('Select a federal-state split.')
-  //     ).not.toBeInTheDocument();
-  //   });
-
-  // it('renders as expected in editable mode(standard)', async () => {
-  //   await setup(
-  //     {},
-  //     {
-  //       initialState: initialState
-  //     }
-  //   );
-  // });
-
-  //   describe('renders correctly', () => {
-  //     it('renders correctly in view-only mode', () => {
-  //       const component = shallow(<CostAllocateFFP {...props} isViewOnly />);
-  //       expect(component).toMatchSnapshot();
-  //     });
-  //     it('renders correctly in editable mode (standard)', () => {
-  //       const component = shallow(<CostAllocateFFP {...props} />);
-  //       expect(component).toMatchSnapshot();
-  //     });
-  //   });
-  //
-  //   xit('handles changes to cost allocation dropdown', () => {
-  //     const component = shallow(<CostAllocateFFP {...props} />);
-  //     component
-  //       .find('Dropdown')
-  //       .at(1)
-  //       .simulate('change', { target: { value: '35-65' } });
-  //
-  //     expect(props.setFundingSplit).toHaveBeenCalledWith(0, '1991', 35, 65);
-  //   });
-  //
-  //   it('renders the budget and cost allocation summary narrative', () => {
-  //     const costAllocation = {
-  //       1: { ffp: { federal: 'f', state: 's' } },
-  //       2: { ffp: { federal: 'f', state: 's' } },
-  //       3: { ffp: { federal: 'f', state: 's' } }
-  //     };
-  //
-  //     const p = {
-  //       activityName: 'activity',
-  //       costAllocation,
-  //       costSummary: {
-  //         total: {
-  //           federalShare: 1,
-  //           medicaidShare: 2,
-  //           otherFunding: 3,
-  //           stateShare: 4,
-  //           totalCost: 5
-  //         }
-  //       },
-  //       stateName: 'the state'
-  //     };
-  //
-  //     expect(shallow(<AllFFYsSummaryNarrative {...p} />)).toMatchSnapshot();
-  //
-  //     costAllocation['2'].ffp.federal = 'ff';
-  //     expect(shallow(<AllFFYsSummaryNarrative {...p} />)).toMatchSnapshot();
-  //
-  //     costAllocation['2'].ffp.federal = 'f';
-  //     costAllocation['3'].ffp.federal = 'ff';
-  //     expect(shallow(<AllFFYsSummaryNarrative {...p} />)).toMatchSnapshot();
-  //   });
-  //
-  //   it('maps redux state to component props', () => {
-  //     const getActivity = jest.fn();
-  //     getActivity.mockReturnValue({ key: 'activity key', name: 'activity name' });
-  //
-  //     const getCostAllocation = jest.fn();
-  //     getCostAllocation.mockReturnValue('cost allocation');
-  //
-  //     const getCostSummary = jest.fn();
-  //     getCostSummary.mockReturnValue('cost summary');
-  //
-  //     const getState = jest.fn();
-  //     getState.mockReturnValue({ name: 'denial' });
-  //
-  //     const getActivityTotal = jest.fn();
-  //     getActivityTotal.mockReturnValue({
-  //       data: {
-  //         otherFunding: {
-  //           2020: { contractors: 0, expenses: 0, statePersonnel: 0, total: 0 },
-  //           2021: { contractors: 0, expenses: 0, statePersonnel: 0, total: 0 }
-  //         }
-  //       }
-  //     });
-  //
-  //     expect(
-  //       mapStateToProps(
-  //         { apd: { adminCheck: true } },
-  //         { activityIndex: 0 },
-  //         {
-  //           getActivity,
-  //           getCostAllocation,
-  //           getCostSummary,
-  //           getState,
-  //           getActivityTotal
-  //         }
-  //       )
-  //     ).toEqual({
-  //       aKey: 'activity key',
-  //       activityName: 'activity name',
-  //       costAllocation: 'cost allocation',
-  //       costSummary: 'cost summary',
-  //       otherFunding: {
-  //         2020: { contractors: 0, expenses: 0, statePersonnel: 0, total: 0 },
-  //         2021: { contractors: 0, expenses: 0, statePersonnel: 0, total: 0 }
-  //       },
-  //       stateName: 'denial',
-  //       adminCheck: true
-  //     });
-  //
-  //     expect(getActivity).toHaveBeenCalledWith(
-  //       { apd: { adminCheck: true } },
-  //       {
-  //         activityIndex: 0
-  //       }
-  //     );
-  //     expect(getCostAllocation).toHaveBeenCalledWith(
-  //       { apd: { adminCheck: true } },
-  //       {
-  //         activityIndex: 0
-  //       }
-  //     );
-  //     expect(getCostSummary).toHaveBeenCalledWith(
-  //       { apd: { adminCheck: true } },
-  //       {
-  //         activityIndex: 0
-  //       }
-  //     );
-  //     expect(getState).toHaveBeenCalledWith({ apd: { adminCheck: true } });
-  //
-  //     // Now test that it builds a default activity name if none is provided
-  //     getActivity.mockReturnValue({ key: 'activity key', name: '' });
-  //
-  //     expect(
-  //       mapStateToProps(
-  //         { apd: { adminCheck: true } },
-  //         { activityIndex: 0, adminCheck: true },
-  //         {
-  //           getActivity,
-  //           getCostAllocation,
-  //           getCostSummary,
-  //           getState,
-  //           getActivityTotal
-  //         }
-  //       )
-  //     ).toEqual({
-  //       aKey: 'activity key',
-  //       activityName: 'Untitled',
-  //       costAllocation: 'cost allocation',
-  //       costSummary: 'cost summary',
-  //       otherFunding: {
-  //         2020: { contractors: 0, expenses: 0, statePersonnel: 0, total: 0 },
-  //         2021: { contractors: 0, expenses: 0, statePersonnel: 0, total: 0 }
-  //       },
-  //       stateName: 'denial',
-  //       adminCheck: true
-  //     });
-  //   });
-  //
-  //   it('maps dispatch actions to props', () => {
-  //     expect(mapDispatchToProps).toEqual({
-  //       setFundingSplit: setCostAllocationFFPFundingSplit
-  //     });
-  //   });
 });
