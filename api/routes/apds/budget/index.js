6c7a521dd706df9746ed3f637c5f10ff85678728
Tiffany Forkner <thetif@gmail.com>
2022-08-18 10:53:06 -0400

    removed budget temporarily

--- api/routes/apds/budget/index.js
+++
@@ -1,7 +0,0 @@
-const logger = require('../../../logger')('apds/budget route index');
-const patch = require('./patch');
-
-module.exports = (app, { patchEndpoint = patch } = {}) => {
-  logger.debug('setting up PATCH endpoint');
-  patchEndpoint(app);
-};
