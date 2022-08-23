a7de23261ddaddfea1a76a32b62042c8fa9bff4c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-23 12:56:00 -0400

    fix lint

--- web/src/pages/apd/proposed-budget/IncentivePayments.test.js
+++ web/src/pages/apd/proposed-budget/IncentivePayments.test.js
@@ -8,26 +8,11 @@ import {
 
 import IncentivePayments from './IncentivePayments';
 
-import {
-  setIncentiveEHCount,
-  setIncentiveEPCount,
-  setIncentiveEHPayment,
-  setIncentiveEPPayment
-} from '../../../redux/actions/editApd';
-
 const defaultProps = {
-  data: {
-    ehAmt: { 2022: {1:1, 2:2, 3:3, 4:4}, 2023: {1:5, 2:6, 3:7, 4:8} },
-    ehCt: { 2022: {1:9, 2:10, 3:11, 4:12}, 2023: {1:13, 2:14, 3:15, 4:16} },
-    epAmt: { 2022: {1:17, 2:18, 3:19, 4:20}, 2023: {1:21, 2:22, 3:23, 4:24} },
-    epCt: { 2022: {1:25, 2:26, 3:27, 4:28}, 2023: {1:29, 2:30, 3:31, 4:32} }
-  },
-  totals: {
-    ehAmt: { allYears: 36, byYear: { 1: 10, 2: 26 } },
-    ehCt: { allYears: 100, byYear: { 1: 42, 2: 58 } },
-    epAmt: { allYears: 164, byYear: { 1: 74, 2: 90 } },
-    epCt: { allYears: 228, byYear: { 1: 106, 2: 122 } }
-  },
+  ehAmt: { 2022: {1:1, 2:2, 3:3, 4:4}, 2023: {1:5, 2:6, 3:7, 4:8} },
+  ehCt: { 2022: {1:9, 2:10, 3:11, 4:12}, 2023: {1:13, 2:14, 3:15, 4:16} },
+  epAmt: { 2022: {1:17, 2:18, 3:19, 4:20}, 2023: {1:21, 2:22, 3:23, 4:24} },
+  epCt: { 2022: {1:25, 2:26, 3:27, 4:28}, 2023: {1:29, 2:30, 3:31, 4:32} },
   years: ['2022', '2023'],
   setEHCount: jest.fn(),
   setEPCount: jest.fn(),
@@ -56,6 +41,7 @@ const initialState = {
 
 const setup = async (props = {}) => {
   let utils;
+  // eslint-disable-next-line testing-library/no-unnecessary-act
   await act(async () => {
     utils = renderWithConnection(<IncentivePayments {...defaultProps} {...props} />, {
       initialState: {
@@ -126,9 +112,18 @@ describe('<IncentivePayments />', () =>{
 
     waitFor(() => {
       expect(screen.getByTestId('ehAmt 2022 total').innerHTML.replace(/<(.|\n)*?>/g, '')).toHaveValue(`$${ehAmtYearOneTotal}`);
+    }).catch(err => console.log(err));
+
+    waitFor(() => {
       expect(screen.getByTestId('epAmt 2022 total').innerHTML.replace(/<(.|\n)*?>/g, '')).toHaveValue(`$${epAmtYearOneTotal}`);
+    }).catch(err => console.log(err));
+
+    waitFor(() => {
       expect(screen.getByTestId('ehAmt 2023 total').innerHTML.replace(/<(.|\n)*?>/g, '')).toHaveValue(`$${ehAmtYearTwoTotal}`);
+    }).catch(err => console.log(err));
+
+    waitFor(() => {
       expect(screen.getByTestId('epAmt 2023 total').innerHTML.replace(/<(.|\n)*?>/g, '')).toHaveValue(`$${epAmtYearTwoTotal}`);
-    });
+    }).catch(err => console.log(err));
   });
 });
