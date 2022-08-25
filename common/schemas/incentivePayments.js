273dd54e15dd9d5094225287725f73baf2b904b4
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-25 11:29:37 -0400

    change error message

--- common/schemas/incentivePayments.js
+++ common/schemas/incentivePayments.js
@@ -5,18 +5,20 @@ const incentivePaySchema = Joi.object({
     ehCt: Joi.any(),
     epCt: Joi.any(),
     ehAmt: Joi.object().pattern(
-        /\d{4}/,
-        Joi.object().pattern(
-          /[1-4]{1}/,
-          Joi.number().integer().positive().allow(0).required().messages({
-            'number.base': 'Provide a number greater than or equal to $0.',
-            'number.empty': 'Provide a number greater than or equal to $0.',
-            'number.integer': 'Use only whole numbers when providing counts',
-            'number.format': 'Provide a number greater than or equal to $0.',
-            'number.positive': 'Provide a number greater than or equal to $0.'
-          })
-        )
-      ),
+      /\d{4}/,
+      Joi.object().pattern(
+        /[1-4]{1}/,
+        Joi.number().integer().positive().allow(0).required().messages({
+          'number.base': 'Provide a number greater than or equal to $0.',
+          'number.empty': 'Provide a number greater than or equal to $0.',
+          'number.integer':
+            'Use only whole numbers when providing dollar amount',
+          'number.format':
+            'Use only whole numbers when providing dollar amount',
+          'number.positive': 'Provide a number greater than or equal to $0.'
+        })
+      )
+    ),
     epAmt: Joi.object().pattern(
       /\d{4}/,
       Joi.object().pattern(
@@ -24,8 +26,10 @@ const incentivePaySchema = Joi.object({
         Joi.number().integer().positive().allow(0).required().messages({
           'number.base': 'Provide a number greater than or equal to $0.',
           'number.empty': 'Provide a number greater than or equal to $0.',
-          'number.integer': 'Use only whole numbers when providing counts',
-          'number.format': 'Provide a number greater than or equal to $0.',
+          'number.integer':
+            'Use only whole numbers when providing dollar amount',
+          'number.format':
+            'Use only whole numbers when providing dollar amount',
           'number.positive': 'Provide a number greater than or equal to $0.'
         })
       )
