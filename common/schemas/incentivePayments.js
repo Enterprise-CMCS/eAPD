ce587ac8447f13a6d93868712a20785773b9d197
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-09-01 10:41:59 -0400

    update incentive payment

--- common/schemas/incentivePayments.js
+++ common/schemas/incentivePayments.js
@@ -9,13 +9,14 @@ const incentivePaySchema = Joi.object({
       Joi.object().pattern(
         /[1-4]{1}/,
         Joi.number().integer().positive().allow(0).required().messages({
-          'number.base': 'Provide a number greater than or equal to $0.',
-          'number.empty': 'Provide a number greater than or equal to $0.',
+          'number.base': 'Provide a whole number greater than or equal to $0.',
+          'number.empty': 'Provide a whole number greater than or equal to $0.',
           'number.integer':
             'Use only whole numbers when providing dollar amount',
           'number.format':
             'Use only whole numbers when providing dollar amount',
-          'number.positive': 'Provide a number greater than or equal to $0.'
+          'number.positive':
+            'Provide a whole number greater than or equal to $0.'
         })
       )
     ),
@@ -24,13 +25,14 @@ const incentivePaySchema = Joi.object({
       Joi.object().pattern(
         /[1-4]{1}/,
         Joi.number().integer().positive().allow(0).required().messages({
-          'number.base': 'Provide a number greater than or equal to $0.',
-          'number.empty': 'Provide a number greater than or equal to $0.',
+          'number.base': 'Provide a whole number greater than or equal to $0.',
+          'number.empty': 'Provide a whole number greater than or equal to $0.',
           'number.integer':
             'Use only whole numbers when providing dollar amount',
           'number.format':
             'Use only whole numbers when providing dollar amount',
-          'number.positive': 'Provide a number greater than or equal to $0.'
+          'number.positive':
+            'Provide a whole number greater than or equal to $0.'
         })
       )
     )
