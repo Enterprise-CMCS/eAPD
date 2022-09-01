6750cc730271678194d57f709da073d55a5dd2cf
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-09-01 14:42:02 -0400

    replace error message

--- common/schemas/incentivePayments.js
+++ common/schemas/incentivePayments.js
@@ -9,14 +9,16 @@ const incentivePaySchema = Joi.object({
       Joi.object().pattern(
         /[1-4]{1}/,
         Joi.number().integer().positive().allow(0).required().messages({
-          'number.base': 'Provide a whole number greater than or equal to $0.',
-          'number.empty': 'Provide a whole number greater than or equal to $0.',
+          'number.base':
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."',
+          'number.empty':
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."',
           'number.integer':
-            'Use only whole numbers when providing dollar amount',
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."',
           'number.format':
-            'Use only whole numbers when providing dollar amount',
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."',
           'number.positive':
-            'Provide a whole number greater than or equal to $0.'
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."'
         })
       )
     ),
@@ -25,14 +27,16 @@ const incentivePaySchema = Joi.object({
       Joi.object().pattern(
         /[1-4]{1}/,
         Joi.number().integer().positive().allow(0).required().messages({
-          'number.base': 'Provide a whole number greater than or equal to $0.',
-          'number.empty': 'Provide a whole number greater than or equal to $0.',
+          'number.base':
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."',
+          'number.empty':
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."',
           'number.integer':
-            'Use only whole numbers when providing dollar amount',
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."',
           'number.format':
-            'Use only whole numbers when providing dollar amount',
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."',
           'number.positive':
-            'Provide a whole number greater than or equal to $0.'
+            'Provide a whole number greater than or equal to $0. Decimals will be rounded to the closest number."'
         })
       )
     )
