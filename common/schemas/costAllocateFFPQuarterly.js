cf3acf8e5520fa1d1e8f9b0870a8582a3288f9de
tbolt <tybolt@gmail.com>
2022-09-02 10:03:28 -0400

    Tbolt/3355 budget and ffp validation (#4224)

---
+++ common/schemas/costAllocateFFPQuarterly.js
@@ -0,0 +1,69 @@
+const Joi = require('joi');
+
+const costAllocateFFPQuarterlySchema = Joi.object({
+  formData: Joi.object({
+    1: Joi.object({
+      combined: Joi.any(),
+      contractors: Joi.object({
+        dollars: Joi.number(),
+        percent: Joi.number()
+      }),
+      inHouse: Joi.object({
+        dollars: Joi.number(),
+        percent: Joi.number()
+      })
+    }),
+    2: Joi.object({
+      combined: Joi.any(),
+      contractors: Joi.object({
+        dollars: Joi.number(),
+        percent: Joi.number()
+      }),
+      inHouse: Joi.object({
+        dollars: Joi.number(),
+        percent: Joi.number()
+      })
+    }),
+    3: Joi.object({
+      combined: Joi.any(),
+      contractors: Joi.object({
+        dollars: Joi.number(),
+        percent: Joi.number()
+      }),
+      inHouse: Joi.object({
+        dollars: Joi.number(),
+        percent: Joi.number()
+      })
+    }),
+    4: Joi.object({
+      combined: Joi.any(),
+      contractors: Joi.object({
+        dollars: Joi.number(),
+        percent: Joi.number()
+      }),
+      inHouse: Joi.object({
+        dollars: Joi.number(),
+        percent: Joi.number()
+      })
+    }),
+    subtotal: Joi.object({
+      combined: Joi.any(),
+      contractors: Joi.object({
+        dollars: Joi.any(),
+        percent: Joi.number().precision(3).valid(1).messages({
+          'any.only':
+            'Private Contractor Costs quarterly percentages must total 100%'
+        })
+      }),
+      inHouse: Joi.object({
+        dollars: Joi.any(),
+        percent: Joi.number().precision(3).valid(1).messages({
+          'any.only':
+            'State Staff and Expenses (In-House Costs) quarterly percentages must total 100%'
+        })
+      })
+    })
+  })
+});
+
+export default costAllocateFFPQuarterlySchema;
