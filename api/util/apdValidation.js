ea0b707559caa15bbbe5643f1449fb5fef63e962
Ty Bolt <tybolt@gmail.com>
2022-09-14 16:32:25 -0400

    wip

--- api/util/apdValidation.js
+++ api/util/apdValidation.js
@@ -1,26 +1,89 @@
 const {
-  activitiesDashboard,
-  activityOverview,
-  apdOverview,
+  activitiesDashboard, // copied
+  activityOverview, // copied
+  apdOverview, // used, added hack to workaround fundingSources
+  assurancesAndCompliance, // used
+  costAllocateFFP, // used, updated schema + frontend
+  costAllocateFFPQuarterly,
+  costAllocation,
+  incentivePayments,
   keyMedicaid,
   keyPerson,
+  milestones,
   nameAndFundingSource,
+  nonPersonnelCosts,
+  otherSources,
   outcomeMetric,
   perconCost,
   plannedActivitySchedule,
   privateContractor,
-  nonPersonnelCosts,
   standardsAndConditions,
-  statePersonnel,
-  milestones
+  statePersonnel
 } = require('@cms-eapd/common');
+
+const Joi = require('joi');
 
 // combine all schemas into one and validate once
 // alternatively: loop through each section of APD and validate against respective schema
 // compile all errors into new object (based on expectations from frontend)
 // return the object of validation errors or null if there are no errors
 const validateAPDDoc = apd => {
-  console.log('test', activitiesDashboard.validate([]));
+  const fundingSources = apd.activities.map(activity => {
+    return activity.fundingSource;
+  });
+  const combinedSchema = Joi.object({
+    // activities schema copied from schemas/activitiesDashboard.js
+    name: Joi.any(),
+    years: Joi.any(),
+    apdOverview: apdOverview,
+    keyStatePersonnel: Joi.any(),
+    previousActivities: Joi.any(),
+    activities: Joi.array()
+      .min(1)
+      .messages({
+        'array.min': 'Activities have not been added for this APD.'
+      })
+      .items({
+        alternatives: Joi.any(),
+        contractorResources: Joi.any(),
+        costAllocation: costAllocateFFP,
+        costAllocationNarrative: Joi.any(),
+        // summary and description schema copied from schemas/activitiesDashboard.js
+        summary: Joi.string().trim().min(1).required().messages({
+          'string.base': 'Provide a short overview of the activity.',
+          'string.empty': 'Provide a short overview of the activity.',
+          'string.min': 'Provide a short overview of the activity.'
+        }),
+        description: Joi.string().trim().min(1).required().messages({
+          'string.base': 'Provide details to explain this activity.',
+          'string.empty': 'Provide details to explain this activity.',
+          'string.min': 'Provide details to explain this activity.'
+        }),
+        expenses: Joi.any(),
+        fundingSource: Joi.any(),
+        outcomes: Joi.any(),
+        name: Joi.any(),
+        plannedEndDate: Joi.any(),
+        plannedStartDate: Joi.any(),
+        schedule: Joi.any(),
+        standardsAndConditions: Joi.any(),
+        statePersonnel: Joi.any(),
+        quarterlyFFP: Joi.any()
+      }),
+    proposedBudget: Joi.any(),
+    assurancesAndCompliances: assurancesAndCompliance
+  });
+  const modifiedApd = {
+    ...apd,
+    apdOverview: {
+      ...apd.apdOverview,
+      fundingSources: fundingSources
+    }
+  };
+  const validationResults = combinedSchema.validate(modifiedApd, {
+    abortEarly: false
+  });
+  console.log(validationResults);
   return apd;
 };
 
--- api/util/apdValidation.test.js
+++ api/util/apdValidation.test.js
@@ -1,11 +1,358 @@
 const tap = require('tap');
-
+const mockApd = {
+  name: 'Test APD',
+  years: ['2022', '2023'],
+  apdOverview: {
+    programOverview: 'abc',
+    narrativeHIT: 'abc',
+    narrativeHIE: 'abc',
+    narrativeMMIS: 'abc'
+  },
+  keyStatePersonnel: {
+    medicaidDirector: {
+      name: 'Cornelius Fudge',
+      email: 'c.fudge@ministry.magic',
+      phone: '5551234567'
+    },
+    medicaidOffice: {
+      address1: '100 Round Sq',
+      address2: '',
+      city: 'Cityville',
+      state: 'AK',
+      zip: '12345'
+    },
+    keyPersonnel: [
+      {
+        name: 'James Holden',
+        position: 'HIT Coordinator',
+        email: 'JimPushesButtons@tycho.com',
+        isPrimary: true,
+        fte: { 2022: 1, 2023: 1 },
+        hasCosts: true,
+        costs: { 2022: 100000, 2023: 100000 }
+      }
+    ]
+  },
+  previousActivities: {
+    previousActivitySummary: 'abc',
+    actualExpenditures: {
+      2019: {
+        hithie: { federalActual: 140000, totalApproved: 280000 },
+        mmis: {
+          50: { federalActual: 23445, totalApproved: 82545 },
+          75: { federalActual: 23440, totalApproved: 75340 },
+          90: { federalActual: 235720, totalApproved: 262460 }
+        }
+      },
+      2020: {
+        hithie: { federalActual: 146346, totalApproved: 234526 },
+        mmis: {
+          50: { federalActual: 129387, totalApproved: 375445 },
+          75: { federalActual: 413246, totalApproved: 654455 },
+          90: { federalActual: 614544, totalApproved: 863455 }
+        }
+      },
+      2021: {
+        hithie: { federalActual: 320000, totalApproved: 540000 },
+        mmis: {
+          50: { federalActual: 0, totalApproved: 0 },
+          75: { federalActual: 0, totalApproved: 0 },
+          90: { federalActual: 0, totalApproved: 0 }
+        }
+      }
+    }
+  },
+  activities: [
+    {
+      alternatives: 'abc',
+      contractorResources: [
+        {
+          description: 'Maintain SLR',
+          end: '',
+          hourly: {
+            2022: { hours: '', rate: '' },
+            2023: { hours: '', rate: '' }
+          },
+          useHourly: false,
+          name: 'Super SLR Incorporated',
+          start: '',
+          totalCost: 32423,
+          years: { 2022: 999756, 2023: 342444 }
+        },
+        {
+          description: 'Technology consulting and planning services.',
+          end: '',
+          hourly: {
+            2022: { hours: '', rate: '' },
+            2023: { hours: '', rate: '' }
+          },
+          useHourly: false,
+          name: 'Tech Consulting Inc.',
+          start: '',
+          totalCost: 473573,
+          years: { 2022: 333000, 2023: 200000 }
+        }
+      ],
+      costAllocation: {
+        2022: { ffp: { federal: 90, state: 10 }, other: 105000 },
+        2023: { ffp: { federal: 90, state: 10 }, other: 0 }
+      },
+      costAllocationNarrative: {
+        years: {
+          2022: {
+            otherSources:
+              '<p>No other funding is provided for this activity for FFY 2022.</p>'
+          },
+          2023: {
+            otherSources:
+              '<p>No other funding is provided for this activity for FFY 2023.</p>'
+          }
+        },
+        methodology: '<p>No cost allocation is necessary for this activity.</p>'
+      },
+      description: 'abc',
+      expenses: [
+        {
+          description: '',
+          category: 'Training and outreach',
+          years: { 2022: 40000, 2023: 40000 }
+        },
+        {
+          description: '',
+          category: 'Travel',
+          years: { 2022: 35000, 2023: 35000 }
+        },
+        {
+          description: '',
+          category: 'Hardware, software, and licensing',
+          years: { 2022: 700000, 2023: 0 }
+        }
+      ],
+      fundingSource: 'HIT',
+      outcomes: [
+        {
+          outcome:
+            'Accept attestations for 2022, and modify SLR to meet new spec sheets released by CMS.',
+          metrics: [
+            { metric: 'Complete SLR modifications by 11/1/21' },
+            { metric: 'Accept attestations through 4/30/22.' }
+          ]
+        },
+        {
+          outcome:
+            'Provide support to EPs and EHs through attestation process.',
+          metrics: [
+            { metric: "Guidance available on Tycho''s websites" },
+            { metric: 'Office hours availble for EPs and EHs' },
+            { metric: 'Site visits, as needed, for EPs and EHs' }
+          ]
+        }
+      ],
+      name: 'Program Administration',
+      plannedEndDate: '2023-09-30',
+      plannedStartDate: '2017-10-01',
+      schedule: [
+        {
+          endDate: '2020-09-07',
+          milestone:
+            'Implementation of Final Rule and Stage 3 System Developments'
+        },
+        {
+          endDate: '2019-12-31',
+          milestone: 'Environmental Scan Completion'
+        },
+        {
+          endDate: '2022-05-30',
+          milestone: 'HIT Roadmap Development'
+        }
+      ],
+      standardsAndConditions: {
+        doesNotSupport: '',
+        supports:
+          '<p class="p1"><span class="s1">We will comply with standards and conditions</span></p>'
+      },
+      statePersonnel: [
+        {
+          title: 'Project Assistant',
+          description:
+            'Coordination and document management support daily administrative support such as meeting minutes and scribe, manages project library, scheduling, and correspondence tracking.',
+          years: {
+            2022: { amt: 86000, perc: 1 },
+            2023: { amt: 88000, perc: 1 }
+          }
+        },
+        {
+          title:
+            'EHR Incentive Program Meaningful Use Coordinator (Research Analyst III)',
+          description:
+            'Develop and monitor reports, assist data users in developing and managing queries.',
+          years: {
+            2022: { amt: 101115, perc: 1 },
+            2023: { amt: 102111, perc: 1 }
+          }
+        },
+        {
+          title: 'IT Liaison',
+          description:
+            'Provide analysis and coordination activities between the HIT Program Office and the IT section, to ensure that adequate resources and priority are established to complete the technology projects as identified.',
+          years: {
+            2022: { amt: 101000, perc: 1 },
+            2023: { amt: 104000, perc: 1 }
+          }
+        },
+        {
+          title: 'Accountant III',
+          description:
+            'Coordinate program state and federal budget and expense reporting, review and validate charges to CMS federal reports.',
+          years: {
+            2022: { amt: 101000, perc: 3 },
+            2023: { amt: 109000, perc: 3 }
+          }
+        },
+        {
+          title: 'Public Information Officer',
+          description:
+            'Develop outreach materials including: written, television and radio publications to support outreach for the Medicaid EHR Incentive Program',
+          years: {
+            2022: { amt: 165000, perc: 0.5 },
+            2023: { amt: 170000, perc: 0.5 }
+          }
+        },
+        {
+          title: 'Systems Chief',
+          description:
+            'Coordinate office resources, manage staff, budget, and resource assignments, and report program status.',
+          years: {
+            2022: { amt: 135000, perc: 0.5 },
+            2023: { amt: 140000, perc: 0.5 }
+          }
+        },
+        {
+          title:
+            'Medicaid EHR Incentive Program Manager (Medical Assistance Administrator III)',
+          description:
+            'Data collection and analysis, reporting, planning, service delivery modification, support administration of the EHR Incentive Payment Program including provider application review.',
+          years: {
+            2022: { amt: 110012, perc: 1 },
+            2023: { amt: 111102, perc: 1 }
+          }
+        },
+        {
+          title: 'System Analyst (Analyst Programmer IV)',
+          description:
+            'Supports design, development and implementation of information technology infrastructure for the projects/programs under the IT Planning office supported by this Implementation Advanced Planning Document.',
+          years: {
+            2022: { amt: 98987, perc: 4 },
+            2023: { amt: 99897, perc: 4 }
+          }
+        }
+      ],
+      summary:
+        '1 Continued Operations of the Medicaid EHR Incentive Payment Program, includes modifications to the SLR, HIT staff, auditing, outreach, and non-personnel expenses for administering the program.',
+      quarterlyFFP: {
+        2022: {
+          1: { combined: 25, contractors: 25, inHouse: 25 },
+          2: { combined: 25, contractors: 25, inHouse: 25 },
+          3: { combined: 25, contractors: 25, inHouse: 25 },
+          4: { combined: 25, contractors: 25, inHouse: 25 }
+        },
+        2023: {
+          1: { combined: 25, contractors: 25, inHouse: 25 },
+          2: { combined: 25, contractors: 25, inHouse: 25 },
+          3: { combined: 25, contractors: 25, inHouse: 25 },
+          4: { combined: 25, contractors: 25, inHouse: 25 }
+        }
+      }
+    }
+  ],
+  proposedBudget: {
+    incentivePayments: {
+      ehAmt: {
+        2022: { 1: 0, 2: 0, 3: 0, 4: 0 },
+        2023: { 1: 0, 2: 0, 3: 0, 4: 0 }
+      },
+      ehCt: {
+        2022: { 1: 0, 2: 0, 3: 0, 4: 0 },
+        2023: { 1: 0, 2: 0, 3: 0, 4: 0 }
+      },
+      epAmt: {
+        2022: { 1: 0, 2: 0, 3: 0, 4: 0 },
+        2023: { 1: 0, 2: 0, 3: 0, 4: 0 }
+      },
+      epCt: {
+        2022: { 1: 0, 2: 0, 3: 0, 4: 0 },
+        2023: { 1: 0, 2: 0, 3: 0, 4: 0 }
+      }
+    }
+  },
+  assurancesAndCompliances: {
+    procurement: [
+      {
+        title: '42 CFR Part 495.348',
+        checked: true,
+        explanation: ''
+      },
+      { title: 'SMM Section 11267', checked: true, explanation: '' },
+      {
+        title: '45 CFR 95.613',
+        checked: true,
+        explanation: ''
+      },
+      { title: '45 CFR 75.326', checked: true, explanation: '' }
+    ],
+    recordsAccess: [
+      {
+        title: '42 CFR Part 495.350',
+        checked: true,
+        explanation: ''
+      },
+      { title: '42 CFR Part 495.352', checked: true, explanation: '' },
+      {
+        title: '42 CFR Part 495.346',
+        checked: true,
+        explanation: ''
+      },
+      {
+        title: '42 CFR 433.112(b)',
+        checked: true,
+        explanation: ''
+      },
+      { title: '45 CFR Part 95.615', checked: true, explanation: '' },
+      {
+        title: 'SMM Section 11267',
+        checked: true,
+        explanation: ''
+      }
+    ],
+    softwareRights: [
+      {
+        title: '42 CFR 495.360',
+        checked: false,
+        explanation:
+          'We were able to get better pricing if we allowed the vendor to maintain ownership of the product they developed.'
+      },
+      { title: '45 CFR 95.617', checked: true, explanation: '' },
+      {
+        title: '42 CFR Part 431.300',
+        checked: true,
+        explanation: ''
+      },
+      { title: '42 CFR Part 433.112', checked: true, explanation: '' }
+    ],
+    security: [
+      {
+        title: '45 CFR 164 Security and Privacy',
+        checked: true,
+        explanation: ''
+      }
+    ]
+  }
+};
 const { validateAPDDoc } = require('./apdValidation');
 
 tap.test('apd document validation', async apdValidationTests => {
   apdValidationTests.test('test import', async test => {
-    const testApd = 'abc';
-    const results = validateAPDDoc(testApd);
-    test.same(results, testApd);
+    const results = validateAPDDoc(mockApd);
+    // test.same(results, testApd);
   });
 });
