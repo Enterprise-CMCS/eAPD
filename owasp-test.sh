221eb0399fb5601e89fdc15591350e5ab391b23a
Tiffany Forkner <thetif@gmail.com>
2022-10-07 11:42:07 -0400

    fixes to owasp config

--- owasp-test.sh
+++ owasp-test.sh
@@ -4,11 +4,11 @@ export NODE_ENV=test
 export API_URL=http://localhost:8081
 
 # Set up API server
-docker-compose -f ../docker-compose.endpoint-tests.yml -p api up -d
+docker-compose -f docker-compose.endpoint-tests.yml -p api up -d
 sleep 60
 
-docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec -e LOG_LEVEL=verbose api-for-testing yarn run migrate
-docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec -e LOG_LEVEL=verbose api-for-testing yarn run seed
+docker-compose -f docker-compose.endpoint-tests.yml -p api exec -e LOG_LEVEL=verbose api-for-testing yarn run migrate
+docker-compose -f docker-compose.endpoint-tests.yml -p api exec -e LOG_LEVEL=verbose api-for-testing yarn run seed
 echo 'Checking to see if the server is running'
 until [ "`docker inspect -f {{.State.Health.Status}} api-container`"=="healthy" ]; do
     sleep 10;
