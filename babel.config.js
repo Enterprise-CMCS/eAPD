ad2b38cc86d42b9d07a047e4a52540b6c53e91d0
Darren Mirano <dmirano@fearless.tech>
2022-11-04 14:55:37 -0400

    trying out new babel config

--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,6 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
-    'babel-plugin-dynamic-import-node',
-    ['istanbul', {}, 'istanbulForE2e']
+    'babel-plugin-dynamic-import-node'
   ]
 };
--- e2e/babel.config.js
+++ e2e/babel.config.js
@@ -1,5 +1,19 @@
-const defaultConfigs = require('../babel.config');
-
 module.exports = {
-  ...defaultConfigs
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    ['@babel/preset-react']
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    'babel-plugin-dynamic-import-node',
+    ['istanbul', {}, 'istanbulForE2e']
+  ]
 };
