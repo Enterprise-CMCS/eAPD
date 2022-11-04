d5743eb670ce11682a88fb065c3ed57281fbbc76
Darren Mirano <dmirano@fearless.tech>
2022-11-04 16:32:10 -0400

    changed config file

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,6 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     'babel-plugin-dynamic-import-node',
-    ['istanbul', {}]
+    ['istanbul', {}, 'babel-plugin-istanbul']
   ]
 };
