f97e63f132a14ded605048de572bf95514d37ecf
Darren Mirano <dmirano@fearless.tech>
2022-11-04 15:48:05 -0400

    reverted changes

--- e2e/babel.config.js
+++ e2e/babel.config.js
@@ -1,19 +1,5 @@
+const defaultConfigs = require('../babel.config');
+
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current'
-        }
-      }
-    ],
-    ['@babel/preset-react']
-  ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    'babel-plugin-dynamic-import-node',
-    ['istanbul', {}, 'istanbulForE2e']
-  ]
+  ...defaultConfigs
 };
