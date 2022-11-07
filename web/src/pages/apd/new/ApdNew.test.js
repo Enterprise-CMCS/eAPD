3e6319efda53ca687a57220273d8f76a9178381c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-07 13:30:33 -0500

    Update jest test

--- web/src/pages/apd/new/ApdNew.test.js
+++ web/src/pages/apd/new/ApdNew.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {
-  renderWithConnection,
-  act,
-  screen
-} from '../../../shared/apd-testing-library';
+import { renderWithConnection, act, screen } from 'apd-testing-library';
 import { mockFlags, resetLDMocks } from 'jest-launchdarkly-mock';
 
 import ApdNew from './ApdNew';
+
+jest.mock('../../../util/api', () => ({
+  get: jest.fn(),
+  post: jest.fn(),
+  patch: jest.fn(),
+  delete: jest.fn()
+}));
 
 const setup = async (props = {}, options = {}) => {
   return renderWithConnection(<ApdNew {...props} />, options);
