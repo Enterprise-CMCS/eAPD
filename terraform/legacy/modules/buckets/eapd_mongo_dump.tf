23a326252d22f8eceb1084dbdd2e208699fd1b7b
cthulhuplus <brookswm@gmail.com>
2022-11-21 16:32:40 -0500

    Updating Terraform version and added code for Mongo bucket

---
+++ terraform/legacy/modules/buckets/eapd_mongo_dump.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "mongo_dump_staging" {
+  bucket = "eapd-mongo-dump_staging"
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
