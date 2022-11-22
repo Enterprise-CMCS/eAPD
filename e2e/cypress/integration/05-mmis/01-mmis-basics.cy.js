216adaa67e0590313711ca3c2b74028b95cf60e0
Darren Mirano <dmirano@fearless.tech>
2022-11-22 09:44:01 -0500

    finished up create new apd cypress test

--- e2e/cypress/integration/05-mmis/01-mmis-basics.cy.js
+++ e2e/cypress/integration/05-mmis/01-mmis-basics.cy.js
@@ -54,12 +54,96 @@ describe('Default APD', { tags: ['@apd', '@default', '@slow'] }, () => {
     /* eslint-disable-next-line prefer-arrow-callback, func-names */
     beforeEach(function () {
       cy.updateFeatureFlags({ enableMmis: true, adminCheckFlag: true });
-      cy.intercept('PATCH', `${Cypress.env('API')}/apds/**`).as('saveAPD');
     });
 
-    describe('default APD Overview', () => {
-      it('should have two checked years', () => {
-        cy.get('[type="checkbox"][checked]').should('have.length', 2);
+    describe('MMIS Workflow', () => {
+      it('tests the create new page', () => {
+        // cy.get('[type="checkbox"][checked]').should('have.length', 2);
+
+        cy.findAllByText('AK APD Home').click();
+        cy.findAllByText('Create new').click();
+
+        cy.contains('Cancel').click();
+        cy.findByRole(
+          'heading',
+          { name: /Alaska APDs/i },
+          { timeout: 100000 }
+        ).should('exist');
+        cy.findAllByText('Create new').click();
+
+        cy.contains(
+          'This selection cannot be changed after creating a new APD.'
+        ).should('exist');
+        cy.findByRole('radio', { name: /MMIS/i }).focus().blur();
+        // cy.contains('Select an APD Type').should('exist');
+
+        // Select HITECH, ensure Update Type section exists and Medicaid Business Areas doesnt exist
+        cy.findByRole('radio', { name: /HITECH/i }).click();
+        cy.contains('Select an APD Type').should('not.exist');
+        cy.contains('Update Type').should('exist');
+        cy.contains('Medicaid Business Areas').should('not.exist');
+
+        cy.findByRole('radio', { name: /MMIS/i }).click();
+        cy.contains('Is this an APD update?').should('exist');
+        cy.contains('Medicaid Business Areas').should('exist');
+
+        cy.findByLabelText('APD Name').clear().blur();
+        // cy.contains('Provide an APD Name.').should('exist')
+        cy.findByLabelText('APD Name').clear().type('MMIS IAPD').blur();
+        cy.contains('Provide an APD Name.').should('not.exist');
+
+        years.forEach(year => {
+          cy.findByRole('checkbox', { name: year }).click();
+        });
+
+        // cy.contains('At least one FFY must be selected to continue with your APD. Select your FFY(s).').should('exist')
+
+        years.forEach(year => {
+          cy.findByRole('checkbox', { name: year }).click();
+        });
+
+        cy.findByRole('radio', { name: /Yes, it is an update./i })
+          .focus()
+          .blur();
+        // cy.contains('Indicate whether this APD is an update.').should('exist');
+        cy.findByRole('radio', { name: /Yes, it is an update./i }).click();
+        cy.contains('Indicate whether this APD is an update.').should(
+          'not.exist'
+        );
+
+        cy.findByRole('checkbox', { name: /Annual update/i })
+          .focus()
+          .blur();
+        // cy.contains('Select an Update Type.').should('exist')
+        cy.findByRole('checkbox', { name: /Annual update/i }).click();
+        cy.contains('Select an Update Type.').should('not.exist');
+
+        cy.findByRole('checkbox', {
+          name: /1115 or Waiver Support Systems/i
+        })
+          .focus()
+          .blur();
+        // cy.contains('Select at least one Medicaid Business Area.').should('exist');
+
+        cy.findByRole('checkbox', {
+          name: /Other/i
+        })
+          .focus()
+          .click();
+        cy.contains('Select at least one Medicaid Business Area.').should(
+          'not.exist'
+        );
+
+        // BLUR TEXTBOX
+        cy.contains('Provide an other Medicaid Business Area(s).').should(
+          'exist'
+        );
+        // TYPE IN TEXTBOX
+        cy.contains('Provide an other Medicaid Business Area(s).').should(
+          'not.exist'
+        );
+
+        // Test that the information actually saved
       });
     });
   });
