b16d00cc758c609bc5774fdc6adbe82a093780d9
cthulhuplus <brookswm@gmail.com>
2022-11-29 12:14:20 -0500

    Adding code for prod S3 mongo dump bucket

--- terraform/legacy/modules/buckets/eapd_mongo_dump.tf
+++ terraform/legacy/modules/buckets/eapd_mongo_dump.tf
@@ -14,3 +14,20 @@ resource "aws_s3_bucket" "mongo_dump_staging" {
     }
   }
 }
+
+resource "aws_s3_bucket" "mongo_dump_production" {
+  bucket = "eapd-mongo-dump-production"
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
