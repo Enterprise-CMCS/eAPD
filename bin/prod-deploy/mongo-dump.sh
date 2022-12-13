4cd01bcda94d1caa3666b792b6266e5ee2a7edbe
cthulhuplus <brookswm@gmail.com>
2022-12-13 14:53:08 -0500

    Removed duplicate wip script and identified issue with current mongo dump strategy

--- bin/prod-deploy/mongo-dump.sh
+++ bin/prod-deploy/mongo-dump.sh
@@ -1,10 +1,12 @@
  #Create var for time so value doesn't drift between creating tarball and aws cp to S3
  TIMESTAMP=$(date +%Y%m%d%H%M%S)
+ echo $TIMESTAMP
  #Create dump
  #mongodump --uri=\"$"$ENVIRONMENT"_MONGO_URL\"
- mongodump --uri=$STAGING_MONGO_URL
+ MONGO_URL="$ENVIRONMENT"_MONGO_URL
+ mongodump --uri=$MONGO_URL
  #Tar zip dump
- tar -cvf "$ENVIRONMENT"_mongo_"$("$TIMESTAMP")".tar.gz dump/
+ tar -cvf "$ENVIRONMENT"_mongo_"$TIMESTAMP".tar.gz dump/
  #Send it
  aws s3 cp "$ENVIRONMENT"_mongo_"$TIMESTAMP".tar.gz s3://eapd-mongo-dump-"$ENVIRONMENT"
- echo $TIMESTAMP
+ 
