bae3cb5ab9c4c70b694963c54a3216b68ff93988
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-12-14 11:00:30 -0500

    can delete message

--- web/src/components/TempAlert.js
+++ web/src/components/TempAlert.js
@@ -1,33 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { Alert } from '@cmsgov/design-system';
+import { Alert, Button } from '@cmsgov/design-system';
 
 import { getTempMessages } from '../redux/reducers/errors';
 
 const TempAlert = props => {
   const { tempMessages } = props;
 
-  if (tempMessages.length > 0) {
-    let messages = [];
-
-    // function removeMsg () => {
-
-    // }
-
-    tempMessages.forEach(el => {
-      messages.push(<div>{el.message}</div>);
-    });
-
-    return (
-      <Alert variation={tempMessages[0].variation} className="ds-u-margin-y--3">
-        {messages}
-      </Alert>
-    );
+  if (!tempMessages) {
+    return null;
   }
 
-  return null;
+  const [tempMsgState, setTempMsgState] = useState(tempMessages);
+
+  const removeMsg = i => {
+    setTempMsgState([
+      ...tempMsgState.slice(0, i),
+      ...tempMsgState.slice(i + 1)
+    ]);
+  };
+
+  return (
+    <div>
+      {tempMsgState.map((value, index) => (
+        <Alert key={`tempMsg-${index + 1}`} variation={value.variation}>
+          <div className="tempMessage">
+            <div>{value.message}</div>
+            <div>
+              <Button onClick={() => removeMsg(index)} variation="ghost">
+                X
+              </Button>
+            </div>
+          </div>
+        </Alert>
+      ))}
+    </div>
+  );
+
+  // let messages = [];
+
+  // function removeMsg (index) {
+  //   console.log('Before: ' + tempMessages);
+  //   tempMessages.splice(index, 1);
+  //   console.log('After: ' + tempMessages);
+  //   setMessages(tempMessages);
+  // }
+
+  // function setMessages (messageList) {
+  //   if (messageList.length > 0) {
+  //     messageList.forEach((value, i) => {
+  //       messages.push(
+  //         <Alert variation={value.variation}>
+  //           <div className="tempMessage">
+  //             <div>
+  //               {value.message}
+  //             </div>
+  //             <div>
+  //               <Button
+  //                 onClick={() => removeMsg(i)}
+  //                 variation="ghost"
+  //               >
+  //                 X
+  //               </Button>
+  //             </div>
+  //           </div>
+  //         </Alert>
+  //       );
+  //     });
+  //   }
+  // }
+
+  // if (tempMessages.length > 0) {
+  //   setMessages(tempMessages);
+
+  //   return (
+  //     <div className="ds-u-margin-y--3">
+  //       {messages}
+  //     </div>
+  //   );
+  // }
+
+  // return null;
 };
 
 TempAlert.propTypes = {
