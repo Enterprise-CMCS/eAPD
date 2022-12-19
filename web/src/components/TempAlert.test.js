dd5080b61867b599f529914cc985c8c69d89ab4c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-12-19 11:41:02 -0500

    Add cypress tests

--- web/src/components/TempAlert.test.js
+++ web/src/components/TempAlert.test.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { renderWithConnection } from 'apd-testing-library';
+// import React from 'react';
+// import { renderWithConnection, screen } from 'apd-testing-library';
 
-import TempAlert from './TempAlert';
+// import TempAlert from './TempAlert';
 
-const setup = (props = {}, options = {}) => {
-  return renderWithConnection(<TempAlert {...props} />, options);
-};
+// const setup = (props = {}, options = {}) => {
+//   console.log(props)
+//   return renderWithConnection(<TempAlert {...props} />, options);
+// };
 
-describe('<TempAlert />', () => {
-  it('displays apd success message');
-});
+// describe('<TempAlert />', () => {
+//   it('displays apd success message', () => {
+//     let msg = 'Test message.';
+
+//     setup({tempMessages: [{
+//       message: msg,
+//       variation: 'success'
+//     }]})
+
+//     console.log(screen)
+
+//     expect(screen.getByText(msg)).toBeTruthy();
+//   });
+// });
