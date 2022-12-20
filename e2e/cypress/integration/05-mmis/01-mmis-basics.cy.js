78e1f051ed0c5462259d04e81caa6854c5006a03
Darren Mirano <dmirano@fearless.tech>
2022-12-20 14:03:48 -0500

    finished up cypress test for create new apd page

--- e2e/cypress/integration/05-mmis/01-mmis-basics.cy.js
+++ e2e/cypress/integration/05-mmis/01-mmis-basics.cy.js
@@ -1,39 +1,40 @@
-// <reference types="cypress" />
+/// <reference types="cypress" />
 
-// Tests the MMIS workflow of an APD
-describe('Default APD', { tags: ['@apd', '@default', '@slow'] }, () => {
+// Tests performing basic MMIS APD tasks
+
+/* eslint-disable no-return-assign */
+/* eslint-disable prefer-arrow-callback */
+
+describe('MMIS Basics', { tags: ['@apd', '@default', '@mmis'] }, () => {
   let apdUrl;
   let apdId;
   const years = [];
 
-  /* eslint-disable-next-line prefer-arrow-callback, func-names */
-  before(function () {
+  before(() => {
     cy.useStateStaff();
     cy.updateFeatureFlags({ enableMmis: true, adminCheckFlag: true });
     cy.reload();
 
-    // Tests new MMIS Create APD workflow
+    // Create a new MMIS APD
     cy.findAllByText('Create new').click();
 
     cy.findByRole('radio', { name: /MMIS/i }).click();
-    cy.findByLabelText('APD Name').clear().type('MMIS IAPD').blur();
+    cy.findByLabelText('APD Name').clear().type('MMIS APD Name!').blur();
     cy.findByRole('radio', { name: /No, this is for a new project./i }).click();
     cy.findByRole('checkbox', {
       name: /1115 or Waiver Support Systems/i
     }).click();
-
-    cy.findByRole('button', { name: /Create an APD/i }).click();
+    cy.get(`[data-cy='create_apd_btn']`).should('not.be.disabled').click();
 
     cy.findByRole(
       'heading',
       { name: /APD Overview/i },
       { timeout: 100000 }
     ).should('exist');
+
     cy.location('pathname').then(pathname => {
       apdUrl = pathname.replace('/apd-overview', '');
-      cy.log({ apdUrl });
       apdId = apdUrl.split('/').pop();
-      cy.log({ apdId });
     });
 
     cy.get('[type="checkbox"][checked]').each((_, index, list) =>
@@ -50,101 +51,111 @@ describe('Default APD', { tags: ['@apd', '@default', '@slow'] }, () => {
     cy.deleteAPD(apdId);
   });
 
-  describe('Form View', () => {
-    /* eslint-disable-next-line prefer-arrow-callback, func-names */
-    beforeEach(function () {
-      cy.updateFeatureFlags({ enableMmis: true, adminCheckFlag: true });
-    });
+  describe('Create MMIS APD', () => {
+    it('tests Create New page', () => {
+      cy.contains('AK APD Home').click();
+      cy.findAllByText('Create new').click();
 
-    describe('MMIS Workflow', () => {
-      it('tests the create new page', () => {
-        // cy.get('[type="checkbox"][checked]').should('have.length', 2);
+      cy.contains(
+        'This selection cannot be changed after creating a new APD.'
+      ).should('exist');
 
-        cy.findAllByText('AK APD Home').click();
-        cy.findAllByText('Create new').click();
+      cy.findByRole('radio', { name: /MMIS/i }).focus().blur();
+      cy.contains('Select an APD Type').should('exist');
 
-        cy.contains('Cancel').click();
-        cy.findByRole(
-          'heading',
-          { name: /Alaska APDs/i },
-          { timeout: 100000 }
-        ).should('exist');
-        cy.findAllByText('Create new').click();
+      cy.findByRole('radio', { name: /HITECH/i }).click();
+      cy.findAllByText('Select an APD Type').should('not.exist');
 
-        cy.contains(
-          'This selection cannot be changed after creating a new APD.'
-        ).should('exist');
-        cy.findByRole('radio', { name: /MMIS/i }).focus().blur();
-        // cy.contains('Select an APD Type').should('exist');
+      // Verify HITECH fields show
+      cy.findAllByText('Update Type').should('exist');
+      cy.findAllByText('Medicaid Business Areas').should('not.exist');
 
-        // Select HITECH, ensure Update Type section exists and Medicaid Business Areas doesnt exist
-        cy.findByRole('radio', { name: /HITECH/i }).click();
-        cy.contains('Select an APD Type').should('not.exist');
-        cy.contains('Update Type').should('exist');
-        cy.contains('Medicaid Business Areas').should('not.exist');
+      cy.findByRole('radio', { name: /MMIS/i }).click();
 
-        cy.findByRole('radio', { name: /MMIS/i }).click();
-        cy.contains('Is this an APD update?').should('exist');
-        cy.contains('Medicaid Business Areas').should('exist');
+      // Verify MMIS fields show
+      cy.findAllByText('Is this an APD update?').should('exist');
+      cy.findAllByText('Medicaid Business Areas').should('exist');
 
-        cy.findByLabelText('APD Name').clear().blur();
-        // cy.contains('Provide an APD Name.').should('exist')
-        cy.findByLabelText('APD Name').clear().type('MMIS IAPD').blur();
-        cy.contains('Provide an APD Name.').should('not.exist');
+      cy.get(`[data-cy='create_apd_btn']`).should('be.disabled');
 
-        years.forEach(year => {
-          cy.findByRole('checkbox', { name: year }).click();
-        });
+      cy.findByLabelText('APD Name').clear().type('MMIS APD Test').blur();
 
-        // cy.contains('At least one FFY must be selected to continue with your APD. Select your FFY(s).').should('exist')
-
-        years.forEach(year => {
-          cy.findByRole('checkbox', { name: year }).click();
-        });
-
-        cy.findByRole('radio', { name: /Yes, it is an update./i })
-          .focus()
-          .blur();
-        // cy.contains('Indicate whether this APD is an update.').should('exist');
-        cy.findByRole('radio', { name: /Yes, it is an update./i }).click();
-        cy.contains('Indicate whether this APD is an update.').should(
-          'not.exist'
-        );
-
-        cy.findByRole('checkbox', { name: /Annual update/i })
-          .focus()
-          .blur();
-        // cy.contains('Select an Update Type.').should('exist')
-        cy.findByRole('checkbox', { name: /Annual update/i }).click();
-        cy.contains('Select an Update Type.').should('not.exist');
-
-        cy.findByRole('checkbox', {
-          name: /1115 or Waiver Support Systems/i
-        })
-          .focus()
-          .blur();
-        // cy.contains('Select at least one Medicaid Business Area.').should('exist');
-
-        cy.findByRole('checkbox', {
-          name: /Other/i
-        })
-          .focus()
-          .click();
-        cy.contains('Select at least one Medicaid Business Area.').should(
-          'not.exist'
-        );
-
-        // BLUR TEXTBOX
-        cy.contains('Provide an other Medicaid Business Area(s).').should(
-          'exist'
-        );
-        // TYPE IN TEXTBOX
-        cy.contains('Provide an other Medicaid Business Area(s).').should(
-          'not.exist'
-        );
-
-        // Test that the information actually saved
+      // Year validation
+      years.forEach(year => {
+        cy.findByRole('checkbox', { name: year }).click();
       });
+      cy.contains('Select at least one year.').should('exist');
+
+      years.forEach(year => {
+        cy.findByRole('checkbox', { name: year }).click();
+      });
+      cy.contains('Select at least one year.').should('not.exist');
+
+      // Update section validation
+      cy.findByRole('radio', { name: /No, this is for a new project./i })
+        .focus()
+        .blur();
+      cy.contains('Indicate whether this APD is an update.').should('exist');
+
+      cy.findByRole('radio', {
+        name: /No, this is for a new project./i
+      }).click();
+      cy.contains('Indicate whether this APD is an update.').should(
+        'not.exist'
+      );
+      cy.findAllByText('Update Type').should('not.exist');
+
+      cy.findByRole('radio', { name: /Yes, it is an update./i }).click();
+      cy.findAllByText('Update Type').should('exist');
+
+      cy.findByRole('checkbox', { name: /Annual update/i })
+        .focus()
+        .blur();
+      cy.contains('Select at least one type of update.').should('exist');
+
+      cy.findByRole('checkbox', { name: /As-needed update/i }).click();
+      cy.contains('Select at least one type of update.').should('not.exist');
+
+      cy.get(`[data-cy='create_apd_btn']`).should('be.disabled');
+
+      // Medicaid Business Area validation
+      cy.findByRole('checkbox', {
+        name: /1115 or Waiver Support Systems/i
+      })
+        .focus()
+        .blur();
+      cy.contains('Provide an other Medicaid Business Area(s)').should('exist');
+
+      cy.findByRole('checkbox', {
+        name: /Program Integrity/i
+      }).click();
+      cy.contains('Provide an other Medicaid Business Area(s)').should(
+        'not.exist'
+      );
+
+      cy.get(`[data-cy='create_apd_btn']`).should('not.be.disabled');
+
+      // Other MBA Validation
+      cy.findByText('Other').click();
+
+      cy.get(`[data-cy='create_apd_btn']`).should('be.disabled');
+
+      cy.get(`[data-cy='other_details']`).focus().blur();
+      cy.contains('Provide an other Medicaid Business Area(s)').should(
+        'not.exist'
+      );
+
+      cy.get(`[data-cy='other_details']`).type('This is other stuff.').blur();
+
+      cy.get(`[data-cy='create_apd_btn']`).should('not.be.disabled');
+
+      // Once MMIS pages have been implemented, we can actually create the
+      // apd here and check that the information saved on the right pages.
+
+      cy.findByRole('button', { name: /Cancel/i }).click();
+
+      // Should redirect to dashboard and not save
+      cy.contains('MMIS APD Test').should('not.exist');
     });
   });
 });
