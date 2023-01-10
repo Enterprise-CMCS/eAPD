8df251261163d4351ee5794bd3f280424aac88a6
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-01-10 15:45:17 -0500

    update test

--- web/src/components/TempAlert.test.js
+++ web/src/components/TempAlert.test.js
@@ -2,6 +2,21 @@ import React from 'react';
 import { renderWithConnection, screen } from 'apd-testing-library';
 
 import TempAlert from './TempAlert';
+import { fireEvent } from '@testing-library/react';
+
+const successMsg =
+  'You have successfully created an APD. Select continue to fill out the rest of the APD.';
+
+const successState = {
+  alerts: {
+    messages: [
+      {
+        message: successMsg,
+        variation: 'success'
+      }
+    ]
+  }
+};
 
 const setup = (props = {}, options = {}) => {
   return renderWithConnection(<TempAlert {...props} />, options);
@@ -13,22 +28,14 @@ describe('temporary alert/message', () => {
   afterAll(() => {});
 
   test('renders temporary message when apd created', async () => {
-    const successMsg =
-      'You have successfully created an APD. Select continue to fill out the rest of the APD.';
-
-    setup(null, {
-      initialState: {
-        alerts: {
-          messages: [
-            {
-              message: successMsg,
-              variation: 'success'
-            }
-          ]
-        }
-      }
-    });
+    setup(null, { initialState: successState });
 
     expect(await screen.findByText(successMsg)).toBeTruthy();
+  });
+
+  test('removes message when close button clicked', () => {
+    setup(null, { initialState: successState });
+    fireEvent.click(screen.getByRole('button'));
+    expect(screen.queryByText(successMsg)).toBeFalsy();
   });
 });
