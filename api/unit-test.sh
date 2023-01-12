97796c9f1948ad6b9a0e963587e53aebc6d0bda1
Tiffany Forkner <tforkner@fearless.tech>
2023-01-12 18:17:09 -0500

    changes to unit test

--- api/unit-test.sh
+++ api/unit-test.sh
@@ -15,7 +15,7 @@ docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec -e LOG_LEVEL=
 docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec api-for-testing yarn test $@
 EXIT_CODE=$? # this must stay right after the test run so that the EXIT_CODE is properly reported
 
-docker cp api-container:/app/api/coverage ./coverage
+docker cp api-container:/app/api/coverage coverage
 docker-compose -f ../docker-compose.endpoint-tests.yml -p api down
 
 exit $EXIT_CODE
