9aba6042ede6048b0cda4ea5ae1bed4f590e2b24
Darren Mirano <dmirano@fearless.tech>
2023-01-17 12:17:38 -0500

    removed hit/hie tables from mmis proposed budget

--- web/src/pages/apd/proposed-budget/ProposedBudget.js
+++ web/src/pages/apd/proposed-budget/ProposedBudget.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import PropTypes from 'prop-types';
 import { Section, Subsection } from '../../../components/Section';
 import CombinedActivityCosts from './CombinedActivityCosts';
 import BudgetSummary from './BudgetSummary';
@@ -7,8 +8,10 @@ import Waypoint from '../../../components/ConnectedWaypoint';
 import IncentivePayments from './IncentivePayments';
 import QuarterlyBudgetSummary from './QuarterlyBudgetSummary';
 import AlertMissingFFY from '../../../components/AlertMissingFFY';
+import { selectApdType } from '../../../redux/selectors/apd.selectors';
+import { connect } from 'react-redux';
 
-const ProposedBudget = () => (
+const ProposedBudget = apdType => (
   <React.Fragment>
     <Waypoint />
     <AlertMissingFFY />
@@ -29,23 +32,37 @@ const ProposedBudget = () => (
         <BudgetSummary />
       </Subsection>
 
-      <Waypoint id="budget-federal-by-quarter" />
-      <Subsection
-        id="budget-federal-by-quarter"
-        resource="proposedBudget.quarterlyBudget"
-      >
-        <QuarterlyBudgetSummary />
-      </Subsection>
+      {apdType.apdType === 'HITECH' && (
+        <Fragment>
+          <Waypoint id="budget-federal-by-quarter" />
+          <Subsection
+            id="budget-federal-by-quarter"
+            resource="proposedBudget.quarterlyBudget"
+          >
+            <QuarterlyBudgetSummary />
+          </Subsection>
 
-      <Waypoint id="budget-incentive-by-quarter" />
-      <Subsection
-        id="budget-incentive-by-quarter"
-        resource="proposedBudget.paymentsByFFYQuarter"
-      >
-        <IncentivePayments />
-      </Subsection>
+          <Waypoint id="budget-incentive-by-quarter" />
+          <Subsection
+            id="budget-incentive-by-quarter"
+            resource="proposedBudget.paymentsByFFYQuarter"
+          >
+            <IncentivePayments />
+          </Subsection>
+        </Fragment>
+      )}
     </Section>
   </React.Fragment>
 );
 
-export default ProposedBudget;
+ProposedBudget.propTypes = {
+  apdType: PropTypes.string
+};
+
+const mapStateToProps = state => ({
+  apdType: selectApdType(state)
+});
+
+export default connect(mapStateToProps)(ProposedBudget);
+
+export { ProposedBudget as plain, mapStateToProps };
