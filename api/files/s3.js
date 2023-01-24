9b8b16598cc8ab690a6cd13f49599a3833282a2d
Tiffany <thetif@gmail.com>
2023-01-24 16:25:08 -0500

    Update s3.js

--- api/files/s3.js
+++ api/files/s3.js
@@ -1,13 +1,13 @@
-import { S3 as awsS3 } from '@aws-sdk/clients';
+import { S3Client as awsS3Client } from "@aws-sdk/client-s3";
 
-export const getFile = async (id, { S3 = awsS3 } = {}) => {
+export const getFile = async (id, { S3Client = awsS3Client } = {}) => {
   if (!process.env.FILE_S3_BUCKET) {
     return Promise.reject(new Error('No S3 bucket specified'));
   }
   if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
     return Promise.reject(new Error('No AWS credentials specified'));
   }
-  return new S3({ apiVersion: '2006-03-01' })
+  return new S3Client({ apiVersion: '2006-03-01' })
     .getObject({
       Bucket: process.env.FILE_S3_BUCKET,
       Key: id
@@ -16,14 +16,14 @@ export const getFile = async (id, { S3 = awsS3 } = {}) => {
     .then(data => data.Body);
 };
 
-export const putFile = async (id, buffer, { S3 = awsS3 } = {}) => {
+export const putFile = async (id, buffer, { S3Client = awsS3Client } = {}) => {
   if (!process.env.FILE_S3_BUCKET) {
     return Promise.reject(new Error('No S3 bucket specified'));
   }
   if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
     return Promise.reject(new Error('No AWS credentials specified'));
   }
-  return new S3({ apiVersion: '2006-03-01' })
+  return new S3Client({ apiVersion: '2006-03-01' })
     .putObject({
       Body: buffer,
       Bucket: process.env.FILE_S3_BUCKET,
