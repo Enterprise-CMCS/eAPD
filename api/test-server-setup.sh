9bcc6a919943d3b0cd89bd5e218303476544bc77
Tiffany Forkner <tforkner@fearless.tech>
2023-01-24 18:00:13 -0500

    cleaning up bash scripts

--- api/test-server-setup.sh
+++ api/test-server-setup.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env sh
 
-#export NODE_ENV=test
-#export COMPOSE_HTTP_TIMEOUT=300
-
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api up -d
-
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec db sh -c 'PGPASSWORD=cms psql -U postgres -tc "DROP DATABASE IF EXISTS hitech_apd_test;"'
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec db sh -c 'PGPASSWORD=cms psql -U postgres -tc "CREATE DATABASE hitech_apd_test;"'
-
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec -e LOG_LEVEL=verbose api-for-testing yarn run migrate
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec -e LOG_LEVEL=verbose api-for-testing yarn run seed
-
 export NODE_ENV=test
 export COMPOSE_HTTP_TIMEOUT=300
 
--- api/unit-test.sh
+++ api/unit-test.sh
@@ -1,29 +1,8 @@
 #!/usr/bin/env sh
 
-#export NODE_ENV=test
-#export COMPOSE_HTTP_TIMEOUT=300
-
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api up -d
-#sleep 60
-
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec db sh -c 'PGPASSWORD=cms psql -U postgres -tc "DROP DATABASE IF EXISTS hitech_apd_test;"'
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec db sh -c 'PGPASSWORD=cms psql -U postgres -tc "CREATE DATABASE hitech_apd_test;"'
-
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec -e LOG_LEVEL=verbose api-for-testing yarn run migrate
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec -e LOG_LEVEL=verbose api-for-testing yarn run seed
-
-
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec api-for-testing yarn test $@
-#EXIT_CODE=$? # this must stay right after the test run so that the EXIT_CODE is properly reported
-
-#docker cp api-container:/app/api/coverage coverage
-#docker-compose -f ../docker-compose.endpoint-tests.yml -p api down
-
-#exit $EXIT_CODE
-
 ./test-server-setup.sh
 
-docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec api-for-testing yarn run test $@
+docker-compose -f ../docker-compose.endpoint-tests.yml -p api exec api-for-testing yarn test $@
 EXIT_CODE=$? # this must stay right after the test run so that the EXIT_CODE is properly reported
 
 docker cp api-container:/app/api/coverage ./coverage
