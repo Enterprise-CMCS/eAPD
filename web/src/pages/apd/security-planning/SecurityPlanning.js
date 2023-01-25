e72615473aec6f6ec415c131d465a853cb58b362
Darren Mirano <dmirano@fearless.tech>
2023-01-25 15:05:33 -0500

    started security planning unit tests

--- web/src/pages/apd/security-planning/SecurityPlanning.js
+++ web/src/pages/apd/security-planning/SecurityPlanning.js
@@ -58,51 +58,71 @@ const SecurityPlanning = ({
             labelFor="security-interface-plan"
             source="securityPlanning.securityInterfacePlan.instruction"
           />
-          <Controller
-            name="securityAndInterfacePlan"
-            control={control}
-            render={({ field: { onChange, ...props } }) => (
-              <RichText
-                {...props}
-                id="security-interface-plan"
-                iframe_aria_text="Security Interface Planning Text Area"
-                content={securityInterfacePlan}
-                onSync={html => {
-                  setPlanForSI(html);
-                  onChange(html);
-                }}
-                editorClassName="rte-textarea-l"
-                error={adminCheck && errors?.securityAndInterfacePlan?.message}
-              />
-            )}
-          />
+          <div className="data-entry-box">
+            <label
+              id="label-for-security-interface-plan"
+              className="ds-c-label--full-width"
+              htmlFor="security-interface-plan"
+            >
+              Security Interface Plan
+            </label>
+            <Controller
+              name="securityAndInterfacePlan"
+              control={control}
+              render={({ field: { onChange, ...props } }) => (
+                <RichText
+                  {...props}
+                  id="security-interface-plan"
+                  iframe_aria_text="Security Interface Planning Text Area"
+                  content={securityInterfacePlan}
+                  onSync={html => {
+                    setPlanForSI(html);
+                    onChange(html);
+                  }}
+                  editorClassName="rte-textarea-l"
+                  error={
+                    adminCheck && errors?.securityAndInterfacePlan?.message
+                  }
+                />
+              )}
+            />
+          </div>
         </div>
         <div className="ds-u-margin-y--3">
           <Instruction
             labelFor="bc-dr-plan"
             source="securityPlanning.bcDrplan.instruction"
           />
-          <Controller
-            name="businessContinuityAndDisasterRecovery"
-            control={control}
-            render={({ field: { onChange, ...props } }) => (
-              <RichText
-                {...props}
-                id="bc-dr-plan"
-                iframe_aria_text="Business Continuity and Disaster Recovery Text Area"
-                content={BCDRPlan}
-                onSync={html => {
-                  setPlanforBCDR(html);
-                  onChange(html);
-                }}
-                editorClassName="rte-textarea-l"
-                error={
-                  adminCheck &&
-                  errors?.businessContinuityAndDisasterRecovery?.message
-                }
-              />
-            )}
-          />
+          <div className="data-entry-box">
+            <label
+              id="label-for-business-continuity-and-disaster-recovery-plan"
+              className="ds-c-label--full-width"
+              htmlFor="bc-dr-plan"
+            >
+              Business Continuity and Disaaster Recovery Plan
+            </label>
+            <Controller
+              name="businessContinuityAndDisasterRecovery"
+              control={control}
+              render={({ field: { onChange, ...props } }) => (
+                <RichText
+                  {...props}
+                  id="bc-dr-plan"
+                  iframe_aria_text="Business Continuity and Disaster Recovery Text Area"
+                  content={BCDRPlan}
+                  onSync={html => {
+                    setPlanforBCDR(html);
+                    onChange(html);
+                  }}
+                  editorClassName="rte-textarea-l"
+                  error={
+                    adminCheck &&
+                    errors?.businessContinuityAndDisasterRecovery?.message
+                  }
+                />
+              )}
+            />
+          </div>
         </div>
       </Section>
     </React.Fragment>
---
+++ web/src/pages/apd/security-planning/SecurityPlanning.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { renderWithConnection, screen, fireEvent } from 'apd-testing-library';
+
+import SecurityPlanning from './SecurityPlanning';
+
+const initialState = {
+  apd: {
+    data: {
+      apdType: 'MMIS',
+      securityPlanning: {
+        securityAndInterfacePlan: 'This is the security and interface plan',
+        businessContinuityAndDisasterRecovery:
+          'Come on and slam and welcome to the jam'
+      },
+      activities: []
+    }
+  }
+};
+
+const setup = (props = {}, options = {}) =>
+  renderWithConnection(<SecurityPlanning {...props} />, options);
+
+describe('<Security Planning />', () => {
+  it('should render correctly', () => {
+    setup({}, { initialState });
+  });
+
+  it.only('should load existing data', () => {
+    setup({}, { initialState });
+    screen.getByLabelText('security-interface-plan');
+  });
+});
