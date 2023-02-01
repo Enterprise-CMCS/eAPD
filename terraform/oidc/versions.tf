654e784f620d96ffd9f7159165a1b39a200fba62
Yonas R <yonas@samtek.io>
2023-02-01 11:08:28 -0500

    update tf s3 location

--- terraform/oidc/versions.tf
+++ terraform/oidc/versions.tf
@@ -9,9 +9,9 @@ terraform {
   required_version = ">= 0.14.6"
 
   backend "s3" {
-    bucket         = "aws-cms-oit-iusg-spe-cmcs-macbis-test-tf-state-us-east-1"
-    key            = "mac-fc-infra/github-oidc/terraform.tfstate"
-    dynamodb_table = "terraform-state-lock"
+    bucket         = "eapd-oidc-terraform-state"
+    key            = "circleci-oidc/terraform.tfstate"
+    # dynamodb_table = "terraform-state-lock"
     region         = "us-east-1"
     encrypt        = "true"
   }
