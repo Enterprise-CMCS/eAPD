b8d8a535fad94cee17cc7411288bab8a9636337d
Amy DiPasquale <adipasquale@fearless.tech>
2023-02-16 11:49:03 -0500

    lint fix

--- web/src/components/Instruction.test.js
+++ web/src/components/Instruction.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'apd-testing-library';
+import { render, screen } from 'apd-testing-library';
 import Instruction from './Instruction';
 import { t } from '../i18n';
 
@@ -13,23 +13,21 @@ describe('Instruction heading', () => {
   it('converts heading to title case by default', () => {
     const originalTitle = 'What type of APD are you creating?';
     t.mockReturnValueOnce(originalTitle);
-    const titleCaseComponent = render(
-      <Instruction source="apd.overview.apdType" />
-    );
+    render(<Instruction source="apd.overview.apdType" />);
 
     const expectedTitleCaseHeading = 'What Type of APD Are You Creating?';
-    const titleCaseHeading = titleCaseComponent.getByRole('heading');
+    const titleCaseHeading = screen.getByRole('heading');
     expect(titleCaseHeading).toHaveTextContent(expectedTitleCaseHeading);
   });
   it('disables title case conversion in the heading when "disableTitleCaseConversion" is passed in', () => {
     const originalTitle = 'What type of APD are you creating?';
     t.mockReturnValueOnce(originalTitle);
 
-    const noTitleCaseComponent = render(
+    render(
       <Instruction source="apd.overview.apdType" disableTitleCaseConversion />
     );
 
-    const noTitleCaseHeading = noTitleCaseComponent.getByRole('heading');
+    const noTitleCaseHeading = screen.getByRole('heading');
     expect(noTitleCaseHeading).toHaveTextContent(originalTitle);
   });
 });
