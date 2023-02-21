9387cc7b6c36b9f0002765caccc3ab13a0166f10
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-02-21 13:03:28 -0500

    update backend test

--- api/util/adminCheck.test.js
+++ api/util/adminCheck.test.js
@@ -460,17 +460,17 @@ const invalidHitechApdOverview = {
   narrativeHIE: '',
   narrativeMMIS: '',
   updateStatus: {
-    isUpdateAPD: null,
-    annualUpdate: null,
-    asNeededUpdate: null
+    isUpdateAPD: false,
+    annualUpdate: false,
+    asNeededUpdate: false
   }
 };
 
 const invalidMmisApdOverview = {
   updateStatus: {
-    isUpdateAPD: null,
-    annualUpdate: null,
-    asNeededUpdate: null
+    isUpdateAPD: false,
+    annualUpdate: false,
+    asNeededUpdate: false
   },
   medicaidBusinessAreas: {
     waiverSupportSystems: false,
