4dd938595cddd6eecde265be3b90e891d032247d
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-02-27 11:41:34 -0500

    update backend unit tests

--- api/util/adminCheck.test.js
+++ api/util/adminCheck.test.js
@@ -2087,7 +2087,7 @@ const expectedErrorsMockHitechApdRandom = [
   {
     section: 'APD Overview',
     link: '/apd/632a0fbc5665670a34b3bbd7/apd-overview',
-    fieldDescription: 'Provide a summary of HIT-funded activities.'
+    fieldDescription: 'Select an update type.'
   },
   {
     section: 'Key State Personnel',
