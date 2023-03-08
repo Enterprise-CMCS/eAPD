4a0a8f51f91e644cc5464ccd2ba12d9b84edd9e9
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-03-08 10:32:30 -0500

    update cypress tests

--- e2e/cypress/helpers/mmis/mmis-navigation.js
+++ e2e/cypress/helpers/mmis/mmis-navigation.js
@@ -1,15 +1,23 @@
-export const testApdNavigation = function () {
+export const testMmisNavigation = function () {
   const pages = [
     {
       title: 'APD Overview',
       type: 'h2'
     },
     {
+      title: 'State Priorities and Scope of APD',
+      type: 'h2'
+    },
+    {
       title: 'Key State Personnel',
+      label: 'Key Personnel and Program Management',
+      subnav: ['#apd-state-profile-office', '#apd-state-profile-key-personnel'],
       type: 'h2'
     },
     {
       title: 'Results of Previous Activities',
+      label: 'Prior Activities Overview',
+      subnav: ['#prev-activities-outline', '#prev-activities-table'],
       type: 'h2'
     },
     {
@@ -22,6 +30,12 @@ export const testApdNavigation = function () {
     },
     {
       title: 'Proposed Budget',
+      label: 'Combined Activity Costs',
+      subnav: ['#combined-activity-costs-table', '#budget-summary-table'],
+      type: 'h2'
+    },
+    {
+      title: 'Security Planning',
       type: 'h2'
     },
     {
@@ -34,6 +48,12 @@ export const testApdNavigation = function () {
     },
     {
       title: 'Export and Submit',
+      label: 'APD Overview Summary',
+      subnav: [
+        '#executive-overview-summary',
+        '#executive-activities-summary',
+        '#executive-summary-budget-table'
+      ],
       type: 'h2'
     }
   ];
@@ -41,8 +61,7 @@ export const testApdNavigation = function () {
   it('confirms Continue and Previous buttons', function () {
     cy.log('Click through Continue buttons');
     cy.wrap([...pages]).each((page, index) => {
-      let title = page.title,
-        type = page.type;
+      const { title, type } = page;
       cy.get('.ds-c-vertical-nav__item').contains(title).click();
       cy.get(type).should('contain', title);
 
@@ -53,8 +72,7 @@ export const testApdNavigation = function () {
 
     cy.log('Click through Previous buttons');
     cy.wrap([...pages].reverse()).each((page, index) => {
-      let title = page.title,
-        type = page.type;
+      const { title, type } = page;
       cy.get('.ds-c-vertical-nav__item').contains(title).click();
       cy.get(type).should('contain', title);
       if (index < pages.length - 1) {
@@ -64,43 +82,11 @@ export const testApdNavigation = function () {
   });
 
   it('confirms side nav buttons to redirect to correct sections', function () {
-    const pageWithAnchors = [
-      {
-        parent: 'Key State Personnel',
-        label: 'Key Personnel and Program Management',
-        subnav: '#apd-state-profile-key-personnel'
-      },
-      {
-        parent: 'Results of Previous Activities',
-        label: 'Prior Activities Overview',
-        subnav: ['#prev-activities-outline', '#prev-activities-table']
-      },
-      {
-        parent: 'Proposed Budget',
-        label: 'Combined Activity Costs',
-        subnav: [
-          '#combined-activity-costs-table',
-          '#budget-summary-table',
-          '#budget-federal-by-quarter',
-          '#budget-incentive-by-quarter'
-        ]
-      },
-      {
-        parent: 'Executive Summary',
-        label: 'APD Overview Summary',
-        subnav: [
-          '#executive-overview-summary',
-          '#executive-activities-summary',
-          '#executive-summary-budget-table'
-        ]
-      }
-    ];
-
-    cy.wrap(pageWithAnchors).each(index => {
-      const { subnav } = index;
+    cy.wrap(pages).each(index => {
+      const { label, subnav, title } = index;
 
       cy.get('.ds-c-vertical-nav__label--parent')
-        .contains(index.parent)
+        .contains(title)
         .then($el => {
           if ($el.attr('aria-expanded') === 'false') {
             // if it's not expanded, expand it
@@ -108,7 +94,7 @@ export const testApdNavigation = function () {
           }
 
           // Click on anchor link
-          cy.get('a.ds-c-vertical-nav__label').contains(index.label).click();
+          cy.get('a.ds-c-vertical-nav__label').contains(label).click();
         });
 
       if (Array.isArray(subnav)) {
@@ -125,23 +111,5 @@ export const testApdNavigation = function () {
           .then(offset => cy.window().its('scrollY').should('eq', offset));
       }
     });
-  });
-
-  it('should go to the Activity Overview page when edit is clicked in Executive Summary', function () {
-    cy.goToExecutiveSummary();
-
-    cy.get('#executive-activities-summary')
-      .parent()
-      .contains('div', 'Activity 1: Program Administration')
-      .parent()
-      .parent()
-      .findByRole('link', { name: 'Edit' })
-      .click();
-
-    cy.findByRole('heading', {
-      name: /^Activity 1:/i,
-      level: 2
-    }).should('exist');
-    cy.findByRole('heading', { name: /Activity Overview/i }).should('exist');
   });
 };
