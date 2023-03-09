f191686550d69d14643c86f289cfefce3cb7f196
Amy DiPasquale <adipasquale@fearless.tech>
2023-03-09 09:31:06 -0500

    create migration file for MMIS budget changes

---
+++ api/migrations/20230224154256_mongoose-mmis-budget-match-rate.js
@@ -0,0 +1,39 @@
+import loggerFactory from '../logger/index.js';
+import { setup, teardown } from '../db/mongodb.js';
+import { MMIS, MMISBudget } from '../models/index.js';
+import { calculateBudget } from '@cms-eapd/common';
+
+const logger = loggerFactory('mongoose-migrate/mmis-budget-match-rate');
+
+/**
+ * Update MMIS APD budgets to remove fields and add new fields (ddi and mando)
+ */
+export const up = async () => {
+  // Grab all MMIS APDs
+  await setup();
+  const apds = await MMIS.find();
+
+  logger.info(`Found ${apds.length} MMIS APDs`);
+
+  await Promise.all(
+    apds.map(async apd => {
+      logger.info(`Updating APD ${apd._id} Budget ${apd.budget}`);
+
+      const budget = calculateBudget(apd);
+
+      try {
+        return MMISBudget.replaceOne({ _id: apd.budget }, { ...budget });
+      } catch (err) {
+        logger.error(err);
+      }
+    })
+  ).catch(err => logger.error(err));
+
+  await teardown();
+};
+
+/**
+ * TODO: Create the down function. Would it just be recalculating the budget again??
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+export const down = async () => {};
