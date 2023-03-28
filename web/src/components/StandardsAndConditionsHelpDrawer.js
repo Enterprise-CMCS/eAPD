118e6e1bc622260a6329cdc3c545d7f0859a7ce2
Tiffany Forkner <tforkner@fearless.tech>
2023-03-28 12:06:46 -0400

    changed help links to open in a new tab

--- web/src/components/StandardsAndConditionsHelpDrawer.js
+++ web/src/components/StandardsAndConditionsHelpDrawer.js
@@ -27,7 +27,10 @@ const StandardsAndConditionsHelpDrawer = () => {
               <div className="ds-u-padding-bottom--1">
                 <strong>For more details on this regulation.</strong>
                 <br />
-                <a href="https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-433/subpart-C/section-433.112">
+                <a
+                  href="https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-433/subpart-C/section-433.112"
+                  target="_blank"
+                >
                   Review 42. CFR 433.112 documentation
                 </a>
               </div>
