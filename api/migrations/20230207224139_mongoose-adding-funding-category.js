153d84a259f9924284b970dd89d9db25e2623d29
Tiffany <tforkner@fearless.tech>
2023-03-28 10:40:55 -0400

    [maintenance] resolved migration issue (#4643)

--- api/migrations/20230207224139_mongoose-adding-funding-category.js
+++ api/migrations/20230207224139_mongoose-adding-funding-category.js
@@ -1,6 +1,6 @@
 import loggerFactory from '../logger/index.js';
 import { setup, teardown } from '../db/mongodb.js';
-import { APD, Budget } from '../models/index.js';
+import { APD, getApdModel, getBudgetModel } from '../models/index.js';
 import {
   APD_TYPE,
   FUNDING_CATEGORY_TYPE,
@@ -30,115 +30,117 @@ const updateFundingCategory = ({ federal = null, state = null } = {}) => {
 export const up = async () => {
   // Grab all APDs
   await setup();
-  const apds = await APD.find({
-    $or: [
-      {
-        'activities.costAllocation.$[].ffp.fundingCategory': { $exists: false }
-      },
-      {
-        'keyStatePersonnel.keyPersonnel.split.$[].fundingCategory': {
-          $exists: false
-        }
-      }
-    ]
-  }).lean();
+  // { virtuals: true } returns apdType along with __t
+  const apds = await APD.find().lean({ virtuals: true });
   logger.info(`Updating ${apds.length} APDs`);
 
-  const apdIds = [];
-  const budgetIds = [];
+  const updatedApds = [];
 
-  const updatedApds = apds.map(apd => {
-    apdIds.push(apd._id);
-    if (apd.budget) {
-      budgetIds.push(apd.budget);
-    }
+  apds.forEach(apd => {
     const apdJSON = deepCopy(apd);
     const years = apd.years;
     const apdType = apd.__t;
 
-    apdJSON.keyStatePersonnel.keyPersonnel =
-      apdJSON.keyStatePersonnel.keyPersonnel.map(keyPersonnel => ({
-        ...keyPersonnel,
-        split: years.reduce(
-          /**
-           * Iterate over each year and return an object with the year as the key and
-           * set the default for existing HITECH APDs to a 90/10 split.
-           * If years is ['2022', '2023'] the resulting object will look like this:
-           * {
-           *   '2022': { federal: 90, state: 10 }
-           *   '2023': { federal: 90, state: 10 }
-           * }
-           */
-          (acc, year) => ({
-            ...acc,
-            [year]: {
-              ...keyPersonnel?.split?.[year],
-              fundingCategory: updateFundingCategory(
-                keyPersonnel?.split?.[year]
-              )
-            }
-          }),
-          {}
-        )
-      }));
+    let updated = false;
 
-    apdJSON.activities = apdJSON.activities.map(activity => ({
-      ...activity,
-      costAllocation: years.reduce((acc, year) => {
-        if (
-          activity?.costAllocation?.[year]?.ffp?.fundingCategory !==
-            undefined &&
-          activity?.costAllocation?.[year]?.ffp?.fundingCategory !== null
-        ) {
+    apdJSON.keyStatePersonnel.keyPersonnel =
+      apdJSON.keyStatePersonnel.keyPersonnel.map(keyPersonnel => {
+        if (keyPersonnel?.split?.[years[0]]?.fundingCategory === undefined) {
+          updated = true;
           return {
-            ...acc,
-            [year]: {
-              ffp: {
-                ...activity?.costAllocation?.[year]?.ffp
-              }
-            }
+            ...keyPersonnel,
+            split: years.reduce(
+              /**
+               * Iterate over each year and return an object with the year as the key and
+               * set the default for existing HITECH APDs to a 90/10 split.
+               * If years is ['2022', '2023'] the resulting object will look like this:
+               * {
+               *   '2022': { federal: 90, state: 10 }
+               *   '2023': { federal: 90, state: 10 }
+               * }
+               */
+              (acc, year) => ({
+                ...acc,
+                [year]: {
+                  ...keyPersonnel?.split?.[year],
+                  fundingCategory: updateFundingCategory(
+                    keyPersonnel?.split?.[year]
+                  )
+                }
+              }),
+              {}
+            )
           };
         }
-        return {
-          ...acc,
-          [year]: {
-            ffp: {
-              federal:
-                apdType === APD_TYPE.HITECH
-                  ? activity?.costAllocation?.[year]?.ffp?.federal
-                  : 0,
-              state:
-                apdType === APD_TYPE.HITECH
-                  ? activity?.costAllocation?.[year]?.ffp?.state
-                  : 0,
-              fundingCategory: null
-            },
-            other:
-              apdType === APD_TYPE.HITECH
-                ? activity?.costAllocation?.[year]?.other
-                : 0
-          }
-        };
-      }, {})
-    }));
+        return keyPersonnel;
+      });
 
-    return apdJSON;
+    apdJSON.activities = apdJSON.activities.map(activity => {
+      if (
+        activity?.costAllocation?.[years[0]]?.ffp?.fundingCategory === undefined
+      ) {
+        updated = true;
+        return {
+          ...activity,
+          costAllocation: years.reduce(
+            (acc, year) => ({
+              ...acc,
+              [year]: {
+                ffp: {
+                  federal:
+                    apdType === APD_TYPE.HITECH
+                      ? activity?.costAllocation?.[year]?.ffp?.federal
+                      : 0,
+                  state:
+                    apdType === APD_TYPE.HITECH
+                      ? activity?.costAllocation?.[year]?.ffp?.state
+                      : 0,
+                  fundingCategory: null
+                },
+                other:
+                  apdType === APD_TYPE.HITECH
+                    ? activity?.costAllocation?.[year]?.other
+                    : 0
+              }
+            }),
+            {}
+          )
+        };
+      }
+      return activity;
+    });
+
+    if (updated) {
+      logger.info(`APD ${apdJSON._id} needs to be updated`);
+      updatedApds.push(apdJSON);
+    }
   });
 
-  if (updatedApds.length === apds.length) {
+  if (updatedApds.length > 0) {
     await Promise.all(
       updatedApds.map(apd => {
         logger.info(`Updating APD ${apd._id} to add funding category`);
-        return APD.replaceOne({ _id: apd._id }, { ...apd });
+        const apdType = apd.__t;
+        return getApdModel(apdType).replaceOne({ _id: apd._id }, { ...apd });
       })
     ).catch(err => logger.error(err));
     await Promise.all(
       updatedApds.map(apd => {
         logger.info(`Updating APD ${apd._id} Budget ${apd.budget}`);
-        const budget = calculateBudget(apd);
-        return Budget.replaceOne({ _id: apd.budget }, { ...budget });
+
+        const apdType = apd.__t;
+        const budget = calculateBudget({
+          ...apd,
+          apdType
+        });
+        return getBudgetModel(apdType).replaceOne(
+          { _id: apd.budget },
+          { ...budget }
+        );
       })
     ).catch(err => logger.error(err));
+  } else {
+    logger.info('Nothing to update');
   }
   await teardown();
 };
