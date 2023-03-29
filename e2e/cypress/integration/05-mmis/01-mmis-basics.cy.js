0ba75f4a7cb9bf7e8c387c613cad9379076a0099
Darren Mirano <dmirano@fearless.tech>
2023-03-29 15:26:06 -0400

    updated admin check format to match navigation test

--- e2e/cypress/integration/05-mmis/01-mmis-basics.cy.js
+++ e2e/cypress/integration/05-mmis/01-mmis-basics.cy.js
@@ -2,6 +2,7 @@
 import BudgetPage from '../../page-objects/budget-page.js';
 import { testApdName } from '../../helpers/apd/apd-name.js';
 import { testMmisNavigation } from '../../helpers/mmis/mmis-navigation.js';
+import { testMmisAdminCheck } from '../../helpers/mmis/mmis-admin-check.js';
 
 /// <reference types="cypress" />
 
@@ -183,156 +184,11 @@ describe('MMIS Basics', { tags: ['@apd', '@default', '@mmis'] }, function () {
     testMmisNavigation();
   });
 
+  describe('Default MMIS APD - Admin Check', function () {
+    testMmisAdminCheck();
+  });
+
   describe('MMIS Pages', function () {
-    describe.only('Admin Check Functionality', function () {
-      it(
-        'tests the Admin check for mmis pages',
-        { tags: ['@state', '@admin'] },
-        function () {
-          const defaultAdminCheck = [
-            {
-              hyperlink: 'Key State Personnel',
-              header: '',
-              fieldType: ['textField', 'input[name="medicaidDirector.name"]'],
-              errorMessage: 'Provide the name of the State Medicaid Director.'
-            },
-            {
-              hyperlink: 'State Priorities and Scope',
-              header: '',
-              fieldType: ['tinyMCE'],
-              errorMessage: 'Provide Medicaid Program and Priorities.'
-            },
-            {
-              hyperlink: 'Activity 1 Activity Overview',
-              header: 'Activity Overview',
-              fieldType: ['textField'],
-              errorMessage: 'Provide an Activity name.'
-            },
-            {
-              hyperlink: 'Activity 1 Conditions for Enhanced Funding',
-              header: 'Conditions for Enhanced Funding',
-              fieldType: ['WHO KNOWS?'],
-              errorMessage: 'PLACE HOLDER ERROR MESSAGE'
-            },
-            {
-              hyperlink: 'Activity 1 Activity Schedule',
-              header: 'Activity Schedule',
-              fieldType: ['dateField'],
-              errorMessage: 'Provide the name of the State Medicaid Director.'
-            },
-            {
-              hyperlink: 'Activity 1 Budget and FFP',
-              header: 'Budget and FFP',
-              fieldType: ['radio'],
-              errorMessage: 'Select a federal-state split.'
-            },
-            {
-              hyperlink: 'Activity 1 Cost Allocation',
-              header: 'Cost Allocation',
-              fieldType: ['tinyMCE'],
-              errorMessage:
-                'Provide a description of the cost allocation methodology.'
-            },
-            {
-              hyperlink: 'Security Planning',
-              header: '',
-              fieldType: ['tinyMCE'],
-              errorMessage: 'Provide Security and Interface Plan'
-            },
-            {
-              hyperlink: 'Assurances and Compliance',
-              header: '',
-              fieldType: ['assurances'],
-              errorMessage: 'Select yes or no.' // Maybe some condition here to check no text box
-            } // Maybe have a list of all validation error messages? then loop through them?
-          ];
-
-          cy.goToActivityDashboard();
-          cy.findAllByText('Add Activity').click();
-
-          cy.turnOnAdminCheck(); // On each page verify the correct number of validation errors show
-
-          cy.get('[class="eapd-admin-check  ds-c-drawer"]').should('exist');
-
-          cy.goToApdOverview();
-          cy.get('[data-cy="validationError"]').should('exist');
-
-          cy.findByRole('button', { name: /Stop Administrative Check/i }).click(
-            {
-              force: true
-            }
-          );
-          cy.get('[data-cy="validationError"]').should('not.exist');
-
-          cy.turnOnAdminCheck();
-
-          cy.get('[data-cy="numRequired"]').should('have.text', '31'); // GET RIGHT NUMBER
-
-          cy.findByRole('button', { name: /Collapse/i }).click({
-            force: true
-          });
-
-          cy.get('[class="eapd-admin-check-list"]').should('not.exist');
-          cy.findByRole('button', {
-            name: /Stop Administrative Check/i
-          }).should('not.exist');
-
-          cy.findByRole('button', { name: /Expand/i }).click({
-            force: true
-          });
-
-          // Check APD Overview validation errors since it's a wierd case
-          // where it has pre filled in values. In this case also test that
-          // the error count changes in collapsed/sxpanded view, and the error
-          // re-appears/disappears from the list
-
-          defaultAdminCheck.forEach(value => {
-            if (value.header === '') {
-              cy.checkAdminCheckHyperlinks(value.hyperlink, value.hyperlink, 2);
-            } else {
-              cy.checkAdminCheckHyperlinks(value.hyperlink, value.header, 3);
-            }
-
-            cy.get('[data-cy="validationError"]')
-              .contains(value.errorMessage)
-              .should('exist');
-
-            // Validation message should disappear/re-appear when value is changed
-            if (value.fieldType[0] === 'textField') {
-              cy.get(value.fieldType[1]).clear().type('Test Name');
-              cy.get('[data-cy="validationError"]')
-                .contains(value.errorMessage)
-                .should('not.exist');
-
-              cy.get(value.fieldType[1]).clear();
-              cy.get('[data-cy="validationError"]')
-                .contains(value.errorMessage)
-                .should('exist');
-            } else if (value.fieldType[0] === 'tinyMCE') {
-              cy.setTinyMceContent(value.fieldType[1], 'Test Value');
-              cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
-              cy.get('[data-cy="validationError"]')
-                .contains(value.errorMessage)
-                .should('not.exist');
-
-              cy.setTinyMceContent(value.fieldType[1], '');
-              cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
-              cy.get('[data-cy="validationError"]')
-                .contains(value.errorMessage)
-                .should('exist');
-            } else if (value.fieldType[0] === 'radio') {
-            } else if (value.fieldType[0] === 'dateField') {
-            } else if (value.fieldType[0] === 'assurances') {
-            } else {
-              throw new Error('Invalid field type!');
-            }
-          });
-
-          // Test admin check on functionally complete APD!!!!!!!!!!!
-        }
-      );
-    });
-
     describe('APD Overview page', () => {
       testApdName(); // Try removing the describe above and moving testApdName to the it below
 
