e0bae3230f6e9d17c60bf3ebea7d8658472080e9
amyd11 <ald5044@gmail.com>
2023-03-31 13:20:14 -0400

    fix typo in test

--- web/src/pages/apd/activities/enhanced-funding/ConditionsForEnhancedFundingReadOnly.test.js
+++ web/src/pages/apd/activities/enhanced-funding/ConditionsForEnhancedFundingReadOnly.test.js
@@ -80,7 +80,7 @@ describe('Conditions for Enhanced Funding Read-Only', () => {
     ).toHaveTextContent('No response was provided.');
   });
 
-  it('should render the Yest qualification with justification values correctly', async () => {
+  it('should render the Yes qualification with justification values correctly', async () => {
     const enhancedFundingJustification =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tellus elit. Pellentesque sed porttitor velit. In at dui vitae elit fringilla mattis. Fusce aliquet tincidunt arcu sit amet volutpat. Aenean maximus ultrices nulla, quis ultrices odio maximus eget. Duis blandit nisl libero, id hendrerit sem elementum nec. In est turpis, sodales sed nisl sed, pulvinar cursus tortor. Ut sit amet imperdiet velit. Ut maximus dui at purus tempor, et accumsan magna aliquam. Pellentesque vitae lorem elementum, eleifend turpis vitae, congue diam. Vestibulum in magna auctor, imperdiet sem vel, placerat sapien. Mauris facilisis neque metus, eu ultricies tortor tempor id.';
     await setup({
