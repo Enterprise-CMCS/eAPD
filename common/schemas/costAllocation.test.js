96377b7d34ee8de98d199183183530ebe710c1e6
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-04-11 11:07:15 -0400

    update common tests

--- common/schemas/costAllocation.test.js
+++ common/schemas/costAllocation.test.js
@@ -1,7 +1,7 @@
 import {
   costAllocationSplitSchema,
   costAllocationMatchRateSchema,
-  costAllocationOtherSchema,
+  hitechCostAllocationFFP,
   hitechCostAllocationSchema,
   mmisCostAllocationSchema
 } from './costAllocation.js';
@@ -160,48 +160,13 @@ describe('cost allocation validation', () => {
     });
   });
 
-  describe('cost allocation other funding', () => {
-    test('valid other funding', () => {
-      const { error: { details: schemaValidation = [] } = {} } =
-        costAllocationOtherSchema.validate(0, { abortEarly: false });
-      expect(schemaValidation).toEqual([]);
-    });
-
-    test('negative other funding', () => {
-      const { error: { details: schemaValidation = [] } = {} } =
-        costAllocationOtherSchema.validate(-1, { abortEarly: false });
-      expect(schemaValidation.length).toEqual(1);
-      expect(schemaValidation[0].message).toEqual(
-        'Provide an other funding amount greater than or equal to $0.'
-      );
-    });
-
-    test('null other funding', () => {
-      const { error: { details: schemaValidation = [] } = {} } =
-        costAllocationOtherSchema.validate(null, { abortEarly: false });
-      expect(schemaValidation.length).toEqual(1);
-      expect(schemaValidation[0].message).toEqual(
-        'Provide an other funding amount greater than or equal to $0.'
-      );
-    });
-
-    test('undefined other funding', () => {
-      const { error: { details: schemaValidation = [] } = {} } =
-        costAllocationOtherSchema.validate(undefined, { abortEarly: false });
-      expect(schemaValidation.length).toEqual(1);
-      expect(schemaValidation[0].message).toEqual(
-        'Provide a valid funding amount.'
-      );
-    });
-  });
-
   describe('cost allocation', () => {
     test('valid cost allocation with split', () => {
       const { error: { details: schemaValidation = [] } = {} } =
-        hitechCostAllocationSchema.validate(
+        hitechCostAllocationFFP.validate(
           {
-            2022: { ffp: { federal: 75, state: 25 }, other: 0 },
-            2023: { ffp: { federal: 50, state: 50 }, other: 0 }
+            2022: { ffp: { federal: 75, state: 25 } },
+            2023: { ffp: { federal: 50, state: 50 } }
           },
           { abortEarly: false }
         );
@@ -212,8 +177,10 @@ describe('cost allocation validation', () => {
       const { error: { details: schemaValidation = [] } = {} } =
         hitechCostAllocationSchema.validate(
           {
-            2022: { ffp: { federal: 0, state: 100 }, other: 0 },
-            2023: { ffp: { federal: 90, state: 10 }, other: 0 }
+            costAllocation: {
+              2022: { ffp: { federal: 0, state: 100 }, other: 0 },
+              2023: { ffp: { federal: 90, state: 10 }, other: 0 }
+            }
           },
           { abortEarly: false }
         );
