13a95a0fc5aa1e6e39f5d447afa444e630916bb9
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-04-20 11:28:18 -0400

    remove error boundary

--- web/src/pages/MainRoutes.js
+++ web/src/pages/MainRoutes.js
@@ -4,7 +4,6 @@ import { Route as PublicRoute, Switch } from 'react-router-dom';
 import routes from './mainRoutesList';
 import AdminRoute from './admin/AdminRoute';
 import PrivateRoute from '../components/PrivateRoute';
-import ErrorBoundary from '../components/ErrorBoundary';
 
 const MainRoutes = () => (
   <Switch>
@@ -13,20 +12,20 @@ const MainRoutes = () => (
       if (isPublic) {
         return (
           <PublicRoute key={key} path={path} {...routeProps}>
-            <ErrorBoundary>{children}</ErrorBoundary>
+            {children}
           </PublicRoute>
         );
       }
       if (isAdmin) {
         return (
           <AdminRoute key={key} path={path} {...routeProps}>
-            <ErrorBoundary>{children}</ErrorBoundary>
+            {children}
           </AdminRoute>
         );
       }
       return (
         <PrivateRoute key={key} path={path} {...routeProps}>
-          <ErrorBoundary>{children}</ErrorBoundary>
+          {children}
         </PrivateRoute>
       );
     })}
