736a4a021fae6151f22abc535ea7eeb6d30d3687
Amy DiPasquale <adipasquale@fearless.tech>
2023-04-20 15:31:23 -0400

    attempt to resolve e2e next test

--- e2e/cypress/page-objects/export-page.js
+++ e2e/cypress/page-objects/export-page.js
@@ -608,7 +608,7 @@ class ExportPage {
   }) => {
     cy.findByRole('heading', { level: 2, name: /Executive Summary/i })
       .parent()
-      .contains('h3', `Activity ${index + 1}: ${activityName}`)
+      .contains('h4', `Activity ${index + 1}: ${activityName}`)
       .next()
       .within(() => {
         if (shortOverview) {
