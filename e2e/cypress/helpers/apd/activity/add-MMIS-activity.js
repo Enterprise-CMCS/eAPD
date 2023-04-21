0d57ab5471d1a03228236cc1a29fe410108eeea7
mirano-darren <87026092+mirano-darren@users.noreply.github.com>
2023-04-21 13:33:31 -0400

    Dmirano/4532 cypress valid mmis apd (#4684)
    * started nav test
    
    * progress on tests
    
    * more progress on tests
    
    * more progress with cypress tests
    
    * polished up tests
    
    * started mmis admin check test
    
    * small changes
    
    * accessibility error - cannot have any element other than <li in <ul. replaced <ul with <div and <li with <p
    
    * finished navigation test, still need updated for program budget table on exec summary
    
    * polished navigation tests
    
    * updated admin check format to match navigation test
    
    * progress
    
    * more progress!
    
    * admin check test progress!
    
    * updates
    
    * finished admin check test
    
    * progress! Apd overview, state priorities and scope of apd, and key state personnel down
    
    * previous activities
    
    * results of previous activities and added a key personnel with a cost
    
    * security planning
    
    * activities progress
    
    * activities!
    
    * save value for mmis previous activities
    
    * finished test
    
    * fixed linter
    
    * Trigger CI
    
    * removed duplicate test
    
    * adjusted beforeEach
    
    * added var
    
    * Trigger CI
    
    * lowered codecov threshold
    
    ---------
    
    Co-authored-by: Nicole Zonnenberg <nzonnenberg@fearless.tech>

--- e2e/cypress/helpers/apd/activity/add-MMIS-activity.js
+++ e2e/cypress/helpers/apd/activity/add-MMIS-activity.js
@@ -1,75 +1,285 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/prefer-default-export */
 
-import BudgetPage from '../../../page-objects/budget-page.js';
-import FillOutActivityPage from '../../../page-objects/fill-out-activity-page.js';
-import ExportPage from '../../../page-objects/export-page.js';
-
 const { _ } = Cypress;
 
 export const addMMISActivity = function () {
-  let budgetPage;
-  let exportPage;
-  let fillOutActivityPage;
-
-  before(function () {
-    budgetPage = new BudgetPage();
-    exportPage = new ExportPage();
-    fillOutActivityPage = new FillOutActivityPage();
-  });
+  const fillDate = (string, list) => {
+    cy.findAllByText(string)
+      .parent()
+      .next()
+      .within(() => {
+        cy.findByLabelText('Month').clear().type(list[0]);
+        cy.findByLabelText('Day').clear().type(list[1]);
+        cy.findByLabelText('Year').clear().type(list[2]).blur();
+      });
+  };
 
   beforeEach(function () {
-    cy.fixture('mmis-basics.json').as('mmisBasic');
-    cy.fixture('mmis-basics-budget.json').as('budget');
-    cy.fixture('mmis-basics-table-data.json').as('tableData');
+    cy.fixture('mmis-with-data.json').as('mmisData');
   });
 
   describe('Add a MMIS Activity', function () {
     it('Adds an MMIS Activity and checks the export view', function () {
-      const years = this.years;
-      const mmisBasic = this.mmisBasic;
-      const budget = this.budget;
-      const tableData = this.tableData;
+      const mmisData = this.mmisData;
+      const activityData = mmisData.activities;
 
-      cy.goToActivityDashboard();
+      activityData.forEach((activity, index) => {
+        cy.goToActivityDashboard();
 
-      // Add activity
-      cy.url().should('include', '/activities');
-      cy.findByRole('heading', { name: /Activities/i, level: 2 }).should(
-        'exist'
-      );
+        cy.contains('Add Activity').click();
+        cy.get('#activities').findAllByText('Edit').eq(index).click();
 
-      cy.contains('Add Activity').click();
-      cy.contains('Activity 1: Untitled').should('exist');
-      cy.get('#activities').findAllByText('Edit').eq(0).click();
+        // Activity Overview
+        const activityOverviewData = activity.activityOverview;
+        cy.findByLabelText('Activity name').type(activity.name);
+        cy.setTinyMceContent(
+          'activity-snapshot-field',
+          activityOverviewData.activitySnapshot
+        );
+        cy.setTinyMceContent(
+          'activity-problem-statement-field',
+          activityOverviewData.problemStatement
+        );
+        cy.setTinyMceContent(
+          'activity-proposed-solution-field',
+          activityOverviewData.proposedSolution
+        );
+        cy.waitForSave();
 
-      // Fill out Activity Overview
-      cy.findByLabelText('Activity name').type(mmisBasic.activities[0].name);
-      cy.findAllByText(`Activity 1: ${mmisBasic.activities[0].name}`).should(
-        'exist'
-      );
+        cy.get('#continue-button').click();
 
-      cy.goToBudgetAndFFP(0);
+        // Analysis of Alternatives and Risk
+        const analysisOfAlternativesAndRisksData =
+          activity.analysisOfAlternativesAndRisks;
+        cy.setTinyMceContent(
+          'alternative-analysis-field',
+          analysisOfAlternativesAndRisksData.alternativeAnalysis
+        );
+        cy.setTinyMceContent(
+          'cost-benefit-analysis-field',
+          analysisOfAlternativesAndRisksData.costBenefitAnalysis
+        );
+        cy.setTinyMceContent(
+          'feasibility-study-field',
+          analysisOfAlternativesAndRisksData.feasibilityStudy
+        );
+        cy.setTinyMceContent(
+          'requirements-analysis-field',
+          analysisOfAlternativesAndRisksData.requirementsAnalysis
+        );
+        cy.setTinyMceContent(
+          'forseeable-risks-field',
+          analysisOfAlternativesAndRisksData.forseeableRisks
+        );
+        cy.waitForSave();
 
-      fillOutActivityPage.checkMmisBudgetAndFFPTables({
-        years,
-        costAllocation: mmisBasic.activities[0].costAllocation,
-        expectedTableData: tableData.activityFedSplitTable
+        cy.get('#continue-button').click();
+
+        // Activity Schedule and Milestones
+        const activityScheduleData = activity.activitySchedule;
+
+        const startDate = activityScheduleData.plannedStartDate;
+        fillDate('Start date', startDate.split('-'));
+
+        const endDate = activityScheduleData.plannedEndDate;
+        fillDate('End date', endDate.split('-'));
+
+        const milestonesData = activity.milestones;
+        milestonesData.forEach(milestone => {
+          cy.contains('Add Milestone').click();
+
+          cy.findByLabelText('Name').type(milestone.milestone);
+          fillDate('Target completion date', milestone.endDate.split('-'));
+          cy.findByRole('button', { name: /Save/i }).click();
+          cy.waitForSave();
+        });
+
+        cy.get('#continue-button').click();
+
+        // Conditions for Enhanced Funding
+        const conditionForEnhancedFundingData =
+          activity.conditionsForEnhancedFunding;
+        if (conditionForEnhancedFundingData.enhancedFundingQualification) {
+          cy.findByRole('radio', {
+            name: /Yes, this activity is qualified for enhanced funding./i
+          }).click();
+          cy.setTinyMceContent(
+            'justification-field',
+            conditionForEnhancedFundingData.enhancedFundingJustification
+          );
+        } else {
+          cy.findByRole('radio', {
+            name: /No, not applicable for enhanced funding, this activity has a 50\/50 federal state split./i
+          }).click();
+        }
+
+        // cy.waitForSave();
+        cy.get('#continue-button').click();
+
+        // Outcomes and Metrics
+        const outcomesAndMetricsData = activity.outcomes;
+        outcomesAndMetricsData.forEach((outcome, index) => {
+          cy.findByRole('button', { name: /Add Outcome/i }).click();
+          cy.get(`[data-cy="outcome-${index}"]`).type(outcome.outcome);
+          if (outcome.metrics.length > 0) {
+            outcome.metrics.forEach((metric, index2) => {
+              cy.findByRole('button', {
+                name: /Add Metric to Outcome/i
+              }).click();
+              cy.get(`[data-cy="metric-${index}-${index2}"]`).type(
+                metric.metric
+              );
+            });
+            cy.findByRole('button', { name: /Save/i }).click();
+            cy.waitForSave();
+          }
+        });
+
+        cy.get('#continue-button').click();
+
+        // State Staff and Expenses
+        const statePersonnelData = activity.statePersonnel;
+        statePersonnelData.forEach(statePersonnel => {
+          cy.findByRole('button', { name: /Add State Staff/i }).click();
+
+          cy.findByLabelText('Personnel title').type(statePersonnel.title);
+          cy.findByLabelText('Description').type(statePersonnel.description);
+
+          mmisData.years.forEach((year, index) => {
+            if (statePersonnel.years[year]) {
+              cy.findAllByLabelText('Cost with benefits')
+                .eq(index)
+                .type(statePersonnel.years[year].amt);
+              cy.findAllByLabelText('Number of FTEs')
+                .eq(index)
+                .type(statePersonnel.years[year].perc)
+                .blur();
+            }
+          });
+
+          cy.findByRole('button', { name: /Save/i }).click();
+        });
+
+        const stateExpenseData = activity.expenses;
+        stateExpenseData.forEach(expense => {
+          cy.findByRole('button', { name: /Add State Expense/i }).click();
+
+          cy.findByLabelText('Category').select(expense.category);
+          cy.findByLabelText('Description').type(expense.description);
+
+          mmisData.years.forEach(year => {
+            cy.findByLabelText(`FFY ${year} Cost`).type(expense.years[year]);
+          });
+
+          cy.findByRole('button', { name: /Save/i }).click();
+        });
+
+        cy.get('#continue-button').click();
+
+        // Private Contractor
+        const contractorData = activity.contractorResources;
+        contractorData.forEach((contractor, index) => {
+          cy.findByRole('button', { name: /Add Contractor/i }).click();
+
+          cy.get('[name="name"]').type(contractor.name);
+          cy.setTinyMceContent(
+            `contractor-description-field-${index}`,
+            contractor.description
+          );
+
+          fillDate('Contract start date', contractor.start.split('-'));
+          fillDate('Contract end date', contractor.end.split('-'));
+
+          cy.get('[name="totalCost"]').type(contractor.totalCost);
+
+          if (contractor.useHourly) {
+            cy.findByRole('radio', { name: /Yes/i }).click();
+            mmisData.years.forEach(year => {
+              cy.get(`[name="hourly[${year}].hours"]`).type(
+                contractor.hourly[year].hours
+              );
+              cy.get(`[name="hourly.${year}.rate"]`)
+                .type(contractor.hourly[year].rate)
+                .blur();
+            });
+          } else {
+            cy.findByRole('radio', { name: /No/i }).click();
+            mmisData.years.forEach(year => {
+              cy.get(`[name="years.${year}"]`)
+                .type(contractor.years[year])
+                .blur();
+            });
+          }
+
+          cy.findByRole('button', { name: /Save/i }).click();
+        });
+
+        cy.get('#continue-button').click();
+
+        // Cost Allocation and Other Funding
+        const costAllocationNarrativeData = activity.costAllocationNarrative;
+        const costAllocationData = activity.costAllocation;
+
+        cy.setTinyMceContent(
+          'cost-allocation-methodology-field',
+          costAllocationNarrativeData.methodology
+        );
+
+        mmisData.years.forEach(year => {
+          if (costAllocationData[year]) {
+            cy.setTinyMceContent(
+              `cost-allocation-narrative-${year}-other-sources-field`,
+              costAllocationNarrativeData.years[year].otherSources
+            );
+            cy.get(`[name="costAllocation.${year}.other"]`).type(
+              costAllocationData[year].other
+            );
+          }
+        });
+
+        cy.get('#continue-button').click();
+
+        // Budget and FFP
+        mmisData.years.forEach((year, index) => {
+          if (costAllocationData[year].ffp.federal === 90) {
+            cy.findAllByText(
+              '90/10 Design, Development, and Installation (DDI)'
+            )
+              .eq(index)
+              .click();
+          } else if (costAllocationData[year].ffp.federal === 75) {
+            cy.findAllByRole('radio', { name: /75\/25/ })
+              .eq(index)
+              .click();
+
+            cy.findAllByRole('radio', { name: /75\/25/ })
+              .eq(index)
+              .parent()
+              .parent()
+              .within(() => {
+                cy.findAllByRole('radio', {
+                  name: costAllocationData[year].ffp.fundingCategory
+                }).click();
+              });
+          } else if (costAllocationData[year].ffp.federal === 50) {
+            cy.findAllByRole('radio', { name: /50\/50/ })
+              .eq(index)
+              .click();
+
+            cy.findAllByRole('radio', { name: /50\/50/ })
+              .eq(index)
+              .parent()
+              .parent()
+              .within(() => {
+                cy.findAllByRole('radio', {
+                  name: costAllocationData[year].ffp.fundingCategory
+                }).click();
+              });
+          }
+        });
+        cy.waitForSave();
       });
-
-      budgetPage.checkMmisFFYtotals({
-        years,
-        activityIndex: 0,
-        activityId: mmisBasic.activities[0].activityId,
-        activityName: mmisBasic.activities[0].name,
-        state: 'New Apdland',
-        fundingSplit: `90/10 DDI (FFY ${years[0]}) and 50/50 M&O (FFY ${years[1]})`,
-        totalOtherFunding: 0,
-        totals:
-          budget.activities[mmisBasic.activities[0].activityId].costsByFFY.total
-      });
-
-      cy.waitForSave();
     });
   });
 };
--- e2e/cypress/helpers/apd/state-priorities-and-scope.js
+++ e2e/cypress/helpers/apd/state-priorities-and-scope.js
@@ -17,47 +17,32 @@ export const testStatePrioritiesAndScope = function () {
 
 export const testStatePrioritiesAndScopeWithData = function () {
   beforeEach(function () {
-    cy.fixture('state-priorities-and-scope.json').as('prioritiesAndScope');
+    cy.fixture('mmis-with-data.json').as('mmisData');
   });
 
   it('Fill out State Priorities and Scope', function () {
-    const prioritiesAndScope = this.prioritiesAndScope;
+    const mmisData = this.mmisData;
+    const prioritiesAndScope = mmisData.statePrioritiesAndScope;
 
-    cy.turnOnAdminCheck();
-    cy.collapseAdminCheck();
     cy.goToStatePrioritiesAndScope();
 
     cy.url().should('include', '/state-priorities-and-scope');
+
     cy.findByRole('heading', {
       name: /State Priorities and Scope of APD/
     }).should('exist');
 
-    cy.findByText('Provide Medicaid Program and Priorities.').should('exist');
-    cy.findByText('Provide a Medicaid Enterprise System Introduction.').should(
-      'exist'
-    );
-    cy.findByText("Provide an Overview of the APD's Scope.").should('exist');
-
     cy.setTinyMceContent(
       'medicaid-program-priorities-field',
-      prioritiesAndScope.priorities
-    );
-    cy.findByText('Provide Medicaid Program and Priorities.').should(
-      'not.exist'
-    );
-    cy.setTinyMceContent(
-      'medicaid-enterprise-system-intro',
-      prioritiesAndScope.mesIntro
-    );
-    cy.findByText('Provide a Medicaid Enterprise System Introduction.').should(
-      'not.exist'
-    );
-    cy.setTinyMceContent('scope-of-apd', prioritiesAndScope.scopeOfApd);
-    cy.findByText("Provide an Overview of the APD's Scope.").should(
-      'not.exist'
+      prioritiesAndScope.medicaidProgramAndPriorities
     );
 
+    cy.setTinyMceContent(
+      'medicaid-enterprise-system-intro',
+      prioritiesAndScope.mesIntroduction
+    );
+    cy.setTinyMceContent('scope-of-apd', prioritiesAndScope.scopeOfAPD);
+
     cy.waitForSave();
-    cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
   });
 };
